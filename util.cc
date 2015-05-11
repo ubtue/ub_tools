98dfe441617dae4287397df8a4d76dd6fca9234f
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2015-05-11 11:47:49 +0200

    Warning() and Error() now issue a warning if the gobal variable "progname" on which they rely has not been set.

--- util.cc
+++ util.cc
@@ -7,13 +7,19 @@ char *progname; // Must be set in main() with "progname = argv[0];";
 
 
 void Error(const std::string &msg) {
-  std::cerr << progname << ": " << msg << '\n';
-  std::exit(EXIT_FAILURE);
+    if (progname == NULL)
+	std::cerr << "You must set \"progname\" in main() with \"progname = argv[0];\" in oder to use Error().\n";
+    else
+	std::cerr << progname << ": " << msg << '\n';
+    std::exit(EXIT_FAILURE);
 }
 
 
 void Warning(const std::string &msg) {
-  std::cerr << progname << ": " << msg << '\n';
+    if (progname == NULL)
+	std::cerr << "You must set \"progname\" in main() with \"progname = argv[0];\" in oder to use Warning().\n";
+    else
+	std::cerr << progname << ": " << msg << '\n';
 }
 
 
