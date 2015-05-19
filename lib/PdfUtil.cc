962c4834eec67acfc97d23c3081188fd644548e4
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2015-05-19 13:23:32 +0200

    Added a new utility function to determine if a PDF contains no text and therefore likely only images.

---
+++ lib/PdfUtil.cc
@@ -0,0 +1,46 @@
+/** \file   PdfUtil.cc
+ *  \brief  Implementation of functions relating to PDF documents.
+ *  \author Dr. Johannes Ruscheinski (johannes.ruscheinski@uni-tuebingen.de)
+ *
+ *  \copyright 2015 Universitätsbiblothek Tübingen.  All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "PdfUtil.h"
+#include <unistd.h>
+#include "ExecUtil.h"
+#include "util.h"
+
+
+const std::string PDFFONTS("/usr/bin/pdffonts");
+
+
+bool PdfDocContainsNoText(const std::string &path) {
+    char filename_template[] = "/tmp/PdfDocContainsNoTextXXXXXX";
+    const std::string output_filename(::mktemp(filename_template));
+    std::vector<std::string> args{ path };
+    const int retval = Exec(PDFFONTS, args, output_filename);
+    if (retval == 0) {
+	std::string output;
+	if (not ReadFile(output_filename, &output)) {
+	    ::unlink(output_filename.c_str());
+	    return false;
+	}
+	return output.length() == 188; // Header only?
+    }
+    ::unlink(output_filename.c_str());
+
+    return retval != 0;
+}
+
