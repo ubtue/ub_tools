dffc8effb4b5a23b9034648a49fc4081c4c559a6
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2015-06-15 17:51:59 +0200

    Added various timer-related functionality.

---
+++ cpp/lib/TimeLimit.cc
@@ -0,0 +1,65 @@
+/** \file    TimeLimit.h
+ *  \brief   Implementation class TimeLimit.
+ *  \author  Dr. Johannes Ruscheinski
+ */
+
+/*
+ *  Copyright 2005 Project iVia.
+ *  Copyright 2005 The Regents of The University of California.
+ *
+ *  This file is part of the libiViaCore package.
+ *
+ *  The libiViaCore package is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  libiViaCore is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with libiViaCore; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "TimeLimit.h"
+#ifndef TIMER_UTIL_H
+#       include "TimerUtil.h"
+#endif
+
+
+TimeLimit::TimeLimit(const unsigned time_limit) {
+    ::gettimeofday(&expire_time_, NULL);
+    expire_time_ += time_limit;
+}
+
+
+unsigned TimeLimit::getRemainingTime() const {
+    timeval now;
+    ::gettimeofday(&now, NULL);
+
+    timeval diff_time;
+    timersub(&expire_time_, &now, &diff_time);
+    if (diff_time.tv_sec < 0 or diff_time.tv_usec < 0)
+	return 0;
+
+    return TimeValToMilliseconds(diff_time);
+}
+
+
+bool TimeLimit::limitExceeded() const {
+    timeval now;
+    ::gettimeofday(&now, NULL);
+
+    timeval diff_time;
+    timersub(&expire_time_, &now, &diff_time);
+
+    return diff_time.tv_sec < 0 or diff_time.tv_usec < 0;
+}
+
+
+bool TimeLimit::operator!=(const TimeLimit &rhs) {
+    return TimeValToMilliseconds(expire_time_) != TimeValToMilliseconds(rhs.expire_time_);
+}
---
+++ cpp/lib/TimeLimit.h
@@ -0,0 +1,75 @@
+/** \file    TimeLimit.h
+ *  \brief   Implements class TimeLimit.
+ *  \author  Dr. Johannes Ruscheinski
+ *  \author  Anthony Moralez
+ */
+
+/*
+ *  Copyright 2005-2009 Project iVia.
+ *  Copyright 2005-2009 The Regents of The University of California.
+ *
+ *  This file is part of the libiViaCore package.
+ *
+ *  The libiViaCore package is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License as published
+ *  by the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  libiViaCore is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with libiViaCore; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef TIME_LIMIT_H
+#define TIME_LIMIT_H
+
+
+#include <sys/time.h>
+#include <time.h>
+
+
+/** \class  TimeLimit
+ *  \brief  Represents a time limit placed upon some operation.
+ *
+ *  The TimeLimit class is used to keep track of a time limit placed on some opertion. A time limit is specified (in
+ *  milliseconds) when the object is created.  From that point on, the limitExceeded() function can be used to test
+ *  whether the time limit has been reached, and the getRemainingTime() function to measure the time left before the
+ *  limit.
+ */
+class TimeLimit {
+	timeval expire_time_;
+public:
+	/** \brief  Construct a TimeLimit by specifying the limit.
+	 *  \param  time_limit  The time until expiration, in milliseconds.
+	 *  \note   This constructor is deliberately not explicit, so that unsigned values can be used in place of
+	 *          TimeLimit objects in function calls.
+	 */
+	TimeLimit(const unsigned time_limit);
+
+	/** TimeLimit Copy Constructor. */
+	TimeLimit(const TimeLimit &rhs): expire_time_(rhs.expire_time_) { }
+
+	/** TimeLimit equality operator. */
+	const TimeLimit operator=(const TimeLimit &rhs) { expire_time_ = rhs.expire_time_; return *this; }
+
+	/** \brief  Test whether the time limit has been exceeded.
+	 *  \return  True if the time limit has been exceeded, otherwise false.
+	 */
+	bool limitExceeded() const;
+
+	/** \brief   Get the time remaining before the limit is exceeded.
+	 *  \return  The time remaining until the limit is exceeded (in milliseconds) or 0 if the limit is already
+	 *           exceeded.
+	 */
+	unsigned getRemainingTime() const;
+
+	bool operator!=(const TimeLimit &rhs);
+};
+
+
+#endif // ifndef TIME_LIMIT_H
