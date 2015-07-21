c78f6392c6ddf56ad93b27da7c31707375796bdf
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2015-07-21 11:56:13 +0200

    Test harness for ExecUtil::Which().

---
+++ cpp/resolve_in_path_test.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "ExecUtil.h"
+
+
+int main(int argc, char *argv[]) {
+    for (int arg_no(1); arg_no < argc; ++arg_no)
+	std::cout << argv[arg_no] << " -> " << ExecUtil::Which(argv[arg_no]) << '\n';
+}
