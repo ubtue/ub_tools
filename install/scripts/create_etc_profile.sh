f0271a21b82ceb9be46a6de6526550eb8defe18f
Oliver Obenland <oliver.obenland@uni-tuebingen.de>
2015-07-22 11:49:48 +0200

    Update install script

--- install/scripts/create_etc_profile.sh
+++ install/scripts/create_etc_profile.sh
@@ -5,7 +5,7 @@
 # Writes vufind path settings to /etc/profile.d/vufind.sh
 #
 
-sudo sh -c "echo export JAVA_HOME=/usr/lib/jvm/default-java    > /etc/profile.d/vufind.sh"
+sudo sh -c "echo export JAVA_HOME=$JAVA_HOME                   > /etc/profile.d/vufind.sh"
 sudo sh -c "echo export VUFIND_HOME=$VUFIND_HOME              >> /etc/profile.d/vufind.sh"
 sudo sh -c "echo export VUFIND_LOCAL_DIR=$VUFIND_LOCAL_DIR    >> /etc/profile.d/vufind.sh"
 sudo chmod 644 "/etc/profile.d/vufind.sh"
