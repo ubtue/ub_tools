6a4b27b3f4bfed3a5850c99f2658fc154d98a22e
Oliver Obenland <oliver.obenland@uni-tuebingen.de>
2015-08-10 18:11:42 +0200

    Use config directory for private configs

---
+++ install/scripts/link_configs.sh
@@ -0,0 +1,36 @@
+#! /bin/bash
+#
+# $VUFIND_HOME and $VUFIND_LOCAL_DIR have to be set!
+#
+# Links some configs.
+#
+set -o errexit -o nounset
+SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+# Make sure only root can run our script
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+show_help() {
+    cat << EOF
+Links some configs.
+
+USAGE: ${0##*/} CONFIGS_DIRECTORY
+
+CONFIGS_DIRECTORY    The directory, which holds the configs.
+
+EOF
+}
+
+if [ "$#" -ne 1 ]; then
+  echo "ERROR: Illegal number of parameters!"
+  echo "Parameters: $*"
+  show_help
+  exit 1
+fi
+
+CONFIGS_DIRECTORY=$1
+
+ln -sf "$CONFIGS_DIRECTORY/cronjobs" "/var/lib/tuelib/cronjobs"
