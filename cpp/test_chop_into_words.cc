82787903f11a10c91a9b3033112ed74ae0f35e0e
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2015-08-18 17:01:54 +0200

    Test harness for TextUtil::ChopIntoWords().

---
+++ cpp/test_chop_into_words.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cstdlib>
+#include <TextUtil.h>
+#include <util.h>
+
+
+void Usage() {
+    std::cerr << "usage: " << ::progname << " phrase\n";
+    std::exit(EXIT_FAILURE);
+}
+
+
+int main(int argc, char *argv[]) {
+    ::progname = argv[0];
+    if (argc != 2)
+	Usage();
+
+    std::vector<std::string> words;
+    if (not TextUtil::ChopIntoWords(argv[1], &words))
+	Error("there was a character code conversion error!");
+
+    for (const auto &word : words)
+	std::cout << word << '\n';
+}
