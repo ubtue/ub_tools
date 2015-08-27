74f24f0dea47a9fba33c1e6853974213c99ecf51
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2015-08-27 17:41:07 +0200

    Add the insertion of the 652$a MARC field w/ ixTheo notations extracted from local data to the pipeline.

--- cpp/ixtheo_marc_pipeline.sh
+++ cpp/ixtheo_marc_pipeline.sh
@@ -56,11 +56,25 @@ echo "*** Phase 4 ***"
 echo "*** Phase 4 ***" >> "${log}"
 augment_bible_references TitelUndLokaldaten-with-issns-and-title-keywords-"${date}".mrc \
                          Normdaten-"${date}".mrc \
-                         TitelUndLokaldaten-with-issns-title-keywords-and-bible-refs-"${date}".mrc
+                         TitelUndLokaldaten-with-issns-title-keywords-and-bible-refs-"${date}".mrc >> "${log}" 2>&1
 cp *.map /var/lib/tuelib/bibleRef/
+
+# Phase 5:
+echo "*** Phase 5 ***"
+echo "*** Phase 5 ***" >> "${log}"
+update_ixtheo_notationsmarc_input \
+    TitelUndLokaldaten-with-issns-title-keywords-and-bible-refs-"${date}".mrc \
+    TitelUndLokaldaten-with-issns-title-keywords-bible-refs-and-ixtheo-notations-"${date}".mrc
+    ../cpp/data/IxTheo_Notation.csv >> "${log}" 2>&1
+update_ixtheo_notationsmarc_input \
+    ÜbergeordneteTitelUndLokaldaten-with-child-refs-"${date}".mrc \
+    ÜbergeordneteTitelUndLokaldaten-with-child-refs-and-ixtheo-notations-"${date}".mrc
+    ../cpp/data/IxTheo_Notation.csv >> "${log}" 2>&1
 
 # Cleanup of intermediate files:
 rm -f ÜbergeordneteTitelUndLokaldaten-with-child-refs-"${date}".mrc
 rm -f TitelUndLokaldaten-with-issns-"${date}".mrc
 rm -f TitelUndLokaldaten-with-issns-and-title-keywords-"${date}".mrc
 rm -f child_refs child_titles parent_refs
+rm -f TitelUndLokaldaten-with-issns-title-keywords-and-bible-refs-"${date}".mrc
+
