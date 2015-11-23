4a23b27f17b48f110d954e23e51c440eb218d01c
jriedl <johannes.riedl@uni-tuebingen.de>
2015-11-23 16:11:58 +0100

    First skeleton

---
+++ solr_plugins/PrefixSortFacetten/src/de/uni_tuebingen/ub/ixTheo/handler/component/FacetComponent.java
@@ -0,0 +1,112 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+//package org.apache.solr.handler.component;
+package de.uni_tuebingen.ub.ixTheo.handler.component;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.lucene.util.FixedBitSet;
+import org.apache.solr.common.SolrException;
+import org.apache.solr.common.SolrException.ErrorCode;
+import org.apache.solr.common.params.CommonParams;
+import org.apache.solr.common.params.FacetParams;
+import org.apache.solr.common.params.ModifiableSolrParams;
+import org.apache.solr.common.params.ShardParams;
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.common.util.SimpleOrderedMap;
+import org.apache.solr.common.util.StrUtils;
+import org.apache.solr.request.SimpleFacets;
+import org.apache.solr.schema.FieldType;
+import org.apache.solr.search.QueryParsing;
+import org.apache.solr.search.SyntaxError;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.apache.solr.handler.component.*;
+
+/**
+ * TODO!
+ *
+ *
+ * @since solr 1.3
+ */
+@SuppressWarnings("rawtypes")
+//public class FacetComponent extends SearchComponent {
+public class FacetComponent extends org.apache.solr.handler.component.FacetComponent {
+
+  @Override
+  public void prepare(ResponseBuilder rb) throws IOException {
+
+	super.prepare(rb);
+
+  }
+  
+  /**
+   * Actually run the query
+   */
+  @Override
+  public void process(ResponseBuilder rb) throws IOException {
+
+	super.process(rb);	
+
+  }
+  
+  private static final String commandPrefix = "{!" + CommonParams.TERMS + "=$";
+  
+  @Override
+  public void handleResponses(ResponseBuilder rb, ShardRequest sreq) {
+	 super.handleResponses(rb, sreq);
+  }
+  
+
+
+
+  /////////////////////////////////////////////
+  ///  SolrInfoMBean
+  ////////////////////////////////////////////
+
+  @Override
+  public String getDescription() {
+    return "Handle Prefix Sort Faceting";
+  }
+
+  @Override
+  public String getSource() {
+    return null;
+  }
+
+  @Override
+  public URL[] getDocs() {
+    return null;
+  }
+
+}
