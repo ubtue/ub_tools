a72e235c650672815116f6a7ea5c31a78d8fb7c4
jriedl <johannes.riedl@uni-tuebingen.de>
2015-12-07 14:38:19 +0100

    Adjusted layout

--- solr_plugins/PrefixSortFacetten/src/de/uni_tuebingen/ub/ixTheo/common/params/FacetPrefixSortParams.java
+++ solr_plugins/PrefixSortFacetten/src/de/uni_tuebingen/ub/ixTheo/common/params/FacetPrefixSortParams.java
@@ -2,11 +2,8 @@ package de.uni_tuebingen.ub.ixTheo.common.params;
 
 import org.apache.solr.common.params.FacetParams;
 
-
 public interface FacetPrefixSortParams extends FacetParams {
 
-  public static final String FACET_SORT_PREFIX = "prefix";
+    public static final String FACET_SORT_PREFIX = "prefix";
 
 }
-
-
