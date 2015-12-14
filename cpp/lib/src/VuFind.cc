4769807028504efa92b83f157a43a007406b54ea
Root@Sobek <root@sobek.uni-tuebingen.de>
2015-12-14 17:07:16 +0100

    Moved some code into a new utility function.

--- cpp/lib/src/VuFind.cc
+++ cpp/lib/src/VuFind.cc
@@ -1,4 +1,5 @@
-/** \brief Test program for interfacing to the VuFind MySQL tables.
+/** \file   VuFind.cc
+ *  \brief  VuFind-related constants and utility functions.
  *  \author Dr. Johannes Ruscheinski (johannes.ruscheinski@uni-tuebingen.de)
  *
  *  \copyright 2015 Universitätsbiblothek Tübingen.  All rights reserved.
@@ -15,35 +16,26 @@
  *
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-#include <iostream>
+ */
+#include "VuFind.h"
 #include <stdexcept>
-#include <string>
-#include <cstdlib>
-#include <DbConnection.h>
-#include <util.h>
-#include <VuFind.h>
+#include "File.h"
+#include "StringUtil.h"
 
 
-void Usage() {
-    std::cerr << "usage: " << ::progname << '\n';
-    std::exit(EXIT_FAILURE);
+namespace VuFind {
+
+
+void GetMysqlURL(std::string * const mysql_url) throw(std::exception) {
+    const std::string database_conf_filename(VuFind::VUFIND_HOME + "/" + VuFind::DATABASE_CONF);
+    File database_conf(database_conf_filename, "r", File::THROW_ON_ERROR);
+    const std::string line(database_conf.getline());
+    const size_t schema_pos(line.find("mysql://"));
+    if (schema_pos == std::string::npos)
+	throw std::runtime_error("MySQL schema not found in \"" + database_conf_filename + "\"!");
+    *mysql_url = StringUtil::RightTrim(line.substr(schema_pos));
+    mysql_url->resize(mysql_url->size() - 1); // Remove trailing double quote.
 }
 
 
-int main(int argc, char *argv[]) {
-    ::progname = argv[0];
-
-    if (argc != 1)
-	Usage();
-
-    try {
-	std::string mysql_url;
-	VuFind::GetMysqlURL(&mysql_url);
-
-	// The following definition would throw an exception if the "mysql_url" was invalid:
-	DbConnection db_connection(mysql_url);
-    } catch (const std::exception &x) {
-        Error("caught exception: " + std::string(x.what()));	
-    }
-}
+} // namespace VuFind
