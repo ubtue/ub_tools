c4704ce9567a695eab7d62eeccc482a56d599335
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2016-01-20 17:11:47 +0100

    Now using a simpler way to find entries with a missing language code.

--- cpp/translation_db_tool.cc
+++ cpp/translation_db_tool.cc
@@ -18,7 +18,6 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 #include <iostream>
-#include <map>
 #include <cstdlib>
 #include <cstring>
 #include "DbConnection.h"
@@ -33,36 +32,30 @@
 void Usage() {
     std::cerr << "Usage: " << progname << " command [args]\n\n";
     std::cerr << "       Possible commands are:\n";
-    std::cerr << "       get_next start_index language_code\n";
+    std::cerr << "       get_missing language_code\n";
     std::cerr << "       insert index language_code text\n";
     std::exit(EXIT_FAILURE);
 }
 
 
-void GetNext(DbConnection * const connection, const unsigned start_index, const std::string &language_code) {
-    const std::string SELECT_STMT("SELECT * FROM translations WHERE index > " + std::to_string(start_index));
-    if (unlikely(not connection->query(SELECT_STMT)))
-        Error("Select failed: " + SELECT_STMT + " (" + connection->getLastErrorMessage() + ")");
+void GetMissing(DbConnection * const connection, const std::string &language_code) {
+    // Find an ID where "language_code" is missing:
+    const std::string SELECT_ID_STMT("SELECT id FROM translations WHERE id NOT IN (SELECT id FROM translations "
+				     "WHERE language_code = \"" + language_code + "\") LIMIT 1");
+    if (unlikely(not connection->query(SELECT_ID_STMT)))
+        Error("Select failed: " + SELECT_ID_STMT + " (" + connection->getLastErrorMessage() + ")");
+    DbResultSet id_result_set(connection->getLastResultSet());
+    if (id_result_set.empty()) // The language code whose absence we're looking for exists for all ID's.!
+	return;
+
+    // Print the contents of all rows with the ID from the last query on stdout:
+    const std::string matching_id(id_result_set.getNextRow()["id"]);
+    const std::string SELECT_MATCHING_ROW_STMT("SELECT * FROM translations WHERE id=" + matching_id);
+    if (unlikely(not connection->query(SELECT_MATCHING_ROW_STMT)))
+        Error("Select failed: " + SELECT_MATCHING_ROW_STMT + " (" + connection->getLastErrorMessage() + ")");
     DbResultSet result_set(connection->getLastResultSet());
-    unsigned current_index(0);
-    std::map<std::string, std::string> language_code_to_text_map;
-    while (const DbRow row = result_set.getNextRow()) {
-	const unsigned index(StringUtil::ToUnsigned(row["id"]));
-	if (index == current_index)
-	    language_code_to_text_map[row["language_code"]] = row["text"];
-	else if (language_code_to_text_map.find(language_code) == language_code_to_text_map.cend()) {
-	    for (const auto &language_code_and_text : language_code_to_text_map)
-		std::cout << language_code_and_text.first << '=' << language_code_and_text.second << '\n';
-	    return;
-	} else {
-	    current_index = index;
-	    language_code_to_text_map.clear();
-	}
-    }
-    if (language_code_to_text_map.find(language_code) == language_code_to_text_map.cend()) {
-	for (const auto &language_code_and_text : language_code_to_text_map)
-	    std::cout << language_code_and_text.first << '=' << language_code_and_text.second << '\n';
-    }
+    while (const DbRow row = result_set.getNextRow())
+	std::cout << row["id"] << ',' << row["language_code"] << ',' << row["text"] << '\n';
 }
 
 
@@ -90,16 +83,13 @@ int main(int argc, char *argv[]) {
 	const std::string sql_password(ini_file.getString("", "sql_password"));
 	DbConnection db_connection("vufind", sql_username, sql_password);
 
-	if (std::strcmp(argv[1], "get_next") == 0) {
-	    if (argc != 4)
-		Error("\"get_next\" requires exactly two arguments: start_index and language_code!");
-	    unsigned start_index;
-	    if (not StringUtil::ToUnsigned(argv[2], &start_index))
-		Error("\"" + std::string(argv[2])  + "\" is not a valid start index!");
-	    const std::string language_code(argv[3]);
+	if (std::strcmp(argv[1], "get_missing") == 0) {
+	    if (argc != 3)
+		Error("\"get_missing\" requires exactly one argument: language_code!");
+	    const std::string language_code(argv[2]);
 	    if (language_code.length() != 3)
 		Error("\"" + language_code + "\" is not a valid 3-letter language code!");
-	    GetNext(&db_connection, start_index, language_code);
+	    GetMissing(&db_connection, language_code);
 	} else if (std::strcmp(argv[1], "insert") == 0) {
 	    if (argc != 5)
 		Error("\"insert\" requires exactly three arguments: index, language_code, and text!");
