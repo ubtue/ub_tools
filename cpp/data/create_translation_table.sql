d14e50b0fa0dc1717dc9d344c3113c908152b0b8
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2016-01-28 12:02:14 +0100

    Translation tools.

--- cpp/data/create_translation_table.sql
+++ cpp/data/create_translation_table.sql
@@ -2,9 +2,12 @@ CREATE TABLE translations (
        id INT(11) NOT NULL,
        language_code CHAR(3) NOT NULL,
        text VARCHAR(100) NOT NULL,
-       CONSTRAINT UNIQUE id_and_language_code (id,language_code)
+       category ENUM('keywords','vufind_translations') NOT NULL,
+       preexists BOOLEAN NOT NULL DEFAULT FALSE,
+       CONSTRAINT UNIQUE id_language_code_and_category (id,language_code,category)
 );
 
 CREATE INDEX translations_idx_id ON translations (id);
 CREATE INDEX translations_idx_language_code ON translations (language_code);
 CREATE INDEX translations_idx_text ON translations (text);
+CREATE INDEX category_idx_text ON translations (category);
