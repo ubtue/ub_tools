7284a8bfe8600720f2db26f9ff7aa3745b7bc046
Oliver Obenland <oliver.obenland@uni-tuebingen.de>
2016-02-03 14:44:15 +0100

    Renaming methods and fixing test cases.

--- solr_plugins/Bibelstellensuche/tests/de/uni_tuebingen/ub/ixTheo/bibleRangeSearch/BibleRangeTests.java
+++ solr_plugins/Bibelstellensuche/tests/de/uni_tuebingen/ub/ixTheo/bibleRangeSearch/BibleRangeTests.java
@@ -1,34 +1,36 @@
 package de.uni_tuebingen.ub.ixTheo.bibleRangeSearch;
 
-import static org.junit.Assert.*;
 
 import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 
 public class BibleRangeTests {
     private Range r1, r2;
     private Range[] rs1, rs2;
 
     @Test
-    public void testGetDistance() {
+    public void testGetMatchingScore() {
+        r1 = new Range(0, 2);
+        r2 = new Range(2, 4);
+        assertEquals(0f, r1.getMatchingScore(r2), 0.001);
+
         r1 = new Range(0, 2);
         r2 = new Range(1, 2);
-        assertEquals(1, r1.getDistance(r2));
+        assertEquals(0.5f, r1.getMatchingScore(r2), 0.001);
 
         r1 = new Range(0, 3);
         r2 = new Range(1, 2);
-        assertEquals(2, r1.getDistance(r2));
-
-        r1 = new Range(0, 2);
-        r2 = new Range(2, 4);
-        assertEquals(4, r1.getDistance(r2));
+        assertEquals(0.33333f, r1.getMatchingScore(r2), 0.001);
 
         r1 = new Range(0, 2);
         r2 = new Range(5, 7);
-        assertEquals(Range.OUT_OF_RANGE, r1.getDistance(r2));
+        assertEquals(-0.4286f, r1.getMatchingScore(r2), 0.001);
     }
 
     @Test
-    public void testStaticGetDistance() {
+    public void testStaticGetMatchingScore() {
         rs1 = new Range[]{
                 new Range(2700320, 2700321),
                 new Range(2700331, 2700335),
@@ -37,7 +39,7 @@ public class BibleRangeTests {
                 new Range(2700320, 2700321),
                 new Range(2700331, 2700335),
         };
-        assertEquals(0, Range.getDistance(rs1, rs2));
+        assertEquals(2f, Range.getMatchingScore(rs1, rs2), 0.001);
 
         rs1 = new Range[]{
                 new Range(2700320, 2700321),
@@ -47,7 +49,7 @@ public class BibleRangeTests {
                 new Range(100000, 199999),
                 new Range(2700000, 2799999),
         };
-        assertEquals(199993, Range.getDistance(rs1, rs2));
+        assertEquals(5E-5f, Range.getMatchingScore(rs1, rs2), 0.001);
 
         rs1 = new Range[]{
                 new Range(2700320, 2700321),
@@ -56,17 +58,17 @@ public class BibleRangeTests {
         rs2 = new Range[]{
                 new Range(2700424, 2700424)
         };
-        assertEquals(Range.OUT_OF_RANGE, Range.getDistance(rs1, rs2));
+        assertEquals(0f, Range.getMatchingScore(rs1, rs2), 0.001);
     }
 
     @Test
-    public void testGetMinimumDistance() {
+    public void testGetBestMatchingScore() {
         Range r1;
         r1 = new Range(0, 2);
-        int distance = r1.getMinimumDistance(new Range[]{
+        float distance = r1.getBestMatchingScore(new Range[]{
                 new Range(10, 11), new Range(-100, 100), new Range(0, 1), new Range(-2, 4)
         });
-        assertEquals(1, distance);
+        assertEquals(0.5f, distance, 0.001);
     }
 
     @Test
@@ -108,22 +110,22 @@ public class BibleRangeTests {
     }
 
     @Test
-    public void testDoAllSourceRangesIntersectsSomeTargetRanges() {
+    public void testHasIntersections() {
         rs1 = new Range[]{new Range(1, 2), new Range(5, 6)};
         rs2 = new Range[]{new Range(0, 2), new Range(3, 4), new Range(6, 10)};
-        assertTrue(Range.doAllSourceRangesIntersectsSomeTargetRanges(rs1, rs2));
+        assertTrue(Range.hasIntersections(rs1, rs2));
 
         rs1 = new Range[]{new Range(1, 2), new Range(5, 6)};
         rs2 = new Range[]{new Range(0, 10)};
-        assertTrue(Range.doAllSourceRangesIntersectsSomeTargetRanges(rs1, rs2));
+        assertTrue(Range.hasIntersections(rs1, rs2));
 
         rs1 = new Range[]{new Range(1, 2), new Range(5, 6)};
         rs2 = new Range[]{new Range(0, 3)};
-        assertFalse(Range.doAllSourceRangesIntersectsSomeTargetRanges(rs1, rs2));
+        assertTrue(Range.hasIntersections(rs1, rs2));
 
         rs1 = new Range[]{new Range(1, 2), new Range(5, 6)};
         rs2 = new Range[]{new Range(10, 13)};
-        assertFalse(Range.doAllSourceRangesIntersectsSomeTargetRanges(rs1, rs2));
+        assertFalse(Range.hasIntersections(rs1, rs2));
     }
 
     @Test
@@ -153,5 +155,4 @@ public class BibleRangeTests {
         assertEquals(10, r1.getLower());
         assertEquals(20, r1.getUpper());
     }
-
 }
