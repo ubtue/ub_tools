83d9bf0960f1dce82637b89646dd23d0bbf8f253
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2016-03-09 17:20:03 +0100

    It looks like due to a change not all records that have 773 fields have 773$a subfields. This tool tries to add some of the missing subfields.

---
+++ cpp/augment_773a.cc
@@ -0,0 +1,167 @@
+/** \file    augment_773a.cc
+ *  \author  Dr. Johannes Ruscheinski
+ *
+ *  A tool for filling in 773$a if the 773 field exists and $a is missing.
+ */
+
+/*
+    Copyright (C) 2016, Library of the University of Tübingen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+#include <cstdlib>
+#include "Compiler.h"
+#include "DirectoryEntry.h"
+#include "Leader.h"
+#include "MarcUtil.h"
+#include "RegexMatcher.h"
+#include "StringUtil.h"
+#include "Subfields.h"
+#include "util.h"
+
+
+void Usage() {
+    std::cerr << "Usage: " << progname << " [--verbose] marc_input aux_marc_input marc_output\n"
+	      << "       \"marc_input\" is the file that will be augmented and converted.\n"
+	      << "       \"marc_input\" and \"aux_marc_input\" will be scoured for titles that\n"
+	      << "       may be filled into 773$a fields where appropriate.\n"
+              << "       Populates 773$a where it is missing and uplinks exist in 773$x.\n";
+    std::exit(EXIT_FAILURE);
+}
+
+
+static std::unordered_map<std::string, std::string> control_numbers_to_titles_map;
+
+
+bool RecordControlNumberToTitleMapping(MarcUtil::Record * const record, XmlWriter * const /*xml_writer*/,
+		 std::string * const /* err_msg */)
+{
+    ssize_t _245_index;
+    if (likely((_245_index = record->getFieldIndex("245")) != -1)) {
+	const std::vector<std::string> &fields(record->getFields());
+	const Subfields _245_subfields(fields[_245_index]);
+	const std::string title(_245_subfields.getFirstSubfieldValue('a'));
+	if (likely(not title.empty()))
+	    control_numbers_to_titles_map[record->getControlNumber()] = title;
+    }
+
+    return true;
+}
+
+
+void CollectControlNumberToTitleMappings(const bool verbose, const std::vector<File *> &inputs) {
+    std::string err_msg;
+    for (auto &input : inputs) {
+	if (verbose)
+	    std::cout << "Extracting control numbers to title mappings from \"" << input->getPath() << "\".\n";
+	if (not MarcUtil::ProcessRecords(input, RecordControlNumberToTitleMapping, /* xml_writer = */nullptr, &err_msg))
+	    Error("error while looking for control numbers to title mappings: " + err_msg);
+    }
+
+    if (verbose)
+	std::cout << "Found " << control_numbers_to_titles_map.size() << " control numbers to title mappings.\n";
+}
+
+
+static unsigned patch_count;
+
+
+// Looks for the existence of a 773 field.  Iff such a field exists and 773$a is missing, we try to add it.
+bool PatchUpOne773a(MarcUtil::Record * const record, XmlWriter * const xml_writer, std::string * const /*err_msg*/) {
+    ssize_t _773_index;
+    if ((_773_index = record->getFieldIndex("773")) != -1) {
+	const std::vector<std::string> &fields(record->getFields());
+        const Subfields _773_subfields(fields[_773_index]);
+	if (not _773_subfields.hasSubfield('a') and _773_subfields.hasSubfield('w')) {
+	    const std::string w_subfield(_773_subfields.getFirstSubfieldValue('w'));
+	    if (StringUtil::StartsWith(w_subfield, "(DE-576)")) {
+		const std::string parent_control_number(w_subfield.substr(8));
+		const auto control_number_and_title(control_numbers_to_titles_map.find(parent_control_number));
+		if (control_number_and_title != control_numbers_to_titles_map.end()) {
+		    record->updateField(_773_index, fields[_773_index] + "\x1F""a" + control_number_and_title->second);
+		    ++patch_count;
+		}
+	    }
+	}
+    }
+
+    record->write(xml_writer);
+
+    return true;
+}
+
+
+// Iterates over all records in a collection and retags all book component parts as artcles
+// unless the object has a monograph as a parent.
+void PatchUp773aSubfields(const bool verbose, File * const input, File * const output) {
+    XmlWriter xml_writer(output);
+    xml_writer.openTag("collection");
+
+    std::string err_msg;
+    if (not MarcUtil::ProcessRecords(input, PatchUpOne773a, &xml_writer, &err_msg))
+	Error("error while adding 773$a subfields to some records: " + err_msg);
+
+    xml_writer.closeTag();
+
+    if (verbose)
+	std::cout << "Added 773$a subfields to " << patch_count << " records.\n";
+}
+
+
+int main(int argc, char **argv) {
+    progname = argv[0];
+
+    if (argc < 2)
+	Usage();
+
+    bool verbose(false);
+    if (std::strcmp(argv[1], "--verbose") == 0) {
+	verbose = true;
+	--argc;
+	++argv;
+    }
+
+    if (argc != 4)
+	Usage();
+
+    std::vector<File *> marc_inputs;
+    for (int arg_no(1); arg_no < 3 ; ++arg_no) {
+	const std::string marc_input_filename(argv[arg_no]);
+	File *marc_input = new File(marc_input_filename, "rbm");
+	if (not *marc_input)
+	    Error("can't open \"" + marc_input_filename + "\" for reading!");
+	marc_inputs.push_back(marc_input);
+    }
+
+    const std::string marc_output_filename(argv[argc - 1]);
+    File marc_output(marc_output_filename, "wb");
+    if (not marc_output)
+        Error("can't open \"" + marc_output_filename + "\" for writing!");
+
+    try {
+	CollectControlNumberToTitleMappings(verbose, marc_inputs);
+
+	marc_inputs[0]->rewind();
+	PatchUp773aSubfields(verbose, marc_inputs[0], &marc_output);
+    } catch (const std::exception &x) {
+	Error("caught exception: " + std::string(x.what()));
+    }
+
+    for (const auto &marc_input : marc_inputs)
+	marc_input->close();
+}
