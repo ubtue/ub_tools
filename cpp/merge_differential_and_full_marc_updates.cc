d285a2a43dcb40b99cea1ef13730a529e6b2b5db
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2016-03-22 08:59:26 +0100

    Send more descriptive error emails.

--- cpp/merge_differential_and_full_marc_updates.cc
+++ cpp/merge_differential_and_full_marc_updates.cc
@@ -120,7 +120,7 @@ void SendEmail(const std::string &subject, const std::string &message_body, cons
 void LogSendEmailAndDie(const std::string &one_line_message) {
     Log(one_line_message);
     SendEmail(GetProgramBasename() + " failed! (from " + GetHostname() + ")",
-	      "Please have a look at the log for details.\n", EmailSender::VERY_HIGH);
+	      one_line_message + "\n", EmailSender::VERY_HIGH);
     std::exit(EXIT_FAILURE);
 }
 
@@ -532,7 +532,7 @@ inline std::string RemoveFileNameSuffix(const std::string &filename, const std::
 
 // Creates a symlink called "link_filename" pointing to "target_filename".
 void CreateSymlink(const std::string &target_filename, const std::string &link_filename) {
-    if (unlikely(::unlink(link_filename.c_str()) == -1 and errno != ENOENT))
+    if (unlikely(::unlink(link_filename.c_str()) == -1 and errno != ENOENT /* "No such file or directory." */))
         throw std::runtime_error("unlink(2) of \"" + link_filename + "\" failed: " + std::string(::strerror(errno)));
     if (unlikely(::symlink(target_filename.c_str(), link_filename.c_str()) != 0))
         LogSendEmailAndDie("failed to create symlink \"" + link_filename + "\" => \"" + target_filename + "\"! ("
