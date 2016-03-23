e0cb747bf7b2b7913da966418e2781e0d3428568
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2016-03-23 14:01:59 +0100

    Preliminary support for bible book ranges.

--- cpp/bib_ref_to_codes_tool.cc
+++ cpp/bib_ref_to_codes_tool.cc
@@ -55,55 +55,214 @@ std::string InsertSpaceAtFirstLetterDigitBoundary(const std::string &s) {
 
 
 // Squeezes out spaces after a leading number, e.g. "1. mos" => "1.mos" or "1 mos" => "1mos".
-std::string CanoniseLeadingNumber(const std::string &bib_ref_candidate) {
+std::string CanoniseLeadingNumber(const std::string &bible_reference_candidate) {
     static const RegexMatcher * const matcher(RegexMatcher::RegexMatcherFactory("^\\d\\.?\\s+\\S+"));
     std::string err_msg;
-    if (not matcher->matched(bib_ref_candidate, &err_msg)) {
+    if (not matcher->matched(bible_reference_candidate, &err_msg)) {
         if (not err_msg.empty())
             Error("unexpected reg ex error: " + err_msg);
-        return bib_ref_candidate;
+        return bible_reference_candidate;
     }
 
     std::string ordinal_string;
-    ordinal_string = bib_ref_candidate[0];
+    ordinal_string = bible_reference_candidate[0];
     size_t rest_start(1);
-    if (bib_ref_candidate[1] == '.') {
+    if (bible_reference_candidate[1] == '.') {
         ordinal_string += '.';
         ++rest_start;
     }
 
-    while (isspace(bib_ref_candidate[rest_start]))
+    while (isspace(bible_reference_candidate[rest_start]))
         ++rest_start;
 
-    return ordinal_string + bib_ref_candidate.substr(rest_start);
+    return ordinal_string + bible_reference_candidate.substr(rest_start);
 }
 
 
-void SplitIntoBookAndChaptersAndVerses(const std::string &bib_ref_candidate, std::string * const book_candidate,
+void SplitIntoBookAndChaptersAndVerses(const std::string &bible_reference_candidate, std::string * const book_candidate,
                                        std::string * const chapters_and_verses_candidate)
 {
     book_candidate->clear();
     chapters_and_verses_candidate->clear();
 
-    const size_t len(bib_ref_candidate.length());
+    const size_t len(bible_reference_candidate.length());
     if (len <= 3)
-        *book_candidate = bib_ref_candidate;
-    else if (isdigit(bib_ref_candidate[len - 1])
-             or (isalpha(bib_ref_candidate[len - 1]) and isdigit(bib_ref_candidate[len - 2])))
+        *book_candidate = bible_reference_candidate;
+    else if (isdigit(bible_reference_candidate[len - 1])
+             or (isalpha(bible_reference_candidate[len - 1]) and isdigit(bible_reference_candidate[len - 2])))
     {
-        const size_t last_space_pos(bib_ref_candidate.rfind(' '));
+        const size_t last_space_pos(bible_reference_candidate.rfind(' '));
         if (last_space_pos == std::string::npos)
-            *book_candidate = bib_ref_candidate;
+            *book_candidate = bible_reference_candidate;
         else {
-            *book_candidate = bib_ref_candidate.substr(0, last_space_pos);
-            *chapters_and_verses_candidate = bib_ref_candidate.substr(last_space_pos + 1);
+            *book_candidate = bible_reference_candidate.substr(0, last_space_pos);
+            *chapters_and_verses_candidate = bible_reference_candidate.substr(last_space_pos + 1);
         }
     } else
-        *book_candidate = bib_ref_candidate;
+        *book_candidate = bible_reference_candidate;
 }
 
 
-void Usage() {
+/** \brief Map from noncanonical bible book forms to the canonical ones.
+ *  \return The mapped name or, if no mapping was found, "bible_book_candidate".
+ */
+std::string CanoniseBibleBook(const bool verbose, const std::string &books_of_the_bible_to_canonical_form_map_filename,
+			      const std::string &bible_book_candidate)
+{
+    std::unordered_map<std::string, std::string> books_of_the_bible_to_canonical_form_map;
+    MapIO::DeserialiseMap(books_of_the_bible_to_canonical_form_map_filename, &books_of_the_bible_to_canonical_form_map);
+    const auto non_canonical_form_and_canonical_form(books_of_the_bible_to_canonical_form_map.find(bible_book_candidate));
+    if (non_canonical_form_and_canonical_form != books_of_the_bible_to_canonical_form_map.end()) {
+        if (verbose)
+            std::cerr << "Replacing \"" << bible_book_candidate << "\" with \""
+                      << non_canonical_form_and_canonical_form->second << "\".\n";
+        return non_canonical_form_and_canonical_form->second;
+    }
+
+    return bible_book_candidate;
+}
+
+
+std::string MapBibleBookToCode(const bool verbose, const std::string &bible_book_candidate,
+			       const std::string &books_of_the_bible_to_code_map_filename)
+{
+    std::unordered_map<std::string, std::string> bible_books_to_codes_map;
+    MapIO::DeserialiseMap(books_of_the_bible_to_code_map_filename, &bible_books_to_codes_map);
+    const auto bible_book_and_code(bible_books_to_codes_map.find(bible_book_candidate));
+    if (bible_book_and_code == bible_books_to_codes_map.end()) {
+        if (verbose)
+            std::cerr << "No mapping from \"" << bible_book_candidate << "\" to a book code was found!\n";
+
+	std::exit(EXIT_FAILURE); // Unknown bible book!
+    }
+
+    return bible_book_and_code->second;
+}
+
+
+void HandlePericopes(const bool verbose, const bool generate_solr_query, const std::string &bible_reference_candidate,
+		     const std::string &pericopes_to_codes_map_filename)
+{
+    std::unordered_multimap<std::string, std::string> pericopes_to_codes_map;
+    MapIO::DeserialiseMap(pericopes_to_codes_map_filename, &pericopes_to_codes_map);
+
+    const auto begin_end(pericopes_to_codes_map.equal_range(bible_reference_candidate));
+    if (begin_end.first != begin_end.second) {
+        if (verbose)
+            std::cerr << "Found a pericope to codes mapping.\n";
+        std::string query;
+        for (auto pair(begin_end.first); pair != begin_end.second; ++pair) {
+            if (generate_solr_query) {
+                if (not query.empty())
+                    query += ' ';
+                query += pair->first + "_" + pair->second;
+            } else
+                std::cout << pair->second << '\n';
+        }
+        if (generate_solr_query)
+            std::cout << query << '\n';
+
+	std::exit(EXIT_SUCCESS);
+    }
+}
+
+
+void HandleBookRanges(const bool verbose, const bool generate_solr_query,
+		      const std::string &books_of_the_bible_to_canonical_form_map_filename,
+		      const std::string &books_of_the_bible_to_code_map_filename,
+		      const std::string &bible_reference_candidate)
+{
+    const RegexMatcher *matcher(RegexMatcher::RegexMatcherFactory("([12])-([23])\\s*([A-Za-z]+)"));
+    if (not matcher->matched(bible_reference_candidate)) {
+	if (bible_reference_candidate.find('-') != std::string::npos) {
+	    if (verbose)
+		std::cerr << "Bad bible book range or random junk: " << bible_reference_candidate << '\n';
+	    std::exit(EXIT_FAILURE);
+	}
+	return;
+    }
+
+    unsigned starting_volume;
+    if (not StringUtil::ToUnsigned((*matcher)[1], &starting_volume) or starting_volume > 2)
+	Error("\"" + (*matcher)[1] + "\" is not a valid starting volume!");
+
+    unsigned ending_volume;
+    if (not StringUtil::ToUnsigned((*matcher)[2], &ending_volume) or ending_volume > 3 or ending_volume <= starting_volume)
+	Error("\"" + (*matcher)[2] + "\" is not a valid ending volume!");
+
+    const std::string bible_book_candidate(CanoniseBibleBook(verbose, books_of_the_bible_to_canonical_form_map_filename,
+							     (*matcher)[3]));
+    if (verbose) {
+	std::cout << "Identified a bible book range.  Starting volume " << starting_volume << ", ending volume "
+		  << ending_volume << ", book is \"" << bible_book_candidate << "\".\n";
+    }
+
+    const std::string first_book_code(MapBibleBookToCode(verbose, std::to_string(starting_volume) + bible_book_candidate,
+							 books_of_the_bible_to_code_map_filename));
+    const std::string second_book_code(MapBibleBookToCode(verbose, std::to_string(ending_volume) + bible_book_candidate,
+							  books_of_the_bible_to_code_map_filename));
+
+    if (generate_solr_query) {
+            std::cout << (first_book_code + "00000") << '_' << (first_book_code + "99999") << '\n';
+            std::cout << (second_book_code + "00000") << '_' << (second_book_code + "99999") << '\n';
+    }
+
+    std::exit(EXIT_SUCCESS);
+}
+
+
+void HandleOrdinaryReferences(const bool verbose, const bool generate_solr_query, std::string bible_reference_candidate,
+			      const std::string &bible_books_to_codes_map_filename,
+			      const std::string &books_of_the_bible_to_canonical_form_map_filename)
+{
+    bible_reference_candidate = CanoniseLeadingNumber(InsertSpaceAtFirstLetterDigitBoundary(
+                                    StringUtil::RemoveChars(" \t", &bible_reference_candidate)));
+    std::string book_candidate, chapters_and_verses_candidate;
+    SplitIntoBookAndChaptersAndVerses(bible_reference_candidate, &book_candidate, &chapters_and_verses_candidate);
+    if (verbose) {
+        std::cerr << "book_candidate = \"" << book_candidate << "\"\n";
+        std::cerr << "chapters_and_verses_candidate = \"" << chapters_and_verses_candidate << "\"\n";
+    }
+
+    book_candidate = CanoniseBibleBook(verbose, books_of_the_bible_to_canonical_form_map_filename, book_candidate);
+    const std::string book_code(MapBibleBookToCode(verbose, book_candidate, bible_books_to_codes_map_filename));
+    if (verbose)
+        std::cerr << "book code = \"" << book_code << "\"\n";
+    if (chapters_and_verses_candidate.empty()) {
+        if (generate_solr_query)
+            std::cout << (book_code + "00000") << '_' << (book_code + "99999") << '\n';
+        else
+            std::cout << book_code << "00000:" << book_code << "99999" << '\n';
+
+	std::exit(EXIT_SUCCESS);
+    }
+
+    std::set<std::pair<std::string, std::string>> start_end;
+    if (not ParseBibleReference(chapters_and_verses_candidate, book_code, &start_end)) {
+        if (verbose)
+            std::cerr << "The parsing of \"" << chapters_and_verses_candidate
+                      << "\" as chapters and verses failed!\n";
+	std::exit(EXIT_FAILURE);
+    }
+
+    std::string query;
+    for (const auto &pair : start_end) {
+        if (generate_solr_query) {
+            if (not query.empty())
+                query += ' ';
+            query += pair.first + "_" + pair.second;
+        } else
+            std::cout << pair.first << ':' << pair.second << '\n';
+    }
+    if (generate_solr_query)
+        std::cout << query << '\n';
+}
+
+
+static void Usage() __attribute__((noreturn));
+
+
+static void Usage() {
     std::cerr << "usage: " << progname << " [--debug|--query] bible_reference_candidate\n";
     std::cerr << "          books_of_the_bible_to_code_map\n";
     std::cerr << "          books_of_the_bible_to_canonical_form_map pericopes_to_codes_map\n";
@@ -132,98 +291,15 @@ int main(int argc, char **argv) {
     if (argc != 5)
         Usage();
 
-    //
-    // Deal with pericopes first...
-    //
+    const std::string books_of_the_bible_to_code_map_filename(argv[2]);
+    const std::string books_of_the_bible_to_canonical_form_map_filename(argv[3]);
+    std::string bible_reference_candidate(StringUtil::Trim(StringUtil::ToLower(argv[1])));
+    StringUtil::CollapseWhitespace(&bible_reference_candidate);
 
-    std::unordered_multimap<std::string, std::string> pericopes_to_codes_map;
-    MapIO::DeserialiseMap(argv[4], &pericopes_to_codes_map);
-
-    std::string bib_ref_candidate(StringUtil::Trim(StringUtil::ToLower(argv[1])));
-    StringUtil::CollapseWhitespace(&bib_ref_candidate);
-
-    const auto begin_end(pericopes_to_codes_map.equal_range(bib_ref_candidate));
-    if (begin_end.first != begin_end.second) {
-        if (verbose)
-            std::cerr << "Found a pericope to codes mapping.\n";
-        std::string query;
-        for (auto pair(begin_end.first); pair != begin_end.second; ++pair) {
-            if (generate_solr_query) {
-                if (not query.empty())
-                    query += ' ';
-                query += pair->first + "_" + pair->second;
-            } else
-                std::cout << pair->second << '\n';
-        }
-        if (generate_solr_query)
-            std::cout << query << '\n';
-
-        return EXIT_SUCCESS;
-    }
-
-    //
-    // ...now deal w/ ordinary references.
-    //
-
-    bib_ref_candidate = CanoniseLeadingNumber(InsertSpaceAtFirstLetterDigitBoundary(
-                            StringUtil::RemoveChars(" \t", &bib_ref_candidate)));
-    std::string book_candidate, chapters_and_verses_candidate;
-    SplitIntoBookAndChaptersAndVerses(bib_ref_candidate, &book_candidate, &chapters_and_verses_candidate);
-    if (verbose) {
-        std::cerr << "book_candidate = \"" << book_candidate << "\"\n";
-        std::cerr << "chapters_and_verses_candidate = \"" << chapters_and_verses_candidate << "\"\n";
-    }
-
-    // Map from noncanonical bible book forms to the canonical ones:
-    std::unordered_map<std::string, std::string> books_of_the_bible_to_canonical_form_map;
-    MapIO::DeserialiseMap(argv[3], &books_of_the_bible_to_canonical_form_map);
-    const auto non_canonical_form_and_canonical_form(books_of_the_bible_to_canonical_form_map.find(book_candidate));
-    if (non_canonical_form_and_canonical_form != books_of_the_bible_to_canonical_form_map.end()) {
-        if (verbose)
-            std::cerr << "Replacing \"" << book_candidate << "\" with \""
-                      << non_canonical_form_and_canonical_form->second << "\".\n";
-        book_candidate = non_canonical_form_and_canonical_form->second;
-    }
-
-    std::unordered_map<std::string, std::string> bible_books_to_codes_map;
-    MapIO::DeserialiseMap(argv[2], &bible_books_to_codes_map);
-    const auto bible_book_and_code(bible_books_to_codes_map.find(book_candidate));
-    if (bible_book_and_code == bible_books_to_codes_map.end()) {
-        if (verbose)
-            std::cerr << "No mapping from \"" << book_candidate << "\" to a book code was found!\n";
-
-        return EXIT_FAILURE; // Unknown bible book!
-    }
-
-    const std::string book_code(bible_book_and_code->second);
-    if (verbose)
-        std::cerr << "book code = \"" << book_code << "\"\n";
-    if (chapters_and_verses_candidate.empty()) {
-        if (generate_solr_query)
-            std::cout << (book_code + "00000") << '_' << (book_code + "99999") << '\n';
-        else
-            std::cout << book_code << "00000:" << book_code << "99999" << '\n';
-
-        return EXIT_SUCCESS;
-    }
-
-    std::set<std::pair<std::string, std::string>> start_end;
-    if (not ParseBibleReference(chapters_and_verses_candidate, book_code, &start_end)) {
-        if (verbose)
-            std::cerr << "The parsing of \"" << chapters_and_verses_candidate
-                      << "\" as chapters and verses failed!\n";
-        return EXIT_FAILURE;
-    }
-
-    std::string query;
-    for (const auto &pair : start_end) {
-        if (generate_solr_query) {
-            if (not query.empty())
-                query += ' ';
-            query += pair.first + "_" + pair.second;
-        } else
-            std::cout << pair.first << ':' << pair.second << '\n';
-    }
-    if (generate_solr_query)
-        std::cout << query << '\n';
+    HandlePericopes(verbose, generate_solr_query, bible_reference_candidate, argv[4]);
+    HandleBookRanges(verbose, generate_solr_query, books_of_the_bible_to_canonical_form_map_filename,
+		     books_of_the_bible_to_code_map_filename, bible_reference_candidate);
+    HandleOrdinaryReferences(verbose, generate_solr_query, bible_reference_candidate,
+			     books_of_the_bible_to_code_map_filename,
+			     books_of_the_bible_to_canonical_form_map_filename);
 }
