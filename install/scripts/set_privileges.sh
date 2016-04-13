a6704bf171c3004ec5455938253107233337f03f
Oliver Obenland <oliver.obenland@uni-tuebingen.de>
2016-04-13 11:19:56 +0200

    Fixing set_se_perms in set_privileges.sh

--- install/scripts/set_privileges.sh
+++ install/scripts/set_privileges.sh
@@ -67,8 +67,8 @@ function set_se_perms() {
   for arg_no in $(seq 2 $#); do
     arg="${!arg_no}"
     if [[ -d "$arg" ]]; then # Recursively process all directories.
-       find "$arg" -type d -print0 | xargs --null semanage fcontext --add --type "$1"
-       find "$arg" -type d -print0 | xargs --null restorecon -R 
+       find "$arg" -type d -print0 | xargs --null --max-lines semanage fcontext --add --type "$1"
+       find "$arg" -type d -print0 | xargs --null --max-lines restorecon -R 
     else # Assume we have an ordinary file.
       semanage fcontext --add --type "$1" "$arg"
       restorecon -R "$arg"
