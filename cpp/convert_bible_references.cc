f7e801e3bcbbb116dcbccff3a696ae9229b820ca
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2016-04-18 16:46:43 +0200

    Deal with some records that look like the had already been converted according to RDA but the 040$e field had not been updated accordingly.

--- cpp/convert_bible_references.cc
+++ cpp/convert_bible_references.cc
@@ -57,12 +57,15 @@ void ProcessRecord(XmlWriter * const xml_writer, MarcUtil::Record * const record
     _040_subfields.setSubfield('e', "rda");
     record->updateField(static_cast<size_t>(_040_index), _040_subfields.toString());
 
-    if (not _130_subfields.moveSubfield('a', 'p'))
-	return;
-    _130_subfields.setSubfield('a', "Bibel");
-    record->updateField(static_cast<size_t>(_130_index), _130_subfields.toString());
+    if (not (_130_subfields.hasSubfield('p') and _130_subfields.getFirstSubfieldValue('a') == "Bibel")) {
+	if (not _130_subfields.moveSubfield('a', 'p'))
+	    return;
+	_130_subfields.setSubfield('a', "Bibel");
+	record->updateField(static_cast<size_t>(_130_index), _130_subfields.toString());
+    }
 
     record->write(xml_writer);
+
     ++conversion_count;
 }
 
