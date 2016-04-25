f05b16e5346a5177bb46177f082eaf46b7079782
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2016-04-25 17:31:44 +0200

    We can now process gzipped tar archives containing MARC files.

--- cpp/marc_grep_multiple.sh
+++ cpp/marc_grep_multiple.sh
@@ -9,16 +9,24 @@ marc_grep_conditional_expression="$1"
 shift
 
 for filename in "$@"; do
-    if [[ $filename =~ \.gz$ ]]; then
-	actual_filename=${filename%.gz}
-	trap 'rm -f "$actual_filename"' ERR
-	gunzip < "$filename" > "$actual_filename"
+    if [[ ! $filename =~ \.tar\.gz$ ]]; then
+	marc_grep_output=$(marc_grep "$filename" "$marc_grep_conditional_expression" 3>&2 2>&1 1>&3 \
+                           |tail -1)
+        if [[ ! $marc_grep_output =~ ^Matched\ 0 && $marc_grep_output =~ ^Matched ]]; then
+	    echo "was found in $filename"
+	fi
     else
-	actual_filename="$filename"
-    fi
-    marc_grep_output=$(marc_grep "$actual_filename" "$marc_grep_conditional_expression" 3>&2 2>&1 1>&3 \
-                       |tail -1)
-    if [[ $marc_grep_output != "Matched 0 "* ]]; then
-	echo "was found in $filename"
+	tar_filename=${filename%.gz}
+	gunzip < "$filename" > "$tar_filename"
+	for archive_member in $(tar --list --file "$tar_filename"); do
+	    tar --extract --file "$tar_filename" "$archive_member"
+	    marc_grep_output=$(marc_grep "$archive_member" "$marc_grep_conditional_expression" 3>&2 2>&1 1>&3 \
+                               |tail -1)
+	    rm "$archive_member"
+            if [[ ! $marc_grep_output =~ ^Matched\ 0 && $marc_grep_output =~ ^Matched ]]; then
+		echo "was found in $tar_filename($archive_member)"
+	    fi
+	done
+	rm "$tar_filename"
     fi
 done
