345c8f7a070e2b96438a6208ce19e1c2f6f6bd3b
jriedl <johannes.riedl@uni-tuebingen.de>
2016-07-04 10:17:02 +0200

    Changes as desired

--- cpp/extract_normdata_translations.cc
+++ cpp/extract_normdata_translations.cc
@@ -1,9 +1,29 @@
+/** \file    extract_normdata_translations.cc
+ *  \brief   Extract IxTheo and MACS translations from the normdata file and write it to 
+ *           language specific text files
+ *  \author  Johannes Riedl
+ */
 
-/* We would like to have a tool that extracts translations for given
-   authority data
-   The german term is found in Field 150 
+/*
+    Copyright (C) 2016, Library of the University of Tübingen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* The German term is found in Field 150 
    Currently there are two different kinds of translations:
-   TxTheo-Translations with the following definitions:
+   IxTheo-Translations with the following definitions:
 
    710: Körperschaft - fremdsprachige Äquivalenz        
    711: Konferenz - fremdsprachige Äquivalenz  
@@ -13,17 +33,13 @@
    751: Geografikum - fremdsprachige Äquivalenz    
 
    LoC/Rameau Translations:
-   
    700: Person - Bevorzugter Name in einem anderen Datenbestand
    710: Körperschaft - Bevorzugter Name in einem anderen Datenbestand              
    711: Konferenz - Bevorzugte Benennung in einem anderen Datenbestand
    730: Einheitstitel - Bevorzugter Name in einem anderen Datenbestand
    750: Sachbegriff - Bevorzugte Benennung in einem anderen Datenbestand
    751: Geografikum - Bevorzugter Name in einem anderen Datenbestand  
-   
 */ 
-
-
 #include <iostream>
 #include <vector>
 #include <cstdlib>
@@ -34,29 +50,31 @@
 #include "Subfields.h"
 #include "util.h"
 
-// Languages to handle
-//enum languages { "en","fr" };
 
+// Languages to handle
 const unsigned int NUMBER_OF_LANGUAGES = 2;
-const std::string languages_to_create_str = "en, fr";
+const std::vector<std::string> languages_to_create{ "en", "fr" };
 enum languages { en, fr };
+
 
 void Usage() {
     std::cerr << "Usage: " << ::progname << " norm_data_marc_input extracted_translations\n";
     std::exit(EXIT_FAILURE);
 }
 
-void augment_ixtheo_tag_with_language(const MarcUtil::Record &record, const std::string tag, std::vector<std::string> &translations) {
-    auto ixtheo_pos = std::find(translations.begin(), translations.end(), "IxTheo");
-    if (ixtheo_pos != translations.end()) {
+
+void augmentIxTheoTagWithLanguage(const MarcUtil::Record &record, const std::string &tag, std::vector<std::string> * const translations) {
+    auto ixtheo_pos = std::find(translations->begin(), translations->end(), "IxTheo");
+    if (ixtheo_pos != translations->end()) {
         std::vector<std::string> ixtheo_lang_code;
         record.extractSubfields(tag, "9" , &ixtheo_lang_code);
         for (auto lang_code : ixtheo_lang_code) {
-            if (lang_code.find("eng") != std::string::npos && *ixtheo_pos != "IxTheo_eng")
-               *ixtheo_pos = *ixtheo_pos + "_eng";
-            // FIXME: There are currently no french translations... 
-            else if (lang_code.find("fra") != std::string::npos && *ixtheo_pos != "IxTheo_fra")
-               *ixtheo_pos = *ixtheo_pos + "_fra";
+            if (lang_code.find("eng") != std::string::npos and *ixtheo_pos != "IxTheo_eng")
+               *ixtheo_pos += "_eng";
+            else if (lang_code.find("fra") != std::string::npos and *ixtheo_pos != "IxTheo_fra")
+               *ixtheo_pos += "_fra";
+            else 
+                Warning("Unsupported language code " + lang_code);
         }
     }
 }
@@ -65,8 +83,8 @@ void augment_ixtheo_tag_with_language(const MarcUtil::Record &record, const std:
 void ExtractTranslations(File* const marc_norm_input, 
                          const std::string german_term_field_spec,
                          const std::string translation_field_spec,
-                         std::map<std::string,std::string> term_to_translation_map[]) {
-
+                         std::map<std::string,std::string> term_to_translation_map[]) 
+{
     std::set<std::string> german_tags_and_subfield_codes;
     if (unlikely(StringUtil::Split(german_term_field_spec, ':', &german_tags_and_subfield_codes) < 1))
         Error("ExtractTranslations: Need at least one translation field");
@@ -78,12 +96,10 @@ void ExtractTranslations(File* const marc_norm_input,
     unsigned count(0);
     
     while (const MarcUtil::Record record = MarcUtil::Record::XmlFactory(marc_norm_input)) {
-
         std::vector<std::string> german_term;
 
-        // Determine the german term we will have translations for
-
-        for (auto tag_and_subfields : german_tags_and_subfield_codes) {
+        // Determine the German term we will have translations for
+        for (const auto tag_and_subfields : german_tags_and_subfield_codes) {
              const std::string tag(tag_and_subfields.substr(0, 3));
              const std::string subfields(tag_and_subfields.substr(3));
              
@@ -101,7 +117,6 @@ void ExtractTranslations(File* const marc_norm_input,
         std::vector<std::string> all_translations;
 
         // Extract all additional translations
-
         for (auto tag_and_subfields : translation_tags_and_subfield_codes) {
             const std::string tag(tag_and_subfields.substr(0, 3));
             const std::string subfields(tag_and_subfields.substr(3));
@@ -110,53 +125,45 @@ void ExtractTranslations(File* const marc_norm_input,
             record.extractSubfields(tag, subfields, &translations);
 
             // For IxTheo-Translations add the language code in the same field
-            augment_ixtheo_tag_with_language(record, tag, translations);
+            augmentIxTheoTagWithLanguage(record, tag, &translations);
             
-            all_translations.insert(std::end(all_translations), std::begin(translations), std::end(translations));
+            all_translations.insert(all_translations.end(), translations.begin(), translations.end());
         }
  
-        for (auto it = all_translations.begin(); it != all_translations.end(); it++) {
+        for (auto it = all_translations.begin(); it != all_translations.end(); ++it) {
             if (*it == "IxTheo_eng") {
-                term_to_translation_map[en].emplace(StringUtil::Join(german_term, ' '), *(it+1));
+                term_to_translation_map[en].emplace(StringUtil::Join(german_term, ' '), *(it + 1));
+                ++it;
+            } else if (*it == "IxTheo_fra") {
+                term_to_translation_map[fr].emplace(StringUtil::Join(german_term, ' '), *(it + 1));
+                ++it;
+            } else if (*it == "lcsh") {
+                term_to_translation_map[en].emplace(StringUtil::Join(german_term, ' '), *(it + 1));
+                ++it;
+            } else if (*it == "ram") {
+                term_to_translation_map[fr].emplace(StringUtil::Join(german_term, ' '), *(it + 1));
                 ++it;
             }
-            else if (*it == "IxTheo_fra") {
-                term_to_translation_map[fr].emplace(StringUtil::Join(german_term, ' '), *(it+1));
-                ++it;
-            }
-
-            else if (*it == "lcsh") {
-                term_to_translation_map[en].emplace(StringUtil::Join(german_term, ' '), *(it+1));
-                ++it;
-            }
-            else if (*it == "ram") {
-                term_to_translation_map[fr].emplace(StringUtil::Join(german_term, ' '), *(it+1));
-                ++it;
-            }
-            
         }
-       
     }
-
-    count++;
-
+    ++count;
 }
 
 
 std::unique_ptr<File> OpenInputFile(const std::string &filename) {
     std::string mode("r");
-    mode += MediaTypeUtil::GetFileMediaType(filename) == "application/lz4" ? "u" : "m";
+    if (MediaTypeUtil::GetFileMediaType(filename) == "application/lz4") 
+        mode += "u";  
     std::unique_ptr<File> file(new File(filename, mode));
-    if (file == nullptr)
+    if (file->fail())
         Error("can't open \"" + filename + "\" for reading!");
 
     return file;
 }
 
 
-
 int main(int argc, char **argv) {
-    progname = argv[0];
+    ::progname = argv[0];
 
     if (argc != 3)
         Usage();
@@ -177,17 +184,13 @@ int main(int argc, char **argv) {
     if (unlikely(StringUtil::Split(extracted_translations_filename, ".", &output_file_components) < 1)) 
         Error("extracted_translations_filename " + extracted_translations_filename + " is not valid");
    
-    std::vector<std::string> languages_to_create;
-    if (unlikely(StringUtil::Split(languages_to_create_str, ',', &languages_to_create) < 1))
-        Error("String " + languages_to_create_str + " is not valid");
-    
-    File* lang_file[NUMBER_OF_LANGUAGES];
+    File *lang_files[NUMBER_OF_LANGUAGES];
     unsigned i(0);
   
     // Derive output components from given input filename
     std::string extension = (output_file_components.size() > 1) ? output_file_components.back() : "";
     std::string basename;
-    if (extension != "")
+    if (not extension.empty())
         output_file_components.pop_back();
     basename = StringUtil::Join(output_file_components, ".");
       
@@ -198,8 +201,8 @@ int main(int argc, char **argv) {
          std::string lang_file_name_str = 
              (extension != "") ? basename + "_" + lang + "." + extension : basename + "_" + lang;
 
-         lang_file[i++] = new File(lang_file_name_str, output_mode);
-         if (lang_file[i] == NULL)
+         lang_files[i++] = new File(lang_file_name_str, output_mode);
+         if (lang_files[i]->fail())
              Error("can't open \"" + lang_file_name_str + "\" for writing!");
     }
 
@@ -209,17 +212,16 @@ int main(int argc, char **argv) {
         ExtractTranslations(norm_data_marc_input.get(), "100a:150a", "750a2", term_to_translation_map);
         for (auto line : term_to_translation_map[en]) {
               
-             *(lang_file[en]) << line.first << "|" << line.second << "\n";
+             *(lang_files[en]) << line.first << "|" << line.second << "\n";
         }
 
         for (auto line : term_to_translation_map[fr]) {
-             *(lang_file[fr]) << line.first << "|" << line.second << "\n";
+             *(lang_files[fr]) << line.first << "|" << line.second << "\n";
         }
 
     } catch (const std::exception &x) {
       Error("caught exception: " + std::string(x.what()));
     }
-
 }
 
 
