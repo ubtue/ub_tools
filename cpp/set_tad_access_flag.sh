be2014ad45b8fa17717a30137effd5ba95634c59
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2016-08-05 11:28:33 +0200

    Wrapper for the C++ command with the same name.

---
+++ cpp/set_tad_access_flag.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+LOG_FILE=/var/log/ixtheo/set_tad_access_flag.log
+
+if [[ $# != 1 ]]; then
+    echo "$(date): script called with invalid argument count!" >> "$LOG_FILE"
+    exit 1
+fi
+
+COMMAND_PATH=/usr/local/bin/set_tad_access_flag
+
+if [ ! -x "$COMMAND_PATH" ]; then
+    echo "$(date): command $COMMAND_PATH is not installed!" >> "$LOG_FILE"
+    exit 1
+fi
+
+"$COMMAND_PATH" "$1" >> "$LOG_FILE" 2>&1
+if [ $? != 0 ]; then
+    echo "$(date): command failed!" >> "$LOG_FILE"
+    exit 1
+fi
+
