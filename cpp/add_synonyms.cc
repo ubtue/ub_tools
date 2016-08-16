2cc5dbbe07bf403c6213fa1b5a4d9c29cff90423
jriedl <johannes.riedl@uni-tuebingen.de>
2016-08-16 09:18:00 +0200

    Desired adjustments

--- cpp/add_synonyms.cc
+++ cpp/add_synonyms.cc
@@ -46,6 +46,7 @@ void Usage() {
     std::exit(EXIT_FAILURE);
 }
 
+
 std::string getTag(const std::string &tag_and_subfields_spec) {
     return tag_and_subfields_spec.substr(0, 3);
 }
@@ -59,59 +60,58 @@ std::string getSubfields(const std::string &tag_and_subfields_spec) {
 void ExtractSynonyms(File * const norm_data_marc_input, const std::set<std::string> &primary_tags_and_subfield_codes,
                     const std::set<std::string> &synonym_tags_and_subfield_codes,  std::map<std::string, std::string> *synonym_map) {
     
-    std::set<std::string>::const_iterator primary;
-    std::set<std::string>::const_iterator synonym;
 
-    while (const MarcUtil::Record record = MarcUtil::Record::XmlFactory(norm_data_marc_input)){
-
+    while (const MarcUtil::Record record = MarcUtil::Record::XmlFactory(norm_data_marc_input)) {
+        std::set<std::string>::const_iterator primary;
+        std::set<std::string>::const_iterator synonym;
         unsigned int i(0);
         for (primary = primary_tags_and_subfield_codes.begin(), synonym = synonym_tags_and_subfield_codes.begin();  
             primary != primary_tags_and_subfield_codes.end();
-            ++primary, ++synonym, ++i) {
-
+            ++primary, ++synonym, ++i) 
+        {
             // Fill maps with synonyms
             std::vector<std::string> primary_values; 
             std::vector<std::string> synonym_values;              
 
             if (record.extractSubfields(getTag(*primary), getSubfields(*primary), &primary_values) and 
-                record.extractSubfields(getTag(*synonym), getSubfields(*synonym), &synonym_values)) {
+                record.extractSubfields(getTag(*synonym), getSubfields(*synonym), &synonym_values))
                     synonym_map[i].emplace(StringUtil::Join(primary_values, ','), StringUtil::Join(synonym_values, ','));
-            }
         }
     }
 }
 
+
 void ProcessRecord(MarcUtil::Record * const record, std::map<std::string, std::string> *synonym_map, 
-                    const std::set<std::string> &primary_tags_and_subfield_codes,
-                    const std::set<std::string> &output_tags_and_subfield_codes) {
-     
+                   const std::set<std::string> &primary_tags_and_subfield_codes,
+                   const std::set<std::string> &output_tags_and_subfield_codes) 
+{
     std::set<std::string>::const_iterator primary;
     std::set<std::string>::const_iterator output;
     unsigned int i(0);
 
     for (primary = primary_tags_and_subfield_codes.begin(), output = output_tags_and_subfield_codes.begin();
         primary != primary_tags_and_subfield_codes.end();
-        ++primary, ++output, ++i) {
-         
+        ++primary, ++output, ++i) 
+    {
         // Insert synonyms
         std::vector<std::string> primary_values;
 
         if (record->extractSubfields(getTag(*primary), getSubfields(*primary), &primary_values)) {
-            std::string synonyms = synonym_map[i][StringUtil::Join(primary_values, ',')];
-
+            std::string synonyms(synonym_map[i][StringUtil::Join(primary_values, ',')]);
             if (synonyms.empty())
                 continue;
+
             // Abort if field is already populated
-            std::string tag = getTag(*output);
+            std::string tag(getTag(*output));
             if (record->getFieldIndex(tag) != -1)
-                Error("Field with tag " + tag + " is not empty for PPN " + record->getFields()[0] + '\n');
+                Error("Field with tag " + tag + " is not empty for PPN " + record->getControlNumber() + '\n');
             std::string subfield_spec = getSubfields(*output);
             if (subfield_spec.size() != 1)
                 Error("We currently only support a single subfield and thus specifying " + subfield_spec + " as output subfield is not valid\n");
             Subfields subfields(' ', ' '); // <- indicators must be set explicitly although empty
             subfields.addSubfield(subfield_spec.at(0), synonyms);
             if (not(record->insertField(tag, subfields.toString())))
-                Warning("Could not insert field " + tag + " for PPN " + record->getFields()[0] + '\n');
+                Warning("Could not insert field " + tag + " for PPN " + record->getControlNumber() + '\n');
             ++modified_count;
         }
     }       
@@ -131,6 +131,7 @@ void InsertSynonyms(File * const marc_input, File * marc_output, const std::set<
 
     std::cerr << "Modified " << modified_count << " of " << record_count << " record(s).\n";
 }
+
 
 int main(int argc, char **argv) {
     progname = argv[0];
@@ -159,10 +160,10 @@ int main(int argc, char **argv) {
 
     try {
         // Determine possible mappings
-        const std::string AUTHORITY_DATA_PRIMARY_SPEC = "110abcd:111abcd:130abcd:150abcd:151abcd";
-        const std::string AUTHORITY_DATA_SYNONYM_SPEC = "410abcd:411abcd:430abcd:450abcd:451abcd";
-        const std::string TITLE_DATA_PRIMARY_SPEC = "610abcd:611abcd:430abcd:650abcd:651abcd";
-        const std::string TITLE_DATA_UNUSED_FIELDS_FOR_SYNONYMS = "180a:181a:182a:183a:184a";
+        const std::string AUTHORITY_DATA_PRIMARY_SPEC("110abcd:111abcd:130abcd:150abcd:151abcd");
+        const std::string AUTHORITY_DATA_SYNONYM_SPEC("410abcd:411abcd:430abcd:450abcd:451abcd");
+        const std::string TITLE_DATA_PRIMARY_SPEC("610abcd:611abcd:430abcd:650abcd:651abcd");
+        const std::string TITLE_DATA_UNUSED_FIELDS_FOR_SYNONYMS("180a:181a:182a:183a:184a");
 
         // Determine fields to handle
         std::set<std::string> primary_tags_and_subfield_codes;
@@ -189,16 +190,14 @@ int main(int argc, char **argv) {
             Error("Number of fields in all field specifications must be identical");
              
         // Set up the array of synonym lists
-        std::map<std::string, std::string> *synonym_map = new std::map<std::string, std::string>[num_of_entries];
+        std::map<std::string, std::string> *synonym_map(new std::map<std::string, std::string>[num_of_entries]);
         ExtractSynonyms(norm_data_marc_input.get(), primary_tags_and_subfield_codes, synonym_tags_and_subfield_codes, synonym_map);
 
         // Iterate over the title data
         InsertSynonyms(marc_input.get(), &marc_output, input_tags_and_subfield_codes, output_tags_and_subfield_codes, synonym_map);
 
-        delete[] synonym_map;
-    }
-    catch (std::exception &x){
+        delete [] synonym_map;
+    } catch (const std::exception &x) {
         Error("caught exception: " + std::string(x.what()));
-
     }
 }
