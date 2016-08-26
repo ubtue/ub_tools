bc8c3db10af23551d7b9165e4bc6043c14b8ab7a
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2016-08-26 11:11:29 +0200

    Fixed many problems.

--- cpp/data/create_translations_tables.sql
+++ cpp/data/create_translations_tables.sql
@@ -1,17 +1,18 @@
-CREATE TABLE `vufind_translations` (
-  `token` varchar(255) NOT NULL,
-  `language_code` char(3) NOT NULL,
-  `translation` varchar(255) NOT NULL,
-  UNIQUE KEY `token_language_code` (`token`,`language_code`),
-  KEY `vufind_translations_idx_token` (`token`),
-  KEY `vufind_translations_idx_language_code` (`language_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE vufind_translations (
+  token VARCHAR(255) NOT NULL,
+  language_code CHAR(3) NOT NULL,
+  translation VARCHAR(255) NOT NULL,
+  UNIQUE KEY token_language_code (token,language_code),
+  KEY vufind_translations_idx_token (token),
+  KEY vufind_translations_idx_language_code (language_code)
+) DEFAULT CHARSET=utf8;
 
-CREATE TABLE `keyword_translations` (
-  `ppn` varchar(9) NOT NULL,
-  `language_code` char(3) NOT NULL,
-  `translation` varchar(255) NOT NULL,
-  `preexists` tinyint(1) NOT NULL DEFAULT '0',
-  KEY `keyword_translations_idx_id` (`id`),
-  KEY `keyword_translations_idx_language_code` (`language_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+CREATE TABLE keyword_translations (
+  ppn CHAR(9) NOT NULL,
+  language_code CHAR(3) NOT NULL,
+  translation VARCHAR(255) NOT NULL,
+  preexists BOOLEAN NOT NULL DEFAULT FALSE,
+  KEY keyword_translations_idx_ppn (ppn),
+  KEY keyword_translations_idx_language_code (language_code)
+) DEFAULT CHARSET="utf8";
