2cc86a90c8437fb6dc19483f8b6de9658c91152e
jriedl <johannes.riedl@uni-tuebingen.de>
2016-09-20 10:28:28 +0200

    Setup script for ramdisk solr

--- solrj_apps/query_reference_ids.sh
+++ solrj_apps/query_reference_ids.sh
@@ -28,10 +28,10 @@ reffile=$1
 outputdir=$2
 
 #Query all matching IDs from Solr and write files for each term respectively
-cat $reffile | awk --file rewrite_query_expression.awk | xargs -0 --max-args=2 --max-procs=8 java -cp $CLASSPATH  $QUERYREF_MAIN $outputdir
+cat $reffile | awk --file rewrite_query_expression.awk | xargs -t -0 --max-args=2 --max-procs=8 java -cp $CLASSPATH  $QUERYREF_MAIN $outputdir
 
 # Create a with with a list of IDs and all matching reference terms
-cat $outputir/* | sort -k1 > $outputdir/UNIFIED
+cat $outputdir/* | sort -k1 > $outputdir/UNIFIED
 awk -F "|" 's != $1 || NR ==1{s=$1;if(p){print p};p=$0;next} {sub($1,"",$0);p=p""$0;}END{print p}' < $outputdir/UNIFIED > $outputdir/MERGED
 
 
