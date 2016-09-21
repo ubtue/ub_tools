3c3984f2957befb5c9b25fd481a7205b968b72de
jriedl <johannes.riedl@uni-tuebingen.de>
2016-09-21 13:19:53 +0200

    Jars are now copied directly from standard vufind

--- cpp/setup_refterm_solr.sh
+++ cpp/setup_refterm_solr.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 set -o errexit -o nounset
 
-#IMPORT_CONFIG_DIRECTORY
-#ZRAM_DEVICE
-#ZRAM_MOUNT_DIRECTORY
-CONFIG_FILE_DIR=/home/qubhw01/zram_tmp2
+VUFIND_HOME=/usr/local/vufind2
+STANDARD_VUFIND_SOLRMARC_HOME=$VUFIND_HOME/import/
+UB_TOOLS_HOME=/usr/local/ub_tools/
+CONFIG_FILE_DIR=$UB_TOOLS_HOME/cpp/data/refterm_solr_conf
 RAMDISK_DIR=/mnt/zram
+SOLR_START_SCRIPT=solr.sh
+LOGDIR=$RAMDISK_DIR/log/ 
 
 export SOLR_BIN=/usr/local/vufind2/solr/vendor/bin/
 export SOLRMARC_HOME=/mnt/zram/import
 export SOLR_HOME=/mnt/zram/solr/vufind/
 export SOLR_PORT=8081
-SOLR_START_SCRIPT="solr.sh"
-LOGDIR=$RAMDISK_DIR/tmp/ 
 
 usage() {
     cat << EOF
@@ -52,11 +52,16 @@ mke2fs -q -m 0 -b 4096 -O sparse_super -L zram /dev/zram0
 
 # Create mount directory if it does not exist
 mkdir -p $RAMDISK_DIR
+mkdir -p $LOGDIR
 
 mount -o relatime,nosuid /dev/zram0 $RAMDISK_DIR
 
 # Copy the config directory to zram
-rsync -ar $CONFIG_FILE_DIR/ $RAMDISK_DIR
+rsync --archive --recursive $CONFIG_FILE_DIR/ $RAMDISK_DIR
+# Copy jars to zram
+rsync --archive --include='*.jar'  --exclude='*' $STANDARD_VUFIND_SOLRMARC_HOME $RAMDISK_DIR/import
+rsync --archive $STANDARD_VUFIND_SOLRMARC_HOME/lib  $RAMDISK_DIR/import
+rsync --archive $STANDARD_VUFIND_SOLRMARC_HOME/bin  $RAMDISK_DIR/import
 
 #Setup and start Solr
 $RAMDISK_DIR/$SOLR_START_SCRIPT start
