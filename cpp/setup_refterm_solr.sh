65f98823c4c71b9d3ca788451a7524fcc4e35e51
jriedl <johannes.riedl@uni-tuebingen.de>
2016-09-26 10:09:39 +0200

    Required changes

--- cpp/setup_refterm_solr.sh
+++ cpp/setup_refterm_solr.sh
@@ -18,13 +18,13 @@ usage() {
     cat << EOF
 Sets up a a temporary Solr instance in a Ramdisk and imports the MARC21 data given as a parameter
 
-USAGE: ${0##*/} FILE_TO_IMPORT
+USAGE: "${0##*/}" FILE_TO_IMPORT
 EOF
 }
 
 #Abort if we are not root or the parameters do not match
 
-if [[ $EUID -ne 0 ]]; then
+if [[ "$EUID" -ne 0 ]]; then
     echo "We can only run as root" 2>&1
     exit 1
 fi
@@ -35,7 +35,7 @@ if [[ "$#" -ne 1 ]]; then
     exit 1
 fi
 
-FILE_TO_IMPORT=$1
+FILE_TO_IMPORT="$1"
 
 # Test whether zram is already active
 if ! modprobe --dry-run --first-time zram 2>/dev/null ; then
@@ -50,20 +50,20 @@ echo 4G > /sys/block/zram0/disksize
 mke2fs -q -m 0 -b 4096 -O sparse_super -L zram /dev/zram0
 
 # Create mount directory if it does not exist
-mkdir -p $RAMDISK_DIR
-mkdir -p $LOGDIR
+mkdir -p "$RAMDISK_DIR"
+mkdir -p "$LOGDIR"
 
-mount -o relatime,nosuid /dev/zram0 $RAMDISK_DIR
+mount -o relatime,nosuid /dev/zram0 "$RAMDISK_DIR"
 
 # Copy the config directory to zram
-rsync --archive --recursive $CONFIG_FILE_DIR/ $RAMDISK_DIR
+rsync --archive --recursive "$CONFIG_FILE_DIR/" "$RAMDISK_DIR"
 # Copy jars to zram
-rsync --archive --include='*.jar'  --exclude='*' $STANDARD_VUFIND_SOLRMARC_HOME $RAMDISK_DIR/import
-rsync --archive $STANDARD_VUFIND_SOLRMARC_HOME/lib  $RAMDISK_DIR/import
-rsync --archive $STANDARD_VUFIND_SOLRMARC_HOME/bin  $RAMDISK_DIR/import
+rsync --archive --include='*.jar'  --exclude='*' "$STANDARD_VUFIND_SOLRMARC_HOME" "$RAMDISK_DIR/import"
+rsync --archive "$STANDARD_VUFIND_SOLRMARC_HOME"/lib"  "$RAMDISK_DIR"/import
+rsync --archive "$STANDARD_VUFIND_SOLRMARC_HOME"/bin  "$RAMDISK_DIR"/import
 
 #Setup and start Solr
-$RAMDISK_DIR/$SOLR_START_SCRIPT start
+"$RAMDISK_DIR"/"$SOLR_START_SCRIPT" start
 
 #Import the files
-$RAMDISK_DIR/import-marc.sh -p $RAMDISK_DIR/import/import-properties $1 2>&1 
+"$RAMDISK_DIR"/import-marc.sh -p "$RAMDISK_DIR"/import/import-properties "$1" 2>&1 
