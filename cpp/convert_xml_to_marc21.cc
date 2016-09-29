9e7fad831fecc8c0009f31377161c184d2a766ab
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2016-09-29 11:20:58 +0200

    Added support for "required" matches.

--- cpp/convert_xml_to_marc21.cc
+++ cpp/convert_xml_to_marc21.cc
@@ -85,7 +85,7 @@ std::string ExtractOptionallyQuotedString(std::string::const_iterator &ch,
     std::string extracted_string;
     if (*ch == '"') { // Extract quoted string.
         ++ch;
-        
+
         bool escaped(false);
         while (ch != end and *ch != '"') {
             if (unlikely(escaped)) {
@@ -119,12 +119,17 @@ void SkipSpaces(std::string::const_iterator &ch, const std::string::const_iterat
 // Loads a config file that specifies the mapping from XML elements to MARC fields.  An entry looks like this
 //
 //     xml_tag_name marc_field_and_subfield optional_match_regex optional_extraction_regex
+//                                     or
+//     "required" xml_tag_name match_regex
 //
 // "xml_tag_name" is the tag for which the rule applies.  "marc_field_and_subfield" is the field which gets created
 // when we have a match.  "optional_match_regex" when present has to match the character data following the tag for
 // the rule to apply and "optional_extraction_regex" specifies which part of the data will be used (group 1).
-void LoadConfig(File * const input, std::map<std::string, std::list<Matcher>> * const xml_tag_to_marc_entry_map) {
+void LoadConfig(File * const input, std::map<std::string, std::list<Matcher>> * const xml_tag_to_marc_entry_map,
+                std::map<std::string, std::list<Matcher>> * const required_matchers)
+{
     xml_tag_to_marc_entry_map->clear();
+    required_matchers->clear();
 
     unsigned line_no(0);
     while (not input->eof()) {
@@ -144,16 +149,43 @@ void LoadConfig(File * const input, std::map<std::string, std::list<Matcher>> * 
             auto ch(line.cbegin());
             const auto end(line.cend());
             SkipSpaces(ch, end);
-            const std::string xml_tag(ExtractOptionallyQuotedString(ch, end));
-            if (unlikely(xml_tag.empty()))
+            const std::string xml_tag_or_required(ExtractOptionallyQuotedString(ch, end));
+            if (unlikely(xml_tag_or_required.empty()))
                 throw std::runtime_error("missing or empty XML tag!");
             SkipSpaces(ch, end);
+
+            RegexMatcher *matching_regex(nullptr);
+            if (xml_tag_or_required == "required") {
+                const std::string xml_tag(ExtractOptionallyQuotedString(ch, end));
+                if (unlikely(xml_tag.empty()))
+                    throw std::runtime_error("missing or empty XML tag after \"required\" keyword!");
+                
+                SkipSpaces(ch, end);
+                const std::string matching_regex_string(ExtractOptionallyQuotedString(ch, end));
+                std::string err_msg;
+                matching_regex = RegexMatcher::RegexMatcherFactory(matching_regex_string, &err_msg);
+                if (unlikely(not err_msg.empty()))
+                    throw std::runtime_error("failed to compile the regular expression for the matching regex for a "
+                                             "required condition! (" + err_msg + ")");
+
+                SkipSpaces(ch, end);
+                if (unlikely(ch != end))
+                    throw std::runtime_error("junk after regular expression!");
+
+                const Matcher new_matcher("", '\0', matching_regex);
+                const auto xml_tag_and_matchers(required_matchers->find(xml_tag));
+                if (xml_tag_and_matchers == required_matchers->end())
+                    required_matchers->emplace(xml_tag, std::list<Matcher>{ new_matcher });
+                else
+                    xml_tag_and_matchers->second.push_back(new_matcher);
+            }
+
             const std::string marc_tag_and_subfield_code(ExtractOptionallyQuotedString(ch, end));
             if (unlikely(marc_tag_and_subfield_code.length() != DirectoryEntry::TAG_LENGTH + 1))
                 throw std::runtime_error("bad MARC tag and subfield code!");
             SkipSpaces(ch, end);
 
-            RegexMatcher *matching_regex(nullptr), *extraction_regex(nullptr);
+            RegexMatcher *extraction_regex(nullptr);
             if (ch != end) {
                 const std::string matching_regex_string(ExtractOptionallyQuotedString(ch, end));
                 std::string err_msg;
@@ -173,16 +205,20 @@ void LoadConfig(File * const input, std::map<std::string, std::list<Matcher>> * 
                         throw std::runtime_error("regular expression for the extraction regex needs exactly one "
                                                  "capture group!");
                 }
+
+                SkipSpaces(ch, end);
+                if (unlikely(ch != end))
+                    throw std::runtime_error("junk after regular expression!");
             }
 
-            Matcher new_matcher(marc_tag_and_subfield_code.substr(0, DirectoryEntry::TAG_LENGTH),
-                                marc_tag_and_subfield_code[DirectoryEntry::TAG_LENGTH],
-                                matching_regex, extraction_regex);
-            const auto xml_tag_and_entries(xml_tag_to_marc_entry_map->find(xml_tag));
-            if (xml_tag_and_entries == xml_tag_to_marc_entry_map->end())
-                xml_tag_to_marc_entry_map->emplace(xml_tag, std::list<Matcher>{ new_matcher });
+            const Matcher new_matcher(marc_tag_and_subfield_code.substr(0, DirectoryEntry::TAG_LENGTH),
+                                      marc_tag_and_subfield_code[DirectoryEntry::TAG_LENGTH],
+                                      matching_regex, extraction_regex);
+            const auto xml_tag_and_matchers(xml_tag_to_marc_entry_map->find(xml_tag_or_required));
+            if (xml_tag_and_matchers == xml_tag_to_marc_entry_map->end())
+                xml_tag_to_marc_entry_map->emplace(xml_tag_or_required, std::list<Matcher>{ new_matcher });
             else
-                xml_tag_and_entries->second.push_back(new_matcher);
+                xml_tag_and_matchers->second.push_back(new_matcher);
         } catch (const std::exception &x) {
             throw std::runtime_error("error while parsing line #" + std::to_string(line_no) + " in \""
                                      + input->getPath() + "\"! (" + std::string(x.what()) + ")");
@@ -204,27 +240,30 @@ enum OutputFormat { MARC_BINARY, MARC_XML };
 
 
 void ProcessRecords(const bool verbose, const OutputFormat output_format, File * const input,
-                    File * const output, const std::map<std::string, std::list<Matcher>> &xml_tag_to_marc_entry_map)
+                    File * const output, const std::map<std::string, std::list<Matcher>> &xml_tag_to_marc_entry_map,
+                    const std::map<std::string, std::list<Matcher>> &required_matchers)
 {
     MarcXmlWriter *xml_writer;
     if (output_format == MARC_XML)
         xml_writer = new MarcXmlWriter(output);
     else
         xml_writer = nullptr;
-    
+
     SimpleXmlParser::Type type;
     std::string data;
     std::map<std::string, std::string> attrib_map;
     SimpleXmlParser xml_parser(input);
     MarcUtil::Record record;
-    unsigned record_count(0);
+    unsigned record_count(0), written_record_count(0);
     bool collect_character_data;
     std::string character_data;
+    unsigned met_required_conditions_count;
     while (xml_parser.getNext(&type, &attrib_map, &data)) {
         switch (type) {
         case SimpleXmlParser::END_OF_DOCUMENT:
             if (verbose)
-                std::cout << "Found " << record_count << " record(s) in the XML input stream.\n";
+                std::cout << "Wrote " << written_record_count << " record(s) of " << record_count
+                          << " record(s) which were found in the XML input stream.\n";
             delete xml_writer;
             return;
         case SimpleXmlParser::OPENING_TAG:
@@ -232,6 +271,7 @@ void ProcessRecords(const bool verbose, const OutputFormat output_format, File *
                 record = MarcUtil::Record();
                 record.insertField("001", GeneratePPN());
                 collect_character_data = false;
+                met_required_conditions_count = 0;
             } else if (xml_tag_to_marc_entry_map.find(data) != xml_tag_to_marc_entry_map.cend()) {
                 character_data.clear();
                 collect_character_data = true;
@@ -240,19 +280,29 @@ void ProcessRecords(const bool verbose, const OutputFormat output_format, File *
             break;
         case SimpleXmlParser::CLOSING_TAG:
             if (data == "record") {
-                (xml_writer == nullptr) ? record.write(output) : record.write(xml_writer);
+                if (met_required_conditions_count == required_matchers.size()) {
+                    (xml_writer == nullptr) ? record.write(output) : record.write(xml_writer);
+                    ++written_record_count;
+                }
+
                 ++record_count;
             } else {
-                const auto xml_tag_and_entries(xml_tag_to_marc_entry_map.find(data));
-                if (xml_tag_and_entries == xml_tag_to_marc_entry_map.cend())
+                const auto xml_tag_and_required_matchers(required_matchers.find(data));
+                if (xml_tag_and_required_matchers != required_matchers.cend()) {
+                    for (const auto &matcher : xml_tag_and_required_matchers->second) {
+                        if (matcher.matched(character_data))
+                            ++met_required_conditions_count;
+                    }
+                }
+
+                const auto xml_tag_and_matchers(xml_tag_to_marc_entry_map.find(data));
+                if (xml_tag_and_matchers == xml_tag_to_marc_entry_map.cend())
                     continue;
-                for (auto matcher(xml_tag_and_entries->second.cbegin());
-                     matcher != xml_tag_and_entries->second.cend(); ++matcher)
-                {
-                    if (matcher->matched(character_data)) {
-                        record.insertSubfield(matcher->getMarcFieldTag(), matcher->getMarcSubfieldCode(),
-                                              matcher->getInsertionData(character_data));
-                        continue;
+                for (const auto &matcher : xml_tag_and_matchers->second) {
+                    if (matcher.matched(character_data)) {
+                        record.insertSubfield(matcher.getMarcFieldTag(), matcher.getMarcSubfieldCode(),
+                                              matcher.getInsertionData(character_data));
+                        break;
                     }
                 }
             }
@@ -297,9 +347,10 @@ int main(int argc, char *argv[]) {
     const std::unique_ptr<File> output(FileUtil::OpenOutputFileOrDie(argv[4]));
 
     try {
-        std::map<std::string, std::list<Matcher>> xml_tag_to_marc_entry_map;
-        LoadConfig(config_input.get(), &xml_tag_to_marc_entry_map);
-        ProcessRecords(verbose, output_format, input.get(), output.get(), xml_tag_to_marc_entry_map);
+        std::map<std::string, std::list<Matcher>> xml_tag_to_marc_entry_map, required_matchers;
+        LoadConfig(config_input.get(), &xml_tag_to_marc_entry_map, &required_matchers);
+        ProcessRecords(verbose, output_format, input.get(), output.get(), xml_tag_to_marc_entry_map,
+                       required_matchers);
     } catch (const std::exception &x) {
         Error("caught exception: " + std::string(x.what()));
     }
