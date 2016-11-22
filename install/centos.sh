e4c66d3c9ca9459675b2d96e4589f03708d471ef
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2016-11-22 14:23:30 +0100

    Added missing software packages and cosmetic fix.

--- install/centos.sh
+++ install/centos.sh
@@ -21,5 +21,20 @@ yum -y install epel-release awk git mariadb mariadb-server httpd php php-devel p
     php-mysql php-xsl php-gd php-mbstring php-mcrypt java-*-openjdk-devel mawk mod_ssl epel-release wget \
     policycoreutils-python curl-openssl mutt golang lsof clang gcc-c++.x86_64 file-devel pcre-devel openssl-devel \
     kyotocabinet-devel tokyocabinet-devel poppler-utils libwebp mariadb-devel.x86_64 libxml2-devel.x86_64 \
-    libcurl-openssl-devel.x86_64 ant lz4 unzip libarchive-devel boost-devel 
+    libcurl-openssl-devel.x86_64 ant lz4 unzip libarchive-devel boost-devel cryptsetup
 systemctl start mariadb.service
+
+
+git clone --recurse-submodules https://github.com/ubtue/ub_tools.git /usr/local/ub_tools
+
+
+if [[ $1 == "ixtheo" ]]; then
+    git clone https://github.com/ubtue/ixtheo.git /usr/local/vufind
+else # krimdok
+    git clone https://github.com/ubtue/krimdok.git /usr/local/vufind
+fi
+
+
+cd /usr/local/ub_tools
+make install
+./cpp/installer $1
