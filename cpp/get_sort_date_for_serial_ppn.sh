32e759d8b75bc9a7a38d62953e3310d2fcb30cc7
jriedl <johannes.riedl@uni-tuebingen.de>
2016-12-12 08:26:19 +0100

    Added serial sort functionality to refterm stage

--- cpp/get_sort_date_for_serial_ppn.sh
+++ cpp/get_sort_date_for_serial_ppn.sh
@@ -4,15 +4,15 @@ set -o errexit -o nounset
 
 if [ $# -ne 2 ]; then
       echo "usage: $0 serial_ppn append_list"
-      exit 1;
+      echo $1 $2
 fi
 
 serial_ppn="$1"
 append_list="$2"
 
 
-#curl_base_string='http://localhost:8081/solr/biblio/select?fl=publishSortDate&wt=csv&rows=1&sort=publishDateSort+asc'
-curl_base_string='http://ptah:8080/solr/biblio/select?fl=publishDate&wt=csv&rows=1&sort=publishDateSort+desc'
+curl_base_string='http://localhost:8081/solr/biblio/select?fl=publishSortDate&wt=csv&rows=1&sort=publishDateSort+asc'
+#curl_base_string='http://ptah:8080/solr/biblio/select?fl=publishDate&wt=csv&rows=1&sort=publishDateSort+desc'
 
 sort_year=$(curl --silent --get --data-urlencode "q=superior_ppn:${serial_ppn}"  ${curl_base_string} | tail  -n +2 | sed -e 's/,$//')
 
--- cpp/query_serial_sort_data.sh
+++ cpp/query_serial_sort_data.sh
@@ -13,17 +13,20 @@ serial_sort_list="$2"
 
 tmpdir="/mnt/zram/serial_ppn_tmp"
 
-mkdir "$tmpdir" && cd "$tmpdir"
+mkdir "$tmpdir"
 
-serial_ppns="serial_ppns"
+serial_ppns_name="serial_ppns"
+serial_ppns="$tmpdir"/"$serial_ppns_name"
 
 #Extract all serial PPNs
+echo "Extract serial PPNs from \"$marc_title_input\" into \"$serial_ppns\""
 create_serial_ppns.sh "$marc_title_input" "$serial_ppns"
 
-#Now find the oldest inferior work 
-> "$serial_ppns"
-cat "$serial_ppns" | xargs  -I '{}' --max-proces=8 get_sort_date_for_serial_ppn.sh  '{}' "$serial_sort_list"
+#echo "Find the oldest inferior work for each PPN" 
+> "$tmpdir"/"$serial_sort_list"
+cat "$serial_ppns" | xargs  -I '{}' --max-procs=8 get_sort_date_for_serial_ppn.sh  '{}' "$tmpdir"/"$serial_sort_list"
 
+cp "$tmpdir"/"$serial_sort_list" .
 
 
 
