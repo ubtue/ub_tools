6831790a871280b6e3fb6f885b0cae0272e8375e
jriedl <johannes.riedl@uni-tuebingen.de>
2016-12-14 09:37:21 +0100

    Porting temporary Solr instance to new solrmarc

--- cpp/data/refterm_solr_conf/import-marc.sh
+++ cpp/data/refterm_solr_conf/import-marc.sh
@@ -120,7 +120,7 @@ fi
 ##################################################
 # Set Command Options
 ##################################################
-JAR_FILE="/mnt/zram/import/SolrMarc.jar"
+JAR_FILE="/mnt/zram/import/solrmarc_core.jar"
 
 #####################################################
 # Verify that JAR_FILE exists
@@ -142,7 +142,7 @@ MARC_FILE=`basename $1`
 # Execute Importer
 #####################################################
 
-RUN_CMD="$JAVA $INDEX_OPTIONS -Duser.timezone=UTC -Dsolr.core.name=$SOLRCORE $EXTRA_SOLRMARC_SETTINGS -jar $JAR_FILE $PROPERTIES_FILE $MARC_PATH/$MARC_FILE"
+RUN_CMD="$JAVA $INDEX_OPTIONS -Duser.timezone=UTC -Dsolr.core.name=$SOLRCORE $EXTRA_SOLRMARC_SETTINGS -jar $JAR_FILE $PROPERTIES_FILE -solrj $VUFIND_HOME/solr/vendor/dist/solrj-lib $MARC_PATH/$MARC_FILE"
 echo "Now Importing $1 ..."
 # solrmarc writes log messages to stderr, write RUN_CMD to the same place
 echo "`date '+%h %d, %H:%M:%S'` $RUN_CMD" >&2
