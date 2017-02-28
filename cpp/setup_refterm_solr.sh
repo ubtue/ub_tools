c78acf7c48c2de8bb5eb6e82feaac62fe5b9d984
jriedl <johannes.riedl@uni-tuebingen.de>
2017-02-28 09:51:38 +0100

    Trying harder to cope with allocation problems

--- cpp/setup_refterm_solr.sh
+++ cpp/setup_refterm_solr.sh
@@ -8,11 +8,15 @@ CONFIG_FILE_DIR=$UB_TOOLS_HOME/cpp/data/refterm_solr_conf
 RAMDISK_DIR=/mnt/zram
 SOLR_START_SCRIPT=solr.sh
 LOGDIR=$RAMDISK_DIR/log/ 
+ZRAM_DISK_SIZE=2G
+ZRAM_DISK_SIZE_CONTROL_FILE=/sys/block/zram0/disksize
 
 export SOLR_BIN=/usr/local/vufind2/solr/vendor/bin/
 export SOLRMARC_HOME=/mnt/zram/import
 export SOLR_HOME=/mnt/zram/solr/vufind/
 export SOLR_PORT=8081
+
+trap ExitHandler SIGINT
 
 usage() {
     cat << EOF
@@ -21,6 +25,54 @@ Sets up a a temporary Solr instance in a Ramdisk and imports the MARC21 data giv
 USAGE: "${0##*/}" FILE_TO_IMPORT
 EOF
 }
+
+
+function ExitHandler {
+   shutdown_ramdisk
+}
+
+
+setup_ramdisk() {
+    # Test whether zram is already active
+    if ! modprobe --dry-run --first-time zram 2>/dev/null ; then
+       echo "Zram module is already loaded"
+       echo "We currently cannot cope with another active zram"
+       exit 1
+    fi
+
+    #Setup RAMDISK
+    modprobe zram
+    sleep 1
+    set_ramdisk_size
+    mke2fs -q -m 0 -b 4096 -O sparse_super -L zram /dev/zram0
+}
+
+
+set_ramdisk_size() {
+   echo $ZRAM_DISK_SIZE > $ZRAM_DISK_SIZE_CONTROL_FILE
+   if [ $(cat $ZRAM_DISK_SIZE_CONTROL_FILE) == 0 ] ; then
+       for  i in $(seq 1 5); do
+          echo $ZRAM_DISK_SIZE > $ZRAM_DISK_SIZE_CONTROL_FILE
+          sleep 5
+          if [ $(cat $ZRAM_DISK_SIZE_CONTROL_FILE) != 0 ] ; then
+              return 0
+          fi
+       done
+   else
+       return 0
+   fi
+   echo "Could not successfully set ramdisk size"
+   shutdown_ramdisk
+   exit 1
+}
+
+
+shutdown_ramdisk() {
+# Centos dows not support wait option for rmmod, so...
+sleep 1
+rmmod zram
+}
+
 
 #Abort if we are not root or the parameters do not match
 
@@ -37,24 +89,12 @@ fi
 
 FILE_TO_IMPORT="$1"
 
-# Test whether zram is already active
-if ! modprobe --dry-run --first-time zram 2>/dev/null ; then
-   echo "Zram module is already loaded"
-   echo "We currently cannot cope with another active zram"
-   exit 1
-fi   
-
-#Setup RAMDISK
-modprobe zram
-sleep 1
-echo 4G > /sys/block/zram0/disksize
-mke2fs -q -m 0 -b 4096 -O sparse_super -L zram /dev/zram0
-
+# Setup zram
+setup_ramdisk
 # Create mount directory if it does not exist
 mkdir -p "$RAMDISK_DIR"
-mkdir -p "$LOGDIR"
-
 mount -o relatime,nosuid /dev/zram0 "$RAMDISK_DIR"
+mkdir -p "$LOGDIR"
 
 # Copy the config directory to zram
 rsync --archive --recursive "$CONFIG_FILE_DIR/" "$RAMDISK_DIR"
