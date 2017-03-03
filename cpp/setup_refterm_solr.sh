dccb72b7263c863746706ccddad6c6f5416ba524
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2017-03-03 11:16:20 +0100

    Tried to deal with ZRAM setup problems.

--- cpp/setup_refterm_solr.sh
+++ cpp/setup_refterm_solr.sh
@@ -2,14 +2,12 @@
 set -o errexit -o nounset
 
 VUFIND_HOME=/usr/local/vufind2
-STANDARD_VUFIND_SOLRMARC_HOME=$VUFIND_HOME/import/
+VUFIND_SOLRMARC_HOME=$VUFIND_HOME/import/
 UB_TOOLS_HOME=/usr/local/ub_tools/
 CONFIG_FILE_DIR=$UB_TOOLS_HOME/cpp/data/refterm_solr_conf
-RAMDISK_DIR=/mnt/zram
-SOLR_START_SCRIPT=solr.sh
-LOGDIR=$RAMDISK_DIR/log/ 
+LOGDIR=/mnt/zram/log/ 
 ZRAM_DISK_SIZE=2G
-ZRAM_DISK_SIZE_CONTROL_FILE=/sys/block/zram0/disksize
+
 
 export SOLR_BIN=/usr/local/vufind2/solr/vendor/bin/
 export SOLRMARC_HOME=/mnt/zram/import
@@ -18,7 +16,7 @@ export SOLR_PORT=8081
 
 trap ExitHandler SIGINT
 
-usage() {
+Usage() {
     cat << EOF
 Sets up a a temporary Solr instance in a Ramdisk and imports the MARC21 data given as a parameter
 
@@ -32,81 +30,79 @@ function ExitHandler {
 }
 
 
-setup_ramdisk() {
-    # Test whether zram is already active
-    if ! modprobe --dry-run --first-time zram 2>/dev/null ; then
-       echo "Zram module is already loaded"
-       echo "We currently cannot cope with another active zram"
-       exit 1
+SetupRamdisk() {
+    if ! modprobe zram num_devices=1; then
+        echo "$0"': Failed to load ZRAM module!'
+        exit 1
     fi
 
-    #Setup RAMDISK
-    modprobe zram
-    sleep 1
-    set_ramdisk_size
-    mke2fs -q -m 0 -b 4096 -O sparse_super -L zram /dev/zram0
+    if [ -b /mnt/zram ]; then
+        if ! umount /mnt/zram; then
+            echo "$0"': Failed to load unmount /mnt/zram!'
+            exit 1
+        fi
+    fi
+
+    # Set the RAM disk size:
+    if ! -e /sys/block/zram0/disksize; then
+        echo "$0"': Missing file: "/sys/block/zram0/disksize"!'
+        exit 1
+    fi
+    echo "$ZRAM_DISK_SIZE" > /sys/block/zram0/disksize
+    if [[ $(cat /sys/block/zram0/disksize) != "$ZRAM_DISK_SIZE" ]]; then
+        echo "$0: Failed to set ZRAM disk size to $ZRAM_DISK_SIZE"'!'
+        exit 1
+    fi
+
+    # Create a file system in RAM...
+    if ! mkfs.ext4 -q -m 0 -b 4096 -O sparse_super -L zram /dev/zram0; then
+        echo "$0: Failed to create an Ext4 file system in RAM"'!'
+        exit 1
+    fi
+    # ...and mount it.
+    if ! mount -o relatime,nosuid /dev/zram0 /mnt/zram; then
+        echo "$0: Failed to mount our RAM disk"'!'
+        exit 1
+    fi
 }
 
 
-set_ramdisk_size() {
-   echo $ZRAM_DISK_SIZE > $ZRAM_DISK_SIZE_CONTROL_FILE
-   if [ $(cat $ZRAM_DISK_SIZE_CONTROL_FILE) == 0 ] ; then
-       for  i in $(seq 1 5); do
-          echo $ZRAM_DISK_SIZE > $ZRAM_DISK_SIZE_CONTROL_FILE
-          sleep 5
-          if [ $(cat $ZRAM_DISK_SIZE_CONTROL_FILE) != 0 ] ; then
-              return 0
-          fi
-       done
-   else
-       return 0
-   fi
-   echo "Could not successfully set ramdisk size"
-   shutdown_ramdisk
-   exit 1
-}
-
-
-shutdown_ramdisk() {
-# Centos dows not support wait option for rmmod, so...
-sleep 1
-rmmod zram
-}
-
-
-#Abort if we are not root or the parameters do not match
-
+# Abort if we are not root or the parameters do not match
 if [[ "$EUID" -ne 0 ]]; then
-    echo "We can only run as root" 2>&1
+    echo "$0: We can only run as root"'!'
     exit 1
 fi
-
 if [[ "$#" -ne 1 ]]; then
-    echo "Invalid number of parameters"
-    usage
+    echo "$0: Invalid number of parameters"'!'
+    Usage
     exit 1
 fi
 
 FILE_TO_IMPORT="$1"
 
 # Setup zram
-setup_ramdisk
-# Create mount directory if it does not exist
-mkdir -p "$RAMDISK_DIR"
-mount -o relatime,nosuid /dev/zram0 "$RAMDISK_DIR"
-mkdir -p "$LOGDIR"
+SetupRamdisk
+mkdir --parents "$LOGDIR"
 
 # Copy the config directory to zram
-rsync --archive --recursive "$CONFIG_FILE_DIR/" "$RAMDISK_DIR"
+rsync --archive --recursive "$CONFIG_FILE_DIR/" /mnt/zram
 # Copy jars to zram
-rsync --archive --include='*.jar'  --exclude='*' "$STANDARD_VUFIND_SOLRMARC_HOME" "$RAMDISK_DIR/import"
-rsync --archive "$STANDARD_VUFIND_SOLRMARC_HOME"/lib  "$RAMDISK_DIR"/import
-rsync --archive "$STANDARD_VUFIND_SOLRMARC_HOME"/bin  "$RAMDISK_DIR"/import
-rsync --archive "$STANDARD_VUFIND_SOLRMARC_HOME"/lib_local "$RAMDISK_DIR"/import
-rsync --archive "$STANDARD_VUFIND_SOLRMARC_HOME"/index_java "$RAMDISK_DIR"/import
+rsync --archive --include='*.jar' --exclude='*' "$VUFIND_SOLRMARC_HOME" /mnt/zram/import
+rsync --archive "$VUFIND_SOLRMARC_HOME"/lib /mnt/zram/import
+rsync --archive "$VUFIND_SOLRMARC_HOME"/bin /mnt/zram/import
+rsync --archive "$VUFIND_SOLRMARC_HOME"/lib_local /mnt/zram/import
+rsync --archive "$VUFIND_SOLRMARC_HOME"/index_java /mnt/zram/import
 
-#Setup and start Solr
-"$RAMDISK_DIR"/"$SOLR_START_SCRIPT" start
+# Set up and start Solr
+/mnt/zram/solr.sh start
+if ! $?; then
+    echo "$0"': Failed to start Solr!'
+    exit 1
+fi
 
-#Import the files
-"$RAMDISK_DIR"/import-marc.sh -p "$RAMDISK_DIR"/import/import.properties "$1" 2>&1 
+# Import the MARC files
+/mnt/zram/import-marc.sh -p /mnt/zram/import/import.properties "$1" 2>&1 
+if ! $?; then
+    echo "$0"': Failed to import MARC files!'
+    exit 1
+fi
