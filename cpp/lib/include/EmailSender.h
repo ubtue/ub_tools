41b5c455c2d5ebb0084add93f2a75761d67e0962
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2017-03-22 10:20:43 +0100

    Fixed a typo.

--- cpp/lib/include/EmailSender.h
+++ cpp/lib/include/EmailSender.h
@@ -2,7 +2,7 @@
  *  \brief  Utility functions etc. related to the sending of email messages.
  *  \author Dr. Johannes Ruscheinski (johannes.ruscheinski@uni-tuebingen.de)
  *
- *  \copyright 2015,2017 Universitätsbiblothek Tübingen.  All rights reserved.
+ *  \copyright 2015,2017 Universitätsbibliothek Tübingen.  All rights reserved.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
