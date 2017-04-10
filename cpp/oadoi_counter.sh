8b338d20e1ba3c88a55a20bff98b4750d809fbe7
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2017-04-10 09:00:05 +0200

    Tool for counting gold and green occurrences at OADOI.

---
+++ cpp/oadoi_counter.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# The API we're using is documented at: https://oadoi.org/api
+set -o errexit -o nounset
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 doi_list"
+    exit 1
+fi
+
+# Who gets the bad news?
+EMAIL="johannes.ruscheinski@uni-tuebingen.de"
+
+declare -i total=0
+declare -i found=0
+while IFS='' read -r doi || [[ -n "$doi" ]]; do
+    oa_color=$(curl 'https://api.oadoi.org/'$doi"?email=$EMAIL" --silent --output - \
+                       | jq --monochrome-output '.results[0].oa_color')
+    ((++total))
+    if [[ $oa_color != "null" ]]; then
+        ((++found))
+    fi
+done < $1
+
+echo "Found $found of $total objects."
