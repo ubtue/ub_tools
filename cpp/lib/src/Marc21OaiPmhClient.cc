67706393f4c95ec7265132094fdec1911e34523e
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2017-06-09 12:02:18 +0200

    Our very own OAI-PMH harvester.

---
+++ cpp/lib/src/Marc21OaiPmhClient.cc
@@ -0,0 +1,27 @@
+/** \brief Implementation of the Marc21OaiPmhClient class.
+ *  \author Dr. Johannes Ruscheinski (johannes.ruscheinski@uni-tuebingen.de)
+ *
+ *  \copyright 2017 Universitätsbibliothek Tübingen.  All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include "Marc21OaiPmhClient.h"
+
+
+bool Marc21OaiPmhClient::processRecord(const OaiPmh::Record &/*record*/, const unsigned /*verbosity*/,
+                                       Logger * const /*logger*/)
+{
+    (void)marc_writer_;
+    return true;
+}
