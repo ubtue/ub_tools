6c89bbf017006f06178ff9cdbd2ee7e345f316ef
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2017-07-26 13:57:53 +0200

    Cleanup.

--- cpp/zts_client.cc
+++ cpp/zts_client.cc
@@ -49,91 +49,105 @@ std::string GetNextSessionId() {
 
 
 bool Download(const std::string &server_address, const unsigned short server_port, const std::string &server_path,
-              const TimeLimit &time_limit, const std::string &harvest_url, std::string * const json_result,
-              std::string * const error_message)
+              const TimeLimit &time_limit, const std::string &request_headers, const std::string &request_body,
+              std::string * const returned_data, std::string * const error_message)
 {
-    json_result->clear();
+    returned_data->clear();
     error_message->clear();
 
-    try {
-        std::string tcp_connect_error_message;
-        const FileDescriptor socket_fd(SocketUtil::TcpConnect(server_address, server_port, time_limit,
-                                                              &tcp_connect_error_message));
-        if (socket_fd == -1) {
-            *error_message = "Could not open TCP connection to " + server_address + ", port "
-                             + std::to_string(server_port) + ": " + tcp_connect_error_message;
+    std::string tcp_connect_error_message;
+    const FileDescriptor socket_fd(SocketUtil::TcpConnect(server_address, server_port, time_limit,
+                                                          &tcp_connect_error_message, SocketUtil::DISABLE_NAGLE));
+    if (socket_fd == -1) {
+        *error_message = "Could not open TCP connection to " + server_address + ", port "
+            + std::to_string(server_port) + ": " + tcp_connect_error_message;
+        *error_message += " (Time remaining: " + std::to_string(time_limit.getRemainingTime()) + ").";
+        return false;
+    }
+
+    std::string request;
+    request += request_headers;
+    request += "\r\n";
+    request += request_body;
+
+    if (SocketUtil::TimedWrite(socket_fd, time_limit, request.data(), request.length()) == -1) {
+        *error_message = "Could not write to socket";
+        *error_message += " (Time remaining: " + std::to_string(time_limit.getRemainingTime()) + ")";
+        *error_message += '!';
+        return false;
+    }
+
+    char http_response_header[10240 + 1];
+    ssize_t no_of_bytes_read(SocketUtil::TimedRead(socket_fd, time_limit, http_response_header,
+                                                   sizeof(http_response_header) - 1));
+        /* Intentionally empty! */;
+    if (no_of_bytes_read == -1) {
+        *error_message = "Could not read from socket (1).";
+        *error_message += " (Time remaining: " + std::to_string(time_limit.getRemainingTime())
+            + ").";
+        return false;
+    }
+    http_response_header[no_of_bytes_read] = '\0';
+    HttpHeader http_header(http_response_header);
+
+    // the 2xx codes indicate success:
+    if (http_header.getStatusCode() < 200 or http_header.getStatusCode() > 299) {
+        *error_message = "Web server returned error status code ("
+            + std::to_string(http_header.getStatusCode()) + "), address was "
+            + server_address + ", port was " + std::to_string(server_port) + ", path was \""
+            + server_path +"\"!";
+        return false;
+    }
+
+    // read the returned document source:
+    std::string response(http_response_header, no_of_bytes_read);
+    char buf[10240];
+    do {
+        no_of_bytes_read = SocketUtil::TimedRead(socket_fd, time_limit, buf, sizeof(buf));
+        if (no_of_bytes_read == -1) {
+            *error_message = "Could not read from socket (2).";
             *error_message += " (Time remaining: " + std::to_string(time_limit.getRemainingTime()) + ").";
             return false;
         }
+        if (no_of_bytes_read > 0)
+            response.append(buf, no_of_bytes_read);
+    } while (no_of_bytes_read > 0);
 
+    std::string::size_type pos = response.find("\r\n\r\n"); // the header ends with two cr/lf pairs!
+    if (pos != std::string::npos) {
+        pos += 4;
+        *returned_data = response.substr(pos);
+    }
+
+    return true;
+}
+
+
+bool Download(const std::string &server_address, const unsigned short server_port, const std::string &server_path,
+              const TimeLimit &time_limit, const std::string &harvest_url, std::string * const json_result,
+              std::string * const error_message)
+{
         const std::string JSON_REQUEST("{\"url\":\"" + harvest_url + "\",\"sessionid\":\"" + GetNextSessionId()
                                        + "\"}");
-        std::string data_to_be_sent;
-        data_to_be_sent += "POST " + server_path + " HTTP/1.0\r\n";
-        data_to_be_sent += "Host: " + server_address + "\r\n";
-        data_to_be_sent += "User-Agent: zts_client/1.0 ub_tools\r\n";
-        data_to_be_sent += "Accept: application/json\r\n";
-        data_to_be_sent += "Connection: close\r\n";
-        data_to_be_sent += "Content-Type: application/json\r\n";
-        data_to_be_sent += "Content-Length: " + std::to_string(JSON_REQUEST.length()) + "\r\n";
-        data_to_be_sent += "\r\n";
-        data_to_be_sent += JSON_REQUEST;
+        std::string headers;
+        headers += "POST " + server_path + " HTTP/1.0\r\n";
+        headers += "Host: " + server_address + "\r\n";
+        headers += "User-Agent: zts_client/1.0 ub_tools\r\n";
+        headers += "Accept: application/json\r\n";
+        headers += "Connection: close\r\n";
+        headers += "Content-Type: application/json\r\n";
+        headers += "Content-Length: " + std::to_string(JSON_REQUEST.length()) + "\r\n";
 
-        if (SocketUtil::TimedWrite(socket_fd, time_limit, data_to_be_sent.c_str(), data_to_be_sent.length())
-            == -1)
-        {
-            *error_message = "Could not write to socket";
-            *error_message += " (Time remaining: " + std::to_string(time_limit.getRemainingTime()) + ")";
-            *error_message += '!';
-            return false;
-        }
+        return Download(server_address, server_port, server_path, time_limit, headers, JSON_REQUEST, json_result,
+                        error_message);
+}
 
-        char http_response_header[10240+1];
-        ssize_t no_of_bytes_read = SocketUtil::TimedRead(socket_fd, time_limit, http_response_header,
-                                                         sizeof(http_response_header) - 1);
-        if (no_of_bytes_read == -1) {
-            *error_message = "Could not read from socket (1).";
-            *error_message += " (Time remaining: " + std::to_string(time_limit.getRemainingTime())
-                + ").";
-            return false;
-        }
-        http_response_header[no_of_bytes_read] = '\0';
-        HttpHeader http_header(http_response_header);
 
-        // the 2xx codes indicate success:
-        if (http_header.getStatusCode() < 200 or http_header.getStatusCode() > 299) {
-            *error_message = "Web server returned error status code ("
-                             + std::to_string(http_header.getStatusCode()) + "), address was "
-                             + server_address + ", port was " + std::to_string(server_port) + ", path was \""
-                             + server_path +"\"!";
-            return false;
-        }
-
-        // read the returned document source:
-        std::string response(http_response_header, no_of_bytes_read);
-        char buf[10240+1];
-        do {
-            no_of_bytes_read = SocketUtil::TimedRead(socket_fd, time_limit, buf, sizeof(buf) - 1);
-            if (no_of_bytes_read == -1) {
-                *error_message = "Could not read from socket (2).";
-                *error_message += " (Time remaining: " + std::to_string(time_limit.getRemainingTime()) + ").";
-                return false;
-            }
-            if (no_of_bytes_read > 0)
-                response += std::string(buf, no_of_bytes_read);
-        } while (no_of_bytes_read > 0);
-
-        std::string::size_type pos = response.find("\r\n\r\n"); // the header ends with two cr/lf pairs!
-        if (pos != std::string::npos) {
-            pos += 4;
-            *json_result = response.substr(pos);
-        }
-
-        return true;
-    } catch (const std::exception &x) {
-        throw std::runtime_error("in Download: (address = " + server_address + ") caught exception: "
-                                 + std::string(x.what()));
-    }
+inline bool Download(const Url &url, const TimeLimit &time_limit, const std::string &harvest_url,
+                     std::string * const json_result, std::string * const error_message)
+{
+    return Download(url.getAuthority(), url.getPort(), url.getPath(), time_limit, harvest_url, json_result,
+                    error_message);
 }
 
 
@@ -143,6 +157,7 @@ void Harvest(const std::string &zts_server_url, const std::string &harvest_url) 
     if (not Download(server_url.getAuthority(), server_url.getPort(), server_url.getPath(), /* time_limit = */ 10000,
                      harvest_url, &json_document, &error_message))
         Error("Download for harvest URL \"" + harvest_url + "\" failed: " + error_message);
+    std::cout << "Server sent: \n" << json_document << '\n';
 }
 
 
