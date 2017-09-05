e345be275dd17bc0f9605c231ff9617f3de4c9aa
Mario Trojan <mario.trojan@googlemail.com>
2017-09-05 13:30:33 +0200

    ZTS => show status bar, asynchronous execution

--- php/public/zts/index.php
+++ php/public/zts/index.php
@@ -3,12 +3,38 @@
      * Test site for Zotero Translation Server, using c++ zts_client and zotero_crawler
      */
     require('inc.php');
+
+    // Helper functions for javascript
+    function updateProgressBar($progress) {
+        print '<script type="text/javascript">updateProgressBar('.$progress.');</script>';
+        ob_flush_real();
+    }
+
+    function updateRuntime($seconds) {
+        print '<script type="text/javascript">updateRuntime('.$seconds.');</script>';
+        ob_flush_real();
+    }
+
 ?>
-
+<!DOCTYPE HTML>
 <html>
-
 <head>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="../res/bootstrap4/css/bootstrap.min.css"/>
+    <link rel="stylesheet" href="style.css"/>
+    <script type="text/javascript" src="../res/jquery/jquery-3.2.1.min.js"></script>
+    <script type="text/javascript">
+        function updateProgressBar(percent) {
+            var progressBar = document.getElementById('progress-bar');
+            progressBar.setAttribute('aria-valuenow', percent);
+            progressBar.style.width = percent + '%';
+            progressBar.innerHTML = percent + '% Complete';
+        }
+
+        function updateRuntime(seconds) {
+            var div_runtime = document.getElementById('runtime');
+            div_runtime.innerHTML = seconds + 's';
+        }
+    </script>
 </head>
 <body>
 <h1>ZTS Test</h1>
@@ -17,8 +43,8 @@
 <h2>Parameters</h2>
 <form method="post" action="index.php">
     <table>
-        <tr><td>Base Url</td><td><input name="UrlBase" type="text" value="<?= isset($_POST['UrlBase']) ? $_POST['UrlBase'] : 'http://allafrica.com/books/' ?>"></input></td><td>e.g. http://allafrica.com/books/</td></tr>
-        <tr><td>Regex</td><td><input name="UrlRegex" type="text" value="<?= isset($_POST['UrlRegex']) ? $_POST['UrlRegex'] : '.*/books/.*' ?>"></input></td><td>e.g. .*/books/.*</td></tr>
+        <tr><td>Base Url</td><td><input name="UrlBase" type="text" value="<?= isset($_POST['UrlBase']) ? $_POST['UrlBase'] : 'https://www.nationalarchives.gov.uk/first-world-war' ?>"></input></td><td>e.g. http://allafrica.com/books/</td></tr>
+        <tr><td>Regex</td><td><input name="UrlRegex" type="text" value="<?= isset($_POST['UrlRegex']) ? $_POST['UrlRegex'] : '.*/first-world-war/.*' ?>"></input></td><td>e.g. .*/books/.*</td></tr>
         <tr>
             <td>Depth</td>
             <td>
@@ -48,29 +74,54 @@
             </td>
             <td>MARC21 currently disabled due to problems with zts_client</td>
         </tr>
-        <tr><td colspan="3">Please note: This process can run for multiple minutes!</td></tr>
+        <tr><td colspan="3">Please note: This process can run for multiple minutes! Max runtime: <?= MAX_SCRIPT_RUNTIME ?>s</td></tr>
         <tr><td colspan="3"><input type="submit"></input></td></tr>
     </table>
 </form>
-
 
 <?php
 if (count($_POST) > 0) {
     $IgnoreRobots = true;
     $zotero = new Zotero\MetadataHarvester(ZOTERO_SERVER_URL);
-    $result = $zotero->Start($_POST['UrlBase'], $_POST['UrlRegex'], $_POST['Depth'], $IgnoreRobots, $_POST['FileExtension']);
+    $operation = $zotero->Start($_POST['UrlBase'], $_POST['UrlRegex'], $_POST['Depth'], $IgnoreRobots, $_POST['FileExtension']);
     ?>
     <h2>Result</h2>
     <table>
-        <tr><td>CMD</td><td><?= $result->Cmd ?></td></tr>
-        <tr><td>CMD Exit Code</td><td style="color: <?= ($result->CmdStatus == 0) ? 'green' : 'red'?>;"><?= $result->CmdStatus ?></td></tr>
-        <tr><td>CMD Duration</td><td><?= $result->Duration . 's' ?></td></tr>
-        <tr><td>CMD Output</td><td><textarea rows="20" readonly="readonly"><?= implode("&#10;", $result->CmdOutput) ?></textarea></td></tr>
-        <?php
-            if ($result->CmdStatus == 0) {
-                print '<tr><td>Download</td><td><a target="_blank" href="getresult.php?id=' . basename($result->MarcPath) . '">Result file</a></td></tr>';
-            }
-        ?>
+        <tr><td>Command</td><td><?= $operation->Cmd ?></td></tr>
+        <tr><td>Runtime</td><td id="runtime"></td></tr>
+        <tr><td>Progress</td><td><div class="progress"><div id="progress-bar" class="progress-bar" role="progressbar" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100" style="width:0%">0% Complete</div></div></td></tr>
+
+    <?php
+
+    // send current state to client
+    ob_flush_real();
+
+    // start status monitoring
+    $progress = 0;
+    $progress_old = false;
+    $starttime = time();
+    $status = null;
+
+    do {
+        sleep(1);
+        updateRuntime(time() - $starttime);
+
+        $progress = Zotero\MetadataHarvester::GetProgress($operation->OperationId);
+        if ($progress !== false && $progress !== $progress_old) {
+            updateProgressBar($progress);
+            $progress_old = $progress;
+        }
+
+        $status = proc_get_status($operation->Resource);
+    } while ($status['running']);
+
+    if ($status['exitcode'] == 0) {
+        print '<tr><td>Download</td><td><a target="_blank" href="getresult.php?id=' . basename($operation->MarcPath) . '">Result file</a></td></tr>';
+    } else {
+        print '<tr><td>ERROR</td><td>Exitcode: '.$status['exitcode'].'</td></tr>';
+    }
+
+    ?>
     </table>
 
     <?php
