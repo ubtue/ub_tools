00335ad344379ed0c89979623467c8c7706be71b
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2017-09-12 10:20:10 +0200

    Hopefully removed last vestiges of PageFetcher.

--- cpp/lib/include/Downloader.h
+++ cpp/lib/include/Downloader.h
@@ -86,6 +86,7 @@ public:
         bool follow_redirects_;
         bool ignore_ssl_certificates_;
         std::string proxy_host_and_port_;
+        std::vector<std::string> additional_headers_;
     public:
         explicit Params(const std::string &user_agent = DEFAULT_USER_AGENT_STRING,
                         const std::string &acceptable_languages = DEFAULT_ACCEPTABLE_LANGUAGES,
@@ -95,7 +96,8 @@ public:
                         const TextTranslationMode text_translation_mode = TRANSPARENT,
                         const PerlCompatRegExps &banned_reg_exps = PerlCompatRegExps(), const bool debugging = false,
                         const bool follow_redirects = true, bool ignore_ssl_certificates = false,
-                        const std::string &proxy_host_and_port = "");
+                        const std::string &proxy_host_and_port = "",
+                        const std::vector<std::string> &additional_headers = {});
     } params_;
 
     typedef size_t (*WriteFunc)(void *data, size_t size, size_t nmemb, void *this_pointer);
@@ -131,6 +133,7 @@ public:
     const std::vector<std::string> &getRedirectUrls() const { return redirect_urls_; }
 
     bool anErrorOccurred() const { return curl_error_code_ != CURLE_OK; }
+    CURLcode getLastErrorCode() const { return curl_error_code_; }
     const std::string &getLastErrorMessage() const;
     const std::string &getUserAgent() const { return params_.user_agent_; }
 
