d4e732cf3a369cf826d442420249e4ed49fed301
mtrojan-ub <mario.trojan@uni-tuebingen.de>
2017-09-14 10:20:31 +0200

    Update install_ubuntu_packages.sh

--- cpp/data/installer/scripts/install_ubuntu_packages.sh
+++ cpp/data/installer/scripts/install_ubuntu_packages.sh
@@ -6,7 +6,7 @@ apt install -y curl wget
 apt install -y ant clang clang++ golang openjdk-8-jdk
 apt install -y apache2 php7.0 php7.0-dev php7.0-gd php7.0-intl php7.0-json php7.0-ldap php7.0-mcrypt php7.0-mysql php7.0-xsl php-pear libapache2-mod-gnutls libapache2-mod-php7.0
 apt install -y libarchive-dev libboost-all-dev libcurl4-openssl-dev libkyotocabinet-dev liblz4-tool libmagic-dev libmysqlclient-dev libpcre3-dev libssl-dev libtokyocabinet-dev libxml2-dev uuid-dev
-apt install -y composer git
+apt install -y composer git make
 #@ruschein: how to do silent mutt (+postfix?) installation?
 #apt install -y mutt
 a2enmod rewrite
