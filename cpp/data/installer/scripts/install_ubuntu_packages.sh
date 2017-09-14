acb8f2fec007938b7e38fa99ac5a301bc00c9977
Mario Trojan <mario.trojan@googlemail.com>
2017-09-14 09:59:21 +0200

    improved zts docker configuration + ub_tools installation script (temporarily disabled mutt)

--- cpp/data/installer/scripts/install_ubuntu_packages.sh
+++ cpp/data/installer/scripts/install_ubuntu_packages.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 apt install -y software-properties-common
-add-apt-repository ppa:ubuntu-lxc/lxd-stable # Needed to get a recent version of Go.
+add-apt-repository -y ppa:ubuntu-lxc/lxd-stable # Needed to get a recent version of Go.
 apt update
-apt install -y clang golang wget curl
-apt install -y git apache2 libapache2-mod-gnutls php7.0 php7.0-dev php-pear php7.0-json php7.0-ldap php7.0-mcrypt php7.0-mysql php7.0-xsl php7.0-intl php7.0-gd libapache2-mod-php7.0 composer openjdk-8-jdk libmagic-dev libpcre3-dev libssl-dev libkyotocabinet-dev mutt libxml2-dev libmysqlclient-dev libcurl4-openssl-dev ant libtokyocabinet-dev liblz4-tool libarchive-dev libboost-all-dev clang-3.8 clang++-3.8 clang golang uuid-dev
+apt install -y curl wget
+apt install -y ant clang clang++ golang openjdk-8-jdk
+apt install -y apache2 php7.0 php7.0-dev php7.0-gd php7.0-intl php7.0-json php7.0-ldap php7.0-mcrypt php7.0-mysql php7.0-xsl php-pear libapache2-mod-gnutls libapache2-mod-php7.0
+apt install -y libarchive-dev libboost-all-dev libcurl4-openssl-dev libkyotocabinet-dev liblz4-tool libmagic-dev libmysqlclient-dev libpcre3-dev libssl-dev libtokyocabinet-dev libxml2-dev uuid-dev
+apt install -y composer git
+#@ruschein: how to do silent mutt (+postfix?) installation?
+#apt install -y mutt
 a2enmod rewrite
 phpenmod mcrypt
 /etc/init.d/apache2 restart
