8016a7f5356b5473b2ec1bcd8efddb979fa08887
Mario Trojan <mario.trojan@googlemail.com>
2017-09-18 11:33:45 +0200

    installation script improvements - use yum to install packages, instead of /mnt/ZE020150 - also not only install these packages in CentOS, but also in Ubuntu

--- cpp/data/installer/scripts/install_centos_packages.sh
+++ cpp/data/installer/scripts/install_centos_packages.sh
@@ -7,6 +7,7 @@ cd /etc/yum.repos.d/
 wget http://download.opensuse.org/repositories/security:shibboleth/CentOS_7/security:shibboleth.repo
 yum -y install curl-openssl mutt golang lsof
 yum -y install clang gcc-c++.x86_64 file-devel pcre-devel openssl-devel kyotocabinet-devel tokyocabinet-devel poppler-utils libwebp mariadb-devel.x86_64 libxml2-devel.x86_64 libcurl-openssl-devel.x86_64 ant lz4 unzip libarchive-devel boost-devel libuuid-devel
+yum -y install ca-certificates leptonica libwebp openjpeg-libs poppler poppler-utils
 
-yum -y install /mnt/ZE020150/IT-Abteilung/02_Projekte/11_KrimDok_neu/05_Pakete/*.rpm
-ln -s /usr/share/tessdata/deu.traineddata /usr/share/tesseract/tessdata/deu.traineddata
+# install tesseract. in centos, there is no language pack for "eng", it seems to be part of the default installation
+yum -y install tesseract tesseract-langpack-deu tesseract-langpack-fra
