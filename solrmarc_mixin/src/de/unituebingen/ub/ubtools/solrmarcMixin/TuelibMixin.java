ccb16220e526294e1a90454c35c1c68f622f7a13
Mario Trojan <mario.trojan@googlemail.com>
2017-09-26 16:55:37 +0200

    moved getJournalIssue from IxTheo to TuelibMixin

--- solrmarc_mixin/src/de/unituebingen/ub/ubtools/solrmarcMixin/TuelibMixin.java
+++ solrmarc_mixin/src/de/unituebingen/ub/ubtools/solrmarcMixin/TuelibMixin.java
@@ -630,6 +630,32 @@ public class TuelibMixin extends SolrIndexerMixin {
         return isils;
     }
 
+    public Set<String> getJournalIssue(final Record record) {
+        final DataField _773Field = (DataField)record.getVariableField("773");
+        if (_773Field == null)
+            return null;
+
+        final Subfield aSubfield = _773Field.getSubfield('a');
+        if (aSubfield == null)
+            return null;
+
+        final Set<String> subfields = new LinkedHashSet<String>();
+        subfields.add(aSubfield.getData());
+
+        final Subfield gSubfield = _773Field.getSubfield('g');
+        if (gSubfield != null)
+            subfields.add(gSubfield.getData());
+
+        final List<Subfield> wSubfields = _773Field.getSubfields('w');
+        for (final Subfield wSubfield : wSubfields) {
+            final String subfieldContents = wSubfield.getData();
+            if (subfieldContents.startsWith("(DE-576)"))
+                subfields.add(subfieldContents);
+        }
+
+        return subfields;
+    }
+
     /**
      * @param record
      *            the record
