28382b0a7491c9b1f079c3fef8bec7b1288d636a
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2017-09-28 15:51:10 +0200

    A test harness for a new function in PdfUtil.

---
+++ cpp/test/extract_text_test.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <cstdlib>
+#include <FileUtil.h>
+#include <PdfUtil.h>
+#include <util.h>
+
+
+void Usage() {
+    std::cerr << "usage: " << ::progname << " pdf_file\n";
+    std::exit(EXIT_FAILURE);
+}
+
+
+int main(int argc, char *argv[]) {
+    ::progname = argv[0];
+    if (argc != 2)
+        Usage();
+
+    try {
+        const std::string input_filename(argv[1]);
+        std::string pdf_document;
+        if (not FileUtil::ReadString(input_filename, &pdf_document))
+            Error("failed to read \"" + input_filename + "\"!");
+        std::cout << PdfUtil::ExtractText(pdf_document) << '\n';
+    } catch (const std::exception &x) {
+        Error("caught exception: " + std::string(x.what()));
+    }
+}
