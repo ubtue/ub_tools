8f1e9ed5f37159fa710e548b8eaa939e5e30c773
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2017-10-05 16:54:39 +0200

    Refactoring and renamed a namespace.

--- cpp/lib/include/BibleUtil.h
+++ cpp/lib/include/BibleUtil.h
@@ -1,8 +1,8 @@
-/** \file   BibleReferenceParser.h
+/** \file   BibleUtil.h
  *  \brief  Declaration of bits related to our bible reference parser.
  *  \author Dr. Johannes Ruscheinski (johannes.ruscheinski@uni-tuebingen.de)
  *
- *  \copyright 2014-2016 Universitätsbiblothek Tübingen.  All rights reserved.
+ *  \copyright 2014-2017 Universitätsbibliothek Tübingen.  All rights reserved.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
@@ -17,8 +17,8 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-#ifndef BIBLE_REFERENCE_PARSER_H
-#define BIBLE_REFERENCE_PARSER_H
+#ifndef BIBLE_UTIL_H
+#define BIBLE_UTIL_H
 
 
 #include <set>
@@ -26,7 +26,7 @@
 #include <utility>
 
 
-namespace BibleReferenceParser {
+namespace BibleUtil {
 
 
 const unsigned BOOK_CODE_LENGTH(2);
@@ -49,7 +49,24 @@ bool ParseBibleReference(std::string bib_ref_candidate, const std::string &book_
 bool CanParseBibleReference(const std::string &bib_ref_candidate);
 
 
-} // namespace BibleReferenceParser
+void SplitIntoBookAndChaptersAndVerses(const std::string &bible_reference_candidate,
+                                       std::string * const book_candidate,
+                                       std::string * const chapters_and_verses_candidate);
 
 
-#endif // ifndef BIBLE_REFERENCE_PARSER_H
+/** \brief Map from noncanonical bible book forms to the canonical ones.
+ *  \return The mapped name or, if no mapping was found, "bible_book_candidate".
+ */
+std::string CanoniseBibleBook(const bool verbose,
+                              const std::string &books_of_the_bible_to_canonical_form_map_filename,
+                              const std::string &bible_book_candidate);
+
+
+std::string MapBibleBookToCode(const bool verbose, const std::string &bible_book_candidate,
+                               const std::string &books_of_the_bible_to_code_map_filename);
+
+
+} // namespace BibleUtil
+
+
+#endif // ifndef BIBLE_UTIL_H
