ad70facb6cd33547486b2be9dafcc35fb5af2322
Mario Trojan <mario.trojan@googlemail.com>
2017-10-05 16:59:26 +0200

    upgraded to php7.1 on ubuntu

--- cpp/data/installer/scripts/install_ubuntu_packages.sh
+++ cpp/data/installer/scripts/install_ubuntu_packages.sh
@@ -4,7 +4,7 @@ add-apt-repository -y ppa:ubuntu-lxc/lxd-stable # Needed to get a recent version
 apt update
 apt-get --yes install curl wget
 apt-get --yes install ant clang golang openjdk-8-jdk
-apt-get --yes install apache2 php7.0 php7.0-dev php7.0-gd php7.0-intl php7.0-json php7.0-ldap php7.0-mcrypt php7.0-mysql php7.0-xsl php-pear libapache2-mod-gnutls libapache2-mod-php7.0
+apt-get --yes install apache2 php7.1 php7.1-dev php7.1-gd php7.1-intl php7.1-json php7.1-ldap php7.1-mcrypt php7.1-mysql php7.1-xsl php-pear libapache2-mod-gnutls libapache2-mod-php7.1
 apt-get --yes install libarchive-dev libboost-all-dev libcurl4-openssl-dev libkyotocabinet-dev liblz4-tool libmagic-dev libmysqlclient-dev libpcre3-dev libssl-dev libtokyocabinet-dev libxml2-dev uuid-dev
 apt-get --yes install composer git make
 #@ruschein: how to do silent mutt (+postfix?) installation?
