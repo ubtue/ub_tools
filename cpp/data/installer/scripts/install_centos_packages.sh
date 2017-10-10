2fae69677f0884dba933788bfd42cafffb9909b6
Mario Trojan <mario.trojan@googlemail.com>
2017-10-10 10:37:53 +0200

    centos packages => php71 via webtatic repository

--- cpp/data/installer/scripts/install_centos_packages.sh
+++ cpp/data/installer/scripts/install_centos_packages.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
+# epel-release needs to be installed first, else packages like kyotocabinet won't be found
 yum -y install curl epel-release wget
+
+# additional repositories (webtatic for php71w, shibboleth for crypto libraries)
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 cd /etc/yum.repos.d/
 wget http://download.opensuse.org/repositories/security:shibboleth/CentOS_7/security:shibboleth.repo
 yum -y update
 
 yum -y install \
     ant clang composer gcc-c++.x86_64 git java-*-openjdk-devel make \
-    httpd mariadb mod_ssl php php-devel php-gd php-intl php-ldap php-mbstring php-mcrypt php-mysql php-xsl \
+    httpd mariadb mod_ssl php71w-cli php71w-common php71w-devel php71w-gd php71w-intl php71w-ldap php71w-mbstring php71w-mcrypt php71w-mysqlnd php71w-xml mod_php71w \
     boost-devel ca-certificates curl-openssl file-devel kyotocabinet-devel leptonica libarchive-devel libcurl-openssl-devel.x86_64 libuuid-devel libwebp libxml2-devel.x86_64 lsof lz4 mariadb-devel.x86_64 mawk openjpeg-libs openssl-devel pcre-devel policycoreutils-python poppler poppler-utils tokyocabinet-devel unzip \
     tesseract tesseract-langpack-bul tesseract-langpack-ces tesseract-langpack-dan tesseract-langpack-deu tesseract-langpack-fin tesseract-langpack-fra tesseract-langpack-grc tesseract-langpack-heb tesseract-langpack-hun tesseract-langpack-ita tesseract-langpack-lat tesseract-langpack-nld tesseract-langpack-nor tesseract-langpack-pol tesseract-langpack-por tesseract-langpack-rus tesseract-langpack-slv tesseract-langpack-spa tesseract-langpack-swe
 
