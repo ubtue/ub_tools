54bb300947bda6cb1d8f4140ef89e7fb37173b29
Mario Trojan <mario.trojan@googlemail.com>
2017-10-10 09:07:22 +0200

    removed golang from installer, also fixed centos script bug

--- cpp/data/installer/scripts/install_centos_packages.sh
+++ cpp/data/installer/scripts/install_centos_packages.sh
@@ -4,9 +4,9 @@ cd /etc/yum.repos.d/
 wget http://download.opensuse.org/repositories/security:shibboleth/CentOS_7/security:shibboleth.repo
 yum update
 
-yum -y install
+yum -y install \
     epel-release \
-    ant clang composer gcc-c++.x86_64 git golang java-*-openjdk-devel make \
+    ant clang composer gcc-c++.x86_64 git java-*-openjdk-devel make \
     httpd mariadb mod_ssl php php-devel php-gd php-intl php-ldap php-mbstring php-mcrypt php-mysql php-xsl \
     boost-devel ca-certificates curl-openssl file-devel kyotocabinet-devel leptonica libarchive-devel libcurl-openssl-devel.x86_64 libuuid-devel libwebp libxml2-devel.x86_64 lsof lz4 mariadb-devel.x86_64 mawk openjpeg-libs openssl-devel pcre-devel policycoreutils-python poppler poppler-utils tokyocabinet-devel unzip \
     tesseract tesseract-langpack-bul tesseract-langpack-ces tesseract-langpack-dan tesseract-langpack-deu tesseract-langpack-fin tesseract-langpack-fra tesseract-langpack-grc tesseract-langpack-heb tesseract-langpack-hun tesseract-langpack-ita tesseract-langpack-lat tesseract-langpack-nld tesseract-langpack-nor tesseract-langpack-pol tesseract-langpack-por tesseract-langpack-rus tesseract-langpack-slv tesseract-langpack-spa tesseract-langpack-swe
