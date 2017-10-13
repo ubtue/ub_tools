8428a9b4dfa34359b16b3f31a4a21a7819859120
Mario Trojan <mario.trojan@googlemail.com>
2017-10-13 14:23:01 +0200

    install_ubtools script to download & build cpp installer & dependencies. - dockerfiles now also use this - ubuntu installation optimized

---
+++ cpp/data/installer/scripts/install_ubtools.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#
+# Install minimum prerequisites (including git)
+# download ub_tools (via git)
+# compile cpp installer
+# run it
+
+# detect OS
+if [ -e /etc/debian_version ]; then
+	# ubuntu
+	echo "Ubuntu detected! installing dependencies..."
+        cd /tmp
+        if [ ! -e ./install_ubuntu_packages.sh ]; then
+            apt-get --yes update
+            apt-get --yes install curl
+            curl https://raw.githubusercontent.com/ubtue/ub_tools/mtrojan/cpp/data/installer/scripts/install_ubuntu_packages.sh -o ./install_ubuntu_packages.sh
+            chmod 700 ./install_ubuntu_packages.sh
+        fi
+	./install_ubuntu_packages.sh
+elif [ -e /etc/redhat-release ]; then
+	# centos
+	echo "CentOS detected! installing dependencies..."
+        cd /tmp
+        if [ ! -e ./install_centos_packages.sh ]; then
+            yum -y update
+            yum -y install curl
+            curl https://raw.githubusercontent.com/ubtue/ub_tools/mtrojan/cpp/data/installer/scripts/install_centos_packages.sh -o ./install_centos_packages.sh
+            chmod 700 ./install_centos_packages.sh
+        fi
+	./install_centos_packages.sh
+else
+	echo "OS type could not be detected! aborting"
+	exit 1
+fi
+
+if [ -d /usr/local/ub_tools ]; then
+	echo "ub_tools already exists, skipping download"
+else
+	echo "cloning ub_tools"
+	git clone -b mtrojan https://github.com/ubtue/ub_tools.git /usr/local/ub_tools
+fi
+
+echo "building prerequisites"
+cd /usr/local/ub_tools/cpp/lib/mkdep && CCC=clang++ make install
+
+echo "building cpp installer"
+cd /usr/local/ub_tools/cpp && CCC=clang++ make installer
+
+echo "starting cpp installer"
+/usr/local/ub_tools/cpp/installer $*
