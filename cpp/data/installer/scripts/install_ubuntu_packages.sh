726fdff7a8caf0c8da3ebb235a2d29a8a7df6249
Mario Trojan <mario.trojan@googlemail.com>
2017-10-13 14:23:01 +0200

    install_ubtools script to download & build cpp installer & dependencies. - dockerfiles now also use this - ubuntu installation optimized

--- cpp/data/installer/scripts/install_ubuntu_packages.sh
+++ cpp/data/installer/scripts/install_ubuntu_packages.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-add-apt-repository -y ppa:ubuntu-lxc/lxd-stable # Needed to get a recent version of Go.
-apt update
+apt-get --yes update
 
-apt-get --yes install \
+# install software-properties-common for apt-add-repository
+apt-get --yes install software-properties-common
+
+# needed for PHP-7.1
+apt-add-repository --yes ppa:ondrej/php
+apt-get --yes update
+
+# main installation
+# (use "quiet" so mysql hopefully doesnt ask for a root password, geographic area and timezone)
+apt-get --quiet --yes --allow-unauthenticated install \
     curl wget \
-    software-properties-common \
     ant clang composer cron gcc git make openjdk-8-jdk sudo \
     apache2 mysql-server php7.1 php7.1-dev php7.1-gd php7.1-intl php7.1-json php7.1-ldap php7.1-mcrypt php7.1-mysql php7.1-xsl php-pear libapache2-mod-gnutls libapache2-mod-php7.1 \
-    ca-certificates libarchive-dev libboost-all-dev libcurl4-openssl-dev libkyotocabinet-dev libleptonica liblz4-tool libmagic-dev libmysqlclient-dev libopenjpeg5 libpcre3-dev libpoppler58 libssl-dev libtokyocabinet-dev libwebp5 libxml2-dev mawk poppler-utils uuid-dev \
+    ca-certificates libarchive-dev libboost-all-dev libcurl4-openssl-dev libkyotocabinet-dev liblept5 liblz4-tool libmagic-dev libmysqlclient-dev libopenjpeg5 libpcre3-dev libpoppler58 libssl-dev libtokyocabinet-dev libwebp5 libxml2-dev mawk poppler-utils uuid-dev \
     tesseract-ocr tesseract-ocr-bul tesseract-ocr-ces tesseract-ocr-dan tesseract-ocr-deu tesseract-ocr-eng tesseract-ocr-fin tesseract-ocr-fra tesseract-ocr-grc tesseract-ocr-heb tesseract-ocr-hun tesseract-ocr-ita tesseract-ocr-lat tesseract-ocr-nld tesseract-ocr-nor tesseract-ocr-pol tesseract-ocr-por tesseract-ocr-rus tesseract-ocr-slv tesseract-ocr-spa tesseract-ocr-swe
 
 a2enmod rewrite
