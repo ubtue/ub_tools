906b9f78dc24c94087c8b0d2b1bb1ae7ab81f7fb
ruschein <ruschein@users.noreply.github.com>
2017-11-05 19:57:34 +0100

    Update remove_unused_authority_records.cc

--- cpp/remove_unused_authority_records.cc
+++ cpp/remove_unused_authority_records.cc
@@ -1,7 +1,7 @@
 /** \brief Utility for removing unreferenced authority records
  *  \author Dr. Johannes Ruscheinski (johannes.ruscheinski@uni-tuebingen.de)
  *
- *  \copyright 2015-2017 Universitätsbibliothek Tübingen.  All rights reserved.
+ *  \copyright 2017 Universitätsbibliothek Tübingen.  All rights reserved.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
