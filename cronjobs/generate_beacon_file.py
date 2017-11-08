c8ae61ea5a33d98aeef6eb31fe5adae25aa188b9
Johannes Ruscheinski <johannes.ruscheinski@uni-tuebingen.de>
2017-11-08 14:26:23 +0100

    Fixed completely messed up argument handling of called programs.

--- cronjobs/generate_beacon_file.py
+++ cronjobs/generate_beacon_file.py
@@ -46,20 +46,19 @@ def Main():
     # Extract the GND numbers from the 035$a subfield of the MARC authority data:
     _035a_contents_filename = "/tmp/035a"
     gnd_numbers_path = "/tmp/gnd_numbers"
-    if len(sys.argv) == 4:
-        util.ExecOrDie("/usr/local/bin/marc_grep",
-                       [ most_recent_authority_filename, "\"035a\"", "no_label" ], _035a_contents_filename)
-    else:
-        util.ExecOrDie(
-            "/usr/local/bin/marc_grep",
-            [ "--control-number-list", sys.argv[4], most_recent_authority_filename, "\"035a\"", "no_label" ],
-            _035a_contents_filename)
+    util.ExecOrDie("/usr/local/bin/marc_grep", [ most_recent_authority_filename, "\"035a\"", "no_label" ],
+                   _035a_contents_filename)
     util.ExecOrDie("/bin/egrep", [ "^\\(DE-588\\)", _035a_contents_filename ], gnd_numbers_path)
 
     # Count GND references in the title data:
     gnd_counts_filename = "/tmp/gnd_counts"
-    util.ExecOrDie("/usr/local/bin/count_gnd_refs",
-                   [ gnd_numbers_path, most_recent_titles_filename, gnd_counts_filename ])
+    if len(sys.argv) == 4:
+        util.ExecOrDie("/usr/local/bin/count_gnd_refs",
+                       [ gnd_numbers_path, most_recent_titles_filename, gnd_counts_filename ])
+    else:
+        util.ExecOrDie("/usr/local/bin/count_gnd_refs",
+                       [ "--control-number-list=" + sys.argv[4], gnd_numbers_path, most_recent_titles_filename,
+                         gnd_counts_filename ])
 
     # Generate a file with a timestamp in the Beacon format:
     timestamp_filename = "/tmp/beacon_timestamp"
