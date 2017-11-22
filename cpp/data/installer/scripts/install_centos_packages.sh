0989cb1af9add0ea87f4a88c9e967b71642ebea7
Mario Trojan <mario.trojan@googlemail.com>
2017-11-22 17:11:34 +0100

    added libxml2-utils as dependency

--- cpp/data/installer/scripts/install_centos_packages.sh
+++ cpp/data/installer/scripts/install_centos_packages.sh
@@ -11,7 +11,7 @@ yum -y update
 yum -y install \
     ant cifs-utils clang crontabs gcc-c++.x86_64 git java-*-openjdk-devel make sudo \
     httpd mariadb mariadb-server mod_ssl \
-    boost-devel ca-certificates curl-openssl file-devel kyotocabinet-devel leptonica libarchive-devel libcurl-openssl-devel libuuid-devel libwebp libxml2-devel.x86_64 lsof lz4 mariadb-devel.x86_64 mawk openjpeg-libs openssl-devel pcre-devel policycoreutils-python poppler poppler-utils tokyocabinet-devel unzip \
+    boost-devel ca-certificates curl-openssl file-devel kyotocabinet-devel leptonica libarchive-devel libcurl-openssl-devel libuuid-devel libwebp libxml2-devel.x86_64 libxml2 lsof lz4 mariadb-devel.x86_64 mawk openjpeg-libs openssl-devel pcre-devel policycoreutils-python poppler poppler-utils tokyocabinet-devel unzip \
     tesseract tesseract-langpack-bul tesseract-langpack-ces tesseract-langpack-dan tesseract-langpack-deu tesseract-langpack-fin tesseract-langpack-fra tesseract-langpack-grc tesseract-langpack-heb tesseract-langpack-hun tesseract-langpack-ita tesseract-langpack-lat tesseract-langpack-nld tesseract-langpack-nor tesseract-langpack-pol tesseract-langpack-por tesseract-langpack-rus tesseract-langpack-slv tesseract-langpack-spa tesseract-langpack-swe
 
 # in centos, there is no "tesseract-langpack-eng", it seems to be part of the default installation
