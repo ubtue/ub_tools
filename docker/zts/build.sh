4d48ebc4c3242f9af68a9af804d46735fb1bf05e
Dr. Johannes Ruscheinski <ruschein@users.noreply.github.com>
2017-12-19 11:05:15 +0100

    Update build.sh

--- docker/zts/build.sh
+++ docker/zts/build.sh
@@ -4,9 +4,7 @@
 # - clone translation-server-repository
 # - overwrite translators repository URL
 # - build docker image
-if [ -e /tmp/zts ]; then
-    rm -r /tmp/zts
-fi
+rm -rf /tmp/zts
 git clone --recursive https://github.com/zotero/translation-server.git /tmp/zts
 sed -i "s,https://github.com/zotero/translators,https://github.com/ubtue/zotero-translators.git," /tmp/zts/Dockerfile
 
