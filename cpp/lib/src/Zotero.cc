47f3c68217a5cd147b33b5221bb231510517d38d
Mario Trojan <mario.trojan@googlemail.com>
2018-02-02 13:52:17 +0100

    zts_client using new Zotero.h

---
+++ cpp/lib/src/Zotero.cc
@@ -0,0 +1,59 @@
+/** \brief Interaction with Zotero Translation Server
+ *         functions are named like endpoints
+ *         see https://github.com/zotero/translation-server
+ *  \author Mario Trojan
+ *
+ *  \copyright 2018 Universitätsbibliothek Tübingen.  All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include "Zotero.h"
+#include <uuid/uuid.h>
+#include "JSON.h"
+#include "StringUtil.h"
+
+
+// We try to be unique for the machine we're on.  Beyond that we may have a problem.
+std::string Zotero::GetNextSessionId() {
+    static unsigned counter;
+    static uint32_t uuid[4];
+    if (unlikely(counter == 0))
+        ::uuid_generate(reinterpret_cast<unsigned char *>(uuid));
+    ++counter;
+    return "ub_tools_zts_client_" + StringUtil::ToString(uuid[0]) + StringUtil::ToString(uuid[1])
+           + StringUtil::ToString(uuid[2]) + StringUtil::ToString(uuid[3]) + "_" + StringUtil::ToString(counter);
+}
+
+
+bool Zotero::Web(const Url &zts_server_url, const TimeLimit &time_limit, Downloader::Params * const downloader_params,
+                 const Url &harvest_url, const std::string &harvested_html,
+                 std::string * const response_body, unsigned * response_code, std::string * const error_message)
+{
+    downloader_params->additional_headers_ = { "Accept: application/json", "Content-Type: application/json" };
+    downloader_params->post_data_ = "{\"url\":\"" + JSON::EscapeString(harvest_url) + "\","
+                                   + "\"sessionid\":\"" + JSON::EscapeString(GetNextSessionId()) + "\"";
+    if (not harvested_html.empty())
+        downloader_params->post_data_ += ",\"cachedHTML\":\"" + JSON::EscapeString(harvested_html) + "\"";
+    downloader_params->post_data_ += "}";
+
+    Downloader downloader(zts_server_url, *downloader_params, time_limit);
+    if (downloader.anErrorOccurred()) {
+        *error_message = downloader.getLastErrorMessage();
+        return false;
+    } else {
+        *response_code = downloader.getResponseCode();
+        *response_body = downloader.getMessageBody();
+        return true;
+    }
+}
