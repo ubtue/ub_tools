eca2aa6cd73ff16e1e009b166ca134f350fc7843
mtrojan <mario.trojan@googlemail.com>
2018-02-07 11:55:45 +0100

    removed <iostream> from Downloader

--- cpp/lib/src/Downloader.cc
+++ cpp/lib/src/Downloader.cc
@@ -24,7 +24,7 @@
  *  along with libiViaCore; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-#include <iostream>
+
 #include "Downloader.h"
 #include "ExecUtil.h"
 #include "FileUtil.h"
