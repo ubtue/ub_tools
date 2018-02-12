4c033e0b2864bcaf59d2039336ca8605b5023f58
Mario Trojan <mario.trojan@googlemail.com>
2018-02-12 13:56:47 +0100

    additional header & write mutexes in downloader

--- cpp/lib/include/Downloader.h
+++ cpp/lib/include/Downloader.h
@@ -45,7 +45,9 @@ class Downloader {
     static unsigned instance_count_;
     static std::mutex *dns_mutex_;
     static std::mutex *cookie_mutex_;
+    static std::mutex *header_mutex_;
     static std::mutex *robots_dot_txt_mutex_;
+    static std::mutex *write_mutex_;
     static std::unordered_map<std::string, RobotsDotTxt> url_to_robots_dot_txt_map_;
 protected:
     bool multi_mode_;
