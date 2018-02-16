5e864e330872c3fd7acf9e4a8c69d4a5ac474355
mtrojan <mario.trojan@googlemail.com>
2018-02-16 09:37:01 +0100

    create_full_text_db: added flush after write

--- cpp/create_full_text_db.cc
+++ cpp/create_full_text_db.cc
@@ -104,7 +104,8 @@ void ProcessNoDownloadRecords(MARC::Reader * const marc_reader, MARC::Writer * c
 
         record_start = marc_reader->tell();
     }
-
+    marc_writer->getFile().flush();
+    
     std::cerr << "Read " << total_record_count << " records.\n";
     std::cerr << "Wrote " << (total_record_count - download_record_offsets_and_urls->size())
               << " records that did not require any downloads.\n";
