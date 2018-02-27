65c82001b34337895920c3b8e13581daef03c97a
Dr. Johannes Ruscheinski <ruschein@users.noreply.github.com>
2018-02-27 09:10:21 +0100

    Update crossref_downloader.cc

--- cpp/crossref_downloader.cc
+++ cpp/crossref_downloader.cc
@@ -18,7 +18,6 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #include <iostream>
-#include <memory>
 #include <stdexcept>
 #include <unordered_set>
 #include <vector>
