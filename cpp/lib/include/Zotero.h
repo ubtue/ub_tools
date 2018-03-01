2f248af95fe4d2c9bdf7c2004ab18f8ff12d82b2
Mario Trojan <mario.trojan@googlemail.com>
2018-03-01 13:33:01 +0100

    moved part of zts_client logic to Zotero, part 1. Also, use supported_urls_regex now!!!

--- cpp/lib/include/Zotero.h
+++ cpp/lib/include/Zotero.h
@@ -24,7 +24,12 @@
 #define ZOTERO_H
 
 
+#include <memory>
 #include "Downloader.h"
+#include "FileUtil.h"
+#include "JSON.h"
+#include "MARC.h"
+#include "RegexMatcher.h"
 #include "TimeLimit.h"
 #include "Url.h"
 
@@ -34,21 +39,178 @@ public:
     // native supported formats, see https://github.com/zotero/translation-server/blob/master/src/server_translation.js#L31-43
     static const std::vector<std::string> ExportFormats;
 
-    /** \brief Use builtin translator to convert JSON to output format. */
-    static bool Export(const Url &zts_server_url, const TimeLimit &time_limit, Downloader::Params downloader_params,
-                       const std::string &format, const std::string &json,
-                       std::string * const response_body, std::string * const error_message);
+    class TranslationServer {
+    public:
+        /** \brief Use builtin translator to convert JSON to output format. */
+        static bool Export(const Url &zts_server_url, const TimeLimit &time_limit, Downloader::Params downloader_params,
+                           const std::string &format, const std::string &json,
+                           std::string * const response_body, std::string * const error_message);
 
-    /** \brief Use builtin translator to convert input format to JSON. */
-    static bool Import(const Url &zts_server_url, const TimeLimit &time_limit, Downloader::Params downloader_params,
-                       const std::string &input_content, std::string * const output_json, std::string * const error_message);
+        /** \brief Use builtin translator to convert input format to JSON. */
+        static bool Import(const Url &zts_server_url, const TimeLimit &time_limit, Downloader::Params downloader_params,
+                           const std::string &input_content, std::string * const output_json, std::string * const error_message);
 
-    /** \brief Download URL and return as JSON. (If harvested_html is given, URL is not downloaded again. ) */
-    static bool Web(const Url &zts_server_url, const TimeLimit &time_limit, Downloader::Params downloader_params,
-                    const Url &harvest_url, const std::string &harvested_html,
-                    std::string * const response_body, unsigned * response_code, std::string * const error_message);
-private:
-    static std::string GetNextSessionId();
+        /** \brief Download URL and return as JSON. (If harvested_html is given, URL is not downloaded again. ) */
+        static bool Web(const Url &zts_server_url, const TimeLimit &time_limit, Downloader::Params downloader_params,
+                        const Url &harvest_url, const std::string &harvested_html,
+                        std::string * const response_body, unsigned * response_code, std::string * const error_message);
+
+    private:
+        static std::string GetNextSessionId();
+    };
+
+    static const std::string DEFAULT_SUBFIELD_CODE;
+
+    // Default timeout values in milliseconds
+    static const unsigned DEFAULT_CONVERSION_TIMEOUT = 60000;
+    static const unsigned DEFAULT_TIMEOUT = 10000;
+    static const unsigned DEFAULT_MIN_URL_PROCESSING_TIME = 200;
+
+    struct HarvestMaps {
+        std::unordered_map<std::string, std::string> ISSN_to_SSG_map_;
+        std::unordered_map<std::string, std::string> ISSN_to_keyword_field_map_;
+        std::unordered_map<std::string, std::string> ISSN_to_language_code_map_;
+        std::unordered_map<std::string, std::string> ISSN_to_licence_map_;
+        std::unordered_map<std::string, std::string> ISSN_to_physical_form_map_;
+        std::unordered_map<std::string, std::string> ISSN_to_superior_ppn_map_;
+        std::unordered_map<std::string, std::string> ISSN_to_volume_map_;
+        std::unordered_map<std::string, std::string> language_to_language_code_map_;
+        std::unordered_set<std::string> previously_downloaded_;
+    };
+
+    // forward declaration
+    class FormatHandler;
+public:
+
+    struct HarvestParams {
+        Url zts_server_url_;
+        TimeLimit min_url_processing_time_ = DEFAULT_MIN_URL_PROCESSING_TIME;
+        unsigned harvested_url_count_ = 0;
+        std::unique_ptr<FormatHandler> format_handler_;
+    };
+
+    class FormatHandler {
+    protected:
+        std::string output_format_;
+        std::string output_file_;
+        std::shared_ptr<HarvestMaps> harvest_maps_;
+        std::shared_ptr<HarvestParams> harvest_params_;
+    public:
+        FormatHandler(const std::string &output_format, const std::string &output_file,
+                      std::shared_ptr<HarvestMaps> harvest_maps,
+                      std::shared_ptr<HarvestParams> harvest_params) :
+                      output_format_(output_format), output_file_(output_file),
+                      harvest_maps_(harvest_maps), harvest_params_(harvest_params) {}
+        virtual ~FormatHandler() {}
+
+        virtual void prepareProcessing() = 0;
+        virtual std::pair<unsigned, unsigned> processRecord(const std::shared_ptr<const JSON::ObjectNode> &object_node) = 0;
+        virtual void finishProcessing() = 0;
+
+        static std::unique_ptr<FormatHandler> Factory(const std::string &output_format,
+                                                      const std::string &output_file,
+                                                      std::shared_ptr<HarvestMaps> harvest_maps,
+                                                      std::shared_ptr<HarvestParams> harvest_params);
+    };
+
+    class JsonFormatHandler : public FormatHandler {
+        unsigned record_count_ = 0;
+        File *output_file_object_;
+    public:
+        using FormatHandler::FormatHandler;
+        void prepareProcessing();
+        std::pair<unsigned, unsigned> processRecord(const std::shared_ptr<const JSON::ObjectNode> &object_node);
+        void finishProcessing();
+    };
+
+
+    class ZoteroFormatHandler : public FormatHandler {
+        unsigned record_count_ = 0;
+        std::string json_buffer_;
+    public:
+        using FormatHandler::FormatHandler;
+        void prepareProcessing();
+        std::pair<unsigned, unsigned> processRecord(const std::shared_ptr<const JSON::ObjectNode> &object_node);
+        void finishProcessing();
+    };
+
+
+    class MarcFormatHandler : public FormatHandler {
+        std::unique_ptr<MARC::Writer> marc_writer_;
+
+
+        inline std::string CreateSubfieldFromStringNode(const std::string &key, const std::shared_ptr<const JSON::JSONNode> node,
+                                                        const std::string &tag, const char subfield_code,
+                                                        MARC::Record * const marc_record, const char indicator1 = ' ',
+                                                        const char indicator2 = ' ')
+        {
+            const std::shared_ptr<const JSON::StringNode> string_node(JSON::JSONNode::CastToStringNodeOrDie(key, node));
+            const std::string value(string_node->getValue());
+            marc_record->insertField(tag, { { subfield_code, value } }, indicator1, indicator2);
+            return value;
+        }
+
+
+        inline std::string CreateSubfieldFromStringNode(const std::pair<std::string, std::shared_ptr<JSON::JSONNode>> &key_and_node,
+                                                        const std::string &tag, const char subfield_code,
+                                                        MARC::Record * const marc_record, const char indicator1 = ' ',
+                                                        const char indicator2 = ' ')
+        {
+            return CreateSubfieldFromStringNode(key_and_node.first, key_and_node.second, tag, subfield_code, marc_record,
+                                                indicator1, indicator2);
+        }
+
+        void ExtractKeywords(std::shared_ptr<const JSON::JSONNode> tags_node, const std::string &issn,
+                             const std::unordered_map<std::string, std::string> &ISSN_to_keyword_field_map,
+                             MARC::Record * const new_record);
+
+        void ExtractVolumeYearIssueAndPages(const JSON::ObjectNode &object_node, MARC::Record * const new_record);
+        void CreateCreatorFields(const std::shared_ptr<const JSON::JSONNode> creators_node, MARC::Record * const marc_record);
+    public:
+        using FormatHandler::FormatHandler;
+        void prepareProcessing();
+        std::pair<unsigned, unsigned> processRecord(const std::shared_ptr<const JSON::ObjectNode> &object_node);
+        void finishProcessing();
+    };
+
+
+    struct Date {
+        static const unsigned INVALID = 0;
+        unsigned day_;
+        unsigned month_;
+        unsigned year_;
+    public:
+        Date(): day_(INVALID), month_(INVALID), year_(INVALID) { }
+    };
+
+    static Date StringToDate(const std::string &date_str);
+    static std::string GetNextControlNumber();
+
+    // If "key" is in "map", then return the mapped value, o/w return "key".
+    static inline std::string OptionalMap(const std::string &key, const std::unordered_map<std::string, std::string> &map);
+
+    // "author" must be in the lastname,firstname format. Returns the empty string if no PPN was found.
+    static std::string DownloadAuthorPPN(const std::string &author);
+
+
+    static void AugmentJsonCreators(const std::shared_ptr<JSON::ArrayNode> creators_array,
+                                    std::vector<std::string> * const comments);
+
+    // Improve JSON result delivered by Zotero Translation Server
+    static void AugmentJson(const std::shared_ptr<JSON::ObjectNode> object_node, const std::shared_ptr<HarvestMaps> harvest_maps);
+
+    static bool ParseLine(const std::string &line, std::string * const key, std::string * const value);
+    static void LoadMapFile(const std::string &filename, std::unordered_map<std::string, std::string> * const from_to_map);
+    static RegexMatcher *LoadSupportedURLsRegex(const std::string &map_directory_path);
+
+public:
+    static std::shared_ptr<HarvestMaps> LoadMapFilesFromDirectory(const std::string &map_directory_path);
+
+    static std::pair<unsigned, unsigned> Harvest(const std::string &harvest_url,
+                                                 const std::string &harvested_html,
+                                                 std::shared_ptr<HarvestParams> harvest_params,
+                                                 std::shared_ptr<HarvestMaps> harvest_maps,
+                                                 bool log = true);
 }; // class Zotero
 
 
