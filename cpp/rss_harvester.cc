b8f2337340c0fe4532247c183d480ebe07b1b6f6
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-03-19 15:12:43 +0100

    First attempt at Zotero harvesting.

--- cpp/rss_harvester.cc
+++ cpp/rss_harvester.cc
@@ -22,35 +22,44 @@
 #include "DbConnection.h"
 #include "DbResultSet.h"
 #include "Downloader.h"
+#include "FileUtil.h"
 #include "IniFile.h"
 #include "SyndicationFormat.h"
 #include "util.h"
+#include "Zotero.h"
 
 
 namespace {
 
 
 [[noreturn]] void Usage() {
-    std::cerr << "usage: " << ::progname << " [--verbose] url1 [url2 ... urlN]\n";
+    std::cerr << "Usage: " << ::progname
+              << " [--verbose] [--proxy=<proxy_host_and_port>] rss_url_list_filename zts_server_url map_directory marc_output\n";
     std::exit(EXIT_FAILURE);
 }
 
 
-void ProcessSyndicationURL(const bool verbose, const std::string &url, DbConnection * const db_connection) {
+unsigned ProcessSyndicationURL(const bool verbose, const std::string &url,
+                               const std::shared_ptr<Zotero::HarvestParams> harvest_params,
+                               const std::shared_ptr<const Zotero::HarvestMaps> harvest_maps,
+                               DbConnection * const db_connection)
+{
+    unsigned successfully_processed_count(0);
+
     if (verbose)
         std::cerr << "Processing URL: " << url << '\n';
 
     Downloader downloader(url);
     if (downloader.anErrorOccurred()) {
         WARNING("Download problem for \"" + url + "\": " + downloader.getLastErrorMessage());
-        return;
+        return successfully_processed_count;
     }
 
     std::string err_msg;
     std::unique_ptr<SyndicationFormat> syndication_format(SyndicationFormat::Factory(downloader.getMessageBody(), &err_msg));
     if (syndication_format == nullptr) {
         WARNING("Problem parsing XML document for \"" + url + "\": " + err_msg);
-        return;
+        return successfully_processed_count;
     }
 
     std::cout << url << " (" << syndication_format->getFormatName() << "):\n";
@@ -72,30 +81,44 @@ void ProcessSyndicationURL(const bool verbose, const std::string &url, DbConnect
             }
             continue;
         }
-        
-        std::cout << "\tItem:\n";
+
         const std::string title(item.getTitle());
-        if (not title.empty())
+        if (not title.empty() and verbose)
             std::cout << "\t\tTitle: " << title << '\n';
-        const std::string description(item.getDescription());
-        if (not description.empty())
-            std::cout << "\t\tDescription: " << description << '\n';
-        const std::string link(item.getLink());
-        if (not link.empty())
-            std::cout << "\t\tLink: " << link << '\n';
-        const std::string id(item.getId());
-        if (not id.empty())
-            std::cout << "\t\tID: " << id << '\n';
-        const time_t publication_date(item.getPubDate());
-        if (publication_date != TimeUtil::BAD_TIME_T)
-            std::cout << "\t\tDate: " << TimeUtil::TimeTToString(publication_date) << '\n';
         const std::unordered_map<std::string, std::string> &dc_and_prism_data(item.getDCAndPrismData());
-        for (const auto &key_and_value : dc_and_prism_data)
-            std::cout << "\t\t" << key_and_value.first << ": " << key_and_value.second << '\n';
+        if (dc_and_prism_data.empty()) {
+            const auto record_count_and_previously_downloaded_count(
+                Zotero::Harvest(item.getLink(), "", harvest_params, harvest_maps));
+            successfully_processed_count += record_count_and_previously_downloaded_count.first;
+        }
 
         db_connection->queryOrDie("INSERT INTO rss SET server_url='" + db_connection->escapeString(url) + "',item_id='"
                                   + db_connection->escapeString(item.getId()) + "'");
+
     }
+
+    return successfully_processed_count;
+}
+
+
+void LoadServerURLs(const std::string &path, std::vector<std::string> * const server_urls) {
+    std::unique_ptr<File> input(FileUtil::OpenInputFileOrDie(path));
+    while (not input->eof()) {
+        std::string line;
+        input->getline(&line);
+        StringUtil::TrimWhite(&line);
+        if (not line.empty())
+            server_urls->emplace_back(line);
+    }
+}
+
+
+std::string GetMarcFormat(const std::string &output_filename) {
+    if (StringUtil::EndsWith(output_filename, ".mrc") or StringUtil::EndsWith(output_filename, ".marc"))
+        return "marc21";
+    if (StringUtil::EndsWith(output_filename, ".xml"))
+        return "marcxml";
+    ERROR("can't determine output format from MARC output filename \"" + output_filename + "\"!");
 }
 
 
@@ -108,7 +131,7 @@ const std::string CONF_FILE_PATH("/usr/local/var/lib/tuelib/rss_client.conf");
 int main(int argc, char *argv[]) {
     ::progname = argv[0];
 
-    if (argc < 2)
+    if (argc < 5)
         Usage();
 
     bool verbose(false);
@@ -117,10 +140,36 @@ int main(int argc, char *argv[]) {
         --argc, ++argv;
     }
 
-    if (argc < 2)
+    std::string proxy_host_and_port;
+    const std::string PROXY_FLAG_PREFIX("--proxy=");
+    if (StringUtil::StartsWith(argv[1], PROXY_FLAG_PREFIX)) {
+        proxy_host_and_port = argv[1] + PROXY_FLAG_PREFIX.length();
+        --argc, ++argv;
+    }
+
+    if (argc != 5)
         Usage();
 
     try {
+        std::vector<std::string> server_urls;
+        LoadServerURLs(argv[1], &server_urls);
+
+        std::shared_ptr<Zotero::HarvestParams> harvest_params(new Zotero::HarvestParams);
+        harvest_params->zts_server_url_ = Url(argv[2]);
+
+        std::string map_directory_path(argv[3]);
+        if (not StringUtil::EndsWith(map_directory_path, '/'))
+            map_directory_path += '/';
+
+        std::shared_ptr<Zotero::HarvestMaps> harvest_maps(Zotero::LoadMapFilesFromDirectory(map_directory_path));
+        const std::shared_ptr<RegexMatcher> supported_urls_regex(Zotero::LoadSupportedURLsRegex(map_directory_path));
+
+        const std::string PREVIOUSLY_DOWNLOADED_HASHES_PATH(map_directory_path + "previously_downloaded.hashes");
+        Zotero::PreviouslyDownloadedHashesManager previously_downloaded_hashes_manager(PREVIOUSLY_DOWNLOADED_HASHES_PATH,
+                                                                                       &harvest_maps->previously_downloaded_);
+        const std::string MARC_OUTPUT_FILE(argv[4]);
+        harvest_params->format_handler_ = Zotero::FormatHandler::Factory(GetMarcFormat(MARC_OUTPUT_FILE), MARC_OUTPUT_FILE,
+                                                                         harvest_maps, harvest_params);
 
         const IniFile ini_file(CONF_FILE_PATH);
         const std::string sql_database(ini_file.getString("Database", "sql_database"));
@@ -128,8 +177,15 @@ int main(int argc, char *argv[]) {
         const std::string sql_password(ini_file.getString("Database", "sql_password"));
         DbConnection db_connection(sql_database, sql_username, sql_password);
 
-        for (int arg_no(1); arg_no < argc; ++arg_no)
-            ProcessSyndicationURL(verbose, argv[arg_no], &db_connection);
+        harvest_params->format_handler_->prepareProcessing();
+
+        unsigned download_count(0);
+        for (const auto &server_url : server_urls)
+            download_count += ProcessSyndicationURL(verbose, server_url, harvest_params, harvest_maps, &db_connection);
+
+        harvest_params->format_handler_->finishProcessing();
+
+        logger->info("Extracted metadata from " + std::to_string(download_count) + " pages.");
     } catch (const std::exception &x) {
         ERROR("caught exception: " + std::string(x.what()));
     }
