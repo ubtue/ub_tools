4d6e24a294a8dc05455e151cb3a601dd1ad123cb
Mario Trojan <mario.trojan@googlemail.com>
2018-03-21 15:10:53 +0100

    ZTS Test now also supports RSS feeds

--- php/public/zts/inc.php
+++ php/public/zts/inc.php
@@ -3,4 +3,5 @@
 // include configuration, all library functions and specific classes
 require_once('inc.conf.php');
 require_once(DIR_LIB . 'inc.func.php');
+require_once(DIR_LIB . 'class/RSS.php');
 require_once(DIR_LIB . 'class/Zotero.php');
