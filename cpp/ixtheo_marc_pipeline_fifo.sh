c8d4071dc085a79457ce0ccae6cfe4555c9d16c6
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-03-28 10:59:02 +0200

    Moved the new --input-format to the correct phase.

--- cpp/ixtheo_marc_pipeline_fifo.sh
+++ cpp/ixtheo_marc_pipeline_fifo.sh
@@ -51,7 +51,6 @@ function CalculateTimeDifference {
 }
 
 
-
 function EndPhase {
     PHASE_DURATION=$(CalculateTimeDifference $START $(date +%s.%N))
     echo "Phase ${PHASE}: Done after ${PHASE_DURATION} minutes." | tee --append "${log}"
@@ -126,14 +125,14 @@ EndPhase || Abort) &
 
 
 StartPhase "Merge Print and Online Superior Records"
-(merge_print_and_online --input-format=marc-21 GesamtTiteldaten-post-phase"$((PHASE-4))"-"${date}".mrc \
+(merge_print_and_online GesamtTiteldaten-post-phase"$((PHASE-4))"-"${date}".mrc \
                         GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc \
                         missing_ppn_partners.list >> "${log}" 2>&1 && \
 EndPhase || Abort) &
 
 
 StartPhase "Normalise URL's"
-(normalise_urls GesamtTiteldaten-post-phase"$((PHASE-1))"-"${date}".mrc \
+(normalise_urls --input-format=marc-21 GesamtTiteldaten-post-phase"$((PHASE-1))"-"${date}".mrc \
                 GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc >> "${log}" 2>&1 && \
 EndPhase || Abort) &
 wait
