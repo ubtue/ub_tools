7f16660c79395b5484d7695268c83ec6132d431f
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-04-05 10:16:50 +0200

    Modified table creation for full Unicode support.

--- cpp/data/rss.sql
+++ cpp/data/rss.sql
@@ -3,7 +3,7 @@ CREATE TABLE rss_feeds (
     feed_url VARCHAR(512) NOT NULL,
     last_build_date DATETIME NOT NULL,
     last_modified DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-);
+) CHARACTER SET utf8mb4;
 CREATE INDEX rss_feeds_ids_index ON rss_feeds_ids_index(id);
 CREATE INDEX rss_feeds_feed_url_index ON rss_feeds(feed_url);
 
@@ -14,7 +14,7 @@ CREATE TABLE rss_items (
     creation_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     UNIQUE KEY feed_url_and_item_id(feed_id,item_id),
     CONSTRAINT feed_id FOREIGN KEY (feed_id) REFERENCES rss_feeds (id) ON DELETE CASCADE
-);
+) CHARACTER SET utf8mb4;
 
 CREATE INDEX rss_items_feed_id_and_item_id_index ON rss_items(feed_id,item_id);
 CREATE INDEX rss_items_creation_datetime_index ON rss_items(creation_datetime);
