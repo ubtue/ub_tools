ee60a255d089bc1262308a8308afd90a25c5f679
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-04-05 14:58:25 +0200

    Added preliminary support for Sqlite3.

--- cpp/lib/src/DbResultSet.cc
+++ cpp/lib/src/DbResultSet.cc
@@ -19,9 +19,10 @@
  */
 #include "DbResultSet.h"
 #include <stdexcept>
+#include "util.h"
 
 
-DbResultSet::DbResultSet(MYSQL_RES * const result_set): result_set_(result_set) {
+DbResultSet::DbResultSet(MYSQL_RES * const result_set): result_set_(result_set), stmt_handle_(nullptr) {
     const MYSQL_FIELD * const fields(::mysql_fetch_fields(result_set_));
     const int COLUMN_COUNT(::mysql_num_fields(result_set));
     for (int col_no(0); col_no < COLUMN_COUNT; ++col_no)
@@ -29,10 +30,24 @@ DbResultSet::DbResultSet(MYSQL_RES * const result_set): result_set_(result_set) 
 }
 
 
+DbResultSet::DbResultSet(sqlite3_stmt * const stmt_handle): result_set_(nullptr), stmt_handle_(stmt_handle) {
+    for (unsigned col_no(0); col_no < getColumnCount(); ++col_no) {
+        const char * const column_name(::sqlite3_column_name(stmt_handle_, col_no));
+        if (column_name == nullptr)
+            ERROR("sqlite3_column_name() failed for index " + std::to_string(col_no) + "!");
+        field_name_to_index_map_.insert(std::pair<std::string, unsigned>(column_name, col_no));
+    }
+    if (::sqlite3_reset(stmt_handle_) != SQLITE_OK)
+        ERROR("sqlite3_reset failed!");
+}
+
+
 DbResultSet::DbResultSet(DbResultSet &&other) {
     if (&other != this) {
         result_set_ = other.result_set_;
         other.result_set_ = nullptr;
+        stmt_handle_ = other.stmt_handle_;
+        other.stmt_handle_ = nullptr;
     }
 }
 
@@ -40,23 +55,44 @@ DbResultSet::DbResultSet(DbResultSet &&other) {
 DbResultSet::~DbResultSet() {
     if (result_set_ != nullptr)
         ::mysql_free_result(result_set_);
+    else if (stmt_handle_ != nullptr and sqlite3_finalize(stmt_handle_) != SQLITE_OK)
+        ERROR("failed to finalise an Sqlite3 statement!");
 }
 
 
 DbRow DbResultSet::getNextRow() {
-    const MYSQL_ROW row(::mysql_fetch_row(result_set_));
-
-    unsigned long *field_sizes;
-    unsigned field_count;
-    if (row == nullptr) {
-        field_sizes = nullptr;
-        field_count = 0;
+    if (stmt_handle_ != nullptr) {
+        switch (::sqlite3_step(stmt_handle_)) {
+        case SQLITE_DONE:
+        case SQLITE_OK:
+            if (::sqlite3_finalize(stmt_handle_) != SQLITE_OK)
+                ERROR("failed to finalise an Sqlite3 statement!");
+            stmt_handle_ = nullptr;
+            field_name_to_index_map_.clear();
+            break;
+        case SQLITE_ROW:
+            break;
+        default:
+            ERROR("an unknown error occurred while calling sqlite3_step()!");
+        }
+        
+        return DbRow(stmt_handle_, field_name_to_index_map_);
     } else {
-        field_sizes = ::mysql_fetch_lengths(result_set_);
-        field_count = ::mysql_num_fields(result_set_);
-    }
+        const MYSQL_ROW row(::mysql_fetch_row(result_set_));
 
-    return DbRow(row, field_sizes, field_count, field_name_to_index_map_);
+        unsigned long *field_sizes;
+        unsigned field_count;
+        if (row == nullptr) {
+            field_sizes = nullptr;
+            field_count = 0;
+            field_name_to_index_map_.clear();
+        } else {
+            field_sizes = ::mysql_fetch_lengths(result_set_);
+            field_count = ::mysql_num_fields(result_set_);
+        }
+
+        return DbRow(row, field_sizes, field_count, field_name_to_index_map_);
+    }
 }
 
 
