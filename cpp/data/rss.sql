40f2d2e5b564ba93b8df3f59086158f7a703e817
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-04-06 09:43:38 +0200

    Fixed a bug and tried to make the statements idempotent.

--- cpp/data/rss.sql
+++ cpp/data/rss.sql
@@ -1,14 +1,14 @@
-CREATE TABLE rss_feeds (
+CREATE TABLE IF NOT EXISTS rss_feeds (
     id INT AUTO_INCREMENT PRIMARY KEY,
     feed_url VARCHAR(512) NOT NULL,
     last_build_date DATETIME NOT NULL,
     last_modified DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) CHARACTER SET utf8mb4;
-CREATE INDEX rss_feeds_ids_index ON rss_feeds_ids_index(id);
-CREATE INDEX rss_feeds_feed_url_index ON rss_feeds(feed_url);
+CREATE INDEX IF NOT EXISTS rss_feeds_ids_index ON rss_feeds(id);
+CREATE INDEX IF NOT EXISTS rss_feeds_feed_url_index ON rss_feeds(feed_url);
 
 
-CREATE TABLE rss_items (
+CREATE TABLE IF NOT EXISTS rss_items (
     feed_id INT NOT NULL,
     item_id VARCHAR(512) NOT NULL,
     creation_datetime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
@@ -16,5 +16,5 @@ CREATE TABLE rss_items (
     CONSTRAINT feed_id FOREIGN KEY (feed_id) REFERENCES rss_feeds (id) ON DELETE CASCADE
 ) CHARACTER SET utf8mb4;
 
-CREATE INDEX rss_items_feed_id_and_item_id_index ON rss_items(feed_id,item_id);
-CREATE INDEX rss_items_creation_datetime_index ON rss_items(creation_datetime);
+CREATE INDEX IF NOT EXISTS rss_items_feed_id_and_item_id_index ON rss_items(feed_id,item_id);
+CREATE INDEX IF NOT EXISTS rss_items_creation_datetime_index ON rss_items(creation_datetime);
