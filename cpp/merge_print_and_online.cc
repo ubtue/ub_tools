6d1296f60a3ebb7f58913807b5fd3a31e14ed94e
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-04-27 11:35:46 +0200

    Added a missing case.

--- cpp/merge_print_and_online.cc
+++ cpp/merge_print_and_online.cc
@@ -199,6 +199,9 @@ MARC::Record MergeRecords(MARC::Record &record1, MARC::Record &record2) {
         if (record1_tags.find(old_and_new_tag.first) != record1_tags.end()
             and record2_tags.find(old_and_new_tag.second) != record2_tags.end())
             tags_to_skip.emplace(old_and_new_tag.first);
+        else if (record1_tags.find(old_and_new_tag.second) != record1_tags.end()
+            and record2_tags.find(old_and_new_tag.first) != record2_tags.end())
+            tags_to_skip.emplace(old_and_new_tag.first);
     }
     
     const auto record1_end_or_lok_start(record1.getFirstField("LOK"));
