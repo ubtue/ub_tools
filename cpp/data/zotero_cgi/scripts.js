59fd72bd6c46bad327352049560b0ab21d04f774
Mario Trojan <mario.trojan@googlemail.com>
2018-05-14 09:50:19 +0200

    code conformity

--- cpp/data/zotero_cgi/scripts.js
+++ cpp/data/zotero_cgi/scripts.js
@@ -1,28 +1,28 @@
-function updateProgress(progress) {
+function UpdateProgress(progress) {
     var div_progress = document.getElementById('progress');
     div_progress.innerHTML = progress;
 }
 
-function updateRuntime(seconds) {
+function UpdateRuntime(seconds) {
     var div_runtime = document.getElementById('runtime');
     div_runtime.innerHTML = seconds + 's';
 }
 
-function updateSelectionDetails() {
+function UpdateSelectionDetails() {
     // trigger change logic,
     // so fields are updated properly to selections
     $('#rss_journal_title').change();
     $('#crawling_journal_title').change();
 }
 
-function onChangeRssJournal() {
+function OnChangeRssJournal() {
     var journal_title = $("#rss_journal_title option:selected").text();
     var journal_options = rss_options_map[journal_title];
     $("#rss_journal_issn").val(journal_options.issn);
     $("#rss_feed_url").val(journal_options.feed_url);
 }
 
-function onChangeCrawlingJournal() {
+function OnChangeCrawlingJournal() {
     var journal_title = $("#crawling_journal_title option:selected").text();
     var journal_options = crawling_options_map[journal_title];
     $("#crawling_journal_issn").val(journal_options.issn);
@@ -31,12 +31,12 @@ function onChangeCrawlingJournal() {
     $("#crawling_depth").val(journal_options.depth);
 }
 
-function tryRss(rss_journal_title) {
+function TryRss(rss_journal_title) {
     $('#home-rss').tab('show');
     $('#rss_journal_title').val(rss_journal_title).change();
 }
 
-function tryCrawling(crawling_journal_title) {
+function TryCrawling(crawling_journal_title) {
     $('#home-crawling').tab('show');
     $('#crawling_journal_title').val(crawling_journal_title).change();
 }
