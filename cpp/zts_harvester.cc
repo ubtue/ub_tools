0859c8996abb732d423da9bc535c6068deb3e3d4
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-05-17 14:08:21 +0200

    We may now be able to process crawl entries but not yet to process RSS entries.

--- cpp/zts_harvester.cc
+++ cpp/zts_harvester.cc
@@ -32,7 +32,7 @@ namespace {
 
 
 [[noreturn]] void Usage() {
-    std::cerr << "Usage: " << ::progname << " [--verbosity=log_level] config_file_path [section1 section2 .. sectionN]\n"
+    std::cerr << "Usage: " << ::progname << " [--verbosity=log_level] [--ignore-robots-dot-txt] config_file_path [section1 section2 .. sectionN]\n"
               << "       Possible log levels are ERROR, WARNING, INFO, and DEBUG with the default being WARNING.\n"
               << "       If any section names have been provided, only those will be processed o/w all sections will be processed.\n\n";
     std::exit(EXIT_FAILURE);
@@ -44,19 +44,28 @@ void ProcessRSS(const IniFile::Section &section) {
     LOG_DEBUG("feed_url: " + feed_url);
 }
 
+
+void InitSiteDescFromIniFileSection(const IniFile::Section &section, SimpleCrawler::SiteDesc * const site_desc) {
+    site_desc->start_url_ = section.getString("base_url");
+    site_desc->max_crawl_depth_ = section.getUnsigned("max_crawl_depth");
+    site_desc->url_regex_matcher_.reset(RegexMatcher::FactoryOrDie(section.getString("extraction_regex")));
+}
+    
     
 void ProcessCrawl(const IniFile::Section &section, const std::string &marc_output_file,
-                      std::shared_ptr<Zotero::HarvestMaps> harvest_maps)
+                  std::shared_ptr<Zotero::HarvestMaps> harvest_maps, const SimpleCrawler::Params &crawler_params,
+                  const std::shared_ptr<RegexMatcher> &supported_urls_regex)
 {
-    const std::string base_url(section.getString("base_url"));
-    std::shared_ptr<RegexMatcher> extraction_regex(RegexMatcher::FactoryOrDie(section.getString("extraction_regex")));
-//    const unsigned max_crawl_depth(section.getUnsigned("max_crawl_depth"));
     const std::string issn(section.getString("issn", ""));
     const std::string optional_strptime_format(section.getString("strptime_format", ""));
 
     std::shared_ptr<Zotero::HarvestParams> harvest_params;
     harvest_params->optional_strptime_format_ = optional_strptime_format;
     harvest_params->format_handler_.reset(new Zotero::MarcFormatHandler(marc_output_file, harvest_maps, harvest_params));
+
+    SimpleCrawler::SiteDesc site_desc;
+    InitSiteDescFromIniFileSection(section, &site_desc);
+    Zotero::HarvestSite(site_desc, crawler_params, supported_urls_regex, harvest_params, harvest_maps);
 }
 
 
@@ -76,6 +85,14 @@ int Main(int argc, char *argv[]) {
     if (argc < 2)
         Usage();
 
+    bool ignore_robots_dot_txt(false);
+    if (std::strcmp(argv[1], "--ignore-robots-dot-txt") == 0) {
+        ignore_robots_dot_txt = true;
+        ++argc, --argv;
+        if (argc < 2)
+            Usage();
+    }
+    
     IniFile ini_file(argv[1]);
 
     std::shared_ptr<Zotero::HarvestParams> harvest_params(new Zotero::HarvestParams);
@@ -94,6 +111,11 @@ int Main(int argc, char *argv[]) {
     const std::string MARC_OUTPUT_FILE(ini_file.getString("", "marc_output_file"));
     harvest_params->format_handler_ = Zotero::FormatHandler::Factory(GetMarcFormat(MARC_OUTPUT_FILE), MARC_OUTPUT_FILE,
                                                                      harvest_maps, harvest_params);
+
+    SimpleCrawler::Params crawler_params;
+    crawler_params.ignore_robots_dot_txt_ = ignore_robots_dot_txt;
+    crawler_params.min_url_processing_time_ = Zotero::DEFAULT_MIN_URL_PROCESSING_TIME;
+    crawler_params.timeout_ = Zotero::DEFAULT_TIMEOUT;
 
     std::unordered_map<std::string, bool> section_name_to_found_flag_map;
     for (int arg_no(2); arg_no < argc; ++arg_no)
@@ -116,7 +138,7 @@ int Main(int argc, char *argv[]) {
         if (type == RSS)
             ProcessRSS(section.second);
         else
-            ProcessCrawl(section.second, MARC_OUTPUT_FILE, harvest_maps);
+            ProcessCrawl(section.second, MARC_OUTPUT_FILE, harvest_maps, crawler_params, supported_urls_regex);
     }
 
     if (section_name_to_found_flag_map.size() > processed_section_count) {
