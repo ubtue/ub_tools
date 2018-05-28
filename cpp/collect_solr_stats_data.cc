051649039095ccfff1f12e7e6254c5eddc1a43e1
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-05-28 14:05:44 +0200

    We now try to shove our collected data direcvtly into Ingo's MySql table.

--- cpp/collect_solr_stats_data.cc
+++ cpp/collect_solr_stats_data.cc
@@ -21,7 +21,11 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <ctime>
+#include "DbConnection.h"
+#include "DnsUtil.h"
 #include "FileUtil.h"
+#include "IniFile.h"
 #include "JSON.h"
 #include "Solr.h"
 #include "TextUtil.h"
@@ -42,8 +46,11 @@ void Usage() {
 
 
 void IssueQueryAndWriteOutput(const std::string &query, const std::string &system_type, const std::string &category,
-                              const std::string &variable, File * const output)
+                              const std::string &variable, DbConnection * const db_connection)
 {
+    static const time_t JOB_START_TIME(std::time(nullptr));
+    static const std::string HOSTNAME(DnsUtil::GetHostname());
+    
     std::string json_result, err_msg;
     if (not Solr::Query(query, /* fields = */"", &json_result, &err_msg, "localhost:8080",
                         /* timeout in seconds = */Solr::DEFAULT_TIMEOUT, Solr::JSON, /* max_no_of_rows = */0))
@@ -54,81 +61,85 @@ void IssueQueryAndWriteOutput(const std::string &query, const std::string &syste
     if (not parser.parse(&tree_root))
         LOG_ERROR("JSON parser failed: " + parser.getErrorMessage());
 
-    *output << TextUtil::CSVEscape(system_type) << ',' << TextUtil::CSVEscape(category) << ','
-            << TextUtil::CSVEscape(variable) << ',' << JSON::LookupInteger("/response/numFound", tree_root) << ','
-            << TextUtil::CSVEscape(TimeUtil::GetCurrentDateAndTime()) << '\n';
+    const time_t NOW(std::time(nullptr));
+    db_connection->queryOrDie("INSERT INTO datenvolumen SET id_lauf=" + std::to_string(JOB_START_TIME) + ", timestamp="
+                              + std::to_string(NOW) + ", Quellrechner='" + HOSTNAME + "', Zielrechner='" + HOSTNAME + "', Systemtyp='"
+                              + system_type + "', Kategorie='" + category + "', Unterkategorie='" + variable + ", value="
+                              + std::to_string(JSON::LookupInteger("/response/numFound", tree_root)));
 }
 
 
-void CollectGeneralStats(const std::string &system_type, File * const output) {
+void CollectGeneralStats(const std::string &system_type, DbConnection * const db_connection) {
     const std::string EXTRA(system_type == "relbib" ? RELBIB_EXTRA : "");
-    IssueQueryAndWriteOutput("*:*" + EXTRA, system_type, "Gesamt", "Gesamttreffer", output);
-    IssueQueryAndWriteOutput("format:Book" + EXTRA, system_type, "Format", "Buch", output);
-    IssueQueryAndWriteOutput("format:Article" + EXTRA, system_type, "Format", "Artikel", output);
-    IssueQueryAndWriteOutput("mediatype:Electronic" + EXTRA, system_type, "Medientyp", "elektronisch", output);
-    IssueQueryAndWriteOutput("mediatype:Non-Electronic" + EXTRA, system_type, "Medientyp", "non-elektronisch", output);
+    IssueQueryAndWriteOutput("*:*" + EXTRA, system_type, "Gesamt", "Gesamttreffer", db_connection);
+    IssueQueryAndWriteOutput("format:Book" + EXTRA, system_type, "Format", "Buch", db_connection);
+    IssueQueryAndWriteOutput("format:Article" + EXTRA, system_type, "Format", "Artikel", db_connection);
+    IssueQueryAndWriteOutput("mediatype:Electronic" + EXTRA, system_type, "Medientyp", "elektronisch", db_connection);
+    IssueQueryAndWriteOutput("mediatype:Non-Electronic" + EXTRA, system_type, "Medientyp", "non-elektronisch", db_connection);
 }
 
 
-void CollectKrimDokSpecificStats(File * const output) {
-    IssueQueryAndWriteOutput("language:German", "krimdok", "Sprache", "Deutsch", output);
-    IssueQueryAndWriteOutput("language:English", "krimdok", "Sprache", "Englisch", output);
+void CollectKrimDokSpecificStats(DbConnection * const db_connection) {
+    IssueQueryAndWriteOutput("language:German", "krimdok", "Sprache", "Deutsch", db_connection);
+    IssueQueryAndWriteOutput("language:English", "krimdok", "Sprache", "Englisch", db_connection);
 }
 
 
-void EmitNotationStats(const char notation_group, const std::string &system_type, const std::string &label, File * const output) {
+void EmitNotationStats(const char notation_group, const std::string &system_type, const std::string &label,
+                       DbConnection * const db_connection)
+{
     const std::string EXTRA(system_type == "relbib" ? RELBIB_EXTRA : "");
     IssueQueryAndWriteOutput("ixtheo_notation:" + std::string(1, notation_group) + "* AND publishDate:[1975 TO 2000]" + EXTRA,
-                             system_type, "IxTheo Notationen", label + "(Alle Medienarten, 1975-2000)", output);
+                             system_type, "IxTheo Notationen", label + "(Alle Medienarten, 1975-2000)", db_connection);
     IssueQueryAndWriteOutput("ixtheo_notation:" + std::string(1, notation_group) + "* AND publishDate:[2001 TO *]" + EXTRA,
-                             system_type, "IxTheo Notationen", label + "(Alle Medienarten, 2001-heute)", output);
+                             system_type, "IxTheo Notationen", label + "(Alle Medienarten, 2001-heute)", db_connection);
     IssueQueryAndWriteOutput("ixtheo_notation:" + std::string(1, notation_group)
                              + "* AND publishDate:[1975 TO 2000] AND format:Book" + EXTRA, system_type, "IxTheo Notationen",
-                             label + "(Bücher, 1975-2000)", output);
+                             label + "(Bücher, 1975-2000)", db_connection);
     IssueQueryAndWriteOutput("ixtheo_notation:" + std::string(1, notation_group) + "* AND publishDate:[2001 TO *] AND format:Book"
-                             + EXTRA, system_type, "IxTheo Notationen", label + "(Bücher, 2001-heute)", output);
+                             + EXTRA, system_type, "IxTheo Notationen", label + "(Bücher, 2001-heute)", db_connection);
     IssueQueryAndWriteOutput("ixtheo_notation:" + std::string(1, notation_group)
                              + "* AND publishDate:[1975 TO 2000] AND format:Article" + EXTRA, system_type, "IxTheo Notationen",
-                             label + "(Bücher, 1975-2000)", output);
+                             label + "(Bücher, 1975-2000)", db_connection);
     IssueQueryAndWriteOutput("ixtheo_notation:" + std::string(1, notation_group)
                              + "* AND publishDate:[2001 TO *] AND format:Article" + EXTRA, system_type, "IxTheo Notationen",
-                             label + "(Aufsätze, 2001-heute)", output);
+                             label + "(Aufsätze, 2001-heute)", db_connection);
 }
 
 
-void CollectIxTheoOrRelBibSpecificStats(const std::string &system_type, File * const output) {
+void CollectIxTheoOrRelBibSpecificStats(const std::string &system_type, DbConnection * const db_connection) {
     const std::string EXTRA(system_type == "relbib" ? RELBIB_EXTRA : "");
-    IssueQueryAndWriteOutput("dewey-raw:*" + EXTRA, system_type, "DDC", "Anzahl der Datensätze", output);
-    IssueQueryAndWriteOutput("rvk:*" + EXTRA, system_type, "RVK", "Anzahl der Datensätze", output);
-    IssueQueryAndWriteOutput("is_open_access:open-access" + EXTRA, system_type, "Open Access", "ja", output);
-    IssueQueryAndWriteOutput("is_open_access:non-open-access" + EXTRA, system_type, "Open Access", "nein", output);
+    IssueQueryAndWriteOutput("dewey-raw:*" + EXTRA, system_type, "DDC", "Anzahl der Datensätze", db_connection);
+    IssueQueryAndWriteOutput("rvk:*" + EXTRA, system_type, "RVK", "Anzahl der Datensätze", db_connection);
+    IssueQueryAndWriteOutput("is_open_access:open-access" + EXTRA, system_type, "Open Access", "ja", db_connection);
+    IssueQueryAndWriteOutput("is_open_access:non-open-access" + EXTRA, system_type, "Open Access", "nein", db_connection);
 
-    IssueQueryAndWriteOutput("language:German" + EXTRA, system_type, "Sprache", "Deutsch", output);
-    IssueQueryAndWriteOutput("language:English" + EXTRA, system_type, "Sprache", "Englisch", output);
-    IssueQueryAndWriteOutput("language:French" + EXTRA, system_type, "Sprache", "Französisch", output);
-    IssueQueryAndWriteOutput("language:Italian" + EXTRA, system_type, "Sprache", "Italienisch", output);
-    IssueQueryAndWriteOutput("language:Latin" + EXTRA, system_type, "Sprache", "Latein", output);
-    IssueQueryAndWriteOutput("language:Spanish" + EXTRA, system_type, "Sprache", "Spanisch", output);
-    IssueQueryAndWriteOutput("language:Dutch" + EXTRA, system_type, "Sprache", "Holländisch", output);
-    IssueQueryAndWriteOutput("language:\"Ancient Greek\"" + EXTRA, system_type, "Sprache", "Altgriechisch", output);
-    IssueQueryAndWriteOutput("language:Hebrew" + EXTRA, system_type, "Sprache", "Hebräisch", output);
-    IssueQueryAndWriteOutput("language:Portugese" + EXTRA, system_type, "Sprache", "Portugiesisch", output);
+    IssueQueryAndWriteOutput("language:German" + EXTRA, system_type, "Sprache", "Deutsch", db_connection);
+    IssueQueryAndWriteOutput("language:English" + EXTRA, system_type, "Sprache", "Englisch", db_connection);
+    IssueQueryAndWriteOutput("language:French" + EXTRA, system_type, "Sprache", "Französisch", db_connection);
+    IssueQueryAndWriteOutput("language:Italian" + EXTRA, system_type, "Sprache", "Italienisch", db_connection);
+    IssueQueryAndWriteOutput("language:Latin" + EXTRA, system_type, "Sprache", "Latein", db_connection);
+    IssueQueryAndWriteOutput("language:Spanish" + EXTRA, system_type, "Sprache", "Spanisch", db_connection);
+    IssueQueryAndWriteOutput("language:Dutch" + EXTRA, system_type, "Sprache", "Holländisch", db_connection);
+    IssueQueryAndWriteOutput("language:\"Ancient Greek\"" + EXTRA, system_type, "Sprache", "Altgriechisch", db_connection);
+    IssueQueryAndWriteOutput("language:Hebrew" + EXTRA, system_type, "Sprache", "Hebräisch", db_connection);
+    IssueQueryAndWriteOutput("language:Portugese" + EXTRA, system_type, "Sprache", "Portugiesisch", db_connection);
 
-    IssueQueryAndWriteOutput("ixtheo_notation:*" + EXTRA, system_type, "IxTheo Notationen", "Mit Notation", output);
-    IssueQueryAndWriteOutput("-ixtheo_notation:*" + EXTRA, system_type, "IxTheo Notationen", "Ohne Notation", output);
-    EmitNotationStats('A', system_type, "Religionswissenschaft allgemein", output);
-    EmitNotationStats('B', system_type, "Einzelne Religionen", output);
-    EmitNotationStats('C', system_type, "Christentum", output);
-    EmitNotationStats('F', system_type, "Christliche Theologie", output);
-    EmitNotationStats('H', system_type, "Bibel; Bibelwissenschaft", output);
-    EmitNotationStats('K', system_type, "Kirchen- und Theologiegeschichte; Konfessionskunde", output);
-    EmitNotationStats('N', system_type, "Systematische Theologie", output);
-    EmitNotationStats('R', system_type, "Praktische Theologie", output);
-    EmitNotationStats('S', system_type, "Kirchenrecht", output);
-    EmitNotationStats('T', system_type, "(Profan-) Geschichte", output);
-    EmitNotationStats('V', system_type, "Philosophie", output);
-    EmitNotationStats('X', system_type, "Recht allgemein", output);
-    EmitNotationStats('Z', system_type, "Sozialwissenschaften", output);
+    IssueQueryAndWriteOutput("ixtheo_notation:*" + EXTRA, system_type, "IxTheo Notationen", "Mit Notation", db_connection);
+    IssueQueryAndWriteOutput("-ixtheo_notation:*" + EXTRA, system_type, "IxTheo Notationen", "Ohne Notation", db_connection);
+    EmitNotationStats('A', system_type, "Religionswissenschaft allgemein", db_connection);
+    EmitNotationStats('B', system_type, "Einzelne Religionen", db_connection);
+    EmitNotationStats('C', system_type, "Christentum", db_connection);
+    EmitNotationStats('F', system_type, "Christliche Theologie", db_connection);
+    EmitNotationStats('H', system_type, "Bibel; Bibelwissenschaft", db_connection);
+    EmitNotationStats('K', system_type, "Kirchen- und Theologiegeschichte; Konfessionskunde", db_connection);
+    EmitNotationStats('N', system_type, "Systematische Theologie", db_connection);
+    EmitNotationStats('R', system_type, "Praktische Theologie", db_connection);
+    EmitNotationStats('S', system_type, "Kirchenrecht", db_connection);
+    EmitNotationStats('T', system_type, "(Profan-) Geschichte", db_connection);
+    EmitNotationStats('V', system_type, "Philosophie", db_connection);
+    EmitNotationStats('X', system_type, "Recht allgemein", db_connection);
+    EmitNotationStats('Z', system_type, "Sozialwissenschaften", db_connection);
 }
 
 
@@ -148,11 +159,14 @@ int main(int argc, char **argv) {
     std::unique_ptr<File> output(FileUtil::OpenOutputFileOrDie(argv[2]));
 
     try {
-        CollectGeneralStats(system_type, output.get());
+        const IniFile ini_file("/usr/local/var/lib/tuelib/collect_solr_stats_data.ini");
+        DbConnection db_connection(ini_file);
+        
+        CollectGeneralStats(system_type, &db_connection);
         if (system_type == "krimdok")
-            CollectKrimDokSpecificStats(output.get());
+            CollectKrimDokSpecificStats(&db_connection);
         else
-            CollectIxTheoOrRelBibSpecificStats(system_type, output.get());
+            CollectIxTheoOrRelBibSpecificStats(system_type, &db_connection);
     } catch (const std::exception &x) {
         LOG_ERROR("caught exception: " + std::string(x.what()));
     }
