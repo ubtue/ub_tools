27b1fe375ef4071f60d2cbc86a8be7c1c7a1ae9f
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-06-07 14:22:59 +0200

    I lied last time.

--- cpp/data/rss_aggregator.sql
+++ cpp/data/rss_aggregator.sql
@@ -1,7 +1,12 @@
+-- The sizes here must be in sync with the constants defined in rss_aggregator.cc!
 CREATE TABLE IF NOT EXISTS rss_aggregator (
+    item_id VARCHAR(100) NOT NULL,
     item_url VARCHAR(512) NOT NULL,
+    title_and_or_description TEXT NOT NULL,
+    serial_name VARCHAR(200) NOT NULL,
     insertion_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    UNIQUE (item_url)
+    UNIQUE (item_id)
 ) CHARACTER SET utf8mb4;
+CREATE INDEX IF NOT EXISTS item_id_index ON rss_aggregator(item_id);
 CREATE INDEX IF NOT EXISTS item_url_index ON rss_aggregator(item_url);
 CREATE INDEX IF NOT EXISTS insertion_time_index ON rss_aggregator(insertion_time);
