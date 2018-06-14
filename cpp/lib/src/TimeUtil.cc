4dc4260de9cafb6230259f26b87beed5ecdb5eaf
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-06-14 14:53:12 +0200

    Fixed a format string.

--- cpp/lib/src/TimeUtil.cc
+++ cpp/lib/src/TimeUtil.cc
@@ -194,7 +194,7 @@ unsigned StringToBrokenDownTime(const std::string &possible_date, unsigned * con
     }
     // Check for ISO 8601 w/ offset:
     else if (possible_date.length() == 25
-             and std::sscanf(possible_date.c_str(), "%4u-%2u-%2uT%2u:%2u:%2u%[+-]%d:%d",
+             and std::sscanf(possible_date.c_str(), "%4u-%2u-%2uT%2u:%2u:%2u%[+-]%2d:%2d",
                              year, month, day, hour, minute, second, plus_or_minus, hour_offset, minute_offset) == 9)
     {
         if (plus_or_minus[0] == '-') {
