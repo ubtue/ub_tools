d50076319145b4b9c6dc77e7e3fa3f2c8f9f28ce
Mario Trojan <mario.trojan@googlemail.com>
2018-06-18 13:56:07 +0200

    use our own zotero-translation-server fork for zts docker image

--- docker/zts/build.sh
+++ docker/zts/build.sh
@@ -5,7 +5,7 @@
 # - overwrite translators repository URL
 # - build docker image
 rm -rf /tmp/zts
-git clone --recursive https://github.com/zotero/translation-server.git /tmp/zts
+git clone --recursive https://github.com/ubtue/zotero-translation-server.git /tmp/zts
 sed --in-place "s,https://github.com/zotero/translators,https://github.com/ubtue/zotero-translators.git," /tmp/zts/Dockerfile
 
 docker build --tag=zts /tmp/zts
