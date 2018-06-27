21b01ed3563a769174f12afdd0de1d981ba37e9e
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2018-06-27 12:09:22 +0200

    Missing quotes

--- cpp/krim_ssoar.sh
+++ cpp/krim_ssoar.sh
@@ -13,7 +13,7 @@ fi
 
 
 echo "Add various selection identifiers"
-augmented_file=krim_ssoar-$(date +%Y%m%d).xml
+augmented_file="krim_ssoar-$(date +%Y%m%d).xml"
 marc_augmentor krim_ssoar.xml "$augmented_file" \
     --insert-field '084:  \x1FaKRIM\x1FqDE-21\x1F2fid' \
     --insert-field '852a:DE-21' \
