a79aca4729d63f019e52231b42dc1614b19e4dc4
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2018-06-27 11:07:23 +0200

    Use temp file when uploading to BSZ FTP server to prevent race conditions

--- cpp/krim_ssoar.sh
+++ cpp/krim_ssoar.sh
@@ -14,6 +14,7 @@ fi
 
 echo "Add various selection identifiers"
 augmented_file=krim_ssoar-$(date +%Y%m%d).xml
+augmented_file_tmp=krim_ssoar-$(date +%Y%m%d).tmp
 marc_augmentor krim_ssoar.xml "$augmented_file" \
     --insert-field '084:  \x1FaKRIM\x1FqDE-21\x1F2fid' \
     --insert-field '852a:DE-21' \
@@ -29,7 +30,8 @@ ftp -invp "${host}" <<EOF
 user ${username} ${password}
 cd /pub/UBTuebingen_Import_Test/krimdok_Test
 bin
-put ${augmented_file}
+put ${augmented_file} ${augmented_file_tmp}
+rename ${augmented_file_tmp} ${augmented_file}
 bye
 EOF
 
