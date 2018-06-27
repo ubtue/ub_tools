b535ef0bd6dba9c598e700292e8f9700d5c38e4c
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-06-27 12:28:21 +0200

    Added support for the sigil.

--- cpp/zts_harvester.cc
+++ cpp/zts_harvester.cc
@@ -151,12 +151,14 @@ std::string GetMarcFormat(const std::string &output_filename) {
 
 struct GroupInfo {
     std::string user_agent_;
+    std::string isil_;
 };
 
 
 void LoadGroup(const IniFile::Section &section, std::map<std::string, GroupInfo> * const group_name_to_info_map) {
     GroupInfo new_group_info;
     new_group_info.user_agent_ = section.getString("user_agent");
+    new_group_info.isil_       = section.getString("isil");
     group_name_to_info_map->emplace(section.getSectionName(), new_group_info);
 }
 
@@ -270,8 +272,8 @@ int Main(int argc, char *argv[]) {
             continue;
 
         const std::string group_name(section.getString("group"));
-        const auto group_info(group_name_to_info_map.find(group_name));
-        if (group_info == group_name_to_info_map.cend())
+        const auto group_name_and_info(group_name_to_info_map.find(group_name));
+        if (group_name_and_info == group_name_to_info_map.cend())
             LOG_ERROR("unknown or undefined group \"" + group_name + "\" in section \"" + section.getSectionName() + "\"!");
 
         std::vector<MARC::EditInstruction> edit_instructions;
@@ -280,8 +282,9 @@ int Main(int argc, char *argv[]) {
         Zotero::GobalAugmentParams global_augment_params(&augment_maps);
 
         Zotero::SiteAugmentParams site_augment_params;
-        site_augment_params.global_params_ = &global_augment_params;
+        site_augment_params.global_params_          = &global_augment_params;
         site_augment_params.marc_edit_instructions_ = edit_instructions;
+        site_augment_params.isil_                   = group_name_and_info->second.isil_;
         ReadGenericSiteAugmentParams(section, &site_augment_params);
 
         harvest_params->format_handler_->setAugmentParams(&site_augment_params);
@@ -293,7 +296,7 @@ int Main(int argc, char *argv[]) {
             section_name_and_found_flag->second = true;
         }
 
-        harvest_params->user_agent_ = group_info->second.user_agent_;
+        harvest_params->user_agent_ = group_name_and_info->second.user_agent_;
 
         LOG_INFO("Processing section \"" + section.getSectionName() + "\".");
         ++processed_section_count;
