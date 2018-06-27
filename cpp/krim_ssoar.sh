e738f5752ab7aaf867e876c814bb08c46468ab1a
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2018-06-27 11:16:24 +0200

    Forgot the tmp extension

--- cpp/krim_ssoar.sh
+++ cpp/krim_ssoar.sh
@@ -14,7 +14,7 @@ fi
 
 echo "Add various selection identifiers"
 augmented_file=krim_ssoar-$(date +%Y%m%d).xml
-augmented_file_tmp=${augmented_file%.*}
+augmented_file_tmp=${augmented_file%.*}.tmp
 marc_augmentor krim_ssoar.xml "$augmented_file" \
     --insert-field '084:  \x1FaKRIM\x1FqDE-21\x1F2fid' \
     --insert-field '852a:DE-21' \
