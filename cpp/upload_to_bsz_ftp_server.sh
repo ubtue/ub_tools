9809b2aace0230dbac78488d902c9ed788781c04
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2018-06-27 12:07:35 +0200

    Double quote paths to allow whitespaces Anneal style

--- cpp/upload_to_bsz_ftp_server.sh
+++ cpp/upload_to_bsz_ftp_server.sh
@@ -3,15 +3,15 @@
 
 set -e
 
-if [ ${#} -ne 2 ]; then
+if [ $# -ne 2 ]; then
     echo "Usage: $0 local_file remote_folder_path"
     exit 1
 fi
 
-input_path=${1}
-filename=$(basename ${input_path})
-filename_tmp=${filename%.*}.tmp
-remote_path=${2}
+input_path=$1
+filename=$(basename "$input_path")
+filename_tmp="${filename%.*}.tmp"
+remote_path=$2
 
 conf_path=/usr/local/var/lib/tuelib/cronjobs/fetch_marc_updates.conf
 host=$(inifile_lookup --suppress-newline "${conf_path}" FTP host)
