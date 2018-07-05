55eb851c14e2265e06c65a6957a376826fb62eb6
jriedl <johannes.riedl@uni-tuebingen.de>
2018-07-05 09:19:22 +0200

    Fixed logical flaw

--- cpp/transfer_fulltext.sh
+++ cpp/transfer_fulltext.sh
@@ -21,22 +21,31 @@ shift
 date_dir=$(date +%y%m%d)
 dirs_to_transfer=$@
 
-#Use a trick to see whether the date directory already exists (https://superuser.com/questions/850158/how-to-check-if-file-exists-in-remote-windows-from-local-linux-script) (180704)
+#Use a trick to see whether the date directory already exists
+#(https://superuser.com/questions/850158/how-to-check-if-file-exists-in-remote-windows-from-local-linux-script) (180704)
 #Create the directory if it doesn't
-
 function CreateDirIfNotExists {
-    dir_to_test=$1
-    echo "df ${dir_to_test}" | sftp -oIdentityFile=${keyfile_path} ${username}@${server}
-    if [ $? -eq 0 ]; then
-        sftp -oIdentityFile=${keyfile_path} ${username}@${server} <<-EOF
-		cd ${remote_top_dir}
-		mkdir ${date_dir}
-		EOF
+    remote_top_dir=$1
+    dir_to_test=$2
+    # Needed since otherwise we would be aborted because of our test
+    set +o errexit
+    echo "df ${dir_to_test}" | sftp -b - -oIdentityFile=${keyfile_path} ${username}@${server}:${remote_top_dir}
+    if [ $? -ne 0 ]; then
+        sftp -oIdentityFile=${keyfile_path} ${username}@${server}:${remote_top_dir} <<-EOF
+	mkdir ${dir_to_test}
+	EOF
+    echo "Created directory ${dir_to_test}"
+    else
+         echo "Directory ${dir_to_test} already present"
     fi
+    set -o errexit
 }
 
+
 function TransferDirectory {
-    dir_to_transfer=$1
+    local_top_dir=$1
+    remote_top_dir=$2
+    dir_to_transfer=$3
     sftp -oIdentityFile=${keyfile_path} ${username}@${server} <<-EOF
 	lcd ${local_top_dir}
 	cd ${remote_top_dir}
@@ -47,11 +56,10 @@ function TransferDirectory {
 
 
 # Create the top level directories
-CreateDirIfNotExists "${remote_top_dir}/${date_dir}"
+CreateDirIfNotExists "${remote_top_dir}" "${date_dir}"
 
 # Upload all the directories that contain 7z files
 for dir_to_transfer in $dirs_to_transfer
 do
-    TransferDirectory ${dir_to_transfer}
+    TransferDirectory ${local_top_dir} ${remote_top_dir} ${dir_to_transfer}
 done
-
