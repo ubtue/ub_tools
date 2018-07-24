65027aa18a911ab2f2fe4c8e2b6533613bc2732e
jriedl <johannes.riedl@uni-tuebingen.de>
2018-07-24 11:10:07 +0200

    Basic support for keywords

--- cpp/convert_dakar.cc
+++ cpp/convert_dakar.cc
@@ -53,37 +53,76 @@ DbResultSet ExecSqlAndReturnResultsOrDie(const std::string &select_statement, Db
 
 
 void GetAuthorsFromDB(DbConnection &db_connection, std::set<std::string> * const authors) {
-    std::string distinct_author_query("SELECT DISTINCT autor from ikr");
+    std::string distinct_author_query("SELECT DISTINCT autor FROM ikr");
     DbResultSet result_set(ExecSqlAndReturnResultsOrDie(distinct_author_query, &db_connection));
     while (const DbRow db_row = result_set.getNextRow()) {
-          const std::string author_row(db_row["autor"]);
-          std::vector<std::string> authors_in_row;
-          StringUtil::Split(author_row, ';', &authors_in_row);
-          for (const auto &author : authors_in_row) {
-              authors->emplace(StringUtil::TrimWhite(author));
-          }
-
-    }
-}
-
-
-void ExtractAuthorsFromAuthorityData(const std::string &authority_file,
-                                     std::unordered_multimap<std::string,std::string> * const author_to_gnd_map) {
-    auto marc_reader(MARC::Reader::Factory(authority_file));
-    auto authority_reader(marc_reader.get());
-    while (const auto &record = authority_reader->read()) {
-        std::vector<std::string> authors(record.getSubfieldValues("100", "a"));
-        std::string gnd_number;
-        if (MARC::GetGNDCode(record, &gnd_number)) {
-               author_to_gnd_map->emplace(StringUtil::Join(authors, " "), gnd_number);
+        const std::string author_row(db_row["autor"]);
+        std::vector<std::string> authors_in_row;
+        StringUtil::Split(author_row, ';', &authors_in_row);
+        for (const auto &author : authors_in_row) {
+            authors->emplace(StringUtil::TrimWhite(author));
         }
     }
 }
 
-void GetAuthorGNDResultMap(DbConnection &db_connection, const std::string &authority_file,
-                           std::unordered_multimap<std::string,std::string> * const author_to_gnds_result_map) {
-     std::unordered_multimap<std::string,std::string> all_authors_to_gnd_map;
-     ExtractAuthorsFromAuthorityData(authority_file, &all_authors_to_gnd_map);
+
+void GetKeywordsFromDB(DbConnection &db_connection, std::set<std::string> * const keywords) {
+    std::string distinct_keyword_query("SELECT DISTINCT stichwort FROM ikr");
+    DbResultSet result_set(ExecSqlAndReturnResultsOrDie(distinct_keyword_query, &db_connection));
+    while (const DbRow db_row = result_set.getNextRow()) {
+        const std::string keyword_row(db_row["stichwort"]);
+        std::vector<std::string> keywords_in_row;
+        StringUtil::Split(keyword_row, ';', &keywords_in_row);
+        for (const auto &keyword : keywords_in_row) {
+            keywords->emplace(StringUtil::TrimWhite(keyword));
+        }
+    }
+}
+
+
+void ExtractAuthorityData(const std::string &authority_file,
+                          std::unordered_multimap<std::string,std::string> * const author_to_gnd_map,
+                          std::unordered_multimap<std::string,std::string> * const keyword_to_gnd_map,
+                          std::unordered_multimap<std::string,std::string> * const cic_to_gnd_map)
+{
+(void)cic_to_gnd_map;
+    auto marc_reader(MARC::Reader::Factory(authority_file));
+    auto authority_reader(marc_reader.get());
+    while (const auto &record = authority_reader->read()) {
+        std::string gnd_number;
+
+        if (not MARC::GetGNDCode(record, &gnd_number))
+            continue;
+
+        // Authors
+        const std::string author(StringUtil::Join(record.getSubfieldValues("100", "a"), " "));
+        if (not author.empty()) {
+            author_to_gnd_map->emplace(author, gnd_number);
+        }
+
+        // Keywords
+        const std::string keyword_110(StringUtil::Join(record.getSubfieldValues("110", "abcdnpt"), " "));
+        const std::string keyword_111(StringUtil::Join(record.getSubfieldValues("111", "abcdnpt"), " "));
+        const std::string keyword_130(StringUtil::Join(record.getSubfieldValues("130", "abcdnpt"), " "));
+        const std::string keyword_150(StringUtil::Join(record.getSubfieldValues("150", "abcdnpt"), " "));
+        if (not keyword_110.empty())
+           keyword_to_gnd_map->emplace(keyword_110, gnd_number);
+        if (not keyword_111.empty())
+           keyword_to_gnd_map->emplace(keyword_111, gnd_number);
+        if (not keyword_130.empty())
+           keyword_to_gnd_map->emplace(keyword_130, gnd_number);
+        if (not keyword_150.empty())
+           keyword_to_gnd_map->emplace(keyword_150, gnd_number);
+
+        // CIC
+        //...
+    }
+}
+
+void GetAuthorGNDResultMap(DbConnection &db_connection,
+                           const std::unordered_multimap<std::string, std::string> &all_authors_to_gnd_map,
+                           std::unordered_multimap<std::string,std::string> * const author_to_gnds_result_map)
+{
      std::set<std::string> authors;
      GetAuthorsFromDB(db_connection, &authors);
      for (auto &author: authors) {
@@ -92,6 +131,21 @@ void GetAuthorGNDResultMap(DbConnection &db_connection, const std::string &autho
               author_to_gnds_result_map->emplace(author, StringUtil::TrimWhite(gnd->second));
           }
      }
+}
+
+
+void GetKeywordGNDResultMap(DbConnection &db_connection,
+                     const std::unordered_multimap<std::string, std::string> &all_keywords_to_gnd_map,
+                     std::unordered_multimap<std::string,std::string> * keyword_to_gnds_result_map)
+{
+    std::set<std::string> keywords;
+    GetKeywordsFromDB(db_connection, &keywords);
+    for (auto &keyword: keywords) {
+        auto keyword_and_gnds(all_keywords_to_gnd_map.equal_range(keyword));
+        for (auto gnd(keyword_and_gnds.first); gnd != keyword_and_gnds.second; ++gnd) {
+            keyword_to_gnds_result_map->emplace(keyword, StringUtil::TrimWhite(gnd->second));
+        }
+    }
 }
 
 
@@ -110,14 +164,23 @@ int Main(int argc, char **argv) {
      const std::string sql_password(ini_file.getString("Database", "sql_password"));
      DbConnection db_connection(sql_database, sql_username, sql_password);
 
-     std::unordered_multimap<std::string,std::string> author_to_gnds_result_map;
-     GetAuthorGNDResultMap(db_connection, authority_file, &author_to_gnds_result_map);
 
+     std::unordered_multimap<std::string, std::string> all_authors_to_gnd_map;
+     std::unordered_multimap<std::string, std::string> all_keywords_to_gnds_map;
+     std::unordered_multimap<std::string, std::string> all_cic_to_gnds_map;
+     ExtractAuthorityData(authority_file, &all_authors_to_gnd_map, &all_keywords_to_gnds_map, &all_cic_to_gnds_map);
+
+     std::unordered_multimap<std::string,std::string> author_to_gnds_result_map;
+     GetAuthorGNDResultMap(db_connection, all_authors_to_gnd_map, &author_to_gnds_result_map);
      for (const auto &author_and_gnds : author_to_gnds_result_map)
-         std::cerr << author_and_gnds.first << "|||| " << author_and_gnds.second << '\n';
+         std::cerr << author_and_gnds.first << "||||" << author_and_gnds.second << '\n';
      std::cerr << "\n\n";
-     //ExtractKeywordsFromAuthorityData(authority_file);
-     //ExtractCICNotaitonFromAuthorityData(authority_file);
+
+     std::unordered_multimap<std::string,std::string> keyword_to_gnds_result_map;
+     GetKeywordGNDResultMap(db_connection, all_keywords_to_gnds_map, &keyword_to_gnds_result_map);
+     for (const auto &keyword_and_gnds : keyword_to_gnds_result_map)
+         std::cerr << keyword_and_gnds.first << "++++" << keyword_and_gnds.second << '\n';
+     std::cerr << "\n\n";
 
      return EXIT_SUCCESS;
 
