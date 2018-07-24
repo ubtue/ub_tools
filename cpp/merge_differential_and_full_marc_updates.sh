9a4466f22b016c001191a0e18b87127e3c9b6e3f
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-07-24 16:20:40 +0200

    Work in progress.

--- cpp/merge_differential_and_full_marc_updates.sh
+++ cpp/merge_differential_and_full_marc_updates.sh
@@ -1,6 +1,42 @@
 #!/bin/bash
 set -o errexit -o nounset
 
+
+function Usage() {
+    echo "Usage: $0 [--keep-intermediate-files]"
+    exit 1
+}
+
+
+# Argument processing
+keep_itermediate_files=1
+if [[ $# == 1 ]]; then
+    if [[ $1 == "--keep-intermediate-files" ]]; then
+        keep_itermediate_files=0
+    else
+        Usage
+    fi
+elif [[ $# != 0 ]]; then
+    Usage
+fi
+
+
+function KeepIntermediateFiles() {
+    if [[ keep_itermediate_files ]]; then
+        echo "--keep-intermediate-files"
+    else
+        echo ""
+    fi
+}
+
+
+declare -i counter=0
+function GetNextTempFilename() {
+    ((++counter))
+    echo temp_file.{$$}.$counter
+}
+
+
 if [[ -z $(printenv TUEFIND_FLAVOUR) ]]; then
     echo "You need to set the environment variable TUEFIND_FLAVOUR in order to run this script!"
     exit 1
@@ -16,14 +52,22 @@ echo "Creating ${target_filename}"
 
 
 input_file=$(generate_merge_order | head --lines=1)
-for f in $(generate_merge_order | tail --lines=+2); do
-    if [[ ${f:0:6} == "LOEPPN" ]]; then
+for update in $(generate_merge_order | tail --lines=+2); do
+    $output_file=$(GetNextTempFilename)
+    if [[ ${update:0:6} == "LOEPPN" ]]; then
         echo "Processing deletion list: $f"
+        echo "archive_delete_ids $(KeepIntermediateFiles) $input_file $update $output_file"
     else
         echo "Processing differential dump: $f"
-        echo "apply_differential_update $last_temp_file $f $output_file"
+        echo "apply_differential_update $(KeepIntermediateFiles) $input_file $update $output_file"
     fi
-    last_temp_file=$f
+    input_file=$output_file
 done
 
+
 echo mv $output_file $target_filename
+
+
+if [[ ! keep_itermediate_files ]]; then
+    rm temp_file.{$$}.*
+fi
