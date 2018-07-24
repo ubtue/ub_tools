2dc22a17e9aa482ce8d2cecf5a060b81c5135fa7
jriedl <johannes.riedl@uni-tuebingen.de>
2018-07-24 09:19:49 +0200

    Some cleanup

--- cpp/convert_dakar.cc
+++ cpp/convert_dakar.cc
@@ -38,7 +38,7 @@
 
 const std::string CONF_FILE_PATH("/usr/local/var/lib/tuelib/dakar.conf");
 
-namespace { 
+namespace {
 
 [[noreturn]] void Usage() {
     std::cerr << "Usage: " << ::progname << " authority_data output_file" << '\n';
@@ -60,7 +60,6 @@ void GetAuthorsFromDB(DbConnection &db_connection, std::set<std::string> * const
           std::vector<std::string> authors_in_row;
           StringUtil::Split(author_row, ';', &authors_in_row);
           for (const auto &author : authors_in_row) {
-std::cerr << "author: " << StringUtil::TrimWhite(author) <<  '\n';
               authors->emplace(StringUtil::TrimWhite(author));
           }
 
@@ -68,22 +67,40 @@ std::cerr << "author: " << StringUtil::TrimWhite(author) <<  '\n';
 }
 
 
-void ExtractAuthorsFromAuthorityData(const std::string &authority_file, 
-                                     std::unordered_multimap<std::string,std::string> * const author_to_ppn_map) {
-     auto marc_reader(MARC::Reader::Factory(authority_file));
-     auto authority_reader(marc_reader.get());
-     while (const auto &record = authority_reader->read()) {
-        std::vector<std::string> authors(record.getSubfieldValues("100", "a")); 
-        author_to_ppn_map->emplace(StringUtil::Join(authors, " "), record.getControlNumber());
+void ExtractAuthorsFromAuthorityData(const std::string &authority_file,
+                                     std::unordered_multimap<std::string,std::string> * const author_to_gnd_map) {
+    auto marc_reader(MARC::Reader::Factory(authority_file));
+    auto authority_reader(marc_reader.get());
+    while (const auto &record = authority_reader->read()) {
+        std::vector<std::string> authors(record.getSubfieldValues("100", "a"));
+        std::string gnd_number;
+        if (MARC::GetGNDCode(record, &gnd_number)) {
+               author_to_gnd_map->emplace(StringUtil::Join(authors, " "), gnd_number);
+        }
+    }
+}
+
+void GetAuthorGNDResultMap(DbConnection &db_connection, const std::string &authority_file,
+                           std::unordered_multimap<std::string,std::string> * const author_to_gnds_result_map) {
+     std::unordered_multimap<std::string,std::string> all_authors_to_gnd_map;
+     ExtractAuthorsFromAuthorityData(authority_file, &all_authors_to_gnd_map);
+     std::set<std::string> authors;
+     GetAuthorsFromDB(db_connection, &authors);
+     for (auto &author: authors) {
+          auto author_and_gnds(all_authors_to_gnd_map.equal_range(author));
+          for (auto gnd(author_and_gnds.first); gnd != author_and_gnds.second; ++gnd) {
+              author_to_gnds_result_map->emplace(author, StringUtil::TrimWhite(gnd->second));
+          }
      }
 }
+
 
 } //unnamed namespace
 
 int Main(int argc, char **argv) {
      if (argc != 3)
          Usage();
-    
+
      const std::string authority_file(argv[1]);
      const std::string output_file(argv[2]);
 
@@ -93,20 +110,15 @@ int Main(int argc, char **argv) {
      const std::string sql_password(ini_file.getString("Database", "sql_password"));
      DbConnection db_connection(sql_database, sql_username, sql_password);
 
-     std::unordered_multimap<std::string,std::string> author_to_ppn_map;
-     ExtractAuthorsFromAuthorityData(authority_file, &author_to_ppn_map);
-     std::set<std::string> authors;
-     GetAuthorsFromDB(db_connection, &authors);
-     for (auto &author: authors) {
-          std::cerr << author; 
-          auto ppns(author_to_ppn_map.equal_range(author));
-          for (auto ppn(ppns.first); ppn != ppns.second; ++ppn)
-              std::cerr << "||||PPN: " << ppn->second << '\n';
-          std::cerr << "\n\n";
-     }
+     std::unordered_multimap<std::string,std::string> author_to_gnds_result_map;
+     GetAuthorGNDResultMap(db_connection, authority_file, &author_to_gnds_result_map);
+
+     for (const auto &author_and_gnds : author_to_gnds_result_map)
+         std::cerr << author_and_gnds.first << "|||| " << author_and_gnds.second << '\n';
+     std::cerr << "\n\n";
      //ExtractKeywordsFromAuthorityData(authority_file);
      //ExtractCICNotaitonFromAuthorityData(authority_file);
- 
+
      return EXIT_SUCCESS;
 
 }
