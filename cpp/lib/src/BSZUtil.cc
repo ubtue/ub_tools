245f526b564b46a665cebf0c670837f7053fe8b3
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-07-25 12:03:18 +0200

    Tried to deal w/ seekor entries.

--- cpp/lib/src/BSZUtil.cc
+++ cpp/lib/src/BSZUtil.cc
@@ -117,6 +117,10 @@ std::string ExtractDateFromFilenameOrDie(const std::string &filename) {
 
 // Hopefully returns 'a', 'b' or 'c'.
 char GetTypeCharOrDie(const std::string &member_name) {
+    if (member_name == "sekkor-aut.mrc")
+        return 'a';
+    if (member_name == "sekkor-tit.mrc")
+        return 'c';
     static auto matcher(RegexMatcher::RegexMatcherFactoryOrDie("([abc])\\d\\d\\d.raw"));
     if (not matcher->matched(member_name))
         LOG_ERROR("bad member type for archive member \"" + member_name + "\"!");
@@ -145,6 +149,9 @@ void ExtractArchiveMembers(const std::string &archive_name, std::vector<std::str
     ArchiveReader reader(archive_name);
     ArchiveReader::EntryInfo file_info;
     while (reader.getNext(&file_info)) {
+        if (file_info.empty())
+            continue;
+
         const std::string member_name(file_info.getFilename());
         if (unlikely(not file_info.isRegularFile()))
             LOG_ERROR("unexpectedly, the entry \"" + member_name + "\" in \"" + archive_name + "\" is not a regular file!");
