b8b9b56fbca447df0eb5ec0c9e97a3ef0ed9b8f3
Mario Trojan <mario.trojan@googlemail.com>
2018-08-03 12:27:24 +0200

    removed iostream include

--- cpp/lib/src/ExecUtil.cc
+++ cpp/lib/src/ExecUtil.cc
@@ -25,8 +25,6 @@
  *  along with libiViaCore; if not, write to the Free Software
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
-#include <iostream>
-
 #include "ExecUtil.h"
 #include <stdexcept>
 #include <cassert>
