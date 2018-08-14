e1fc6353110720512220aa779722dda8b36a2f99
Mario Trojan <mario.trojan@googlemail.com>
2018-08-14 12:13:14 +0200

    minor optimizations in rewrite_keywords_and_authors_from_authority_data.cc

--- cpp/rewrite_keywords_and_authors_from_authority_data.cc
+++ cpp/rewrite_keywords_and_authors_from_authority_data.cc
@@ -43,21 +43,10 @@ namespace {
 }
 
 
-// Create a list of PPNs and File Offsets
-void CreateAuthorityOffsets(MARC::Reader * const authority_reader, std::map<std::string, off_t> * const authority_offsets) {
-   off_t record_offset(authority_reader->tell());
-   while (const MARC::Record record = authority_reader->read()) {
-      authority_offsets->emplace(record.getControlNumber(), record_offset);
-      // Shift to next record
-      record_offset = authority_reader->tell();
-   }
-}
-
-
 // Return the first matching primary field (Vorzugsbenennung) from authority data
 // This implicitly assumes that the correct tag can be uniquely identified from the PPN
 MARC::Record::const_iterator GetFirstPrimaryField(const MARC::Record& authority_record) {
-     const std::vector<std::string> tags_to_check {"100", "151", "150", "110", "111", "130", "153"} ;
+     static const std::vector<std::string> tags_to_check {"100", "151", "150", "110", "111", "130", "153"} ;
      for (const auto &tag_to_check : tags_to_check) {
          MARC::Record::const_iterator primary_field(authority_record.findTag(tag_to_check));
          if (primary_field != authority_record.end())
@@ -68,7 +57,7 @@ MARC::Record::const_iterator GetFirstPrimaryField(const MARC::Record& authority_
 
 
 bool GetAuthorityRecordFromPPN(const std::string &bsz_authority_ppn, MARC::Record * const authority_record,
-                               MARC::Reader * const authority_reader, const std::map<std::string, off_t> &authority_offsets)
+                               MARC::Reader * const authority_reader, const std::unordered_map<std::string, off_t> &authority_offsets)
 {
     auto authority_offset(authority_offsets.find(bsz_authority_ppn));
     if (authority_offset != authority_offsets.end()) {
@@ -121,10 +110,10 @@ void UpdateTitleDataField(MARC::Record::Field * const field, const MARC::Record 
 
 
 void AugmentAuthors(MARC::Record * const record, MARC::Reader * const authority_reader,
-                    const std::map<std::string, off_t> &authority_offsets,
+                    const std::unordered_map<std::string, off_t> &authority_offsets,
                     RegexMatcher * const matcher, bool * const modified_record)
 {
-    std::vector<std::string> tags_to_check({"100", "110", "111", "700", "710", "711"});
+    static std::vector<std::string> tags_to_check({"100", "110", "111", "700", "710", "711"});
     for (auto tag_to_check : tags_to_check) {
         for (auto &field : record->getTagRange(tag_to_check)) {
             std::string _author_content(field.getContents());
@@ -141,7 +130,7 @@ void AugmentAuthors(MARC::Record * const record, MARC::Reader * const authority_
 
 
 void AugmentKeywords(MARC::Record * const record, MARC::Reader * const authority_reader,
-                     const std::map<std::string, off_t> &authority_offsets,
+                     const std::unordered_map<std::string, off_t> &authority_offsets,
                      RegexMatcher * const matcher, bool * const modified_record)
 {
     for (auto &field : record->getTagRange("689")) {
@@ -158,7 +147,7 @@ void AugmentKeywords(MARC::Record * const record, MARC::Reader * const authority
 
 
 void AugmentKeywordsAndAuthors(MARC::Reader * const marc_reader, MARC::Reader * const authority_reader, MARC::Writer * const marc_writer,
-                               const std::map<std::string, off_t>& authority_offsets)
+                               const std::unordered_map<std::string, off_t>& authority_offsets)
 {
     std::string err_msg;
     RegexMatcher * const matcher(RegexMatcher::RegexMatcherFactory("\x1F""0\\(DE-576\\)([^\x1F]+).*\x1F?", &err_msg));
@@ -201,9 +190,9 @@ int Main(int argc, char **argv) {
     std::unique_ptr<MARC::Reader> authority_reader(MARC::Reader::Factory(authority_data_marc_input_filename,
                                                                          MARC::FileType::BINARY));
     std::unique_ptr<MARC::Writer> marc_writer(MARC::Writer::Factory(marc_output_filename));
-    std::map<std::string, off_t> authority_offsets;
+    std::unordered_map<std::string, off_t> authority_offsets;
 
-    CreateAuthorityOffsets(authority_reader.get(), &authority_offsets);
+    MARC::CollectRecordOffsets(authority_reader.get(), &authority_offsets);
     AugmentKeywordsAndAuthors(marc_reader.get(), authority_reader.get(), marc_writer.get(), authority_offsets);
 
     return EXIT_SUCCESS;
