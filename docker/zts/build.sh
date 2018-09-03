36d1e52b58a8e2cd0cb7064b3e338bcdfe241ad9
Mario Trojan <mario.trojan@googlemail.com>
2018-09-03 16:04:49 +0200

    zts2 => zts

--- docker/zts/build.sh
+++ docker/zts/build.sh
@@ -2,8 +2,8 @@
 #
 rm -rf /tmp/zotero-translation-server
 git clone --recurse-submodules https://github.com/ubtue/zotero-translation-server /tmp/zotero-translation-server
-cp /usr/local/ub_tools/docker/zts2/* /tmp/zotero-translation-server
-cp /usr/local/ub_tools/docker/zts2/.dockerignore /tmp/zotero-translation-server
+cp /usr/local/ub_tools/docker/zts/* /tmp/zotero-translation-server
+cp /usr/local/ub_tools/docker/zts/.dockerignore /tmp/zotero-translation-server
 cd /tmp/zotero-translation-server
-docker build -t zts2 .
+docker build -t zts .
 
