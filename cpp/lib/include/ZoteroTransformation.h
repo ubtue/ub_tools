f292af067f1febc315fb13c7b7f3677e17b38b49
jriedl <johannes.riedl@uni-tuebingen.de>
2018-09-10 14:09:14 +0200

    Forcing previously detached version

--- cpp/lib/include/ZoteroTransformation.h
+++ cpp/lib/include/ZoteroTransformation.h
@@ -25,14 +25,41 @@ namespace Zotero {
 
 namespace ZoteroTransformation {
 
-   extern const std::map<std::string, std::string> CREATOR_TYPES_TO_MARC21_MAP;
-   extern const std::map<std::string, std::string> CREATOR_TYPES_TO_MARC21_MAP;
-   extern const std::map<std::string, MARC::Record::BibliographicLevel> ITEM_TYPE_TO_BIBLIOGRAPHIC_LEVEL_MAP;
+    extern const std::map<std::string, std::string> CREATOR_TYPES_TO_MARC21_MAP;
+    extern const std::map<std::string, std::string> CREATOR_TYPES_TO_MARC21_MAP;
+    extern const std::map<std::string, MARC::Record::BibliographicLevel> ITEM_TYPE_TO_BIBLIOGRAPHIC_LEVEL_MAP;
+
+/*    class PPNandTitle {
+        std::string PPN_;
+        std::string title_;
+    public:
+        PPNandTitle(const std::string &PPN, const std::string &title): PPN_(PPN), title_(title) { }
+        PPNandTitle() = default;
+        PPNandTitle(const PPNandTitle &other) = default;
+    
+        inline const std::string &getPPN() const { return PPN_; }
+        inline const std::string &getTitle() const { return title_; }
+    };*/
+
+
+/*    struct AugmentMaps {
+        std::unordered_map<std::string, std::string> ISSN_to_SSG_map_;
+        std::unordered_map<std::string, std::string> ISSN_to_keyword_field_map_;
+        std::unordered_map<std::string, std::string> ISSN_to_language_code_map_;
+        std::unordered_map<std::string, std::string> ISSN_to_licence_map_;
+        std::unordered_map<std::string, std::string> ISSN_to_physical_form_map_;
+        std::unordered_map<std::string, std::string> ISSN_to_volume_map_;
+        std::unordered_map<std::string, std::string> language_to_language_code_map_;
+        std::unordered_map<std::string, PPNandTitle> ISSN_to_superior_ppn_and_title_map_;
+    public:
+        explicit AugmentMaps(const std::string &map_directory_path);
+    };*/
 
    const std::string GetCreatorTypeForMarc21(const std::string &zotero_creator_type);
    MARC::Record::BibliographicLevel MapBiblioLevel(const std::string item_type);
    bool IsValidItemType(const std::string item_type);
-   std::string DownloadAuthorPPN(const std::string &author, const struct SiteParams &site_params);
+//   std::string DownloadAuthorPPN(const std::string &author, const struct Zotero::SiteParams &site_params);
+   std::string OptionalMap(const std::string &key, const std::unordered_map<std::string, std::string> &map);
 
 } // end ZoteroTransformation
 
