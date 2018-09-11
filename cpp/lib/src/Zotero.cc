fab725354d656857d1a8f627238a260c39d82eda
jriedl <johannes.riedl@uni-tuebingen.de>
2018-09-11 15:11:05 +0200

    Abstract out AugmentMaps

--- cpp/lib/src/Zotero.cc
+++ cpp/lib/src/Zotero.cc
@@ -786,31 +786,6 @@ std::pair<unsigned, unsigned> MarcFormatHandler::processRecord(const std::shared
 }
 
 
-void LoadISSNToPPNMap(std::unordered_map<std::string, PPNandTitle> * const ISSN_to_superior_ppn_map) {
-     enum ISSN_TO_PPN_OFFSET {ISSN_OFFSET = 0, PPN_OFFSET = 1, TITLE_OFFSET = 4};
-     std::vector<std::vector<std::string>> parsed_issn_to_superior_content;
-     TextUtil::ParseCSVFileOrDie(ISSN_TO_MISC_BITS_MAP_PATH_LOCAL, &parsed_issn_to_superior_content, ',', (char) 0x00);
-     for (const auto parsed_line : parsed_issn_to_superior_content) {
-         const std::string ISSN(parsed_line[ISSN_OFFSET]);
-         const std::string PPN(parsed_line[PPN_OFFSET]);
-         const std::string title(StringUtil::RightTrim(" \t", parsed_line[TITLE_OFFSET]));
-         ISSN_to_superior_ppn_map->emplace(ISSN, PPNandTitle(PPN, title));
-     }
-}
-
-
-AugmentMaps::AugmentMaps(const std::string &map_directory_path) {
-    MiscUtil::LoadMapFile(map_directory_path + "language_to_language_code.map", &language_to_language_code_map_);
-    MiscUtil::LoadMapFile(map_directory_path + "ISSN_to_language_code.map", &ISSN_to_language_code_map_);
-    MiscUtil::LoadMapFile(map_directory_path + "ISSN_to_licence.map", &ISSN_to_licence_map_);
-    MiscUtil::LoadMapFile(map_directory_path + "ISSN_to_keyword_field.map", &ISSN_to_keyword_field_map_);
-    MiscUtil::LoadMapFile(map_directory_path + "ISSN_to_physical_form.map", &ISSN_to_physical_form_map_);
-    MiscUtil::LoadMapFile(map_directory_path + "ISSN_to_volume.map", &ISSN_to_volume_map_);
-    MiscUtil::LoadMapFile(map_directory_path + "ISSN_to_SSG.map", &ISSN_to_SSG_map_);
-    LoadISSNToPPNMap(&ISSN_to_superior_ppn_and_title_map_);
-}
-
-
 // If "key" is in "map", then return the mapped value, o/w return "key".
 inline std::string OptionalMap(const std::string &key, const std::unordered_map<std::string, std::string> &map) {
     const auto &key_and_value(map.find(key));
