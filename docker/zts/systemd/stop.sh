53c8e9d420c11fc1748099f5bbc4b5ebc0f7e0ad
Mario Trojan <mario.trojan@googlemail.com>
2018-09-17 10:08:51 +0200

    changed zts systemd unit type to forking

--- docker/zts/systemd/stop.sh
+++ docker/zts/systemd/stop.sh
@@ -2,8 +2,12 @@
 #
 LATEST_CONTAINER_ID=$(docker ps -q --filter ancestor=zts)
 if [ -z "$LATEST_CONTAINER_ID" ]; then
-	echo "no running container detected"
+    echo "no running container detected"
 else
-	echo "stopping container $LATEST_CONTAINER_ID"
-	docker stop $LATEST_CONTAINER_ID
+    echo "stopping container $LATEST_CONTAINER_ID"
+    docker stop $LATEST_CONTAINER_ID
+fi
+
+if [ -e /var/run/zts.pid ]; then
+    rm /var/run/zts.pid
 fi
