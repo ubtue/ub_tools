7710081be24509e10ffa6bfc0e87c033191470ca
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-10-16 09:35:17 +0200

    Fixed a bug involving a comment on a continuation line.

--- cpp/ixtheo_marc_pipeline_fifo.sh
+++ cpp/ixtheo_marc_pipeline_fifo.sh
@@ -109,7 +109,7 @@ EndPhase || Abort) &
 
 StartPhase "Extract Translations and Generate Interface Translation Files"
 (extract_vufind_translations_for_translation \
-    "$VUFIND_HOME"/local/tuefind/languages/de.ini \ # German terms before all others.
+    "$VUFIND_HOME"/local/tuefind/languages/de.ini \ `# German terms before all others.`
     $(ls -1 "$VUFIND_HOME"/local/tuefind/languages/??.ini | grep -v 'de.ini$') >> "${log}" 2>&1 && \
 generate_vufind_translation_files "$VUFIND_HOME"/local/tuefind/languages/ >> "${log}" 2>&1 && \
 EndPhase || Abort) &
