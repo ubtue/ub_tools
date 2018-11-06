6ea122d59ca2975a6d6c3ff7e88d19d2e04d9c14
jriedl <johannes.riedl@uni-tuebingen.de>
2018-11-06 14:44:13 +0100

    Add a missing single quote in a comment.....

--- cpp/ixtheo_marc_pipeline_fifo.sh
+++ cpp/ixtheo_marc_pipeline_fifo.sh
@@ -276,7 +276,7 @@ mkfifo GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc
 EndPhase || Abort) &
 
 
-StartPhase "Add Additional Open Access URLs"
+StartPhase "Add Additional Open Access URL's"
 (add_oa_urls oadoi_urls.json GesamtTiteldaten-post-phase"$((PHASE-1))"-"${date}".mrc \
     GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc >> "${log}" 2>&1 && \
 EndPhase || Abort) &
