e818b9d63524fb97c0526e495354e08b93e5a389
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2018-11-06 10:13:47 +0100

    Better locale support.

--- cpp/lib/include/Locale.h
+++ cpp/lib/include/Locale.h
@@ -6,6 +6,7 @@
 /*
  *  Copyright 2008 Project iVia.
  *  Copyright 2008 The Regents of The University of California.
+ *  Copyright 2018
  *
  *  This file is part of the libiViaCore package.
  *
@@ -31,29 +32,31 @@
 
 
 class Locale {
-        std::string old_locale_;
-        const int category_;
-        const bool restore_;
+    bool is_valid_;
+    std::string old_locale_;
+    const int category_;
+    const bool restore_;
 public:
-        /** \brief  Constructs a new locale setting object.
-         *  \param  new_locale  The new locale to switch to.
-         *  \param  category    The category to change (see locale(3) for documentation).
-         *  \param  restore     If true, restore the original locale for "category" upon
-         *                      object destruction (for example when an auto object goes
-         *                      out of scope).
-         *  \note   This constructor will throw if the requested locale change fails.
-         */
-        Locale(const std::string &new_locale, const int category, const bool restore = true);
+    /** \brief  Constructs a new locale setting object.
+     *  \param  new_locale  The new locale to switch to.
+     *  \param  category    The category to change (see locale(3) for documentation).
+     *  \param  restore     If true, restore the original locale for "category" upon
+     *                      object destruction (for example when an auto object goes
+     *                      out of scope).
+     */
+    Locale(const std::string &new_locale, const int category, const bool restore = true);
 
-        /** Restores the original locale/category if requested by the "restore" argument to
-            the constructor otherwise does nothing. */
-        ~Locale();
+    /** Restores the original locale/category if requested by the "restore" argument to
+        the constructor otherwise does nothing. */
+    ~Locale();
 
-        /** \brief   Returns the name of the locale that is currently in effect for category "category".
-         *  \param   category  The category for which we'd like the current locale for.  See setlocale(3) for
-         *                     a list of possible categories LC_*.
-         *  \return  The opaque locale name associated with "category".
-         *  \note    In the case that anything goes wrong (invalid "category"?) this function throws an exception!
-         */
-        static std::string GetLocaleName(const int category = LC_CTYPE);
+    inline bool isValid() const { return is_valid_; }
+
+    /** \brief   Returns the name of the locale that is currently in effect for category "category".
+     *  \param   category  The category for which we'd like the current locale for.  See setlocale(3) for
+     *                     a list of possible categories LC_*.
+     *  \return  The opaque locale name associated with "category".
+     *  \note    In the case that anything goes wrong (invalid "category"?) this function throws an exception!
+     */
+    static std::string GetLocaleName(const int category = LC_CTYPE);
 };
