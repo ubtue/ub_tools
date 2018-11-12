aecd87b301d88edafd128ec7e926c35d4213b74f
Mario Trojan <mario.trojan@googlemail.com>
2018-11-12 16:12:35 +0100

    squid docker container basics

---
+++ docker/squid/systemd/stop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+LATEST_RUNNING_CONTAINER_ID=$(docker ps --latest --quiet --filter ancestor=squid)
+if [ -z "$LATEST_RUNNING_CONTAINER_ID" ]; then
+    echo "no running container detected"
+else
+    echo "stopping container $LATEST_RUNNING_CONTAINER_ID"
+    docker stop $LATEST_RUNNING_CONTAINER_ID
+fi
+
+if [ -e /var/run/squid.pid ]; then
+    rm /var/run/squid.pid
+fi
