80a8e21eaeabc6c2150d0f8b7d45768da02bc025
Mario Trojan <mario.trojan@googlemail.com>
2018-11-22 15:29:01 +0100

    ZTS_PROXY environment file also being parsed directly (for CGI environment)

--- cpp/lib/include/SimpleCrawler.h
+++ cpp/lib/include/SimpleCrawler.h
@@ -56,6 +56,7 @@ public:
         bool print_redirects_;
         std::string user_agent_;
         std::string url_ignore_pattern_;
+        bool ignore_ssl_certificates_;
         std::string proxy_host_and_port_;
     public:
         explicit Params(const std::string &acceptable_languages = "",
@@ -67,6 +68,7 @@ public:
                         const bool print_redirects = false,
                         const std::string &user_agent = "ub_tools (https://ixtheo.de/docs/user_agents)",
                         const std::string &url_ignore_pattern = "(?i)\\.(js|css|bmp|pdf|jpg|gif|png|tif|tiff)(\\?[^?]*)?$",
+                        const bool ignore_ssl_certificates_ = false,
                         const std::string &proxy_host_and_port = ""
                         );
         ~Params() = default;
