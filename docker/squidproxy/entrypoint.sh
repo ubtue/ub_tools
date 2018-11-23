3af53440f9944fc13857bd6314fbc5f2074a0c97
Mario Trojan <mario.trojan@googlemail.com>
2018-11-23 13:59:09 +0100

    squidproxy entrypoint only creates cache files on first start (else reuse of existing container is impossible)

--- docker/squidproxy/entrypoint.sh
+++ docker/squidproxy/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-/apps/squid/sbin/squid -z -f /apps/squid.conf.https_proxy
+if [ ! -d "/apps/squid/var/cache/squid/00" ]; then
+    /apps/squid/sbin/squid -z -f /apps/squid.conf.https_proxy
+fi
 /apps/squid/sbin/squid -NsY -f /apps/squid.conf.https_proxy
