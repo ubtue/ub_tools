ea9911331ac80c7ca835bb829cccbd2fb86df127
Mario Trojan <mario.trojan@googlemail.com>
2018-11-30 11:32:54 +0100

    added create_match_db to ixtheo pipeline

--- cpp/ixtheo_marc_pipeline_fifo.sh
+++ cpp/ixtheo_marc_pipeline_fifo.sh
@@ -125,8 +125,14 @@ EndPhase || Abort) &
 wait
 
 
+StartPhase "Populate ControlNumberGuesser Database"
+(create_match_db GesamtTiteldaten-post-phase"$((PHASE-3))"-"${date}".mrc >> "${log}" 2>&1 && \
+EndPhase || Abort) &
+wait
+
+
 StartPhase "Merge Print and Online Superior Records"
-(merge_print_and_online GesamtTiteldaten-post-phase"$((PHASE-3))"-"${date}".mrc \
+(merge_print_and_online GesamtTiteldaten-post-phase"$((PHASE-4))"-"${date}".mrc \
                         GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc \
                         missing_ppn_partners.list >> "${log}" 2>&1 && \
 EndPhase || Abort) &
