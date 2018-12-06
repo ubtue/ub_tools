700eebc81def5c62cadb7e6e5dd89a01d7bb8f11
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2018-12-06 16:48:07 +0100

    change the order of an #include

--- cpp/lib/src/Zeder.cc
+++ cpp/lib/src/Zeder.cc
@@ -16,8 +16,8 @@
  *  You should have received a copy of the GNU Affero General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
-#include <cmath>
 #include "Zeder.h"
+#include <cmath>
 #include "Downloader.h"
 #include "FileUtil.h"
 #include "StringUtil.h"
