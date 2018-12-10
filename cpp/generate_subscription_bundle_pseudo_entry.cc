25b41f9308c5ecf6f69b9cf9d740f1d1c21827b6
jriedl <johannes.riedl@uni-tuebingen.de>
2018-12-10 17:18:24 +0100

    Generate entries from configuration file and support entries only for certain instances

--- cpp/generate_subscription_bundle_pseudo_entry.cc
+++ cpp/generate_subscription_bundle_pseudo_entry.cc
@@ -15,14 +15,16 @@
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Affero General Public License for more details.
-    
+
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/  
+*/
 
 #include <iostream>
 #include "IniFile.h"
 #include "MARC.h"
+#include "StringUtil.h"
+#include "TimeUtil.h"
 #include "UBTools.h"
 
 
@@ -36,21 +38,34 @@ namespace {
 }
 
 
-MARC::Record GenerateRecord(const std::string &record_id, const std::string &bundle_name) {
+MARC::Record GenerateRecord(const std::string &record_id, const std::string &bundle_name, const std::vector<std::string> instances) {
+    const std::string today(TimeUtil::GetCurrentDateAndTime("%y%m%d"));
+    // exclude from ixtheo e.g. because it's a pure relbib list
+    const bool exclude_ixtheo(std::find(instances.begin(), instances.end(), "ixtheo") != instances.end() ? false : true);
+    const bool include_relbib(std::find(instances.begin(), instances.end(), "relbib") != instances.end() ? true : false);
     MARC::Record record("00000nac a2200000 u 4500");
     record.insertField("001", record_id);
-    record.insertField("005", "20181206120000.0:");
-    record.insertField("008", "181206s2018");
+    record.insertField("005", today + "12000000.0:");
+    record.insertField("008", today + 's' + TimeUtil::GetCurrentYear());
     record.insertField("245", MARC::Subfields( { { 'a', bundle_name }, { 'b', "Testentry" }, { 'h', "Subscription Bundle"} }));
-    record.insertField("SPR", MARC::Subfields( { { 'a', "1" /* is superior work */ }, 
+    record.insertField("SPR", MARC::Subfields( { { 'a', "1" /* is superior work */ },
                                                  { 'b', "1" /* series has not been completed */ } }));
     record.insertField("935", MARC::Subfields( { { 'c', "subskriptionspaket" } }));
+
+    if (exclude_ixtheo)
+        record.addSubfield("935", 'x', std::to_string(exclude_ixtheo));
+    if (include_relbib)
+        record.insertField("REL", MARC::Subfields( { {'a', "1" } }));
     return record;
 }
 
 
-void ProcessBundle(MARC::Writer * const marc_writer, const std::string &bundle_name) {
-    marc_writer->write(GenerateRecord("bundle_" + bundle_name, bundle_name));
+void ProcessBundle(MARC::Writer * const marc_writer, const std::string &bundle_name, const IniFile &bundles_config) {
+         const std::string instances_string(bundles_config.getString(bundle_name, "instances", ""));
+         std::vector<std::string> instances;
+         if (not instances_string.empty())
+             StringUtil::SplitThenTrim(instances_string, ",", " \t", &instances);
+         marc_writer->write(GenerateRecord("bundle_" + bundle_name, bundle_name, instances));
 }
 
 } //unnamed namespace
@@ -65,7 +80,7 @@ int Main(int argc, char **argv) {
     const IniFile bundles_config(UBTools::GetTuelibPath() + "journal_alert_bundles.conf");
     for (const auto &bundle_name : bundles_config.getSections()) {
          if (not bundle_name.empty())
-             ProcessBundle(marc_writer.get(), bundle_name);
+             ProcessBundle(marc_writer.get(), bundle_name, bundles_config);
     }
     return EXIT_SUCCESS;
 }
