056af599980f71189f2f0a239595d58d78b36856
jriedl <johannes.riedl@uni-tuebingen.de>
2019-01-16 16:59:44 +0100

    Fixing index deletion and creation

--- cpp/elasticsearch/delete_index.sh
+++ cpp/elasticsearch/delete_index.sh
@@ -4,10 +4,10 @@ set -e
 
 CONFIG_FILE=/usr/local/var/lib/tuelib/Elasticsearch.conf
 
-host_and_port=$(cat inifile_lookup $CONFIG_FILE Elasticsearch host)
-index=$(cat inifile_lookup $CONFIG_FILE Elasticsearch index)
-type=$(cat inifile_lookup $CONFIG_FILE Elasticsearch type)
+host_and_port=$(inifile_lookup $CONFIG_FILE Elasticsearch host)
+index=$(inifile_lookup $CONFIG_FILE Elasticsearch index)
+type=$(inifile_lookup $CONFIG_FILE Elasticsearch type)
 
 
 # Delete the index:
-curl -X DELETE "${host_and_port}/${index}"
+curl -X DELETE -H 'Content-Type: application/json' "${host_and_port}/${index}"
