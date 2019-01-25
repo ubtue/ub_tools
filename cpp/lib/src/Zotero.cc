acfc7072a9269d3afef27869197a696888901f2e
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2019-01-25 14:43:07 +0100

    Zotero: add default min crawl delay, remove superfluous min_url_processing_time timeout

--- cpp/lib/src/Zotero.cc
+++ cpp/lib/src/Zotero.cc
@@ -1042,13 +1042,22 @@ bool ValidateAugmentedJSON(const std::shared_ptr<JSON::ObjectNode> &entry) {
 
 
 void ApplyCrawlDelay(const std::string &harvest_url) {
+    static const unsigned int MINIMUM_CRAWL_DELAY_TIME = 1000;  // in ms
+
     struct CrawlDelayParams {
         RobotsDotTxt robots_dot_txt_;
         TimeLimit crawl_timeout_;
     public:
         CrawlDelayParams(const std::string &robots_dot_txt)
-         : robots_dot_txt_(robots_dot_txt), crawl_timeout_(robots_dot_txt_.getCrawlDelay("*") * 1000) {}
-        CrawlDelayParams(const TimeLimit &crawl_timeout) : crawl_timeout_(crawl_timeout) {}
+         : robots_dot_txt_(robots_dot_txt), crawl_timeout_(robots_dot_txt_.getCrawlDelay("*") * 1000)
+        {
+            if (crawl_timeout_.getLimit() < MINIMUM_CRAWL_DELAY_TIME)
+                crawl_timeout_ = MINIMUM_CRAWL_DELAY_TIME;
+        }
+        CrawlDelayParams(const TimeLimit &crawl_timeout) : crawl_timeout_(crawl_timeout) {
+            if (crawl_timeout_.getLimit() < MINIMUM_CRAWL_DELAY_TIME)
+                crawl_timeout_ = MINIMUM_CRAWL_DELAY_TIME;
+        }
     };
 
     static std::unordered_map<std::string, CrawlDelayParams> HOSTNAME_TO_DELAY_PARAMS_MAP;
@@ -1060,11 +1069,10 @@ void ApplyCrawlDelay(const std::string &harvest_url) {
     if (delay_params == HOSTNAME_TO_DELAY_PARAMS_MAP.end()) {
         Downloader robots_txt_downloader(parsed_url.getRobotsDotTxtUrl());
         if (robots_txt_downloader.anErrorOccurred()) {
-            // ignore for the rest of the session by giving it a very low delay
-            CrawlDelayParams default_delay_params(1);
+            CrawlDelayParams default_delay_params(MINIMUM_CRAWL_DELAY_TIME);
             HOSTNAME_TO_DELAY_PARAMS_MAP.insert(std::make_pair(hostname, default_delay_params));
 
-            LOG_WARNING("couldn't retrieve robots.txt for domain '" + hostname + "'");
+            LOG_DEBUG("couldn't retrieve robots.txt for domain '" + hostname + "'");
             return;
         }
 
@@ -1108,14 +1116,12 @@ std::pair<unsigned, unsigned> Harvest(const std::string &harvest_url, const std:
 
     std::string response_body, error_message;
     unsigned response_code;
-    harvest_params->min_url_processing_time_.sleepUntilExpired();
     Downloader::Params downloader_params;
     downloader_params.user_agent_ = harvest_params->user_agent_;
     bool download_succeeded(TranslationServer::Web(harvest_params->zts_server_url_, /* time_limit = */ DEFAULT_TIMEOUT,
                                                    downloader_params, Url(harvest_url), &response_body, &response_code,
                                                    &error_message));
 
-    harvest_params->min_url_processing_time_.restart();
     if (not download_succeeded) {
         error_logger_context.log(HarvesterErrorLogger::ZTS_CONVERSION_FAILED, error_message);
         return record_count_and_previously_downloaded_count;
