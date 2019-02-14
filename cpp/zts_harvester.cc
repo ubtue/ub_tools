d3c6af0a4434bd7f499ce35fb96ff883d7043996
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2019-02-14 11:30:27 +0100

    zts_harvester: add the option to specify text fields to be used when automatically detecting language

--- cpp/zts_harvester.cc
+++ cpp/zts_harvester.cc
@@ -87,7 +87,12 @@ void ReadGenericSiteAugmentParams(const IniFile &ini_file, const IniFile::Sectio
         site_params->strptime_format_ += common_strptime_format;
     }
 
-    const auto expected_languages(bundle_reader.zotero(section_name).value(JournalConfig::Zotero::EXPECTED_LANGUAGES, ""));
+    auto expected_languages(bundle_reader.zotero(section_name).value(JournalConfig::Zotero::EXPECTED_LANGUAGES, ""));
+    const auto field_separator(expected_languages.find(':'));
+    if (field_separator != std::string::npos) {
+        site_params->expected_languages_text_fields_ = expected_languages.substr(0, field_separator);
+        expected_languages = expected_languages.substr(field_separator + 1);
+    }
     StringUtil::Split(expected_languages, ',', &site_params->expected_languages_);
 
     for (const auto &entry : section) {
