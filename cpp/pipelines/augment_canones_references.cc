944a7ead5c9f77faf0270774ba66216887978243
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2019-03-08 11:42:00 +0100

    A new pipleline tool for canon law references.

---
+++ cpp/pipelines/augment_canones_references.cc
@@ -0,0 +1,210 @@
+/** \file    augment_canones_references.cc
+ *  \brief   A tool for adding numerical canon law references to MARC-21 datasets.
+ *  \author  Dr. Johannes Ruscheinski
+ */
+
+/*
+    Copyright (C) 2019, Library of the University of Tübingen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <cstdlib>
+#include "BibleUtil.h"
+#include "Compiler.h"
+#include "MARC.h"
+#include "StringUtil.h"
+#include "UBTools.h"
+#include "util.h"
+
+
+namespace {
+
+
+void ParseRanges(const std::string &ppn, const std::string &ranges, unsigned * const range_start, unsigned * const range_end) {
+    std::vector<std::string> parts;
+    if (unlikely(StringUtil::SplitThenTrimWhite(ranges, ',', &parts, /* suppress_empty_components = */false) > 3))
+        LOG_ERROR("bad codex parts \"" + ranges + "\"! (PPN: " + ppn + ")");
+
+    unsigned canones;
+    if (parts.size() == 1) {
+        const auto dash_pos(parts[0].find('-'));
+        if (dash_pos == std::string::npos) {
+            if (unlikely(not StringUtil::ToUnsigned(parts[0], &canones)))
+                LOG_ERROR("bad codex canones \"" + parts[0] + "\"! (PPN: " + ppn + ")");
+            *range_start = canones * 10000;
+            *range_end   = canones * 10000 + 9999;
+        } else {
+            if (unlikely(not StringUtil::ToUnsigned(parts[0].substr(0, dash_pos), range_start)
+                         or not StringUtil::ToUnsigned(parts[0].substr(dash_pos + 1), range_end)))
+                LOG_ERROR("bad canones range \"" + parts[0] + "\"! (PPN: " + ppn + ")");
+            *range_start *= 10000;
+            *range_end   *= 10000;
+        }
+
+        return;
+    } else if (parts.size() == 2) {
+        if (unlikely(not StringUtil::ToUnsigned(parts[0], &canones)))
+            LOG_ERROR("bad codex canones \"" + parts[0] + "\"! (PPN: " + ppn + ")");
+
+        const auto dash_pos(parts[1].find('-'));
+        if (dash_pos == std::string::npos) {
+            unsigned pars;
+            if (unlikely(not StringUtil::ToUnsigned(parts[1], &pars)))
+                LOG_ERROR("bad codex pars \"" + parts[1] + "\"! (PPN: " + ppn + ")");
+            *range_start = canones * 10000 + pars * 100;
+            *range_end   = canones * 10000 + 9999;
+        } else {
+            unsigned start_pars, end_pars;
+            if (unlikely(not StringUtil::ToUnsigned(parts[1].substr(0, dash_pos), &start_pars)
+                         or not StringUtil::ToUnsigned(parts[1].substr(dash_pos + 1), &end_pars)))
+                LOG_ERROR("bad pars range \"" + parts[1] + "\"! (PPN: " + ppn + ")");
+            *range_start = canones * 10000 + start_pars * 100;
+            *range_end   = canones * 10000 + end_pars   * 100 + 99;
+        }
+
+        return;
+    }
+
+    LOG_ERROR("don't know how to parse codex parts \"" + ranges + "\"! (PPN: " + ppn + ")");
+}
+
+
+// To understand this code read https://github.com/ubtue/tuefind/wiki/Codices
+std::string FieldToCanonLawCode(const std::string &ppn, const MARC::Record::Field &_110_field) {
+    const std::string t_subfield(_110_field.getFirstSubfieldWithCode('t'));
+    const std::string year(_110_field.getFirstSubfieldWithCode('f'));
+    const std::string p_subfield(_110_field.getFirstSubfieldWithCode('p'));
+
+    enum Codex { CIC1917, CIC1983, CCEO } codex;
+    if (t_subfield == "Codex canonum ecclesiarum orientalium")
+        codex = CCEO;
+    else {
+        if (unlikely(year.empty()))
+            LOG_ERROR("missing year for Codex Iuris Canonici!");
+        if (year == "1917")
+            codex = CIC1917;
+        else if (year == "1983")
+            codex = CIC1983;
+        else
+            LOG_ERROR("bad year for Codex Iuris Canonici \"" + year + "\"!");
+    }
+
+    unsigned range_start, range_end;
+    if (p_subfield.empty()) {
+        range_start = 0;
+        range_end = 99999999;
+    } else
+        ParseRanges(ppn, p_subfield, &range_start, &range_end);
+
+    switch (codex) {
+    case CIC1917:
+        return StringUtil::ToString(100000000 + range_start) + "_" + StringUtil::ToString(100000000 + range_end);
+    case CIC1983:
+        return StringUtil::ToString(200000000 + range_start) + "_" + StringUtil::ToString(200000000 + range_end);
+    case CCEO:
+        return StringUtil::ToString(300000000 + range_start) + "_" + StringUtil::ToString(300000000 + range_end);
+    }
+}
+
+
+void LoadAuthorityData(MARC::Reader * const reader,
+                       std::unordered_map<std::string, std::string> * const authority_ppns_to_canon_law_codes_map)
+{
+    unsigned total_count(0);
+    while (auto record = reader->read()) {
+        ++total_count;
+
+        const auto _110_field(record.findTag("110"));
+        if (_110_field == record.end() or _110_field->getFirstSubfieldWithCode('a') != "Katholische Kirche")
+            continue;
+
+        const std::string t_subfield(_110_field->getFirstSubfieldWithCode('t'));
+        if (t_subfield != "Codex Iuris Canonici" and t_subfield != "Codex canonum ecclesiarum orientalium")
+            continue;
+
+        (*authority_ppns_to_canon_law_codes_map)[record.getControlNumber()] = FieldToCanonLawCode(record.getControlNumber(), *_110_field);
+    }
+
+    LOG_INFO("found " + std::to_string(authority_ppns_to_canon_law_codes_map->size()) + " canon law records among "
+             + std::to_string(total_count) + " authority records.");
+}
+
+
+void ProcessRecords(MARC::Reader * const reader, MARC::Writer * const writer,
+                    const std::unordered_map<std::string, std::string> &authority_ppns_to_canon_law_codes_map)
+{
+    unsigned total_count(0), augmented_count(0);
+    while (auto record = reader->read()) {
+        ++total_count;
+
+        bool augmented_record(false);
+        for (const auto &_689_field : record.getTagRange("689")) {
+            if (_689_field.getFirstSubfieldWithCode('2') != "gnd")
+                continue;
+
+            std::vector<std::string> authority_ppns;
+            for (const auto &subfield : _689_field.getSubfields()) {
+                if (subfield.code_ == '0' and StringUtil::StartsWith(subfield.value_, "(DE-576)"))
+                    authority_ppns.emplace_back(subfield.value_.substr(__builtin_strlen("(DE-576)")));
+            }
+
+            for (const auto &authority_ppn : authority_ppns) {
+                const auto ppn_and_canon_law_code(authority_ppns_to_canon_law_codes_map.find(authority_ppn));
+                if (ppn_and_canon_law_code != authority_ppns_to_canon_law_codes_map.cend()) {
+                    record.insertField("CAL", { { 'a', ppn_and_canon_law_code->second } });
+                    augmented_record = true;
+                }
+            }
+        }
+        if (augmented_record)
+            ++augmented_count;
+
+        writer->write(record);
+    }
+
+    LOG_INFO("augmented " + std::to_string(augmented_count) + " of " + std::to_string(total_count) + " records.");
+}
+
+
+} // unnamed namespace
+
+
+int Main(int argc, char **argv) {
+    if (argc != 4)
+        ::Usage("ix_theo_titles authority_records augmented_ix_theo_titles");
+
+    const std::string title_input_filename(argv[1]);
+    const std::string authority_filename(argv[2]);
+    const std::string title_output_filename(argv[3]);
+    if (unlikely(title_input_filename == title_output_filename))
+        LOG_ERROR("Title input file name equals title output file name!");
+    if (unlikely(title_input_filename == authority_filename))
+        LOG_ERROR("Title input file name equals authority file name!");
+    if (unlikely(title_output_filename == authority_filename))
+        LOG_ERROR("Title output file name equals authority file name!");
+
+    auto authority_reader(MARC::Reader::Factory(authority_filename));
+    std::unordered_map<std::string, std::string> authority_ppns_to_canon_law_codes_map;
+    LoadAuthorityData(authority_reader.get(), &authority_ppns_to_canon_law_codes_map);
+
+    auto title_reader(MARC::Reader::Factory(title_input_filename));
+    auto title_writer(MARC::Writer::Factory(title_output_filename));
+    ProcessRecords(title_reader.get(), title_writer.get(), authority_ppns_to_canon_law_codes_map);
+
+    return EXIT_SUCCESS;
+}
