82546ff2cb60a75310bbbf9a99265b3f1e482693
jriedl <johannes.riedl@uni-tuebingen.de>
2019-04-03 09:13:44 +0200

    Requested changes

--- cpp/lib/src/DbConnection.cc
+++ cpp/lib/src/DbConnection.cc
@@ -127,7 +127,7 @@ bool DbConnection::query(const std::string &query_statement) {
                                std::string(::progname) + ": " +  query_statement + '\n');
 
     if (type_ == T_MYSQL) {
-        if (::mysql_query(&mysql_, query_statement.c_str()))
+        if (::mysql_query(&mysql_, query_statement.c_str()) != 0)
 	    LOG_ERROR("Could not successfully execute statement \"" + query_statement + "\": SQL error code:"
                       + std::to_string(::mysql_errno(&mysql_)));
 	return true;
