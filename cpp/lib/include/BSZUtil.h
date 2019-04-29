a3e9f903674c00e0ec25661971aac0f27c0d5690
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2019-04-29 09:42:34 +0200

    Update copyright year.

--- cpp/lib/include/BSZUtil.h
+++ cpp/lib/include/BSZUtil.h
@@ -3,7 +3,7 @@
  *  \author Dr. Johannes Ruscheinski (johannes.ruscheinski@uni-tuebingen.de)
  *  \author Oliver Obenland (oliver.obenland@uni-tuebingen.de)
  *
- *  \copyright 2017,2018 Universitätsbibliothek Tübingen.  All rights reserved.
+ *  \copyright 2017-2019 Universitätsbibliothek Tübingen.  All rights reserved.
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Affero General Public License as
