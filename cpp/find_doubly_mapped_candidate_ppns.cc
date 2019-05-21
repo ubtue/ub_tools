fe5fa4fd6783e90ac4ca365ef5abec28c60574db
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2019-05-21 14:55:32 +0200

    Classify the output.

--- cpp/find_doubly_mapped_candidate_ppns.cc
+++ cpp/find_doubly_mapped_candidate_ppns.cc
@@ -25,7 +25,6 @@
 #include <cstdio>
 #include <cstdlib>
 #include "FileUtil.h"
-#include "MapUtil.h"
 #include "MARC.h"
 #include "StringUtil.h"
 #include "util.h"
@@ -34,7 +33,23 @@
 namespace {
 
 
-void ProcessRecords(MARC::Reader * const marc_reader, std::unordered_map<std::string, std::string> * const old_bsz_to_new_k10plus_ppns_map,
+struct PPNAndRecordType {
+    std::string ppn_;
+    MARC::Record::RecordType record_type_;
+public:
+    PPNAndRecordType(const std::string &ppn, const MARC::Record::RecordType record_type): ppn_(ppn), record_type_(record_type) { }
+    PPNAndRecordType(const PPNAndRecordType &other) = default;
+    PPNAndRecordType() = default;
+    inline const PPNAndRecordType &operator=(const PPNAndRecordType &rhs) {
+        ppn_ = rhs.ppn_;
+        record_type_ = rhs.record_type_;
+        return *this;
+    }
+};
+
+
+void ProcessRecords(MARC::Reader * const marc_reader,
+                    std::unordered_map<std::string, PPNAndRecordType> * const old_bsz_to_new_k10plus_ppns_map,
                     std::unordered_set<std::string> * const new_k10plus_ppns)
 {
     unsigned identity_count(0), old_to_new_count(0);
@@ -47,7 +62,7 @@ void ProcessRecords(MARC::Reader * const marc_reader, std::unordered_map<std::st
                 if (unlikely(old_bsz_ppn == record.getControlNumber()))
                     ++identity_count;
                 else {
-                    (*old_bsz_to_new_k10plus_ppns_map)[old_bsz_ppn] = record.getControlNumber();
+                    (*old_bsz_to_new_k10plus_ppns_map)[old_bsz_ppn] = PPNAndRecordType(record.getControlNumber(), record.getRecordType());
                     ++old_to_new_count;
                 }
                 continue;
@@ -67,7 +82,7 @@ int Main(int argc, char *argv[]) {
     if (argc != 4)
         ::Usage("title_records authority_records backpatch.map");
 
-    std::unordered_map<std::string, std::string> old_bsz_to_new_k10plus_ppns_map;
+    std::unordered_map<std::string, PPNAndRecordType> old_bsz_to_new_k10plus_ppns_map;
     std::unordered_set<std::string> new_k10plus_ppns;
 
     auto marc_reader(MARC::Reader::Factory(argv[1]));
@@ -76,25 +91,26 @@ int Main(int argc, char *argv[]) {
     auto marc_reader2(MARC::Reader::Factory(argv[2]));
     ProcessRecords(marc_reader2.get(), &old_bsz_to_new_k10plus_ppns_map, &new_k10plus_ppns);
 
+    const auto map_file(FileUtil::OpenOutputFileOrDie(argv[3]));
+
     std::unordered_map<std::string, std::string> k10plus_to_k10plus_map;
     for (const auto &bsz_and_k10plus_ppns : old_bsz_to_new_k10plus_ppns_map) {
         // Is the replaced PPN an old BSZ PPN?
         unsigned replacement_count(0);
         std::string final_k10plus_ppn(bsz_and_k10plus_ppns.first);
-        const std::string correct_substitution(bsz_and_k10plus_ppns.second);
+        const std::string correct_substitution(bsz_and_k10plus_ppns.second.ppn_);
         for (;;) {
             auto bsz_and_k10plus_ppn2(old_bsz_to_new_k10plus_ppns_map.find(final_k10plus_ppn));
             if (bsz_and_k10plus_ppn2 == old_bsz_to_new_k10plus_ppns_map.cend())
                 break;
-            final_k10plus_ppn = bsz_and_k10plus_ppn2->second;
+            final_k10plus_ppn = bsz_and_k10plus_ppn2->second.ppn_;
             ++replacement_count;
         }
         if (replacement_count > 1)
-            k10plus_to_k10plus_map[final_k10plus_ppn] = correct_substitution;
+            (*map_file) << (bsz_and_k10plus_ppns.second.record_type_ == MARC::Record::RecordType::AUTHORITY ? "authority:" : "title:")
+                        << k10plus_to_k10plus_map[final_k10plus_ppn] << "->" << correct_substitution << '\n';
     }
     LOG_INFO("Found " + std::to_string(k10plus_to_k10plus_map.size()) + " doubly mapped candidates.");
-
-    MapUtil::SerialiseMap(argv[3], k10plus_to_k10plus_map);
 
     return EXIT_SUCCESS;
 }
