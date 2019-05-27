9e9a725b1a8a2e10aa63c2fe01046a74a5e6960b
jriedl <johannes.riedl@uni-tuebingen.de>
2019-05-27 10:28:47 +0200

    Use scrolling and handle several entries

--- cpp/elasticsearch/import_es_fulltext_to_solr.sh
+++ cpp/elasticsearch/import_es_fulltext_to_solr.sh
@@ -8,6 +8,16 @@ SOLR_HOST="localhost"
 SOLR_PORT="8080"
 SOLR_INDEX="biblio"
 
+
+if [ $# != 1 ]; then
+    echo "usage: $0 --all"
+    echo "       $0 PPN"
+    echo "       Extract fulltext from elasticsearch and import into solr"
+    echo "       \"--all\" uses all entries in the elasticsearch full_text_cache"
+    exit 1
+fi
+
+
 function generate_match_query() {
 if [ $# == 1 ]; then
 PPN=$1
@@ -21,13 +31,43 @@ EOF
 fi
 }
 
-if [ $# != 1 ]; then
-    echo "usage: $0 --all"
-    echo "       $0 PPN"
-    echo "       Extract fulltext from elasticsearch and import into solr"
-    echo "       \"--all\" uses all entries in the elasticsearch full_text_cache"
-    exit 1
-fi
+function get_scroll_id() {
+   response=$@
+   echo $response | jq -r ._scroll_id
+}
+
+
+function get_hit_count() {
+   response=$@
+   echo $response | jq -r '.hits.hits | length'
+}
+
+
+function upload_fulltext_to_solr() {
+    response=$@
+    echo $response | jq  '[.hits.hits[]."_source"]  | . as $t | group_by(.id) | map({id:.[0].id, fulltext : { set : (map(.full_text) | join(" ")) } } )' | \
+        curl -s  http://$SOLR_HOST:$SOLR_PORT/solr/$SOLR_INDEX/update?commit=true -H "Content-Type: application/json" --data @-
+}
+
+
+function was_solr_successful() {
+    response=$@
+    status=$(echo $response | jq '.responseHeader.status')
+    echo $status
+}
+
+
+function solr_response_handler() {
+    response=$@
+    solr_success=$(was_solr_successful "$solr_response")
+    if [ "$solr_success" == "0" ]; then
+         echo "Successfully imported to SOLR"
+    else
+         echo "Import to SOLR not successful"
+         exit 1
+    fi;
+}
+
 
 if [ "$1" = "--all" ]; then
     PPN=''
@@ -35,11 +75,23 @@ else
     PPN=$1
 fi
 
-# We still handle potential chunking 
-# Extract the data from Elasticsearch, flatten it in case there are several chunks and tranform it to a valid Solr-Fulltext field and load it up to Solr
-curl -s -X GET -H "Content-Type: application/json" http://$ES_HOST:$ES_PORT/$ES_INDEX/_search/ --data '{ "query":'"$(generate_match_query $PPN)"'} }' | \
-jq  '[.hits.hits[]."_source"]  | . as $t | group_by(.id) | map({id:.[0].id, fulltext : { set : (map(.full_text) | join(" ")) } } )' | \
-curl http://$SOLR_HOST:$SOLR_PORT/solr/$SOLR_INDEX/update?commit=true -H "Content-Type: application/json" --data @-
+# Handle potential chunking
+# Extract the data from Elasticsearch, flatten it in case there are several chunks and transform it to a valid Solr-Fulltext field and load it up to Solr
+response=$(curl -s -X GET -H "Content-Type: application/json" 'http://'$ES_HOST:$ES_PORT/$ES_INDEX'/_search/?scroll=1m' --data '{ "query":'"$(generate_match_query $PPN)"'} }')
+scroll_id=$(get_scroll_id "$response")
+hit_count=$(get_hit_count "$response")
 
+#First iteration
+solr_response=$(upload_fulltext_to_solr "$response")
+solr_response_handler "$solr_response"
 
+# Continue until there are no further results
+while [ "$hit_count" != "0" ]; do
+    response=$(curl -s -X GET -H "Content-Type: application/json" 'http://'$ES_HOST:$ES_PORT'/_search/scroll' --data '{ "scroll" : "1m", "scroll_id": "'$scroll_id'" }')
+    scroll_id=$(get_scroll_id "$response")
+    hit_count=$(get_hit_count "$response")
+    solr_response=$(upload_fulltext_to_solr "$response")
+done
+
+echo "LEAVING"
 
