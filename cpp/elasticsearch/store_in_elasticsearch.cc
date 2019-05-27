76b1fb5c769854327221161d26e928b0cb798f61
jriedl <johannes.riedl@uni-tuebingen.de>
2019-05-27 09:40:52 +0200

    Some of the requested changes

--- cpp/elasticsearch/store_in_elasticsearch.cc
+++ cpp/elasticsearch/store_in_elasticsearch.cc
@@ -32,28 +32,35 @@ namespace {
 
 
 [[noreturn]] void Usage() {
-    ::Usage("fulltext_file1 --force-overwrite [fulltext_file2 .. fulltext_fileN]");
+    ::Usage("[--force-overwrite] [--verbose] fulltext_file1  [fulltext_file2 .. fulltext_fileN]");
 }
 
 
-bool ImportDocument(ControlNumberGuesser &control_number_guesser, FullTextCache * const full_text_cache, const bool force_overwrite, const std::string &filename) {
+bool ImportDocument(const ControlNumberGuesser &control_number_guesser, FullTextCache * const full_text_cache, 
+                    const std::string &filename, const bool force_overwrite=false, const bool verbose=false) 
+    {
     const auto input(FileUtil::OpenInputFileOrDie(filename));
     FullTextImport::FullTextData full_text_data;
     FullTextImport::ReadExtractedTextFromDisk(input.get(), &full_text_data);
 
     std::string ppn;
-    if (not FullTextImport::CorrelateFullTextData(control_number_guesser, full_text_data, &ppn))
+    if (not FullTextImport::CorrelateFullTextData(control_number_guesser, full_text_data, &ppn)) {
+        if (verbose)
+            LOG_INFO("Could not correlate data for file \"" + filename + "\"");
         return false;
+    }
     FullTextCache::Entry entry;
-    bool entry_present(full_text_cache->getEntry(ppn, &entry));
+    const bool entry_present(full_text_cache->getEntry(ppn, &entry));
     if (not force_overwrite and entry_present) {
-        LOG_INFO("Skip inserting PPN \"" + ppn + "\" since entry already present");
+        if (verbose)
+            LOG_INFO("Skip inserting PPN \"" + ppn + "\" since entry already present");
         return true;
     }
 
     if (entry_present)
         full_text_cache->deleteEntry(ppn);
     full_text_cache->insertEntry(ppn, full_text_data.full_text_, /* entry_urls = */{});
+    LOG_INFO("Inserted text from \"" + filename + "\" as entry for PPN \"" + ppn + "\"");
 
     return true;
 }
@@ -64,8 +71,15 @@ bool ImportDocument(ControlNumberGuesser &control_number_guesser, FullTextCache 
 
 int Main(int argc, char *argv[]) {
     bool force_overwrite(false);
-    if (argc > 2 and std::strcmp(argv[1], "--force-overwrite") == 0) {
+    bool verbose(false);
+    if (argc > 1 and std::strcmp(argv[1], "--force-overwrite") == 0) {
         force_overwrite = true;
+        --argc;
+        ++argv;
+    }
+
+    if (argc > 1 and std::strcmp(argv[1], "--verbose") == 0) {
+        verbose = true;
         --argc;
         ++argv;
     }
@@ -77,7 +91,7 @@ int Main(int argc, char *argv[]) {
     unsigned total_count(0), failure_count(0);
     for (int arg_no(1); arg_no < argc; ++arg_no) {
         ++total_count;
-        if (not ImportDocument(control_number_guesser, &full_text_cache, force_overwrite, argv[arg_no]))
+        if (not ImportDocument(control_number_guesser, &full_text_cache, argv[arg_no], force_overwrite, verbose))
             ++failure_count;
     }
 
