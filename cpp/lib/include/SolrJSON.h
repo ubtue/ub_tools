d3e077869df54aeb5c806168c88eb70d97c91661
jriedl <johannes.riedl@uni-tuebingen.de>
2019-05-28 17:28:46 +0200

    Utility functions for Solr Json results

---
+++ cpp/lib/include/SolrJSON.h
@@ -0,0 +1,34 @@
+/** \file   Solr.h
+ *  \brief  Utility functions for extracting common fields from a Solr JSON response
+ *  \author Johannes Riedl
+ *
+ *  \copyright 2019 Universitätsbibliothek Tübingen.  All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#pragma once
+
+#include <vector>
+#include <string>
+#include "JSON.h"
+
+namespace SolrJSON {
+
+void ParseTreeAndGetDocs(const std::string &json_results, std::shared_ptr<const JSON::ArrayNode> &docs);
+std::string GetId(const std::shared_ptr<const JSON::ObjectNode> &doc_obj);
+std::string GetTitle(const std::shared_ptr<const JSON::ObjectNode> &doc_obj);
+std::vector<std::string> GetAuthors(const std::shared_ptr<const JSON::ObjectNode> &doc_obj);
+std::string GetFirstPublishDate(const std::shared_ptr<const JSON::ObjectNode> &doc_obj);
+
+} // namespace SolrJSON
