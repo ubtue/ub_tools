5acc7521d32d92524f909176e6815dbf29c08361
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2019-07-18 16:44:11 +0200

    zts_harvester: add support for suppressing metadata fields

--- cpp/lib/include/Zotero.h
+++ cpp/lib/include/Zotero.h
@@ -213,6 +213,7 @@ struct SiteParams {
     std::set<std::string> expected_languages_;
     std::string expected_languages_text_fields_;
     bool force_automatic_language_detection_;
+    std::map<std::string, std::unique_ptr<RegexMatcher>> metadata_suppression_filters_;
     std::vector<std::string> additional_fields_;
     std::vector<std::string> non_standard_metadata_fields_;
     std::map<std::string, std::unique_ptr<RegexMatcher>> field_exclusion_filters_;
