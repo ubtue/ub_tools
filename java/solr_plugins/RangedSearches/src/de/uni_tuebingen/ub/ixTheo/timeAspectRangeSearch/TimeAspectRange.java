9369f0b0dc03bbdfc8a92b56c2a449963ffe4859
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2019-07-24 11:19:22 +0200

    Added support for ranged searched on time aspects.

---
+++ java/solr_plugins/RangedSearches/src/de/uni_tuebingen/ub/ixTheo/timeAspectRangeSearch/TimeAspectRange.java
@@ -0,0 +1,44 @@
+package de.uni_tuebingen.ub.ixTheo.timeAspectRangeSearch;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import de.uni_tuebingen.ub.ixTheo.rangeSearch.Range;
+
+
+public class TimeAspectRange extends Range {
+    final static int TIME_ASPECT_CODE_LENGTH = 12;
+
+    public TimeAspectRange(final String range) {
+        super(getLower(range), getUpper(range));
+    }
+
+    private static long getLower(final String range) {
+        if (range.length() == TIME_ASPECT_CODE_LENGTH + 1 + TIME_ASPECT_CODE_LENGTH) {
+            return Long.valueOf(range.substring(0, TIME_ASPECT_CODE_LENGTH));
+        } else {
+            return 0;
+        }
+    }
+
+    private static long getUpper(final String range) {
+        if (range.length() == TIME_ASPECT_CODE_LENGTH + 1 + TIME_ASPECT_CODE_LENGTH) {
+            return Long.valueOf(range.substring(TIME_ASPECT_CODE_LENGTH + 1, TIME_ASPECT_CODE_LENGTH + 1 + TIME_ASPECT_CODE_LENGTH));
+        } else {
+            return 999999999999L;
+        }
+    }
+
+    public static TimeAspectRange[] getRanges(final String[] ranges) {
+        final TimeAspectRange[] queryRanges = new TimeAspectRange[ranges.length];
+        for (int i = 0; i < queryRanges.length; ++i) {
+            queryRanges[i] = new TimeAspectRange(ranges[i]);
+        }
+        return queryRanges;
+    }
+
+    public static TimeAspectRange[] getRanges(final String input, String separator) {
+        final String[] fields = input.split(separator);
+        return getRanges(fields);
+    }
+}
