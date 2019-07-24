c66564591507634fe57ee64767232ffe572d180e
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2019-07-24 13:39:54 +0200

    Fixed copyright notice.

--- cpp/lib/include/RangeUtil.h
+++ cpp/lib/include/RangeUtil.h
@@ -6,19 +6,18 @@
 /*
  *  Copyright 2014-2019 Universitätsbibliothek Tübingen.
  *
- *  The libiViaCore package is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License as
- *  published by the Free Software Foundation; either version 2 of the License,
- *  or (at your option) any later version.
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
  *
- *  libiViaCore is distributed in the hope that it will be useful,
+ *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ *  GNU Affero General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with libiViaCore; if not, write to the Free Software Foundation, Inc.,
- *  59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 #pragma once
 
