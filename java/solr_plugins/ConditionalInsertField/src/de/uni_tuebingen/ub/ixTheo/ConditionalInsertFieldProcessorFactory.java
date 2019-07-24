37de3943dfc711c6b6662d18909efed05de1b1fe
jriedl <johannes.riedl@uni-tuebingen.de>
2019-07-24 10:28:17 +0200

    Solr plugin for the conditional insertion of fields

---
+++ java/solr_plugins/ConditionalInsertField/src/de/uni_tuebingen/ub/ixTheo/ConditionalInsertFieldProcessorFactory.java
@@ -0,0 +1,45 @@
+package de.uni_tuebingen.ub.ixTheo.conditionalInsertField;
+
+import java.io.IOException;
+
+import org.apache.solr.common.SolrInputDocument;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.response.SolrQueryResponse;
+import org.apache.solr.update.AddUpdateCommand;
+import org.apache.solr.update.processor.UpdateRequestProcessor;
+import org.apache.solr.update.processor.UpdateRequestProcessorFactory;
+
+
+public class ConditionalInsertFieldProcessorFactory extends UpdateRequestProcessorFactory
+{
+    @Override
+    public UpdateRequestProcessor getInstance(SolrQueryRequest req, SolrQueryResponse rsp, UpdateRequestProcessor next)
+    {
+      return new ConditionalInsertProcessor(next);
+    }
+}
+
+
+class ConditionalInsertProcessor extends UpdateRequestProcessor
+{
+    public ConditionalInsertProcessor( UpdateRequestProcessor next) {
+      super( next );
+    }
+  
+    
+    @Override
+    public void processAdd(AddUpdateCommand cmd) throws IOException {
+        addHasFulltext(cmd);
+        // pass it up the chain
+        super.processAdd(cmd);
+    }
+
+
+    // Add has_fulltext flag fi fulltext is present
+    protected void addHasFulltext(AddUpdateCommand cmd) throws IOException {
+        SolrInputDocument doc = cmd.getSolrInputDocument();
+        String fulltext = (String) doc.getFieldValue("fulltext");
+        if (fulltext != null && !fulltext.isEmpty())
+            doc.addField("has_fulltext", "true");
+    }
+}
