3db97a7944e492f5c52891a931912d4001501967
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2019-07-24 13:38:08 +0200

    Removed incorrect statement.

--- cpp/lib/include/RangeUtil.h
+++ cpp/lib/include/RangeUtil.h
@@ -6,8 +6,6 @@
 /*
  *  Copyright 2014-2019 Universitätsbibliothek Tübingen.
  *
- *  This file is part of the libiViaCore package.
- *
  *  The libiViaCore package is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License as
  *  published by the Free Software Foundation; either version 2 of the License,
