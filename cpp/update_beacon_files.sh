fdc0d07474a966ff3c1f4b8c0e1ce6a767f5d17b
Mario Trojan <mario.trojan@uni-tuebingen.de>
2019-07-30 13:08:00 +0200

    update_beacon_files: replace GND-BEACON with BEACON in kalliope header

--- cpp/update_beacon_files.sh
+++ cpp/update_beacon_files.sh
@@ -3,5 +3,7 @@
 set -o errexit -o nounset
 
 cd /usr/local/ub_tools/bsz_daten
-wget http://kalliope.staatsbibliothek-berlin.de/beacon/beacon.txt -O kalliope.staatsbibliothek-berlin.beacon
 wget https://labs.ddb.de/app/beagen/item/213 -O archivportal-d.beacon
+wget http://kalliope.staatsbibliothek-berlin.de/beacon/beacon.txt -O kalliope.staatsbibliothek-berlin.beacon
+
+sed -i -e 's/#FORMAT: GND-BEACON/#FORMAT: BEACON/g' kalliope.staatsbibliothek-berlin.beacon
