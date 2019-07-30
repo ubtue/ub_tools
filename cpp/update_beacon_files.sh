2ef9a2d79be745ad9ac0b8bd1c8e399a3355771e
Mario Trojan <mario.trojan@uni-tuebingen.de>
2019-07-30 11:58:12 +0200

    changed archivportal-d beacon url

--- cpp/update_beacon_files.sh
+++ cpp/update_beacon_files.sh
@@ -4,4 +4,4 @@ set -o errexit -o nounset
 
 cd /usr/local/ub_tools/bsz_daten
 wget http://kalliope.staatsbibliothek-berlin.de/beacon/beacon.txt -O kalliope.staatsbibliothek-berlin.beacon
-wget https://www.archivportal-d.de/static/de/beacon-archivportal-persons.txt -O archivportal-d.beacon
+wget https://labs.ddb.de/app/beagen/item/213 -O archivportal-d.beacon
