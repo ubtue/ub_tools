a2a3fc3c9633fb467956be8c0d828662cc4fdf75
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2019-07-31 13:51:37 +0200

    Added a bogus 008 field.

--- cpp/pipelines/create_literary_remains_records.cc
+++ cpp/pipelines/create_literary_remains_records.cc
@@ -98,7 +98,8 @@ void AppendLiteraryRemainsRecords(
                                 "LR" + StringUtil::ToString(++creation_count, /* base = */10, /* width= */6, /* padding_char = */'0'));
         const std::string &author_name(gnd_numbers_and_literary_remains_infos.second.front().author_name_);
         new_record.insertField("003", "PipeLineGenerated");
-        new_record.insertField("005", TimeUtil::GetCurrentDateAndTime("%Y%m%d%H:%M:%S") + ".0");
+        new_record.insertField("005", TimeUtil::GetCurrentDateAndTime("%Y%m%d%H%M%S") + ".0");
+        new_record.insertField("008", "190606s2019    xx |||||      00| ||ger c");
         new_record.insertField("100", { { 'a', author_name }, { '0', "(DE-588)" + gnd_numbers_and_literary_remains_infos.first } });
         new_record.insertField("245", { { 'a', "Nachlass von " + author_name } });
 
