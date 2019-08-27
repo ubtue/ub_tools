267cd2dac4a23d2cc69d7fa01c3ed9a664221766
jriedl <johannes.riedl@uni-tuebingen.de>
2019-08-27 15:58:11 +0200

    Adjust function names

--- cpp/elasticsearch/extract_existing_fulltext_ids.sh
+++ cpp/elasticsearch/extract_existing_fulltext_ids.sh
@@ -6,25 +6,25 @@ ES_CONFIG_FILE="/usr/local/var/lib/tuelib/Elasticsearch.conf"
 ES_HOST_AND_PORT=$(inifile_lookup $ES_CONFIG_FILE Elasticsearch host)
 ES_INDEX=$(inifile_lookup $ES_CONFIG_FILE Elasticsearch index)
 
-function get_scroll_id() {
+function GetScrollId() {
    response=$@
    echo $response | jq -r ._scroll_id
 }
 
 
-function get_hit_count() {
+function GetHitCount() {
    response=$@
    echo $response | jq -r '.hits.hits | length'
 }
 
 
-function obtain_ids() {
+function ObtainIds() {
     response=$@
     echo $response | jq  '[.hits.hits[]."_source"]  | . as $t | group_by(.id) | map(.[0].id)' 
 }
 
 
-function get_download_stats() {
+function GetDownloadStats() {
     response=$@
     echo "------------------------------------" >&2
     echo -n "Received " >&2
@@ -47,25 +47,25 @@ id_output_file=$1
 
 # Handle potential chunking
 response=$(curl -s -X GET -H "Content-Type: application/json" $ES_HOST_AND_PORT/$ES_INDEX'/_search/?scroll=1m' --data '{ "_source": ["id"], "size" : 1000, "query":{ "match_all": {} } }')
-get_download_stats "$response"
-scroll_id=$(get_scroll_id "$response")
-hit_count=$(get_hit_count "$response")
+GetDownloadStats "$response"
+scroll_id=$(GetScrollId "$response")
+hit_count=$(GetHitCount "$response")
 
 #First iteration
-id_arrays=$(obtain_ids "$response")
+id_arrays=$(ObtainIds "$response")
 
 # Continue until there are no further results
 while [ "$hit_count" != "0" ]; do
     echo "Obtaining batch with "$hit_count" items"
     response=$(curl -s -X GET -H "Content-Type: application/json" $ES_HOST_AND_PORT'/_search/scroll' --data '{ "scroll" : "1m", "scroll_id": "'$scroll_id'" }')
-    get_download_stats "$response"
-    scroll_id=$(get_scroll_id "$response")
-    hit_count=$(get_hit_count "$response")
+    GetDownloadStats "$response"
+    scroll_id=$(GetScrollId "$response")
+    hit_count=$(GetHitCount "$response")
     if [ "$hit_count" == "0" ]; then
         echo "Finished obtaining results set"
         break;
     fi
-    id_arrays+=$(obtain_ids "$response")
+    id_arrays+=$(ObtainIds "$response")
 done
 echo "Flatten and deduplicate the scroll results and write them to file $id_output_file"
 echo $id_arrays | jq -s add | sed -e 's/[][", ]//g' | sed  '/^$/d' | sort | uniq > "$id_output_file"
