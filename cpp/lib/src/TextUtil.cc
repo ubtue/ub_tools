bc704741a5a9486e1eb130926e52768bdfd809d8
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2019-09-11 13:55:14 +0200

    Added new utility function.

--- cpp/lib/src/TextUtil.cc
+++ cpp/lib/src/TextUtil.cc
@@ -1782,5 +1782,18 @@ bool ConvertToUTF8(const std::string &encoding, const std::string &text, std::st
 }
 
 
+bool ConsistsEntirelyOfLetters(const std::string &utf8_string) {
+    std::wstring wstring;
+    if (not UTF8ToWCharString(utf8_string, &wstring))
+        return false;
+
+    for (const auto wch : wstring) {
+        if (not std::iswalpha(wch))
+            return false;
+    }
+
+    return true;
+}
+
 
 } // namespace TextUtil
