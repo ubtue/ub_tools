829dcd4272d24472745ab9f2358b2dc7ecd8ceaa
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2019-10-02 16:23:20 +0200

    Zotero: do not generate 877 if the author has neither a PPN nor a GND number

--- cpp/lib/src/Zotero.cc
+++ cpp/lib/src/Zotero.cc
@@ -570,8 +570,10 @@ void MarcFormatHandler::generateMarcRecord(MARC::Record * const record, const st
         else
             record->insertField("700", subfields, /* indicator 1 = */'1');
 
-        const std::string _887_data("Autor in der Zoterovorlage [" + creator->last_name_ + ", " + creator->first_name_ + "] maschinell zugeordnet");
-        record->insertField("887", { { 'a', _887_data }, { '2', "ixzom" } });
+        if (not creator->ppn_.empty() or not creator->gnd_number_.empty()) {
+            const std::string _887_data("Autor in der Zoterovorlage [" + creator->last_name_ + ", " + creator->first_name_ + "] maschinell zugeordnet");
+            record->insertField("887", { { 'a', _887_data }, { '2', "ixzom" } });
+        }
 
         --num_creators_left;
     }
