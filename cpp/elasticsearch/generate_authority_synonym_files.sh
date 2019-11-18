3104516901879b03a786b5cabf728fe2de4d1c9a
jriedl <johannes.riedl@uni-tuebingen.de>
2019-11-18 14:06:33 +0100

    Some syntactic changes

--- cpp/elasticsearch/generate_authority_synonym_files.sh
+++ cpp/elasticsearch/generate_authority_synonym_files.sh
@@ -1,36 +1,35 @@
+#!/bin/bash
 # Extract authority synonyms from translator database and create synonym
 # files usable in Solr and Elasticsearch
-#!/bin/bash
 
-config_file="/usr/local/var/lib/tuelib/translations.conf"
-db=$(inifile_lookup ${config_file} "Database" "sql_username")
-db_user=$(inifile_lookup ${config_file} "Database" "sql_username")
-db_password=$(inifile_lookup ${config_file} "Database" "sql_password")
-outdir="/tmp/synonyms"
+CONFIG_FILE="/usr/local/var/lib/tuelib/translations.conf"
+DB=$(inifile_lookup ${CONFIG_FILE} "Database" "sql_username")
+DB_USER=$(inifile_lookup ${CONFIG_FILE} "Database" "sql_username")
+DB_PASSWORD=$(inifile_lookup ${CONFIG_FILE} "Database" "sql_password")
+OUTDIR="/tmp/synonyms"
 
-mkdir -p ${outdir}
+mkdir -p ${OUTDIR}
 
 declare -A langmap=(["eng"]="en" ["fre"]="fr" ["ger"]="de" ["gre"]="el"  ["ita"]="it"  ["por"]="pt" ["rus"]="ru" ["spa"]="es")
 
 function GetGermanSynonyms {
-    lang='ger'
-    sshpass -p ${db_password} mysql --user=${db_user} --password --skip-column-names ${db} <<EOF
-    SELECT GROUP_CONCAT(translation) FROM keyword_translations WHERE language_code='${lang}'
-    AND gnd_code IN (SELECT gnd_code FROM keyword_translations WHERE language_code='${lang}' AND status='reliable') GROUP BY gnd_code;
+    sshpass -p ${DB_PASSWORD} mysql --user=${DB_USER} --password --skip-column-names ${DB} <<EOF
+    SELECT GROUP_CONCAT(translation) FROM keyword_translations WHERE language_code='ger'
+        AND gnd_code IN (SELECT gnd_code FROM keyword_translations WHERE language_code='ger' AND status='reliable') GROUP BY gnd_code;
 EOF
 }
 
 
 function GetSynonymsForLanguage {
    lang=${1}
-   sshpass -p "${db_password}" mysql --user="${db_user}" --password --skip-column-names ${db} <<EOF
+   sshpass -p "${DB_PASSWORD}" mysql --user="${DB_USER}" --password --skip-column-names ${DB} <<EOF
    SELECT GROUP_CONCAT(translation) FROM keyword_translations WHERE language_code='${lang}' GROUP BY gnd_code;
 EOF
 }
 
 
 function GetAllSynonyms {
-   sshpass -p "${db_password}" mysql --user="${db_user}" --password --skip-column-names ${db} <<EOF
+   sshpass -p "${DB_PASSWORD}" mysql --user="${DB_USER}" --password --skip-column-names ${DB} <<EOF
    SELECT GROUP_CONCAT(translation) FROM keyword_translations GROUP BY gnd_code;
 EOF
 }
@@ -46,26 +45,23 @@ function CleanUp {
    sed --in-place --regexp-extended --expression 's/#/, /g' $file
 }
 
+
 #German synonyms
-outfile_de=${outdir}/synonyms_de.txt
-echo "Creating ${outfile_de}"
-GetGermanSynonyms > ${outfile_de}
+OUTFILE_DE=${OUTDIR}/synonyms_de.txt
+echo "Creating ${OUTFILE_DE}"
+GetGermanSynonyms > ${OUTFILE_DE}
 
 
 #Synonyms for translated languages
 for lang in "eng" "fre" "gre" "ita" "por" "rus" "spa"; do
-   outfile=${outdir}/synonyms_${langmap[$lang]}.txt
-   echo "Creating ${outfile}"
-   GetSynonymsForLanguage $lang > ${outfile}
-   CleanUp ${outfile}
+   OUTFILE=${OUTDIR}/synonyms_${langmap[$lang]}.txt
+   echo "Creating ${OUTFILE}"
+   GetSynonymsForLanguage $lang > ${OUTFILE}
+   CleanUp ${OUTFILE}
 done
 
 #All Synonyms
-outfile_all="${outdir}/synonyms_all.txt"
-echo "Creating ${outfile_all}"
-GetAllSynonyms > ${outfile_all}
-CleanUp ${outfile_all}
-
-
-
-
+OUTFILE_ALL="${OUTDIR}/synonyms_all.txt"
+echo "Creating ${OUTFILE_ALL}"
+GetAllSynonyms > ${OUTFILE_ALL}
+CleanUp ${OUTFILE_ALL}
