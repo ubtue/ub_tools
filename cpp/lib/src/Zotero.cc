9a8169de65165ed2cb99a5f14958cbd21fb82c0a
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2019-11-27 11:04:36 +0100

    Zotero: update 655 tag for reviews

--- cpp/lib/src/Zotero.cc
+++ cpp/lib/src/Zotero.cc
@@ -620,8 +620,10 @@ void MarcFormatHandler::generateMarcRecord(MARC::Record * const record, const st
     }
 
     // Review-specific modifications
-    if (item_type == "review")
-        record->insertField("655", { { 'a', "!106186019!" }, { '0', "(DE-588)" } }, /* indicator1 = */' ', /* indicator2 = */'7');
+    if (item_type == "review") {
+        record->insertField("655", { { 'a', "!106186019!" }, { '0', "(DE-588)" }, { '2', "gnd-content" } },
+                            /* indicator1 = */' ', /* indicator2 = */'7');
+    }
 
     // License data
     const std::string license(node_parameters.license_);
