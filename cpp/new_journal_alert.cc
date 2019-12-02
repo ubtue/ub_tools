950d7c3d77ecb44efb8e69d9dae15fbedf24efad
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2019-12-02 10:37:15 +0100

    Even better!

--- cpp/new_journal_alert.cc
+++ cpp/new_journal_alert.cc
@@ -88,18 +88,8 @@ public:
 };
 
 
-inline bool ConsistsOfAllASCIIDigits(const std::string &s) {
-    for (const char ch : s) {
-        if (not StringUtil::IsDigit(ch))
-            return false;
-    }
-
-    return true;
-}
-
-
 bool StartPageLessThan(const std::string &start_page1, const std::string &start_page2) {
-    if (not ConsistsOfAllASCIIDigits(start_page1) or not ConsistsOfAllASCIIDigits(start_page2))
+    if (not StringUtil::ConsistsOfAllASCIIDigits(start_page1) or not StringUtil::ConsistsOfAllASCIIDigits(start_page2))
         return start_page1 < start_page2; // Arbitrary but consistent.
 
     return StringUtil::ToUnsigned(start_page1) < StringUtil::ToUnsigned(start_page2);
@@ -113,7 +103,7 @@ bool NewIssueInfo::operator<(const NewIssueInfo &rhs) const {
         return true;
     if ((not year_.empty() or not rhs.year_.empty()) and year_ < rhs.year_)
         return true;
-    if ((not start_page_.empty() or not rhs.start_page_.empty()) and StartPageLessThan(start_page_, rhs.start_page_))
+    if (StartPageLessThan(start_page_, rhs.start_page_))
         return true;
 
     return false;
