97b8d25bfe847c313c1ac29b6bfa47ab63dfb601
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2019-12-18 11:18:29 +0100

    More fixes and documentation

--- cpp/lib/include/ZoteroHarvesterUtil.h
+++ cpp/lib/include/ZoteroHarvesterUtil.h
@@ -39,18 +39,30 @@
 namespace ZoteroHarvester {
 
 
+// This namespace contains classes that provide the necessary primitives to implement
+// scalable harvesting of metadata using the Zotero Translation Server.
 namespace Util {
 
 
 class HarvestableItemManager;
 
 
+// Represents a URI that contains one or more harvestable resources. All operations are
+// keyed to a specific instance of this class. It holds the relevant contextual information
+// about the resource it points to (such as its parent journal and its respective parameters).
+// It also doubles as a unique handle that can used to track and sort operations that are
+// executing concurrently.
 struct HarvestableItem {
     friend class HarvestableItemManager;
 
-    // Sortable unique ID that indicates the position of the harvestable item in a specific journal's harvest queue
+    // Sortable unique ID that indicates the position of the harvestable item in
+    // a specific journal's harvest queue.
     const unsigned id_;
+
+    // URL pointing to the harvestable resource.
     const Url url_;
+
+    // Journal to which this harvestable resource belongs.
     const Config::JournalParams &journal_;
 private:
     HarvestableItem(const unsigned id, const std::string &url, const Config::JournalParams &journal)
@@ -65,6 +77,9 @@ public:
 };
 
 
+// Allocates HarvestableItem instances whilst ensuring that there are collisions of
+// unique IDs between different journals. The IDs provide a way to preserve the relative
+// ordering of asynchronously executing operations.
 class HarvestableItemManager {
     std::unordered_map<const Config::JournalParams *, ThreadUtil::ThreadSafeCounter<unsigned>> counters_;
 public:
@@ -108,7 +123,14 @@ namespace Util {
 struct TaskletContext;
 
 
-// Wrapper around the default logger to facilitate order-preserving logging in multi-threaded Zotero Harvester contexts.
+// Wrapper around the default logger that facilitates order-preserving logging in multi-threaded contexts.
+// Ensures that given a specific asynchronous operation/task, the ordering of log statements is preserved.
+// This is achieved by tracking active asynchronous contexts and accumulating messages in separate buffers.
+// When a context deregisters itself from the logger, its buffer is flushed to stdout.
+//
+// The error, warning, info and debug member function overrides attempt to obtain the calling thread's
+// TaskletContext. When found, the message is written directly to the context's buffer. When not, it's
+// written directly to stdout.
 class ZoteroLogger : public ::Logger {
     struct ContextData {
         static constexpr unsigned BUFFER_SIZE = 64 * 1024;
@@ -117,8 +139,9 @@ class ZoteroLogger : public ::Logger {
         std::string buffer_;
     public:
         explicit ContextData(const Util::HarvestableItem &item) : item_(item)
-         { buffer_.reserve(static_cast<size_t>(BUFFER_SIZE)); }
+            { buffer_.reserve(static_cast<size_t>(BUFFER_SIZE)); }
     };
+
 
     std::unordered_map<HarvestableItem, ContextData> active_contexts_;
     std::recursive_mutex active_context_mutex_;
@@ -136,10 +159,14 @@ public:
     void popContext(const Util::HarvestableItem &context_item);
 
     // Replaces the global logger instance with one of this class
+    // so that all LOG_XXX calls are routed through it.
     static void Init();
  };
 
 
+// Represents the context of an asynchronous operation/task. Each operation has an
+// associated HarvestableItem upon which the it is performed and a human-readable
+// description of the (type of) operation itself.
 struct TaskletContext {
     const HarvestableItem associated_item_;
     const std::string description_;
@@ -149,6 +176,10 @@ public:
 };
 
 
+// Used to associate an asynchronous operation's context (TaskletContext)
+// to the underlying thread that hosts it. A global key that is used to
+// allocate memory in each executing thread's TLS (thread-local segment).
+// The memory is used to store a pointer to the corresponding TaskletContext instance.
 class TaskletContextManager {
     pthread_key_t tls_key_;
 public:
@@ -166,15 +197,25 @@ extern const TaskletContextManager TASKLET_CONTEXT_MANAGER;
 template <typename Parameter, typename Result> class Future;
 
 
+// Base class of all asynchronous operations. Provides an interface to spin up
+// a new thread of execution and run arbitrary code on it. Tasklets are self-contained
+// in that they host their own copy of inputs and outputs and maintain their own state.
 template <typename Parameter, typename Result>
 class Tasklet {
     friend class Future<Parameter, Result>;
 public:
+    // Determines how the final result is delivered to the user of the class.
+    // YIELD causes the tasklet instance to relinquish ownership of the result to
+    // the user, while COPY returns a copy of the computed result.
     enum ResultPolicy { YIELD, COPY };
 private:
     enum Status { NOT_STARTED, RUNNING, COMPLETED_SUCCESS, COMPLETED_ERROR };
 
 
+    // The thread routine that gets executed once the tasklet is started.
+    // The single parameter points to the calling Tasklet instance. The user
+    // of the Tasklet class must ensure that instance pointed to is valid
+    // until the thread routine returns.
     static void *ThreadRoutine(void * parameter);
 
 
@@ -182,16 +223,26 @@ private:
     pthread_t thread_id_;
     mutable std::mutex mutex_;
     Status status_;
+    // Incremented by one for the duration of the task.
     ThreadUtil::ThreadSafeCounter<unsigned> * const running_instance_counter_;
+
+    // Functor that executes the actual payload code.
     std::function<void(const Parameter &, Result * const)> runnable_;
+
     std::unique_ptr<const Parameter> parameter_;
     std::unique_ptr<Result> result_;
     ResultPolicy result_policy_;
 
+    // Some SFINAE/tag-dispatching magic to apply the constraint that the return
+    // type must be copy constructable if the ResultPolicy is COPY.
+    // For copy-constructable classes
     inline Result *getResultImpl(std::true_type)
         { return new Result(*result_); }
+
+    // For non-copy-constructable classes
     inline Result *getResultImpl(std::false_type)
         { return nullptr; }
+
     inline void setStatus(const Status new_status) {
         std::lock_guard<std::mutex> lock(mutex_);
         status_ = new_status;
@@ -202,8 +253,11 @@ public:
             std::unique_ptr<Result> default_result, std::unique_ptr<Parameter> parameter, const ResultPolicy result_policy);
     virtual ~Tasklet();
 public:
+    // Spins up a new thread and executes the payload.
     void start();
-    inline pthread_t getID() const { return thread_id_; }
+
+    inline pthread_t getID() const
+        { return thread_id_; }
     inline Status getStatus() const {
         std::lock_guard<std::mutex> lock(mutex_);
         return status_;
@@ -212,23 +266,32 @@ public:
         std::lock_guard<std::mutex> lock(mutex_);
         return status_ == Status::COMPLETED_SUCCESS or status_ == Status::COMPLETED_ERROR;
     }
-    inline const Parameter &getParameter() const { return *parameter_.get(); }
+    inline const Parameter &getParameter() const
+        { return *parameter_.get(); }
+
+    // Returns the result based on the ResultPolicy. If the tasklet is complete, returns
+    // immediately. Otherwise, blocks the calling thread until the tasklet has run to completion.
     std::unique_ptr<Result> getResult();
+
+    // Blocks the calling thread until the task has run to completion.
     void await();
 };
 
 
+// Used for debugging.
 extern ThreadUtil::ThreadSafeCounter<unsigned> tasklet_instance_counter;
 
 
 template<typename Parameter, typename Result> void *Tasklet<Parameter, Result>::ThreadRoutine(void * parameter) {
-    // the user of the Tasklet class must ensure that the tasklet instances pointed to by 'parameter'
-    // is valid until the thread routine returns
     Tasklet<Parameter, Result> * const tasklet(reinterpret_cast<Tasklet<Parameter, Result> *>(parameter));
     const auto zotero_logger(dynamic_cast<ZoteroLogger *>(::logger));
 
     pthread_setname_np(pthread_self(), tasklet->context_.description_.c_str());
+    // Store the tasklet context in the thread-local data segment.
+    // We do not need to worry about cleaning it up as the context will
+    // be automatically released when the tasklet gets destroyed.
     TASKLET_CONTEXT_MANAGER.setThreadLocalContext(tasklet->context_);
+    // Register the tasklet context with the logger to track messages from this thread.
     zotero_logger->pushContext(tasklet->context_.associated_item_);
     ++(*tasklet->running_instance_counter_);
 
@@ -246,10 +309,11 @@ template<typename Parameter, typename Result> void *Tasklet<Parameter, Result>::
         completion_status = Status::COMPLETED_ERROR;
     }
 
+    // Deregister the tasklet context and flush its log messages.
     zotero_logger->popContext(tasklet->context_.associated_item_);
     --(*tasklet->running_instance_counter_);
 
-    // flagged at the very end of the routine to prevent races
+    // Flagged at the very end of the routine to prevent data races.
     tasklet->setStatus(completion_status);
     pthread_exit(nullptr);
 }
@@ -319,7 +383,7 @@ template<typename Parameter, typename Result> std::unique_ptr<Result> Tasklet<Pa
 
 
 template<typename Parameter, typename Result> void Tasklet<Parameter, Result>::await() {
-    // wait until the tasklet has started, as the thread won't be allocated until then
+    // Wait until the tasklet has started, as the thread won't be allocated until then.
     while (getStatus() == Status::NOT_STARTED)
         ::usleep(32 * 1000);
 
@@ -334,9 +398,12 @@ template<typename Parameter, typename Result> void Tasklet<Parameter, Result>::a
 }
 
 
+// Used for debugging.
 extern ThreadUtil::ThreadSafeCounter<unsigned> future_instance_counter;
 
 
+// Wrapper around a Tasklet that can be passed around in place of its result. Once the
+// tasklet has run to completion, the Future can be used to retrieve its result.
 template <typename Parameter, typename Result>
 class Future {
     enum Status { WAITING, NO_RESULT, HAS_RESULT };
@@ -351,9 +418,15 @@ public:
     Future(const Future<Parameter, Result> &rhs) = delete;
 
     bool isComplete() const;
+
+    // Returns false if the tasklet encountered an error, true otherwise.
     bool hasResult() const;
+
+    // Returns the result of the tasklet. Will block if the task is still running.
     Result &getResult();
-    inline const Parameter &getParameter() const { return source_tasklet_->getParameter(); }
+
+    inline const Parameter &getParameter() const
+        { return source_tasklet_->getParameter(); }
 };
 
 
@@ -432,14 +505,11 @@ public:
     bool urlAlreadyDelivered(const std::string &url, Entry * const entry = nullptr) const;
     bool hashAlreadyDelivered(const std::string &hash, Entry * const entry = nullptr) const;
 
-    /** \brief Lists all journals that haven't had a single URL delivered for a given number of days.
-     *  \return The number of outdated journals.
-     */
+    // Lists all journals that haven't had a single URL delivered for a given number of days.
     size_t listOutdatedJournals(const unsigned cutoff_days, std::unordered_map<std::string, time_t> * const outdated_journals) const;
 
-    /** \brief Returns when the last URL of the given journal was delivered to the BSZ.
-     *  \return Timestamp of the last delivery if found, TimeUtil::BAD_TIME_T otherwise.
-     */
+    // Returns when the last URL of the given journal was delivered to the BSZ. If found,
+    // returns the timestamp of the last delivery, TimeUtil::BAD_TIME_T otherwise.
     time_t getLastUploadTime(const std::string &journal_name) const;
 };
 
