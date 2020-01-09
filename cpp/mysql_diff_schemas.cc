70ea1d78da7725d09fedb6ce2ba8d2ce155a44ac
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2020-01-09 09:56:01 +0100

    New tool for chomparing MySQL schemas.

---
+++ cpp/mysql_diff_schemas.cc
@@ -0,0 +1,143 @@
+/** \file mysql_diff_schemas.cc
+ *  \brief A tool for listing the differences between two schemas.
+ *  \author Dr. Johannes Ruscheinski (johannes.ruscheinski@uni-tuebingen.de)
+ *
+ *  \copyright 2019 Universitätsbibliothek Tübingen.  All rights reserved.
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+#include <algorithm>
+#include <iostream>
+#include <map>
+#include <set>
+#include <vector>
+#include <cstdlib>
+#include "FileUtil.h"
+#include "StringUtil.h"
+#include "util.h"
+
+
+std::string ExtractBackQuotedString(const std::string &s) {
+    auto ch(s.cbegin());
+    if (unlikely(*ch != '`'))
+        LOG_ERROR("\"" + s + "\" does not start with a backtick!");
+    ++ch;
+
+    std::string extracted_string;
+    while (ch != s.cend() and *ch != '`')
+        extracted_string += *ch++;
+    if (unlikely(ch == s.cend()))
+        LOG_ERROR("\"" + s + "\" does not end with a backtick!");
+
+    return extracted_string;
+}
+
+
+inline bool SchemaLineIsLessThan(const std::string &line1, const std::string &line2) {
+    if ((*line1.c_str() == '`') != (*line2.c_str() == '`'))
+        return *line1.c_str() == '`';
+
+    return line1 < line2;
+}
+
+
+void LoadSchema(const std::string &filename, std::map<std::string, std::vector<std::string>> * const table_name_to_schema_map) {
+    std::string current_table;
+    std::vector<std::string> current_schema;
+    for (auto line : FileUtil::ReadLines(filename)) {
+        StringUtil::Trim(&line);
+        if (StringUtil::StartsWith(line, "CREATE TABLE ")) {
+            if (not current_table.empty()) {
+                std::sort(current_schema.begin(), current_schema.end(), SchemaLineIsLessThan);
+                (*table_name_to_schema_map)[current_table] = current_schema;
+            }
+            current_table = ExtractBackQuotedString(line.substr(__builtin_strlen("CREATE TABLE ")));
+            current_schema.clear();
+        } else
+            current_schema.emplace_back(line);
+    }
+}
+
+
+class StartsWithColumnName {
+    std::string quoted_reference_column_name_;
+public:
+    explicit StartsWithColumnName(const std::string &reference_column_name)
+        : quoted_reference_column_name_("`" + reference_column_name + "`") { }
+    inline bool operator ()(const std::string &line) { return StringUtil::StartsWith(line, quoted_reference_column_name_); }
+};
+
+
+void DiffSchemas(const std::map<std::string, std::vector<std::string>> &table_name_to_schema_map1,
+                 const std::map<std::string, std::vector<std::string>> &table_name_to_schema_map2)
+{
+    std::set<std::string> already_processed_table_names;
+    for (const auto &table_name1_and_schema1 : table_name_to_schema_map1) {
+        const auto &table_name1(table_name1_and_schema1.first);
+        already_processed_table_names.emplace(table_name1);
+        const auto table_name2_and_schema2(table_name_to_schema_map2.find(table_name1));
+        if (table_name2_and_schema2 == table_name_to_schema_map2.cend()) {
+            std::cout << "Table was deleted: " << table_name1 << '\n';
+            continue;
+        }
+
+        const auto &schema1(table_name1_and_schema1.second);
+        const auto &schema2(table_name2_and_schema2->second);
+
+        // Compare column definitions first:
+        const auto last_column_def1(std::find_if(schema1.crbegin(), schema1.crend(),
+                                                 [](const std::string &line){ return not line.empty() and line[0] == '`'; }));
+        const auto last_column_def2(std::find_if(schema2.crbegin(), schema2.crend(),
+                                                 [](const std::string &line){ return not line.empty() and line[0] == '`'; }));
+
+        std::set<std::string> already_processed_column_names;
+        for (auto column_def1(schema1.cbegin()); column_def1 != last_column_def1.base(); ++column_def1) {
+            const auto column_name1(ExtractBackQuotedString(*column_def1));
+            already_processed_column_names.emplace(column_name1);
+            const auto column_def2(std::find_if(schema2.cbegin(), last_column_def2.base(), StartsWithColumnName(column_name1)));
+            if (column_def2 == last_column_def2.base())
+                std::cout << "Column was deleted: " << table_name1 << '.' << column_name1 << '\n';
+            else if (*column_def1 != *column_def2)
+                std::cout << "Column definition changed: " << *column_def1 << " -> " << *column_def2 << '\n';
+        }
+
+        for (auto column_def2(schema2.cbegin()); column_def2 != last_column_def2.base(); ++column_def2) {
+            const auto column_name2(ExtractBackQuotedString(*column_def2));
+            if (already_processed_column_names.find(column_name2) == already_processed_column_names.cend())
+                std::cout << "Column was added: " << table_name1 << '.' << column_name2 << '\n';
+        }
+    }
+
+    for (const auto &table_name2_and_schema2 : table_name_to_schema_map2) {
+        const auto &table_name2(table_name2_and_schema2.first);
+        if (already_processed_table_names.find(table_name2) == already_processed_table_names.cend())
+            std::cout << "Table was added: " << table_name2 << '\n';
+    }
+}
+
+
+int Main(int argc, char *argv[]) {
+    if (argc != 3)
+        ::Usage("schema1 schema2");
+
+    std::map<std::string, std::vector<std::string>> table_name_to_schema_map1;
+    LoadSchema(argv[1], &table_name_to_schema_map1);
+
+    std::map<std::string, std::vector<std::string>> table_name_to_schema_map2;
+    LoadSchema(argv[2], &table_name_to_schema_map2);
+
+    DiffSchemas(table_name_to_schema_map1, table_name_to_schema_map2);
+
+    return EXIT_SUCCESS;
+}
