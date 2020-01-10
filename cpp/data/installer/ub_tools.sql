7a3b0e550e50dc2132a191994fe9c13052d2340f
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2020-01-10 10:03:39 +0100

    Update varchar fields in other tables as well

--- cpp/data/installer/ub_tools.sql
+++ cpp/data/installer/ub_tools.sql
@@ -1,5 +1,5 @@
--- under CentOS we still have MariaDB 5, which has a limitation of 767 bytes for keys.
--- this means e.g. for VARCHAR with utf8mb4, we can use at most a VARCHAR(191)!
+-- under CentOS we still have MariaDB 5, which has a limitation of 768 bytes for index keys.
+-- this means we need to use a prefix length <= 768 when creating an index on a particular VARCHAR column.
 -- Also, the default collating sequence is a Swedish one.  This leads to aliasing problems for characters with
 -- diacritical marks => we need to override it and use utf8mb4_bin.
 
@@ -11,27 +11,27 @@ CREATE TABLE database_versions (
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 CREATE TABLE rss_aggregator (
-    item_id VARCHAR(191) NOT NULL,
-    item_url VARCHAR(512) NOT NULL,
-    item_title VARCHAR(200) NOT NULL,
+    item_id VARCHAR(1000) NOT NULL,
+    item_url VARCHAR(1000) NOT NULL,
+    item_title VARCHAR(1000) NOT NULL,
     item_description TEXT NOT NULL,
-    serial_name VARCHAR(200) NOT NULL,
-    feed_url VARCHAR(512) NOT NULL,
+    serial_name VARCHAR(1000) NOT NULL,
+    feed_url VARCHAR(1000) NOT NULL,
     pub_date DATETIME NOT NULL,
     insertion_time TIMESTAMP DEFAULT NOW() NOT NULL,
     UNIQUE (item_id),
-    INDEX item_id_index(item_id),
-    INDEX item_url_index(item_url),
+    INDEX item_id_index(item_id(768)),
+    INDEX item_url_index(item_url(768)),
     INDEX insertion_time_index(insertion_time)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 -- Table to be used w/ our validate_harvested_records tool:
 CREATE TABLE metadata_presence_tracer (
-       journal_name VARCHAR(191) NOT NULL,
-       metadata_field_name VARCHAR(191) NOT NULL,
+       journal_name VARCHAR(1000) NOT NULL,
+       metadata_field_name CHAR(4) NOT NULL,
        field_presence ENUM('always', 'sometimes', 'ignore') NOT NULL,
        UNIQUE(journal_name, metadata_field_name),
-       INDEX journal_name_and_metadata_field_name_index(journal_name, metadata_field_name)
+       INDEX journal_name_and_metadata_field_name_index(journal_name(768), metadata_field_name)
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
 
 
@@ -44,9 +44,9 @@ CREATE TABLE delivered_marc_records (
     journal_name VARCHAR(1000) NOT NULL,
     main_title VARCHAR(1000) NOT NULL,
     publication_year CHAR(4) DEFAULT NULL,
-    volume CHAR(40) DEFAULT NULL,
-    issue CHAR(40) DEFAULT NULL,
-    pages CHAR(20) DEFAULT NULL,
+    volume VARCHAR(20) DEFAULT NULL,
+    issue VARCHAR(20) DEFAULT NULL,
+    pages VARCHAR(20) DEFAULT NULL,
     resource_type ENUM('print','online','unknown') NOT NULL,
     record BLOB NOT NULL,
     INDEX delivered_marc_records_url_index(url(768)),
@@ -60,6 +60,6 @@ CREATE TABLE delivered_marc_records (
 CREATE TABLE delivered_marc_records_superior_info (
     zeder_id VARCHAR(10) PRIMARY KEY,
     control_number VARCHAR(20) DEFAULT NULL,
-    title VARCHAR(191) NOT NULL,
+    title VARCHAR(1000) NOT NULL,
     CONSTRAINT zeder_id FOREIGN KEY (zeder_id) REFERENCES delivered_marc_records (zeder_id) ON DELETE CASCADE ON UPDATE CASCADE
 ) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin;
