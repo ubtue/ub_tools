6872891f66a7dbfc894e7fadedc7a190784d72fa
jriedl <johannes.riedl@uni-tuebingen.de>
2020-02-20 11:03:30 +0100

    Appropriately handle empty results

--- cpp/update_beacon_files.sh
+++ cpp/update_beacon_files.sh
@@ -1,21 +1,34 @@
 #!/bin/bash
 set -o nounset
 
+function IsResultEmpty() {
+    item_count=$(grep '^[^#]' "$1" | wc -l)
+    [ $item_count == "0" ]
+}
+
 cd /usr/local/ub_tools/bsz_daten
 
 error_message=""
 
 wget https://labs.ddb.de/app/beagen/item/person/archive/latest -O archivportal-d.beacon.temp
 if [ $? == 0 ]; then
-    mv archivportal-d.beacon.temp archivportal-d.beacon
+    if [ ! $(IsResultEmpty archivportal-d.beacon.temp) ]; then
+        mv archivportal-d.beacon.temp archivportal-d.beacon
+    else
+        error_message .= "Obtained empty file from Kalliope"
+    fi
 else
     error_message .= "Failed to download the Beacon file from Archiveportal-D."
 fi
 
 wget http://kalliope.staatsbibliothek-berlin.de/beacon/beacon.txt -O kalliope.staatsbibliothek-berlin.beacon.temp
 if [ $? == 0 ]; then
-    mv kalliope.staatsbibliothek-berlin.beacon.temp kalliope.staatsbibliothek-berlin.beacon
-    sed -i -e 's/#FORMAT: GND-BEACON/#FORMAT: BEACON/g' kalliope.staatsbibliothek-berlin.beacon
+    if [ ! $(IsResultEmpty kalliope.staatsbibliothek-berlin.beacon.temp) ]; then
+        mv kalliope.staatsbibliothek-berlin.beacon.temp kalliope.staatsbibliothek-berlin.beacon
+        sed -i -e 's/#FORMAT: GND-BEACON/#FORMAT: BEACON/g' kalliope.staatsbibliothek-berlin.beacon
+    else
+        error_message .= "Obtained empty file from Kalliope"
+    fi
 else
     error_message .= "Failed to download the Beacon file from Kalliope."
 fi
