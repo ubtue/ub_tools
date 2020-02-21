bc06d739e2d16c3b6a58bc13c8dffc6784cc2485
Mario Trojan <mario.trojan@uni-tuebingen.de>
2020-02-21 10:27:20 +0100

    ZoteroHarvesterConversion: Map language code from translators to 3-letter english language code, see ubtue/DatenProbleme#423

--- cpp/lib/src/ZoteroHarvesterConversion.cc
+++ cpp/lib/src/ZoteroHarvesterConversion.cc
@@ -24,6 +24,7 @@
 #include "StlHelpers.h"
 #include "StringUtil.h"
 #include "TimeUtil.h"
+#include "TranslationUtil.h"
 #include "UBTools.h"
 #include "UrlUtil.h"
 #include "ZoteroHarvesterConversion.h"
@@ -619,6 +620,13 @@ void AugmentMetadataRecord(MetadataRecord * const metadata_record, const Config:
         LOG_WARNING("expected language '" + *journal_params.language_params_.expected_languages_.begin() + "' but found '"
                     + metadata_record->language_ + "'");
         metadata_record->language_ = *journal_params.language_params_.expected_languages_.begin();
+    }
+    else if (metadata_record->language_.length() == 2 or metadata_record->language_.length() == 3) {
+        if (TranslationUtil::IsValidInternational2LetterCode(metadata_record->language_))
+            metadata_record->language_ = TranslationUtil::MapInternational2LetterCodeToGerman3Or4LetterCode(metadata_record->language_);
+
+        if (TranslationUtil::IsValidGerman3Or4LetterCode(metadata_record->language_))
+            metadata_record->language_ = TranslationUtil::MapGermanLanguageCodesToFake3LetterEnglishLanguagesCodes(metadata_record->language_);
     }
 
     // fill-in license and SSG values
