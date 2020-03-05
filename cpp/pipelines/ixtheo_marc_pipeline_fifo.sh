ce34d58a37b8d9a4695d2e56d27960714bc62377
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2020-03-05 15:05:18 +0100

    Also patch SYGa.

--- cpp/pipelines/ixtheo_marc_pipeline_fifo.sh
+++ cpp/pipelines/ixtheo_marc_pipeline_fifo.sh
@@ -362,6 +362,8 @@ mkfifo GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc
              --globally-substitute 109a "v([0-9]+) ?- ?v([0-9]+)" "\\1 v. Chr. - \\2 v. Chr." `# Replace "v384-v322" with "384 v. Chr. - 322 v. Chr."` \
              --globally-substitute 109a "v([0-9]+)" "\\1 v. Chr." `# Replace "v384" with "384 v. Chr."` \
              --globally-substitute 109a 'v(\d+) ?- ?(\d+)' '\1 v.Chr.-\2' `# Replace v1-29 with 1 v.Chr-29` \
+             --globally-substitute SYGa "v([0-9]+)" "\\1 v. Chr." `# Replace "v384" with "384 v. Chr."` \
+             --globally-substitute SYGa 'v(\d+) ?- ?(\d+)' '\1 v.Chr.-\2' `# Replace v1-29 with 1 v.Chr-29` \
              >> "${log}" 2>&1 && \
 EndPhase || Abort) &
 
