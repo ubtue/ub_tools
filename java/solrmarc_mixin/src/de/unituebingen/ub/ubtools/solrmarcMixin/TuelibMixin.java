d6949f6e7b938c04a246bf1069cc323e2f6bbd58
jriedl <johannes.riedl@uni-tuebingen.de>
2020-03-18 15:04:05 +0100

    Address problem of missing text_type fields for old Krimdok fields

--- java/solrmarc_mixin/src/de/unituebingen/ub/ubtools/solrmarcMixin/TuelibMixin.java
+++ java/solrmarc_mixin/src/de/unituebingen/ub/ubtools/solrmarcMixin/TuelibMixin.java
@@ -3074,8 +3074,9 @@ public class TuelibMixin extends SolrIndexerMixin {
         for (final Object obj : hits) {
              JSONObject hit = (JSONObject) obj;
              JSONObject _source = (JSONObject) hit.get("_source");
-             final String description = mapTextTypeToDescription((String) _source.get("text_type"));
-             if (description.equals(text_type_description))
+             final String description = _source.containsKey("text_type") ?
+		                        mapTextTypeToDescription((String) _source.get("text_type")) : "";
+             if (description.isEmpty() || description.equals(text_type_description))
                  fulltextBuilder.append(_source.get("full_text"));
         }
         return fulltextBuilder.toString();
