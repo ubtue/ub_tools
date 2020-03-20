ef2164f533492e66a11587777dd754b8966c70c8
Madeesh Kannan <madeeswaran.kannan@uni-tuebingen.de>
2020-03-20 14:37:21 +0100

    Bug fixes

--- cpp/lib/src/ZoteroHarvesterZederInterop.cc
+++ cpp/lib/src/ZoteroHarvesterZederInterop.cc
@@ -30,14 +30,18 @@ namespace ZederInterop {
 
 const std::map<Config::JournalParams::IniKey, std::string> INI_KEY_TO_ZEDER_COLUMN_MAP {
     { Config::JournalParams::IniKey::NAME, "tit" },
-    { Config::JournalParams::IniKey::ZEDER_ID, "Z" },
-    { Config::JournalParams::IniKey::ZEDER_MODIFIED_TIME, "Mtime" },
     { Config::JournalParams::IniKey::ONLINE_PPN, "eppn" },
     { Config::JournalParams::IniKey::PRINT_PPN, "pppn" },
     { Config::JournalParams::IniKey::ONLINE_ISSN, "essn" },
     { Config::JournalParams::IniKey::PRINT_ISSN, "issn" },
     { Config::JournalParams::IniKey::EXPECTED_LANGUAGES, "spr" },
     { Config::JournalParams::IniKey::SSGN, "ber" }
+//  The following two columns/INI keys are intentionally excluded as they are special cases.
+//  Even though there is a one-to-one correspondence for each to the two columns,
+//  they are stored differently in memory (in the Zeder::Entry class) than all other
+//  columns. Therefore, they can't be trivially mapped to each other.
+//  { Config::JournalParams::IniKey::ZEDER_ID, "Z" },
+//  { Config::JournalParams::IniKey::ZEDER_MODIFIED_TIME, "Mtime" },
 };
 
 
