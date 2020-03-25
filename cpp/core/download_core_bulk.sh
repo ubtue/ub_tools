851b2fc093c51906eb381493ae46bf88dd92703e
jriedl <johannes.riedl@uni-tuebingen.de>
2020-03-25 09:11:55 +0100

    API key as parameter and  added sleep between partial downloads

--- cpp/core/download_core_bulk.sh
+++ cpp/core/download_core_bulk.sh
@@ -8,9 +8,11 @@ fi
 
 partial_dir=${1}
 output_file=${2}
+API_KEY="XXXXX"
 
 > ${output_file}
 
 for query_file in $(ls -d1v $(realpath ${partial_dir})/*.json); do
-    curl -s -g --header "Content-Type: application/json" -H 'apiKey: XXXX' --request POST --data-binary  @${query_file} 'https://core.ac.uk/api-v2/articles/search'  >> ${output_file}
+    curl -s -g --header "Content-Type: application/json" -H "apiKey:${API_KEY}" --request POST --data-binary  @${query_file} 'https://core.ac.uk/api-v2/articles/search'  >> ${output_file}
+    sleep 15
 done
