4a321af08ade63ba2dcf9a38a414e742fa90ceaa
Mario Trojan <mario.trojan@uni-tuebingen.de>
2020-04-07 10:39:23 +0200

    cronjobs/update_zotero_enhancement_maps.sh: uppercase constants, kill ssh-agent on exit

--- cronjobs/update_zotero_enhancement_maps.sh
+++ cronjobs/update_zotero_enhancement_maps.sh
@@ -5,22 +5,28 @@
 set -o errexit -o nounset
 
 # Set up the log file:
-readonly logdir=/usr/local/var/log/tuefind
-readonly log_filename=$(basename "$0")
-readonly log="${logdir}/${log_filename%.*}.log"
-rm -f "${log}"
+readonly LOGDIR=/usr/local/var/log/tuefind
+readonly LOG_FILENAME=$(basename "$0")
+readonly LOG="${LOGDIR}/${LOG_FILENAME%.*}.log"
+rm -f "${LOG}"
 
 no_problems_found=false
+ssh_agent_pid=0
 function SendEmail {
+    if [[ $ssh_agent_pid != 0 && $(ps -p $ssh_agent_pid) ]]; then
+        Echo "cleanup: killing ssh-agent pid $ssh_agent_pid"
+        kill $ssh_agent_pid
+    fi
+
     if [ "$no_problems_found" = true ]; then
-        send_email --priority=low --sender="zts_harvester_delivery_pipeline@uni-tuebingen.de" --recipients="$email_address" \
+        send_email --priority=low --sender="zts_harvester_delivery_pipeline@uni-tuebingen.de" --recipients="$EMAIL_ADDRESS" \
                    --subject="$0 passed on $(hostname)" --message-body="No problems were encountered."
         exit 0
     else
-        send_email --priority=high --sender="zts_harvester_delivery_pipeline@uni-tuebingen.de" --recipients="$email_address" \
+        send_email --priority=high --sender="zts_harvester_delivery_pipeline@uni-tuebingen.de" --recipients="$EMAIL_ADDRESS" \
                    --subject="$0 failed on $(hostname)" \
-                   --message-body="Check the log file at $log for details."
-        echo "*** ZOTERO ENHANCEMENT MAPS UPDATE FAILED ***" | tee --append "${log}"
+                   --message-body="Check the log file at $LOG for details."
+        echo "*** ZOTERO ENHANCEMENT MAPS UPDATE FAILED ***" | tee --append "${LOG}"
         exit 1
     fi
 }
@@ -37,29 +43,30 @@ if [ $# != 1 ]; then
     Usage
 fi
 
-readonly email_address=$1
-readonly working_dir=/usr/local/var/lib/tuelib/zotero-enhancement-maps
-readonly github_ssh_key=~/.ssh/github-robot
+readonly EMAIL_ADDRESS=$1
+readonly WORKING_DIR=/usr/local/var/lib/tuelib/zotero-enhancement-maps
+readonly GITHUB_SSH_KEY=~/.ssh/github-robot
 
-echo -e "*** ZOTERO ENHANCEMENT MAPS UPDATE START ***\n" | tee --append "${log}"
-cd $working_dir
+echo -e "*** ZOTERO ENHANCEMENT MAPS UPDATE START ***\n" | tee --append "${LOG}"
+cd $WORKING_DIR
 
-echo -e "Start SSH agent\n" | tee --append "${log}"
+echo -e "Start SSH agent\n" | tee --append "${LOG}"
 eval "$(ssh-agent -s)"
-ssh-add "$github_ssh_key"
+ssh_agent_pid=$(pgrep -n ssh-agent)
+ssh-add "$GITHUB_SSH_KEY"
 
-echo -e "Pull changes from upstream\n" | tee --append "${log}"
-git pull >> "${log}" 2>&1
+echo -e "Pull changes from upstream\n" | tee --append "${LOG}"
+git pull >> "${LOG}" 2>&1
 
-echo -e "Generate maps and push changes to upstream\n" | tee --append "${log}"
+echo -e "Generate maps and push changes to upstream\n" | tee --append "${LOG}"
 generate_issn_maps_from_zeder   \
     --min-log-level=DEBUG       \
     --find-duplicate-issns      \
     --push-to-github            \
-    $working_dir                \
+    $WORKING_DIR                \
     ssg=ISSN_to_SSG.map         \
-    >> "${log}" 2>&1
+    >> "${LOG}" 2>&1
 
-echo -e "*** ZOTERO ENHANCEMENT MAPS UPDATE DONE ***" | tee --append "${log}"
+echo -e "*** ZOTERO ENHANCEMENT MAPS UPDATE DONE ***" | tee --append "${LOG}"
 no_problems_found=true
 exit 0
