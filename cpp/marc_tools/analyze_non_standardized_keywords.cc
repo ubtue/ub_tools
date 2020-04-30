bc10617ea65da878e688ee9d60319ac89ef8ed1d
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2020-04-30 09:54:11 +0200

    First complete version.

--- cpp/marc_tools/analyze_non_standardized_keywords.cc
+++ cpp/marc_tools/analyze_non_standardized_keywords.cc
@@ -17,13 +17,24 @@
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <algorithm>
+#include <unordered_map>
 #include <unordered_set>
 #include <cstdlib>
+#include "FileUtil.h"
 #include "MARC.h"
+#include "TextUtil.h"
 #include "util.h"
 
 
 namespace {
+
+
+std::string NormalizeKeyword(std::string keyword) {
+    TextUtil::CollapseAndTrimWhitespace(&keyword);
+    TextUtil::UTF8ToLower(&keyword);
+    return keyword;
+}
 
 
 void CollectNormalizedKeywordsAndTranslations(MARC::Reader * const reader,
@@ -35,8 +46,9 @@ void CollectNormalizedKeywordsAndTranslations(MARC::Reader * const reader,
 
         MARC::Record::KeywordAndSynonyms keyword_synonyms;
         if (record.getKeywordAndSynonyms(&keyword_synonyms)) {
-            normalized_keywords->emplace(keyword_synonyms.getKeyword());
-            normalized_keywords->insert(keyword_synonyms.begin(), keyword_synonyms.end());
+            normalized_keywords->emplace(NormalizeKeyword(keyword_synonyms.getKeyword()));
+            for (const auto &keyword_synonym : keyword_synonyms)
+                normalized_keywords->emplace(NormalizeKeyword(keyword_synonym));
         }
     }
 
@@ -45,13 +57,57 @@ void CollectNormalizedKeywordsAndTranslations(MARC::Reader * const reader,
 }
 
 
-void ProcessTitleRecords(MARC::Reader * const marc_reader) {
+const std::vector<std::string> non_normlized_keyword_tags{ "650" };
+
+
+void ProcessField(const MARC::Record::Field &field, const std::unordered_set<std::string> &normalized_keywords,
+                  std::unordered_map<std::string, unsigned> * const unmatched_keywords_to_counts_map,
+                  unsigned * const matched_count, unsigned * const not_matched_count)
+{
+    const auto non_normlized_keyword(NormalizeKeyword(field.getFirstSubfieldWithCode('a')));
+    if (non_normlized_keyword.empty())
+        return;
+
+    if (normalized_keywords.find(non_normlized_keyword) != normalized_keywords.cend())
+        ++*matched_count;
+    else {
+        auto keyword_and_count(unmatched_keywords_to_counts_map->find(non_normlized_keyword));
+        if (keyword_and_count != unmatched_keywords_to_counts_map->end())
+            ++(keyword_and_count->second);
+        else
+            unmatched_keywords_to_counts_map->emplace(non_normlized_keyword, 1);
+        ++*not_matched_count;
+    }
+}
+
+
+void ProcessTitleRecords(MARC::Reader * const marc_reader, const std::unordered_set<std::string> &normalized_keywords,
+                         std::unordered_map<std::string, unsigned> * const unmatched_keywords_to_counts_map,
+                         unsigned * const matched_count, unsigned * const not_matched_count)
+{
     unsigned record_count(0);
     while (const MARC::Record record = marc_reader->read()) {
         ++record_count;
+        for (const auto &tag : non_normlized_keyword_tags) {
+            for (const auto &field : record.getTagRange(tag))
+                ProcessField(field, normalized_keywords, unmatched_keywords_to_counts_map, matched_count, not_matched_count);
+        }
     }
 
     LOG_INFO("Data set contains " + std::to_string(record_count) + " MARC record(s) of which ");
+}
+
+
+void ListUnmatchedKeywords(File * const output,
+                           const std::unordered_map<std::string, unsigned> &unmatched_keywords_to_counts_map)
+{
+    std::vector<std::pair<std::string, unsigned>> unmatched_keywords_and_counts(unmatched_keywords_to_counts_map.cbegin(),
+                                                                                unmatched_keywords_to_counts_map.cend());
+    std::sort(unmatched_keywords_and_counts.begin(), unmatched_keywords_and_counts.end(),
+              [](const auto &a, const auto &b){ return a.second > b.second; });// Sort in descending order of counts.
+
+    for (const auto [keyword, count] : unmatched_keywords_and_counts)
+        (*output) << keyword << " -> " << count << '\n';
 }
 
 
@@ -59,12 +115,24 @@ void ProcessTitleRecords(MARC::Reader * const marc_reader) {
 
 
 int Main(int argc, char *argv[]) {
-    if (argc != 3)
-        ::Usage("authority_records title_records");
+    if (argc != 4)
+        ::Usage("authority_records title_records keyword_stats_output");
 
-    const auto authority_records(MARC::Reader::Factory(argv[1]));
+    const auto authority_reader(MARC::Reader::Factory(argv[1]));
     std::unordered_set<std::string> normalized_keywords;
-    CollectNormalizedKeywordsAndTranslations(authority_records.get(), &normalized_keywords);
+    CollectNormalizedKeywordsAndTranslations(authority_reader.get(), &normalized_keywords);
+
+    const auto title_reader(MARC::Reader::Factory(argv[2]));
+    std::unordered_map<std::string, unsigned> unmatched_keywords_to_counts_map;
+    unsigned matched_count, not_matched_count;
+    ProcessTitleRecords(title_reader.get(), normalized_keywords, &unmatched_keywords_to_counts_map,
+                        &matched_count, &not_matched_count);
+    const double matched_percentage(100.0 * double(matched_count) / double(matched_count + not_matched_count));
+    LOG_INFO("Found " + std::to_string(matched_percentage)
+             + "% of the non-standardized keywords matched known, standardized keywords.");
+
+    const auto output(FileUtil::OpenOutputFileOrDie(argv[3]));
+    ListUnmatchedKeywords(output.get(), unmatched_keywords_to_counts_map);
 
     return EXIT_SUCCESS;
 }
