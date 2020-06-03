609e2276dbe264fb4d17383225f96a2b13b4d2bf
jriedl <johannes.riedl@uni-tuebingen.de>
2020-06-03 10:26:37 +0200

    Incron setup

---
+++ cpp/pipelines/disable_pipeline_triggers.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -o errexit -o nounset
+
+if [[ $# != 0 ]]; then
+    echo "Usage: $0"
+    exit 1
+fi
+
+if [[ -f /etc/redhat-release ]]; then
+    systemctl stop incrond
+elif [[ -f /etc/debian_version ]]; then
+    systemctl stop incron
+else
+    echo "Could not detect Linux distribution"
+    exit 1
+fi
+
+CRONTAB_BAK="/usr/local/var/tmp/crontab.bak"
+crontab ${CRONTAB_BAK}
+
+
