92168d47abe86af45fcf7f9ccc913ea973161cc9
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2020-06-04 13:16:28 +0200

    Date range support for bible ranges.

---
+++ java/solr_plugins/RangedSearches/src/de/uni_tuebingen/ub/ixTheo/bibleDateRangeSearch/BibleDateRangeParserPlugin.java
@@ -0,0 +1,22 @@
+package de.uni_tuebingen.ub.ixTheo.bibleDateRangeSearch;
+
+
+import org.apache.solr.common.params.SolrParams;
+import org.apache.solr.common.util.NamedList;
+import org.apache.solr.request.SolrQueryRequest;
+import org.apache.solr.search.QParser;
+import org.apache.solr.search.QParserPlugin;
+
+
+public class BibleDateRangeParserPlugin extends QParserPlugin {
+
+    @Override
+    @SuppressWarnings("rawtypes")
+    public void init(final NamedList args) {
+    }
+
+    @Override
+    public QParser createParser(final String queryString, final SolrParams localParams, final SolrParams params, final SolrQueryRequest req) {
+        return new BibleDateRangeParser(queryString, localParams, params, req);
+    }
+}
