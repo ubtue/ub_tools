5d0431fc140f5215d5c15acb086f308ad5bd2881
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2020-07-15 09:59:25 +0200

    Fixed pipeline problem and increased parallelization.

--- cpp/pipelines/ixtheo_marc_pipeline_fifo.sh
+++ cpp/pipelines/ixtheo_marc_pipeline_fifo.sh
@@ -138,15 +138,17 @@ mkfifo GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc
 EndPhase || Abort) &
 
 # Note: It is necessary to run this phase after articles have had their journal's PPN's inserted!
+mkfifo GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc
 StartPhase "Populate the Zeder Journal Timeliness Database Table"
 (collect_journal_stats.cc GesamtTiteldaten-post-phase"$((PHASE-1))"-"${date}".mrc >> "${log}" 2>&1 && \
+                          GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc >> "${log}" 2>&1 && \
 EndPhase || Abort) &
 
 
 StartPhase "Add Additional Open Access URL's"
 # Execute early for programs that rely on it for determining the open access property
 OADOI_URLS_FILE="/mnt/ZE020150/FID-Entwicklung/oadoi/oadoi_urls_ixtheo.json"
-(add_oa_urls ${OADOI_URLS_FILE} GesamtTiteldaten-post-phase"$((PHASE-2))"-"${date}".mrc \
+(add_oa_urls ${OADOI_URLS_FILE} GesamtTiteldaten-post-phase"$((PHASE-1))"-"${date}".mrc \
     GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc >> "${log}" 2>&1 && \
 EndPhase || Abort) &
 
