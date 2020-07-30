2527ef97db87c66d4f37666cbeaed3a82e56904e
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2020-07-30 17:18:52 +0200

    Better logging.

--- cpp/mail_krimdok_newsletter.sh
+++ cpp/mail_krimdok_newsletter.sh
@@ -7,11 +7,13 @@ cd .
 
 most_recent_email_contents=$(ls -Art | tail -n 1)
 if [ -z "$most_recent_email_contents" ]; then
+    echo "No newsletters found"
     exit 0
 fi
 
 time_since_last_modification=$(($(date +%s) - $(stat --format=%Y -- "$most_recent_email_contents")))
 if [ $time_since_last_modification -gt 86400 ]; then
+    echo "No recent newsletters found"
     exit 0
 fi
 
