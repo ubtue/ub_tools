556508bd1459a754133c5da1a5764bfea3532d59
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2020-08-04 09:44:20 +0200

    Moved into tuefind.

--- cpp/data/installer/tuefind.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE vufind.user ADD tuefind_subscribed_to_newsletter BOOL NOT NULL DEFAULT FALSE;
-CREATE INDEX tuefind_subscribed_to_newsletter_index ON vufind.user (tuefind_subscribed_to_newsletter);
