aeec91314e56bddb9dc604d985edf4a4b5f08848
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2020-08-14 12:21:06 +0200

    Simplified the code and some minor cleanup.

--- cpp/merge_differential_and_full_marc_updates.sh
+++ cpp/merge_differential_and_full_marc_updates.sh
@@ -108,24 +108,21 @@ for update in $(generate_merge_order | tail --lines=+2); do
         apply_differential_update $KEEP_ITERMEDIATE_FILES $input_directory $update $temp_directory
     fi
     if [[ -n "$last_temp_directory" ]]; then
-        rm -r ${last_temp_directory}
+        rm --recursive ${last_temp_directory}
     fi
     input_directory=$temp_directory
     last_temp_directory=$temp_directory
 done
 
 
-# If we did not execute the for-loop at all, $temp_directory is unset and we need to set it to the empty string:
-temp_directory=${temp_directory:-}
-
-if [ -z ${temp_directory} ]; then
+if [ -z ${temp_directory} ]; then # We did not execute the for-loop above!
     ln --symbolic --force $input_filename Complete-MARC-current.tar.gz
 else
-    rm -r "$extraction_directory"
+    rm --recursive "$extraction_directory"
     cd ${temp_directory}
     tar czf ../$target_filename *mrc
     cd ..
-    rm -r ${temp_directory}
+    rm --recursive ${temp_directory}
 
     if [[ ! keep_itermediate_filenames ]]; then
         rm temp_directory.$BASHPID.* TA-*.tar.gz WA-*.tar.gz SA-*.tar.gz
