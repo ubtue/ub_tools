52c52110909ec9f551f2a95a7fe756e46efa32d0
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2020-09-04 10:53:09 +0200

    Now we only update changed entries.

--- cpp/pipelines/collect_journal_stats.cc
+++ cpp/pipelines/collect_journal_stats.cc
@@ -139,15 +139,16 @@ bool DbEntry::operator==(const DbEntry &rhs) const {
 }
 
 
-size_t GetExistingDbEntries(DbConnection * const db_connection, const std::string &hostname, const std::string &system_type,
+size_t GetExistingDbEntries(const IniFile &ini_file, const std::string &hostname, const std::string &system_type,
                             std::unordered_map<std::string, DbEntry> * const existing_entries)
 {
-    db_connection->queryOrDie("SELECT MAX(timestamp),Zeder_ID,PPN_Typ,Jahr,Band,Heft,Seitenbereich"
-                              " FROM zeder.erschliessung WHERE Quellrechner='" + hostname + "' AND Systemtyp='"
-                              + system_type + "'");
-    auto result_set(db_connection->getLastResultSet());
+    DbConnection db_connection(ini_file, "DatabaseSelect");
+    db_connection.queryOrDie("SELECT MAX(timestamp),Zeder_ID,PPN,Jahr,Band,Heft,Seitenbereich"
+                             " FROM zeder.erschliessung WHERE Quellrechner='" + hostname + "' AND Systemtyp='"
+                             + system_type + "' GROUP BY Zeder_ID,PPN");
+    auto result_set(db_connection.getLastResultSet());
     while (const auto row = result_set.getNextRow())
-        (*existing_entries)[row["Zeder_ID"] + "+" + row["PPN_Typ"]] =
+        (*existing_entries)[row["Zeder_ID"] + "+" + row["PPN"]] =
             DbEntry(row["Jahr"], row["Band"], row["Heft"], row["Seitenbereich"]);
 
     return existing_entries->size();
@@ -155,9 +156,9 @@ size_t GetExistingDbEntries(DbConnection * const db_connection, const std::strin
 
 
 bool AlreadyPresentInDB(const std::unordered_map<std::string, DbEntry> &existing_entries,
-                        const std::string &zeder_id, const std::string &ppn_type, const DbEntry &test_entry)
+                        const std::string &zeder_id, const std::string &ppn, const DbEntry &test_entry)
 {
-    const auto key_and_entry(existing_entries.find(zeder_id + "+" + ppn_type));
+    const auto key_and_entry(existing_entries.find(zeder_id + "+" + ppn));
     if (key_and_entry == existing_entries.cend())
         return false;
 
@@ -167,7 +168,7 @@ bool AlreadyPresentInDB(const std::unordered_map<std::string, DbEntry> &existing
 
 void ProcessRecords(MARC::Reader * const reader, MARC::Writer * const writer, const std::string &system_type,
                     const std::unordered_map<std::string, ZederIdAndType> &ppns_to_zeder_ids_and_types_map,
-                    DbConnection * const db_connection)
+                    const IniFile &ini_file, DbConnection * const db_connection)
 {
     const auto zeder_flavour(system_type == "krimdok" ? Zeder::KRIMDOK : Zeder::IXTHEO);
     const auto JOB_START_TIME(std::to_string(std::time(nullptr)));
@@ -175,7 +176,8 @@ void ProcessRecords(MARC::Reader * const reader, MARC::Writer * const writer, co
     const std::string ZEDER_URL_PREFIX(Zeder::GetFullDumpEndpointPath(zeder_flavour) + "#suche=Z%3D");
 
     std::unordered_map<std::string, DbEntry> existing_entries;
-    GetExistingDbEntries(db_connection, HOSTNAME, system_type, &existing_entries);
+    LOG_INFO("Found " + std::to_string(GetExistingDbEntries(ini_file, HOSTNAME, system_type, &existing_entries))
+             + " existing database entries.");
 
     const std::vector<std::string> COLUMN_NAMES{ "timestamp", "Quellrechner", "Systemtyp", "Zeder_ID", "Zeder_URL", "PPN_Typ",
                                                  "PPN", "Jahr", "Band", "Heft", "Seitenbereich", "Startseite", "Endseite" };
@@ -191,8 +193,8 @@ void ProcessRecords(MARC::Reader * const reader, MARC::Writer * const writer, co
         if (superior_control_number.empty())
             continue;
 
-        const auto zeder_id_and_type(ppns_to_zeder_ids_and_types_map.find(superior_control_number));
-        if (zeder_id_and_type == ppns_to_zeder_ids_and_types_map.cend())
+        const auto ppn_and_zeder_id_and_ppn_type(ppns_to_zeder_ids_and_types_map.find(superior_control_number));
+        if (ppn_and_zeder_id_and_ppn_type == ppns_to_zeder_ids_and_types_map.cend())
             continue;
 
         const auto _936_field(record.findTag("936"));
@@ -208,25 +210,25 @@ void ProcessRecords(MARC::Reader * const reader, MARC::Writer * const writer, co
             volume = _936_field->getFirstSubfieldWithCode('d');
         const std::string year(_936_field->getFirstSubfieldWithCode('j'));
 
-        const std::string zeder_id(std::to_string(zeder_id_and_type->second.zeder_id_));
-        const std::string ppn_type(1, zeder_id_and_type->second.type_);
+        const std::string zeder_id(std::to_string(ppn_and_zeder_id_and_ppn_type->second.zeder_id_));
+        const std::string ppn_type(1, ppn_and_zeder_id_and_ppn_type->second.type_);
         const std::string year_as_string(std::to_string(YearStringToShort(year)));
-        if (AlreadyPresentInDB(existing_entries, zeder_id, ppn_type,
+        if (AlreadyPresentInDB(existing_entries, zeder_id, superior_control_number,
                                DbEntry(year_as_string, volume, issue, pages)))
             continue;
 
         std::vector<std::optional<std::string>> new_column_values{
-            { /* timestamp */     JOB_START_TIME                                                         },
-            { /* Quellrechner */  HOSTNAME                                                               },
-            { /* Systemtyp */     system_type,                                                           },
-            { /* Zeder_ID */      zeder_id                                                               },
-            { /* Zeder_URL */     ZEDER_URL_PREFIX + std::to_string(zeder_id_and_type->second.zeder_id_) },
-            { /* PPN_Typ */       ppn_type                                                               },
-            { /* PPN */           superior_control_number                                                },
-            { /* Jahr */          year_as_string                                                         },
-            { /* Band */          volume                                                                 },
-            { /* Heft */          issue                                                                  },
-            { /* Seitenbereich */ pages                                                                  },
+            { /* timestamp */     JOB_START_TIME                                                                     },
+            { /* Quellrechner */  HOSTNAME                                                                           },
+            { /* Systemtyp */     system_type,                                                                       },
+            { /* Zeder_ID */      zeder_id                                                                           },
+            { /* Zeder_URL */     ZEDER_URL_PREFIX + std::to_string(ppn_and_zeder_id_and_ppn_type->second.zeder_id_) },
+            { /* PPN_Typ */       ppn_type                                                                           },
+            { /* PPN */           superior_control_number                                                            },
+            { /* Jahr */          year_as_string                                                                     },
+            { /* Band */          volume                                                                             },
+            { /* Heft */          issue                                                                              },
+            { /* Seitenbereich */ pages                                                                             },
         };
 
         unsigned start_page, end_page;
@@ -273,11 +275,12 @@ int Main(int argc, char *argv[]) {
     const auto ppns_to_zeder_ids_and_types_map(GetPPNsToZederIdsAndTypesMap(system_type));
 
     IniFile ini_file;
-    DbConnection db_connection(ini_file);
+    DbConnection db_connection(ini_file, "DatabaseInsert");
 
     const auto marc_reader(MARC::Reader::Factory(argv[2]));
     const auto marc_writer(MARC::Writer::Factory(argv[3]));
-    ProcessRecords(marc_reader.get(), marc_writer.get(), system_type, ppns_to_zeder_ids_and_types_map, &db_connection);
+    ProcessRecords(marc_reader.get(), marc_writer.get(), system_type, ppns_to_zeder_ids_and_types_map,
+                   ini_file, &db_connection);
 
     return EXIT_SUCCESS;
 }
