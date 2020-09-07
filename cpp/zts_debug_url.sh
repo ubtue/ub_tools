72aec5c5137a4433231074a4084f8ea80f54a6e8
jriedl <johannes.riedl@uni-tuebingen.de>
2020-09-07 13:53:24 +0200

    Tool for for obtaining zts debug output for a given url

---
+++ cpp/zts_debug_url.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Send request to local zts docker container and get get the logging output 
+# since the request started
+
+ZTS_SERVER_AND_PORT="127.0.0.1:1969"
+
+
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 url"
+    exit 1
+fi
+
+url="$1"
+
+last_request=$(date --rfc-3339=seconds | sed 's/ /T/') && curl -d "${url}" -H 'Content-Type: text/plain' http://"${ZTS_SERVER_AND_PORT}"/web && docker logs --since "$last_request" $(docker ps --filter ancestor=zts --last 1 --format "{{.ID}}")
+
