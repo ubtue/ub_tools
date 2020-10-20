3f0dfb6f4f0b704c6f9a5ef99c0206687385861e
jriedl <johannes.riedl@uni-tuebingen.de>
2020-10-20 16:51:12 +0200

    Minor changes

--- cpp/throwaways/extract_both_700_and_71X.cc
+++ cpp/throwaways/extract_both_700_and_71X.cc
@@ -28,21 +28,24 @@
 
 
 namespace {
-    [[noreturn]] void Usage() {
-        ::Usage("title_data list.txt\n"
-                "Extracts list of PPN that contain both 700 and 710 and 711 fields\n");
+
+
+[[noreturn]] void Usage() {
+    ::Usage("title_data list.txt\n"
+            "Extracts list of PPN that contain both 700 and 710 and 711 fields\n");
+}
+
+
+void ProcessRecords(MARC::Reader * const marc_reader, std::unordered_set<std::string> * const target_ppns, unsigned * record_count) {
+    while (const MARC::Record &record = marc_reader->read()) {
+        ++(*record_count);
+        if (record.hasTag("710") and (record.hasTag("710") or record.hasTag("711")))
+            target_ppns->emplace(record.getControlNumber());
     }
+}
 
 
-    void ProcessRecords(MARC::Reader * const marc_reader, std::unordered_set<std::string> * const target_ppns, unsigned &record_count) {
-        while (const MARC::Record record = marc_reader->read()) {
-            ++record_count;
-            if (record.hasTag("710") and (record.hasTag("710") or record.hasTag("711")))
-                target_ppns->emplace(record.getControlNumber());
-        }
-    }
-
-} // end unnamed namespace
+} // unnamed namespace
 
 int Main(int argc, char *argv[]) {
     if (argc != 3)
@@ -52,7 +55,7 @@ int Main(int argc, char *argv[]) {
     std::unordered_set<std::string> target_ppns;
     const auto output(FileUtil::OpenOutputFileOrDie(argv[2]));
     unsigned record_count(0);
-    ProcessRecords(marc_reader.get(), &target_ppns, record_count);
+    ProcessRecords(marc_reader.get(), &target_ppns, &record_count);
     for (const auto &ppn : target_ppns)
          (*output) << ppn << '\n';
     LOG_INFO("Found " + std::to_string(target_ppns.size()) + " records of " +  std::to_string(record_count) + '\n'); 
