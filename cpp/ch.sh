7cb2a56377d96c709565e066f1829de6d6d9630c
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2020-12-02 10:34:19 +0100

    As reqested by Martin.

--- cpp/ch.sh
+++ cpp/ch.sh
@@ -13,6 +13,14 @@ rm --force /usr/local/vufind/public/docs/IxTheoDaten-*.xml.7z
 7za u -p"${PASSWORD}" /usr/local/vufind/public/docs/"${output}".7z "${output}"
 rm "${output}"
 chcon unconfined_u:object_r:usr_t:s0 /usr/local/vufind/public/docs/"${output}".7z
-send_email --reply-to=ixtheo@ub.uni-tuebingen.de \
+send_email --sender=ixtheo@ub.uni-tuebingen.de \
            --recipients=johannes.ruscheinski@uni-tuebingen.de,martin.fassnacht@uni-tuebingen.de,waldvogel@globethics.net,support@novalogix.ch \
-           --subject="Neue IxTheo-Daten verfügbar" --message-body="URL: http://ixtheo.de/docs/"${output}".7z"
+           --subject="Neue IxTheo-Daten verfügbar" \
+           --message-body="URL: http://ixtheo.de/docs/"${output}".7z\nSincerely,
+Das IxTheo Team
+--
+Falls es irgendwelche Fragen oder Probleme mit dieser Datenlieferung gab,
+bitte kontaktieren Sie
+ixtheo@ub.uni-tuebingen.de  <mailto:ixtheo@ub.uni-tuebingen.de>
+oder antworten Sie einfach auf diese Email.
+"
