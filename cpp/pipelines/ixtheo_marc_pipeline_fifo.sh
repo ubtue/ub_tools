ba3ccb9b82b35cc47209cb0b0b48983f769c4729
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2020-12-10 08:20:47 +0100

    Fix pipeline problem.

--- cpp/pipelines/ixtheo_marc_pipeline_fifo.sh
+++ cpp/pipelines/ixtheo_marc_pipeline_fifo.sh
@@ -354,7 +354,7 @@ wait
 
 StartPhase "Cross-link Type Tagging"
 (add_cross_link_type GesamtTiteldaten-post-phase"$((PHASE-1))"-"${date}".mrc \
-    GesamtTiteldaten-post-pipeline-"${date}".mrc >> "${log}" 2>&1 && \
+    GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc >> "${log}" 2>&1 && \
 EndPhase || Abort) &
 wait
 
