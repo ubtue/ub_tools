7b5c19bd4546b7b767e7fce9d0db8ba72e165ed6
Mario Trojan <mario.trojan@uni-tuebingen.de>
2021-02-08 17:22:29 +0100

    system_updates: generate hmac.conf in tuefind's local_overrides if not existing

---
+++ cpp/data/system_updates/5.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+HMAC_FILE=/usr/local/vufind/local/tuefind/local_overrides/hmac.conf
+if [ ! -f ${HMAC_FILE} ]; then
+    HASH=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    echo "HMACkey = $HASH" > $HMAC_FILE
+fi
