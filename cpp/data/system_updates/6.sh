f1b883714619dbc9adee3f84050917948426b671
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-02-23 15:42:39 +0100

    Use long options.

--- cpp/data/system_updates/6.sh
+++ cpp/data/system_updates/6.sh
@@ -2,7 +2,7 @@
 set -o errexit -o nounset
 
 readonly SYSTEMD_SERVICE_DIR=/etc/systemd/system/
-mkdir -p $(SYSTEMD_SERVICE_DIR)
+mkdir --parents $(SYSTEMD_SERVICE_DIR)
 install --owner=root --group=root --mode=644 \
         /usr/local/ub_tools/cpp/data/installer/boot_notification.service \
         $(SYSTEMD_SERVICE_DIR)
