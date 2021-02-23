3994c99c7363141bd355d79a1f3ce14b058c873d
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-02-23 15:18:54 +0100

    A boot notification service.

---
+++ cpp/boot_notification.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -o errexit -o nounset
+
+
+# The following command does not return until the system is fully up and running!
+/usr/bin/systemctl is-system-running --wait
+
+/usr/local/bin/send_email --recipients=ixtheo-team@ub.uni-tuebingen.de --subject='One of our systems just booted up!' \
+                          --message-body="$(hostname --fqdn) was just booted." --priority=very_high
