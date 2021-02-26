276af85e78ee7b84bde6995e5ff0fc6c4a090fee
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-02-26 09:44:32 +0100

    Added the capability to suppress boot notifications on specific systems.

--- cpp/boot_notification.sh
+++ cpp/boot_notification.sh
@@ -2,8 +2,10 @@
 set -o errexit -o nounset
 
 
-# The following command does not return until the system is fully up and running!
-/usr/bin/systemctl is-system-running --wait
+if [[ $(/usr/local/bin/inifile_lookup /usr/local/var/lib/tuelib/boot_notification.conf "" notify) == "true" ]]; then
+    # The following command does not return until the system is fully up and running!
+    /usr/bin/systemctl is-system-running --wait
 
-/usr/local/bin/send_email --recipients=ixtheo-team@ub.uni-tuebingen.de --subject="$(hostname) just booted up!" \
-                          --message-body="$(hostname --fqdn) was just booted." --priority=very_high
+    /usr/local/bin/send_email --recipients=ixtheo-team@ub.uni-tuebingen.de --subject="$(hostname) just booted up!" \
+                              --message-body="$(hostname --fqdn) was just booted." --priority=very_high
+fi
