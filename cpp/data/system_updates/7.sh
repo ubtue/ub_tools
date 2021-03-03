3e88eef85961cf868a1af941351bb7d4ca83613c
Mario Trojan <mario.trojan@uni-tuebingen.de>
2021-03-03 12:17:34 +0100

    Update 7.sh

--- cpp/data/system_updates/7.sh
+++ cpp/data/system_updates/7.sh
@@ -5,4 +5,5 @@ readonly SYSTEMD_SERVICE_DIR=/etc/systemd/system/
 install --owner=root --group=root --mode=644 \
         /usr/local/ub_tools/cpp/data/installer/boot_notification.service \
         "$SYSTEMD_SERVICE_DIR"
-/usr/bin/systemctl start boot_notification.service
+systemctl daemon-reload
+systemctl enable boot_notification.service
