3597ceec6bec86576bb95e489cd077ce29466fe3
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-03-09 15:35:44 +0100

    This one actually seems to work.

--- cpp/throwaways/check_archive_org_isbn_presence.cc
+++ cpp/throwaways/check_archive_org_isbn_presence.cc
@@ -48,7 +48,7 @@ void WorkerThread(Downloader * const downloader, std::deque<std::set<std::string
                   std::mutex * const output_mutex)
 {
     std::set<std::string> isbns;
-    while (work_available) {
+    for (;;) {
         {
             std::lock_guard<std::mutex> task_queue_mutex_locker(*task_queue_mutex);
             if (not task_queue->empty()) {
@@ -58,6 +58,8 @@ void WorkerThread(Downloader * const downloader, std::deque<std::set<std::string
         }
 
         if (isbns.empty()) {
+            if (not work_available)
+                return;
             std::this_thread::sleep_for(std::chrono::seconds(20));
             continue;
         }
@@ -65,7 +67,9 @@ void WorkerThread(Downloader * const downloader, std::deque<std::set<std::string
         for (const auto &isbn : isbns) {
             const std::string url("https://archive.org/metadata/isbn_" + isbn + "/created");
             if (not downloader->newUrl(url)) {
-                LOG_WARNING("URL \"" + url + " failed to download!");
+                std::lock_guard<std::mutex> output_mutex_locker(*output_mutex);
+                LOG_WARNING("URL \"" + url + " failed to download! ("
+                            + downloader->getLastErrorMessage() + ")");
                 continue;
             }
 
@@ -84,25 +88,18 @@ void WorkerThread(Downloader * const downloader, std::deque<std::set<std::string
 
 
 void ProcessRecords(MARC::Reader * const marc_reader, std::deque<std::set<std::string>> * const task_queue,
-                    std::mutex * const task_queue_mutex, unsigned * const isbn_found_count)
+                    std::mutex * const task_queue_mutex)
 {
-    Downloader downloader;
-    unsigned record_count(0), monographs_with_isbns_found_count(0);
+    unsigned record_count(0);
     while (const MARC::Record record = marc_reader->read()) {
         ++record_count;
         const auto isbns(record.getISBNs());
         if (isbns.empty())
             continue;
 
-        ++monographs_with_isbns_found_count;
-
         std::lock_guard<std::mutex> task_queue_mutex_locker(*task_queue_mutex);
         task_queue->push_back(isbns);
     }
-    work_available = false; // Let our worker threads return.
-
-    LOG_INFO("Processed " + std::to_string(record_count) + " record(s) and found "
-             + std::to_string(*isbn_found_count) + " ISBN('s) that are/is present on archive.org.");
 }
 
 
@@ -124,10 +121,12 @@ int Main(int argc, char *argv[]) {
                                      &isbn_found_count, isbn_list_output.get(), &output_mutex);
 
     auto marc_reader(MARC::Reader::Factory(argv[2]));
-    ProcessRecords(marc_reader.get(), &task_queue, &task_queue_mutex, &isbn_found_count);
+    ProcessRecords(marc_reader.get(), &task_queue, &task_queue_mutex);
 
+    work_available = false; // Let our worker threads return.
     for (size_t i(0); i < WORKER_THREAD_COUNT; ++i)
         thread_pool[i].join();
+    LOG_INFO("Found " + std::to_string(isbn_found_count) + " monographs on Archive.org.");
 
     return EXIT_SUCCESS;
 }
