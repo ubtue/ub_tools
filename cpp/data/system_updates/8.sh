9977c90278ba9af3142db27a678af98e8f74ca31
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-03-10 10:38:50 +0100

    Added missing parameters.

--- cpp/data/system_updates/8.sh
+++ cpp/data/system_updates/8.sh
@@ -2,6 +2,6 @@
 set -o errexit -o nounset -o pipefail
 cd /usr/local/ub_tools/cpp
 make import_rss_aggregator_conf
-./import_rss_aggregator_conf
+./import_rss_aggregator_conf relbib /usr/local/var/lib/tuelib/rss_aggregator_relbib.conf
 make convert_rss_db_tables
 ./convert_rss_db_tables
