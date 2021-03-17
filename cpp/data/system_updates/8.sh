0d1862bb77a5ddb7e2e7f1f3bbaa2e6545871d59
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-03-17 14:32:36 +0100

    Fixed comparison operator.

--- cpp/data/system_updates/8.sh
+++ cpp/data/system_updates/8.sh
@@ -3,7 +3,7 @@ set -o errexit -o nounset -o pipefail
 
 cd /usr/local/ub_tools/cpp
 
-if [[ "$TUEFIND_FLAVOUR" != "ixtheo" ]]; then
+if [[ "$TUEFIND_FLAVOUR" == "ixtheo" ]]; then
     make import_rss_aggregator_conf
     ./import_rss_aggregator_conf relbib /usr/local/var/lib/tuelib/rss_aggregator_relbib.conf
 fi
