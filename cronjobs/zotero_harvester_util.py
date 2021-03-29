57627ba3afdd363c82166b1afcc6ff4ebb422a5f
jriedl <johannes.riedl@uni-tuebingen.de>
2021-03-29 14:53:48 +0200

    Remove explicit close()

--- cronjobs/zotero_harvester_util.py
+++ cronjobs/zotero_harvester_util.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 import configparser
 
+
 def GetZoteroConfiguration():
     zotero_harvester_conf_path = "/usr/local/var/lib/tuelib/zotero-enhancement-maps/zotero_harvester.conf"
     zotero_harvester_conf_tmp_path = "/tmp/zotero_harvester.conf"
@@ -10,7 +11,6 @@ def GetZoteroConfiguration():
          open(zotero_harvester_conf_tmp_path, "w") as zotero_harvester_tmp_conf:
             zotero_harvester_tmp_conf.write("[DEFAULT]\n")
             zotero_harvester_tmp_conf.write(zotero_harvester_conf.read())
-            zotero_harvester_tmp_conf.close()
     config = configparser.ConfigParser()
     config.read(zotero_harvester_conf_tmp_path)
     return config
