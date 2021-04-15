ff44ebc7bb0032476b7170a20df8fc6f6b4078d3
jriedl <johannes.riedl@uni-tuebingen.de>
2021-04-15 12:08:46 +0200

    Read key from environment

--- docker/zts/build.sh
+++ docker/zts/build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 #
+DOCKER_ZTS_DIR=/usr/local/ub_tools/docker/zts
+for keyfile in ${DOCKER_ZTS_DIR}/keys/*; do
+    export $(cat ${keyfile})
+done
 rm -rf /tmp/zotero-translation-server
 git clone --recurse-submodules https://github.com/ubtue/zotero-translation-server /tmp/zotero-translation-server
-cp /usr/local/ub_tools/docker/zts/* /tmp/zotero-translation-server
-cp /usr/local/ub_tools/docker/zts/.dockerignore /tmp/zotero-translation-server
-cp -r /usr/local/ub_tools/docker/zts/extra_certs /tmp/zotero-translation-server
+cp ${DOCKER_ZTS_DIR}/.dockerignore /tmp/zotero-translation-server
+cp -r ${DOCKER_ZTS_DIR}/extra_certs /tmp/zotero-translation-server
 cd /tmp/zotero-translation-server
-docker build -t zts .
+cat Dockerfile | envsubst | docker build -t zts_dev . -f -
 rm -rf /tmp/zotero-translation-server
