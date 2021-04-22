bf50e45a5f7aa1ea58cc63e06344a3d7c2333e9d
jriedl <johannes.riedl@uni-tuebingen.de>
2021-04-22 09:43:40 +0200

    Remove intermediate files

--- docker/zts/build.sh
+++ docker/zts/build.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 #
-DOCKER_ZTS_DIR=/usr/local/ub_tools/docker/zts
-set -o allexport
-for keyfile in ${DOCKER_ZTS_DIR}/keys/*; do
-    . ${keyfile}
-done
-set +o allexport
 rm -rf /tmp/zotero-translation-server
 git clone --recurse-submodules https://github.com/ubtue/zotero-translation-server /tmp/zotero-translation-server
-cp ${DOCKER_ZTS_DIR}/.dockerignore /tmp/zotero-translation-server
-cp -r ${DOCKER_ZTS_DIR}/extra_certs /tmp/zotero-translation-server
+cp /usr/local/ub_tools/docker/zts/* /tmp/zotero-translation-server
+cp /usr/local/ub_tools/docker/zts/.dockerignore /tmp/zotero-translation-server
+cp -r /usr/local/ub_tools/docker/zts/extra_certs /tmp/zotero-translation-server
 cd /tmp/zotero-translation-server
-cat Dockerfile | envsubst | docker build -t zts . -f -
+docker build -t zts .
 rm -rf /tmp/zotero-translation-server
