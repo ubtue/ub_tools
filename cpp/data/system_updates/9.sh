2394445a1736906b32030aa12caa0a91fdaa68d1
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-05-25 11:02:39 +0200

    Update the IxTheo crontab.

---
+++ cpp/data/system_updates/9.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -o errexit
+
+
+if [[ $(hostname) == "ub16.uni-tuebingen.dex" ]]; then
+    if [ -e /var/spool/cron/root ]; then # CentOS
+        root_crontab=/var/spool/cron/root
+    else # Ubuntu
+        root_crontab=/var/spool/cron/crontabs/root
+    fi
+
+    # Make sure our crontab ends with a newline:
+    readonly $newline=$(echo -e \\n)
+    if [[ $(tail -c 1 $root_crontab) != $newline ]]; then
+        echo $newline >> $root_crontab
+    fi
+    
+    echo '0 0 1 * * "$BIN/cologne.sh' >> $root_crontab
+fi
