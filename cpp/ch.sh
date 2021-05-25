1c226f40d11ae54d42c3242ac26b7dec50f1ce65
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-05-25 13:47:37 +0200

    Adjustments for config file changes.

--- cpp/ch.sh
+++ cpp/ch.sh
@@ -2,7 +2,9 @@
 # Datenlieferung an schweizer Kooperationspartner.
 set -o errexit -o nounset
 
-PASSWORD="EsIst1FehlerAufgetreten"
+readonly CONFIG_PATH=/usr/local/var/lib/tuelib/cologne.conf
+readonly RECIPIENTS=$(inifile_lookup "$CONFIG_PATH" "" recipients)
+readonly PASSWORD=$(inifile_lookup "$CONFIG_PATH" "" password)
 
 cd /usr/local/ub_tools/bsz_daten
 newest=$(ls -t GesamtTiteldaten-??????.mrc | head -1)
@@ -14,9 +16,10 @@ rm --force /usr/local/vufind/public/docs/IxTheoDaten-*.xml.7z
 rm "${output}"
 chcon unconfined_u:object_r:usr_t:s0 /usr/local/vufind/public/docs/"${output}".7z
 send_email --sender=ixtheo@ub.uni-tuebingen.de \
-           --recipients=johannes.ruscheinski@uni-tuebingen.de,martin.fassnacht@uni-tuebingen.de,waldvogel@globethics.net,support@novalogix.ch \
+           --recipients=$RECIPIENTS \
            --subject="Neue IxTheo-Daten verfügbar" \
            --message-body="URL: http://ixtheo.de/docs/"${output}".7z\\n
+Das Passwort für die 7-Zip-Datei (https://de.wikipedia.org/wiki/7-Zip) lautet "$PASSWORD".\\n
 Das IxTheo Team\\n
 --\\n
 Falls es irgendwelche Fragen oder Probleme mit dieser Datenlieferung gab,\\n
