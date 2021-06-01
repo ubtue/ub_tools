6af7b10367975ba1b603db47e33c170fb2263a5e
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-06-01 11:42:33 +0200

    Faster and simplified.

--- cpp/core/download_core_bulk.sh
+++ cpp/core/download_core_bulk.sh
@@ -1,48 +1,37 @@
 #!/bin/bash
+set -o errexit -o nounset
 
 if [ $# != 1 ]; then
-    echo "Usage: $0 output_file (Must end in .json!)"
+    echo "Usage: $0 output_file"
     exit 1
 fi
 
 
-readonly output_file=$1
-if [[ ${output_file##*.} != "json" ]]; then
-    echo "$output_file does not end in .json"'!'
-    exit 1
-fi
-> "$output_file"
-
-
-readonly query_file=query_file.$$
-
-
-function CreateQueryFile {
-    declare -r -i offset=$1
-    declare -r -i pages_per_bulk=20
-    echo "[" > $query_file
-    for i in $(seq $((${pages_per_bulk} * ${offset} + 1)) $(((${pages_per_bulk} * ${offset}) + ${pages_per_bulk}))); do
-        cat >> $query_file << EOF
-    {
-        "page": $i, 
-        "pageSize": 100, 
-        "query": "criminolog*"
-    },
-EOF
-    done
-    sed --in-place '$s/,//g' ${query_file}
-    echo "]" >> $query_file
-}
+readonly OUTPUT_FILE=$1
+> "$OUTPUT_FILE"
 
 
 declare -r API_KEY=$(< /usr/local/var/lib/tuelib/CORE-API.key)
 declare -r CORE_API_URL=https://core.ac.uk/api-v2/articles/search
 declare -r -i CHUNK_COUNT=11
-
-
-for i in $(seq 0 $(($CHUNK_COUNT-1))); do
-    CreateQueryFile $i
-    curl --silent --header "Content-Type: application/json" --header "apiKey:${API_KEY}" \
-         --request POST --data-binary @$query_file $CORE_API_URL >> "$output_file"
-    sleep 15
+declare -r -i MAX_HITS_PER_PAGE=1000
+declare -r QUERY=criminolog*
+declare -i page_no=0
+> chunk
+while true; do
+    ((++page_no))
+    curl --silent --header "Content-Type: application/json" --header "apiKey:${API_KEY}" --request POST \
+         --data-binary "[{ \"page\":$page_no, \"pageSize\":$MAX_HITS_PER_PAGE, \"query\":\"$QUERY\" }]" \
+         $CORE_API_URL > chunk
+    if [[ $(jq '.[].status' < chunk) == '"Not found"' ]]; then break; fi
+    cat chunk >> $OUTPUT_FILE
+    sleep 2
 done
+rm --force chunk
+
+
+jq . < $OUTPUT_FILE > $OUTPUT_FILE.$$
+mv $OUTPUT_FILE.$$ $OUTPUT_FILE
+sed --in-place --regexp-extended --expression \
+    ':a;N;$!ba;s/\s+\}\n\]\n\[\n\s+\{\n\s+\"status\"\:\s*\"OK\",\n\s+\"totalHits\":\s+[0-9]+\s*,\n\s+\"data\"\:\s*\[\n/,\n/g' \
+    $OUTPUT_FILE
