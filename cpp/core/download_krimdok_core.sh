51c304dfceac533f390a56eb40b07966f5665f7c
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-06-01 16:59:42 +0200

    We now also generate MARC data from the downloaded JSON.

--- cpp/core/download_krimdok_core.sh
+++ cpp/core/download_krimdok_core.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 set -o errexit -o nounset
 
-if [ $# != 1 ]; then
-    echo "Usage: $0 output_file"
+if [ $# != 0 ]; then
+    echo "Usage: $0"
     exit 1
 fi
 
 
-readonly OUTPUT_FILE=$1
-> "$OUTPUT_FILE"
+readonly WORK_FILE=download_krimdok_core.json
+> "$WORK_FILE"
 
 
 declare -r API_KEY=$(< /usr/local/var/lib/tuelib/CORE-API.key)
@@ -23,17 +23,23 @@ while true; do
          --data-binary "[{ \"page\":$page_no, \"pageSize\":$MAX_HITS_PER_PAGE, \"query\":\"$QUERY\" }]" \
          $CORE_API_URL > chunk
     if [[ $(jq '.[].status' < chunk) == '"Not found"' ]]; then break; fi
-    cat chunk >> $OUTPUT_FILE
+    cat chunk >> "$WORK_FILE"
     sleep 2
 done
 rm --force chunk
 
 
-jq . < $OUTPUT_FILE > $OUTPUT_FILE.$$
-mv $OUTPUT_FILE.$$ $OUTPUT_FILE
+jq . < "$WORK_FILE" > "$WORK_FILE".$$
+mv "$WORK_FILE".$$ "$WORK_FILE"
 
 # The following sed expression merges the metadata arrays from the individually downloaded chunks into one huge
 # JSON array.  It also eliminates the "status" and "totalHits" entries at the boundaries between two arrays.
 sed --in-place --regexp-extended --expression \
     ':a;N;$!ba;s/\n\s+\]\n\s+}\n\]\n\[\n\s+\{\n\s+\"status\"\:\s*\"OK\",\n\s+\"totalHits\":\s+[0-9]+\s*,\n\s+\"data\"\:\s*\[\n/,\n/g' \
-    $OUTPUT_FILE
+    "$WORK_FILE"
+
+# Convert to MARC:
+declare -r MARC_OUTPUT=KrimDok-CORE-$(date +%Y%M%d).mrc
+convert_json_to_marc --create-unique-id-db /usr/local/var/lib/tuelib/core.conf \
+                     "$WORK_FILE" unmapped_issn.list "$MARC_OUTPUT"
+echo "Generated $MARC_OUTPUT, unmapped ISSN's are in unmapped_issn.list"
