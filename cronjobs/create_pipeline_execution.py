f1e728761089c40fe59ef2dd23f20a7ad6df7023
andreas-ub <andreas.nutz@uni-tuebingen.de>
2021-06-08 12:50:13 +0200

    added flag for title of norm, etc.

--- cronjobs/create_pipeline_execution.py
+++ cronjobs/create_pipeline_execution.py
@@ -5,6 +5,17 @@
 
 import argparse
 
+class Phase:
+    key = "" 
+    mode = ""
+    name = ""
+    title_norm = ""
+    command = ""
+    def __init__(self, _key, _mode, _title_norm):
+        self.key = _key
+        self.mode = _mode
+        self.title_norm = _title_norm
+
 # Dependency list has to be built dynamically later
 # Preprocessing
 # Check Record Integrity at the Beginning of the Pipeline
@@ -15,78 +26,64 @@ import argparse
 #   leading "_" can only run after predecessor has finished
 #   trailing "_" successor must not run before job has finished
 #   leading and trailing "_" has to run independently
-phases = {}
-phases["remove_dangling_references"] = ["Remove Dangling References"]
-phases["add_local_data_from_database"] = ["Add Local Data from Database"]
-phases["swap_and_delete_ppns"] = ["Swap and Delete PPN's in Various Databases"]
-phases["filter_856_etc"] = ["Filter out Self-referential 856 Fields AND Remove Sorting Chars From Title Subfields AND Remove blmsh Subject Heading Terms AND Fix Local Keyword Capitalisations AND Standardise German B.C. Year References"]
-phases["rewrite_authors_from_authority_data"] = ["Rewrite Authors and Standardized Keywords from Authority Data"]
-phases["add_missing_cross_links"] = ["Add Missing Cross Links Between Records"]
-phases["extract_translations"] = ["Extract Translations and Generate Interface Translation Files"]
-phases["transfer_880_to_750"] = ["Transfer 880 Authority Data Translations to 750"]
-phases["augment_authority_data_with_keyword_translations"] = ["Augment Authority Data with Keyword Translations"]
-phases["add_beacon_to_authority_data"] = ["Add BEACON Information to Authority Data"]
-phases["cross_link_articles"] = ["Cross Link Articles"]
-phases["normalize_urls"] = ["Normalise URL's"]
-phases["parent_to_child_linking"] = ["Parent-to-Child Linking and Flagging of Subscribable Items"]
-phases["populate_zeder_journal"] = ["Populate the Zeder Journal Timeliness Database Table"]
-phases["add_additional_open_access"] = ["Add Additional Open Access URL's"]
-phases["extract_normdata_translations"] = ["Extract Normdata Translations"]
-phases["add_author_synomyns_from_authority"] = ["Add Author Synonyms from Authority Data"]
-phases["add_aco_fields"] = ["Add ACO Fields to Records That Are Article Collections"]
-phases["add_isbn_issn"] = ["Adding of ISBN's and ISSN's to Component Parts"]
-phases["extract_keywords_from_titles"] = ["Extracting Keywords from Titles"]
-phases["flag_electronic_and_open_access"] = ["Flag Electronic and Open-Access Records"]
-phases["augment_bible_references"] = ["Augment Bible References"]
-phases["augment_canon_law_references"] = ["Augment Canon Law References"]
-phases["augment_time_aspect_references"] = ["Augment Time Aspect References"]
-phases["update_ixtheo_notations"] = ["Update IxTheo Notations"]
-phases["replace_689a_689q"] = ["Replace 689\$A with 689\$q"]
-phases["map_ddc_to_ixtheo_notations"] = ["Map DDC to IxTheo Notations"]
-phases["add_keyword_synonyms_from_authority_data"] = ["Add Keyword Synonyms from Authority Data"]
-phases["fill_missing_773a"] = ["Fill in missing 773\$a Subfields"]
-phases["tag_further_potential_relbib_entries"] = ["Tag further potential relbib entries"]
-phases["integrate_sort_year_for_serials"] = ["Integrate Reasonable Sort Year for Serials"]
-phases["integrate_refterms"] = ["Integrate Refterms"]
-phases["tag_tue_records_with_ita_field"] = ["Tag Records that are Available in Tübingen with an ITA Field"]
-phases["add_entries_for_subscription_bundles"] = ["Add Entries for Subscription Bundles and Tag Journals"]
-phases["add_tags_for_subsystems"] = ["Add Tags for subsystems"]
-phases["appending_literary_remains"] = ["Appending Literary Remains Records"]
-phases["tag_pda_candidates"] = ["Tag PDA candidates"]
-phases["patch_transitive_records"] = ["Patch Transitive Church Law, Religous Studies and Bible Studies Records"]
-phases["cross_link_type_tagging"] = ["Cross-link Type Tagging"]
-phases["tag_inferior_records"] = ["Tags Which Subsystems have Inferior Records in Superior Works Records"]
-phases["_check_end_of_pipeline"] = ["Check Record Integrity at the End of the Pipeline"]
-phases["_cleanup_"] = ["Cleanup of Intermediate Files"]
 
-#phases = {"3":"n.a","4":"n.a","5":"n.a","6":"n.a","14":"n.a","7":"n.a","8":"n.a","_9":"n.a","13":"n.a","a":"n.a","b":"n.a","c":"n.a","k":"n.a","m":"n.a","x":"n.a","z":"n.a"}
+phases = {
+        Phase("3","","t"),
+        Phase("4","","t"),
+        Phase("5","","t"),
+        Phase("6","","t"),
+        Phase("14","","t"),
+        Phase("7","","t"),
+        Phase("8","","t"),
+        Phase("9","_i","t"),
+        Phase("13","","t"),
+        Phase("a","","t"),
+        Phase("b","","t"),
+        Phase("c","","t"),
+        Phase("k","","t"),
+        Phase("m","","t"),
+        Phase("x","","t"),
+        Phase("z","","t")
+        }
+
+def get_phase_by_key(key):
+    for phase in phases:
+        if phase.key == key:
+            return phase
+    return None
+
+def contains_phase_title_norm(title_norm):
+    for phase in phases:
+        if phase.title_norm == title_norm:
+            return True
+    return False
 
 # e.g. ["5","3"] means that "5" can only be run after "3"
-deps = [
-        ["_cleanup_", "_check_end_of_pipeline"],
-        ["_check_end_of_pipeline", "tag_inferior_records"]
-       ]
-#deps = [["5","3"],["5","14"],["7","4"],["8","5"],["_9","7"],["6","13"],["3","13"],["b","a"],["c","a"],["m","k"],["z","x"],["z","k"],["c","b"]]
+deps = [["5","3"],["5","14"],["7","4"],["8","5"],["9","7"],["6","13"],["3","13"],["b","a"],["c","a"],["m","k"],["z","x"],["z","k"],["c","b"]]
 #deps = [["5","3"],["7","4"],["8","5"],["_9","7"],["6","13"],["3","13"],["3","8"]] #not valid
 
 def split_to_equal_parts(lst, n):
     chunks = []
     each_chunk = []
+    last_phase_title_norm = None
     for elem in lst:
-        if elem.startswith("_") == False:
+        phase_mode = get_phase_by_key(elem).mode
+        phase_title_norm = get_phase_by_key(elem).title_norm
+        if phase_mode.startswith("_") == False and (last_phase_title_norm == None or last_phase_title_norm == phase_title_norm):
             each_chunk.append(elem) 
         else:
             if len(each_chunk) > 0:
                 chunks.append(each_chunk)
                 each_chunk = []
             each_chunk.append(elem)
-        if elem.endswith("_") == True:
+        if phase_mode.endswith("_") == True:
             chunks.append(each_chunk)
             each_chunk = []
         if len(each_chunk) == n or elem == lst[-1]:
             if len(each_chunk) > 0:
                 chunks.append(each_chunk)
             each_chunk = []
+        last_phase_title_norm = phase_title_norm 
     return chunks
 
 
@@ -113,6 +110,18 @@ def Main():
     number_of_groups = args.par_runs # def. 2, 1 = without parallelization
     # Number of running phases in a running group
     items_in_group = args.run_steps
+
+    # Checking if all phases used in deps are defined
+    for elem in deps:
+        if not any(x.key == elem[0] for x in phases):
+            print(elem[0], " used in dependency: ", elem, " but not defined in phases")
+            exit()
+        if not any(x.key == elem[1] for x in phases):
+            print(elem[1], " used in dependency: ", elem, " but not defined in phases")
+            exit()
+        if get_phase_by_key(elem[0]).title_norm != get_phase_by_key(elem[1]).title_norm:
+            print("Dependecies of Phases must not be mixed: ", elem[0], " and ", elem[1])
+            exit()
 
     # determine independent groups
     # if both phases of a dependency have not yet occured -> create new group
@@ -155,7 +164,7 @@ def Main():
         print("Grouping possibilities dependencies only: ", grouped)
 
     # rearrange groups to number of parallel running groups
-    while len(grouped) > number_of_groups and len(grouped)>1:
+    while len(grouped) > number_of_groups and len(grouped) > 1:
         [min_position, min_list] = min_length_list(grouped)
         del grouped[min_position]
         [min_position_new, min_list_ignore] = min_length_list(grouped) #adding to new min after deletion
@@ -189,21 +198,13 @@ def Main():
 
     # Adding phases without dependencies
     for elem in phases:
-        if not contains_elem(deps, elem):
+        if not contains_elem(deps, elem.key):
             if len(totalorder) < number_of_groups:
-                totalorder.append([elem])
+                totalorder.append([elem.key])
             else:
                 [min_position, min_list_ignore] = min_length_list(totalorder)
-                totalorder[min_position].append(elem)
+                totalorder[min_position].append(elem.key)
 
-    # Checking if all phases used in deps are defined
-    for elem in deps:
-        if elem[0] not in phases:
-            print(elem[0], " used in dependency: ", elem, " but not defined in phases")
-            exit()
-        if elem[1] not in phases:
-            print(elem[1], " used in dependency: ", elem, " but not defined in phases")
-            exit()
 
     if is_debug:
         print("Added phases without dependencies: ", totalorder)
