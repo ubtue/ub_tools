6df5a6cb90b4e643e1c377962604151e5ea920ab
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-06-08 11:31:51 +0200

    Fixed another critical problem.

--- cpp/cologne.sh
+++ cpp/cologne.sh
@@ -16,11 +16,11 @@ rm --force /usr/local/vufind/public/docs/IxTheoDatenFürKöln-*.xml.7z
 rm "${output}"
 chcon unconfined_u:object_r:usr_t:s0 /usr/local/vufind/public/docs/"${output}".7z
 send_email --sender=ixtheo@ub.uni-tuebingen.de \
-           --recipients=martin.fassnacht@uni-tuebingen.de,"$EMAIL" \
+           --recipients="$RECIPIENTS" \
            --expand-newline-escapes \
            --subject="Neue IxTheo-Daten verfügbar" \
            --message-body="URL: http://ixtheo.de/docs/$(urlencode ${output}).7z"\\n
-Das Passwort für die 7-Zip-Datei (https://de.wikipedia.org/wiki/7-Zip) lautet "$PASSWORD".\\n
+Das Passwort für die 7-Zip-Datei (https://de.wikipedia.org/wiki/7-Zip) lautet: "$PASSWORD"\\n
 Das IxTheo Team\\n
 --\\n
 Falls es irgendwelche Fragen oder Probleme mit dieser Datenlieferung gab,\\n
