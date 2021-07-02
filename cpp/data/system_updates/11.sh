2b5c672d67576e4e0482dd7c163db113f2b0805b
Mario Trojan <mario.trojan@uni-tuebingen.de>
2021-07-02 13:35:19 +0200

    java: Remove solrmarc_mixin from makefile (& remove old binaries if they exist)

--- cpp/data/system_updates/11.sh
+++ cpp/data/system_updates/11.sh
@@ -5,3 +5,8 @@ FILE=/usr/local/vufind/import/lib/TuelibSolrmarcMixin.jar
 if [ -f "$FILE" ]; then
     rm -f "$FILE"
 fi
+
+DIR=/usr/local/ub_tools/java/solrmarc_mixin
+if [ -d "$DIR" ]; then
+    rm -d "$DIR"
+fi
