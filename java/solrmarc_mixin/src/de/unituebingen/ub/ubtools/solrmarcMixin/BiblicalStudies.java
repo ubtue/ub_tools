26251413f9d778de2a637a30c4ee784103b5fd96
Mario Trojan <mario.trojan@uni-tuebingen.de>
2021-07-02 09:34:17 +0200

    move solrmarcMixin from ub_tools/java to vufind/import/index_java, see ubtue/tuefind#1554

--- java/solrmarc_mixin/src/de/unituebingen/ub/ubtools/solrmarcMixin/BiblicalStudies.java
+++
@@ -1,9 +0,0 @@
-package de.unituebingen.ub.ubtools.solrmarcMixin;
-
-import org.marc4j.marc.Record;
-
-public class BiblicalStudies extends IxTheo {
-    public String getIsBiblicalStudies(final Record record) {
-        return record.getVariableFields("BIB").isEmpty() ? "false" : "true";
-    }
-}
