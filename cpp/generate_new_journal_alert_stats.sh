a63e341bb32a843b80a8b440c8f602e8e5512ed6
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-07-27 11:41:52 +0200

    Obsolete.

--- cpp/generate_new_journal_alert_stats.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-set -o errexit -o nounset
-
-readonly CONFIG_PATH=/usr/local/var/lib/tuelib/generate_new_journal_alert_stats.conf
-generate_new_journal_alert_stats relbib $(inifile_lookup "$CONFIG_PATH" "" relbib_recipients)
-generate_new_journal_alert_stats ixtheo $(inifile_lookup "$CONFIG_PATH" "" ixtheo_recipients)
