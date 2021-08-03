ef77461629bc98a94b904b7dafd52a2be495a14b
jriedl <johannes.riedl@uni-tuebingen.de>
2021-08-03 16:40:08 +0200

    More empty lines

--- cronjobs/extract_kalliope_originators.py
+++ cronjobs/extract_kalliope_originators.py
@@ -3,7 +3,9 @@ import re
 import sys
 import xml.etree.ElementTree as ET
 
+
 valid_gnd = re.compile('[0-9\-X]+')
+
 
 def ExtractKalliopeOriginators(filename):
     root = ET.parse(filename).getroot()
