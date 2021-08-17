804098e8a7d378fa017080c50e1f77b9f2e79591
Dr. Johannes Ruscheinski <johannes.ruscheinki@uni-tuebingen.de>
2021-08-17 08:43:42 +0200

    Renamed from 15.sh.

--- cpp/data/system_updates/16.sh
+++ cpp/data/system_updates/16.sh
@@ -1,24 +1,7 @@
 #!/bin/bash
-<<<<<<< HEAD
 set -o errexit -o nounset
 
 if [ -f /etc/lsb-release ]; then
     apt-get --quiet --yes install libpq-dev postgresql postgresql-client
 else
     dnf --assumeyes install libpq-devel postgresql
-=======
-#replace quotation due to error in 12.sh
-set -o errexit
-
-
-if [[ $TUEFIND_FLAVOUR == "ixtheo" ]]; then
-    if [ -e /var/spool/cron/root ]; then # CentOS
-        root_crontab=/var/spool/cron/root
-    elif [ -e /var/spool/cron/crontabs/root ]; then # Ubuntu
-        root_crontab=/var/spool/cron/crontabs/root
-    else
-        exit 0 # There is no crontab for root.
-    fi
-    sed --in-place --regexp-extended 's/("\$BIN\/generate_new_journal_alert_stats) (ixtheo|relbib) (days_in_last_month")/\1" "\2" "\3 "$EMAIL"/g' $root_crontab
->>>>>>> 75541ca1a806a180bb87347e8bce573fcb298fe7
-fi
