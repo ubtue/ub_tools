101316e345dd68f5e5007569520d896fd405b583
Mario Trojan <mario.trojan@uni-tuebingen.de>
2021-10-13 16:11:47 +0200

    remove duplicate semicolon

--- cpp/lib/src/MARC.cc
+++ cpp/lib/src/MARC.cc
@@ -1249,7 +1249,7 @@ std::set<std::string> Record::getISSNs() const {
         const std::string first_subfield_a(field.getFirstSubfieldWithCode('a'));
         std::string normalised_issn;
         if (MiscUtil::NormaliseISSN(first_subfield_a, &normalised_issn))
-            issns.insert(normalised_issn);;
+            issns.insert(normalised_issn);
     }
 
     return issns;
