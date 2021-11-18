98c1cce04f24cbacf2c2a6ca6f1cc5c7087910cd
jriedl <johannes.riedl@uni-tuebingen.de>
2021-11-18 13:40:14 +0100

    Table Lookup

--- cpp/keibi/convert_keibi_to_marc.cc
+++ cpp/keibi/convert_keibi_to_marc.cc
@@ -1,4 +1,4 @@
-/** \brief 
+/** \brief Convert the KeiBi Database entries to MARC 21 Records
  *  \author Johannes Riedl
  *
  *  \copyright 2021 Universitätsbibliothek Tübingen.  All rights reserved.
@@ -20,13 +20,16 @@
 #include <functional>
 #include <iostream>
 #include <iomanip>
-#include <sstream>
 #include <map>
+#include <sstream>
 #include "DbConnection.h"
+#include "FileUtil.h"
 #include "IniFile.h"
 #include "MARC.h"
+#include "StringUtil.h"
 #include "UBTools.h"
 #include "util.h"
+
 
 namespace {
 
@@ -34,19 +37,25 @@ namespace {
     ::Usage("db_inifile map_file marc_output");
 }
 
+
+
+using ConversionFunctor = std::function<void(const std::string, const char, MARC::Record * const, const std::string)>;
+
+
 struct DbFieldToMARCMapping {
     const std::string db_field_name_;
     const std::string marc_tag_;
     const char subfield_code_;
     std::function<void(MARC::Record * const, const std::string)> extraction_function_;
     DbFieldToMARCMapping(const std::string &db_field_name, const std::string marc_tag, const char subfield_code,
-                         std::function<void(const std::string, const char, MARC::Record * const, const std::string)> extraction_function) : 
+                         ConversionFunctor extraction_function) :
                          db_field_name_(db_field_name), marc_tag_(marc_tag),
                          subfield_code_(subfield_code),
                          extraction_function_(std::bind(extraction_function, marc_tag, subfield_code, std::placeholders::_1, std::placeholders::_2)) {}
 };
 
 const auto DbFieldToMarcMappingComparator = [](const DbFieldToMARCMapping &lhs, const DbFieldToMARCMapping &rhs) { return lhs.db_field_name_ < rhs.db_field_name_;};
+using DbFieldToMARCMappingMultiset = std::multiset<DbFieldToMARCMapping, decltype(DbFieldToMarcMappingComparator)>;
 
 
 void InsertField(const std::string &tag, const char subfield_code, MARC::Record * const record, const std::string &data) {
@@ -55,9 +64,9 @@ void InsertField(const std::string &tag, const char subfield_code, MARC::Record 
 }
 
 
-void ConvertCitations(DbConnection * const db_connection, 
-                      const std::multiset<DbFieldToMARCMapping, decltype(DbFieldToMarcMappingComparator)> &dbfield_to_marc_mappings,
-                      MARC::Writer * const marc_writer) 
+void ConvertCitations(DbConnection * const db_connection,
+                      const DbFieldToMARCMappingMultiset &dbfield_to_marc_mappings,
+                      MARC::Writer * const marc_writer)
 {
     db_connection->queryOrDie("SELECT * FROM citations");
     DbResultSet result_set(db_connection->getLastResultSet());
@@ -68,23 +77,60 @@ void ConvertCitations(DbConnection * const db_connection,
         formatted_number << std::setfill('0') << std::setw(8) << i;
         MARC::Record new_record(MARC::Record::TypeOfRecord::MIXED_MATERIALS, MARC::Record::BibliographicLevel::COLLECTION,
                                 "KEI" + formatted_number.str());
-        for (auto dbfield_to_marc_mapping(dbfield_to_marc_mappings.begin()); 
-             dbfield_to_marc_mapping != dbfield_to_marc_mappings.end(); 
+        for (auto dbfield_to_marc_mapping(dbfield_to_marc_mappings.begin());
+             dbfield_to_marc_mapping != dbfield_to_marc_mappings.end();
              ++dbfield_to_marc_mapping)
         {
             dbfield_to_marc_mapping->extraction_function_(&new_record, row[dbfield_to_marc_mapping->db_field_name_]);
 
         }
         marc_writer->write(new_record);
-
-
     }
 }
 
 
-void CreateDbFieldToMarcMappings(std::multiset<DbFieldToMARCMapping, decltype(DbFieldToMarcMappingComparator)> * const dbfield_to_marc_mappings) {
-    dbfield_to_marc_mappings->emplace(DbFieldToMARCMapping("title", "265", 'c', InsertField));
-    dbfield_to_marc_mappings->emplace(DbFieldToMARCMapping("author", "100", 'a', InsertField));
+const std::map<std::string, ConversionFunctor> name_to_functor_map {
+    {  "InsertField", InsertField }
+};
+
+ConversionFunctor GetConversionFunctor(const std::string &functor_name) {
+    if (not name_to_functor_map.contains(functor_name))
+        LOG_ERROR("Unknown functor " + functor_name);
+    return name_to_functor_map.find(functor_name)->second;
+}
+
+
+void ExtractTagAndSubfield(const std::string combined, std::string * tag, char * subfield_code) {
+    if (combined.length() != 4)
+        LOG_ERROR("Invalid Tag and Subfield format " + combined);
+    *tag = combined.substr(0,3);
+    *subfield_code = combined[3];
+}
+
+
+void CreateDbFieldToMarcMappings(File * const map_file, DbFieldToMARCMappingMultiset * const dbfield_to_marc_mappings) {
+    unsigned linenum(0);
+    while (not map_file->eof()) {
+        ++linenum;
+        std::string line;
+        map_file->getline(&line);
+        std::cerr << line << '\n';
+        std::vector<std::string> mapping;
+        StringUtil::SplitThenTrim(line, '|', " \t", &mapping);
+        if (unlikely(mapping.size() < 2)) {
+            LOG_WARNING("Invalid line format in line " + std::to_string(linenum));
+            continue;
+        }
+        std::string tag;
+        char subfield_code;
+        if (mapping.size() == 2) {
+            ExtractTagAndSubfield(mapping[1], &tag, &subfield_code);
+
+            dbfield_to_marc_mappings->emplace(DbFieldToMARCMapping(mapping[0], tag, subfield_code, GetConversionFunctor("InsertField")));
+        }
+    }
+//    dbfield_to_marc_mappings->emplace(DbFieldToMARCMapping("title", "265", 'c', InsertField));
+//    dbfield_to_marc_mappings->emplace(DbFieldToMARCMapping("author", "100", 'a', InsertField));
 }
 
 } // unnamed namespace
@@ -98,10 +144,11 @@ int Main(int argc, char *argv[]) {
     const std::string marc_output_path(argv[3]);
 
     DbConnection db_connection(DbConnection::MySQLFactory(IniFile(ini_file_path)));
+    std::unique_ptr<File> map_file(FileUtil::OpenInputFileOrDie(map_file_path));
     const std::unique_ptr<MARC::Writer> marc_writer(MARC::Writer::Factory(marc_output_path));
-    std::multiset<DbFieldToMARCMapping, decltype(DbFieldToMarcMappingComparator)> dbfield_to_marc_mappings(DbFieldToMarcMappingComparator);
-    CreateDbFieldToMarcMappings(&dbfield_to_marc_mappings);
-    ConvertCitations(&db_connection, dbfield_to_marc_mappings, marc_writer.get()); 
+    DbFieldToMARCMappingMultiset dbfield_to_marc_mappings(DbFieldToMarcMappingComparator);
+    CreateDbFieldToMarcMappings(map_file.get(), &dbfield_to_marc_mappings);
+    ConvertCitations(&db_connection, dbfield_to_marc_mappings, marc_writer.get());
 
     return EXIT_SUCCESS;
 }
