49bbf81f0d4218ee216af7798f8e7fae8350ff91
jriedl <johannes.riedl@uni-tuebingen.de>
2021-12-08 15:32:37 +0100

    Integrate word feature extraction

--- hkl_conversion/hkl_grammar.py
+++ hkl_conversion/hkl_grammar.py
@@ -3,18 +3,33 @@
 import re
 import regex
 import sys
+import nltk, re, pprint
+#nltk.download('punkt')
+#nltk.download('averaged_perceptron_tagger')
+from nltk.corpus import state_union
+from nltk.tokenize.punkt import PunktSentenceTokenizer, PunktTrainer
+nltk.download('stopwords')
+import random
+#from nltk.tokenize import PunktTrainer
 from lark import Lark, Transformer
+from langdetect import detect
 
 hkl_parser = Lark(r"""
 //    ?author_bibliography: bibliographic_item+ NL*
 //    ?bibliographic_item: full_citation_with_short_title
-    ?bibliographic_item: full_citation_with_short_title
-    full_citation_with_short_title: short_title /\s*=\s*/ full_title place " " year END_OF_SENTENCE note
+//    ?bibliographic_item: full_citation_with_short_title
+//    full_citation_with_short_title: short_title /\s*=\s*/ full_title place " " year END_OF_SENTENCE note
+    ?bibliographic_item: bibliographic_reference | place_and_year | title
+//    title.4: /\s*[^\s]+(\s+[^\s]+)*/
+    title.4: (WORD|NUMBER|BRACKET_EXPRESSION) ((IWS|NL|COW|"=") (WORD|NUMBER|BRACKET_EXPRESSION))*
     short_title: WORD (/\s+/ WORD)*
     full_title: sentence
-//    place: WORD (/\s+/ WORD)*
+    //place: WORD (/\s+/ WORD)*
+    bibliographic_reference.0: abbrev /\s+/ number
+    abbrev: /[A-Z]+/
+    place_and_year.2: place /\s+/ year
     place: WORD
-    note: (WORD|NUMBER) (IWS (WORD|NUMBER))* | sentence* (IWS sentence)*
+    note.3: (WORD|NUMBER) (IWS (WORD|NUMBER))* | sentence* (IWS sentence)*
     year: /[12]\d{3}/
     some_other_stuff: /[A-Z][a-z].*/
     non_empty_string: /[^\t\s\n\r,]+/
@@ -24,10 +39,10 @@ hkl_parser = Lark(r"""
     number: INT
     IWS : /[\s\t]+/
     NL : /\n/
-    END_OF_SENTENCE: "." (IWS|NL)*
+    END_OF_SENTENCE: /[.]/
     EMPTY_LINE_SEPARATOR: "\n\n"
     COW : IWS* "," IWS* // Comma with optional whitespace
-    WORD: /\w+/
+    WORD: /[\w']+/
 
 
 //    %import common._STRING_INNER
@@ -35,7 +50,7 @@ hkl_parser = Lark(r"""
 //    %import common.WORD
     %import common.NUMBER
 //    %import common.WS
-    """, start='bibliographic_item', ambiguity="explicit")
+    """, start='bibliographic_item', parser='earley')
 
 
 
@@ -61,31 +76,120 @@ def SplitToAuthorEntries(file):
 def ReduceMultipleEmptyLinesToOne(file):
     return re.sub(r'(\n\s*)+\n', '\n\n', file);
 
+
 def GetBufferLikeFile(file):
     return (line + '\n' for line in file.split('\n'))
+
 
 def FilterPageHeadings(file):
     return re.sub(r'(?:^\d+\n\n+[^,]+\n)|(?:^[^,]+\n\n+\d+\n)', '\n', file, flags=re.MULTILINE)
 
+
+def GetPunktSentenceTokenizer(file):
+    punkt_trainer = PunktTrainer()
+    abbreviations = "v. Chr., Sarg."
+    punkt_params = set(abbreviations)
+    punkt_sentence_trainer = PunktSentenceTokenizer()
+    punkt_trainer.train(file, finalize=False, verbose=False)
+    punkt_trainer.train(abbreviations, finalize=False, verbose=False)
+    return PunktSentenceTokenizer(punkt_trainer.get_params())
+
+
+def ContainsProbableEditionYear(sentence):
+    return bool(re.search(r'\s+[12][0789]\d{2}\b', sentence))
+
+
+def ContainsCf(sentence):
+    return bool(re.search(r'\bCf\b', sentence))
+
+
+def ContainsCapitalAbbreviation(sentence):
+    return bool(re.search(r'\b.[A-Z]+\b', sentence))
+
+
+def ContainsPageSequence(sentence):
+    return bool(re.search(r'\b\d+(ff?[.])\b', sentence))
+
+
+class GetFrequentlyUsedWordFeatures:
+    def __init__(self, traindata):
+        all_words = nltk.FreqDist(w for w in nltk.word_tokenize(' '.join(traindata)))
+        self.most_frequent_words_ = list(all_words)[:1000]
+    def __call__(self):
+        if self.most_frequent_words_ == {}:
+            raise Exception("Uninitialized frequencies")
+        return self.most_frequent_words_
+
+
+def GetWordFeatures(word_selection, sentence):
+    features = {}
+    sentence_words = set(nltk.word_tokenize(sentence))
+    for word in word_selection:
+        features['contains({})'.format(word)] = (word in sentence_words)
+    return features
+
+
+def ExtractFeatures(sentence):
+    try:
+        lang = detect(sentence)
+    except Exception as e:
+        lang = 'Unknown'
+
+    features = { 'lang' : lang, 'length': len(nltk.word_tokenize(sentence)), 'edition_years': ContainsProbableEditionYear(sentence),
+                 'contains_cf' : ContainsCf(sentence),  'contains_cap_abbrev' : ContainsCapitalAbbreviation(sentence),
+                 'contains_page_seq' : ContainsPageSequence(sentence),  **GetWordFeatures(word_selection(), sentence) }
+    return features
+
+
+def SetupWordFeatures(labeled):
+    all_trainsentences = list(map(lambda pair: pair[0], labeled))
+    global word_selection
+    word_selection = GetFrequentlyUsedWordFeatures(all_trainsentences)
+
+
+def CreateClassifier():
+    with open("./training/titles.txt") as titles:
+       titles = [ (title, 'title') for title in titles ]
+    with open("./training/comments.txt") as comments:
+       comments = [ (comment, 'comment') for comment in comments ]
+    with open("./training/bib_infos.txt") as bib_infos:
+       bib_infos = [ (bib_info, 'bib_info') for bib_info in bib_infos ]
+    with open("./training/years_and_places.txt") as years_and_places:
+       years_and_places = [ (year_and_place, 'year_and_place') for year_and_place in years_and_places ]
+    labeled = titles + comments + bib_infos + years_and_places
+    SetupWordFeatures(labeled)
+    random.shuffle(labeled)
+    training = [(ExtractFeatures(example), sentence_type) for (example, sentence_type) in labeled]
+    classifier = nltk.NaiveBayesClassifier.train(training)
+
+    return classifier
+
+
 def Main():
     try:
+         if len(sys.argv) != 2:
+             print("Invalid arguments: usage " + sys.argv[0] + " hkl_extraction_file")
+             sys.exit(-1)
          with open(sys.argv[1]) as f:
              file = ReduceMultipleEmptyLinesToOne(f.read())
              file = FilterPageHeadings(file)
-             #print(file)
              entries = SplitToAuthorEntries(GetBufferLikeFile(file))
-#             for author, entry in entries[:4]:
+             punkt_sentence_tokenizer = GetPunktSentenceTokenizer(file)
+             classifier = CreateClassifier()
              for author, entry in [entries[0]]:
                  print("---------------------------------------\nAUTHOR: " + author + '\n')
                  normalized_separations = re.sub(r'-\n', '', ''.join(entry))
                  normalized_newlines= re.sub(r'\n(?!\n)', ' ', normalized_separations)
-                 print(normalized_newlines)
                  print("#################################")
-                 #print(normalized_newlines.split('\n')[0])
-                 to_parse = "Assyrian manual = An Assyrian manual for the use of beginners in the study of the Assyrian language. Chicago 1886. 2. Auflage Chicago 1892 (mir nicht zugänglich). Nicht berücksichtigt."
+                 to_parse = normalized_newlines.split('\n')[2]
                  print(to_parse + '\n#########################################\n')
-                 #hkl_parser.parse(normalized_newlines.split('\n')[0] + '\n')
-                 print(hkl_parser.parse(to_parse).pretty())
+                 sentences = punkt_sentence_tokenizer.tokenize(to_parse)
+                 i = 0
+                 for sentence in sentences:
+                     print(sentence + " XXX " + classifier.classify(ExtractFeatures(sentence)))
+                     #i += 1
+                     #if i >= 10:
+                     #    break
     except Exception as e:
         print("ERROR: " + e)
 
