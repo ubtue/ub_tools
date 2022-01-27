f78c000566bf159ce791b0f5fa98448ea52725d5
andreas-ub <andreas.nutz@uni-tuebingen.de>
2022-01-27 09:44:00 +0100

    added system update script to remove old files

---
+++ cpp/data/system_updates/105.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit
+if [[ ! -e /usr/local/ub_tools/bsz_daten/beacon_downloads ]]; then
+    mkdir /usr/local/ub_tools/bsz_daten/beacon_downloads
+fi
+if [[ -f /usr/local/ub_tools/bsz_daten/kalliope.staatsbibliothek-berlin.beacon ]]; then
+    mv /usr/local/ub_tools/bsz_daten/kalliope.staatsbibliothek-berlin.beacon /usr/local/ub_tools/bsz_daten/beacon_downloads
+fi
+if [[ -f /usr/local/ub_tools/bsz_daten/archivportal-d.beacon ]]; then
+    mv /usr/local/ub_tools/bsz_daten/archivportal-d.beacon /usr/local/ub_tools/bsz_daten/beacon_downloads
+fi
