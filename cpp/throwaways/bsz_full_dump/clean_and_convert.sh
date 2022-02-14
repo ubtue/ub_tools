8251a5fbe10632d9b563622b8b5627e223a28303
jriedl <johannes.riedl@uni-tuebingen.de>
2022-02-14 15:22:37 +0100

    BSZ OD fulldump handling

---
+++ cpp/throwaways/bsz_full_dump/clean_and_convert.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+source_file=${1}
+intermediate="/mnt/zram/intermediate.xml"
+target_file=${source_file%.xml.gz}.mrc
+
+#Make sure zram is present
+echo -n "Check ZRAM..."
+test -e /mnt/zram && mountpoint --quiet /mnt/zram || { echo "ZRAM not mounted"; exit 1; }
+echo "Present"
+
+time zcat ${source_file} | sed -r -e 's/[<]([/])?marc:/<\1/g' | sed -r -e 's/(xmlns:marc=["]http:[/][/]www.loc.gov[/]MARC21[/]slim["])//g'  | sed -r -e 's/xmlns=["]{2}//' > ${intermediate}
+time marc_convert ${intermediate} ${target_file}
+> ${intermediate}
