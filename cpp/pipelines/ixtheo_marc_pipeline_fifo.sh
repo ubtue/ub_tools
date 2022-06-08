5967a6e4b93a616fbab98addf9dc38fdeccb2737
jriedl <johannes.riedl@uni-tuebingen.de>
2022-06-08 13:58:28 +0200

    Move LOK866 Embargo Links to ordinary 856

--- cpp/pipelines/ixtheo_marc_pipeline_fifo.sh
+++ cpp/pipelines/ixtheo_marc_pipeline_fifo.sh
@@ -395,6 +395,19 @@ make_named_pipe --buffer-size=$FIFO_BUFFER_SIZE GesamtTiteldaten-post-phase"$PHA
 EndPhase || Abort) &
 
 
+StartPhase "Copy Embargo Information to ordinary 856"
+make_named_pipe --buffer-size=$FIFO_BUFFER_SIZE GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc >> "${log}" 2>&1
+(marc_augmentor GesamtTiteldaten-post-phase"$((PHASE-1))"-"${date}".mrc \
+                GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc \
+                --insert-field-if-regex  '856u:/(EMBARGO#.*)/\1/' 'LOKx:EMBARGO#' \
+                --add-subfield-if-matching '8563:Volltext' '856u:EMBARGO#' \
+                --add-subfield-if-regex '856x:/EMBARGO#(.*)#(.*)/Embargo - \1/' '856u:EMBARGO#' \
+                --replace-subfield-if-regex '856u:/EMBARGO#(.*)#(.*)/\2/' '856u:EMBARGO#.*' \
+        >> "${log}" 2>&1 && \
+EndPhase || Abort) &
+
+
+
 StartPhase "Tag PDA candidates"
 # Use the most recent GVI PPN list.
 (augment_pda \
