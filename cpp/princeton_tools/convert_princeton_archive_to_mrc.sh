e5ef3a978aa625ec3287597407e082364efd4d1b
jriedl <johannes.riedl@uni-tuebingen.de>
2022-06-08 15:33:56 +0200

    Adjust xslt filename

--- cpp/princeton_tools/convert_princeton_archive_to_mrc.sh
+++ cpp/princeton_tools/convert_princeton_archive_to_mrc.sh
@@ -10,12 +10,11 @@ archive_file="$1"
 output_file="$2"
 archive_contents=$(7z l ${archive_file} | grep 'xml$' | awk '{ print $6 }')
 TOOL_BASE_PATH="/usr/local/ub_tools/cpp/princeton_tools"
-XSLT_FILE="MODS3-7_MARC21slim_XSLT1-0.xsl"
+XSLT_FILE="MODS3-7_MARC21slim_XSLT1-0_ubtue.xsl"
 PPN_BASE=$(echo ${archive_file} | sed -r -e 's/ptsem-([[:digit:]]+)\.zip/\1/')
-SKIP_LIST=()
+SKIP_LIST=('annbi00bibl.xml')
 
 > ${output_file}
-
 echo '<?xml version="1.0" encoding="UTF-8"?>' >> ${output_file}
 echo '<collection>' >> ${output_file}
 i=0
@@ -30,6 +29,7 @@ for content in ${archive_contents}; do
          continue
      fi
      7z x -so ${archive_file} ${content} | \
+         xmlstarlet ed -d '//_:subject[@authority="aat" or @authority="local" or @authority="rbgenr"]' | \
          xmlstarlet tr ${TOOL_BASE_PATH}/${XSLT_FILE} | \
          xmlstarlet ed -O -a //marc:leader -t elem -n 'marc:controlfield' -v "${ppn}"  \
                     --var new_node '$prev' -i '$new_node' -t attr -n "tag" -v "001" | \
