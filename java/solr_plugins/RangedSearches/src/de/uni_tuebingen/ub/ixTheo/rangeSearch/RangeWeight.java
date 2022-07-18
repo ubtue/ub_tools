972f5f14b27d47f19236b814945b43d2f5deda73
Mario Trojan <mario.trojan@uni-tuebingen.de>
2022-07-18 14:49:29 +0200

    RangeScorer / RangeWeight optimizations - Replace BitSetIterator / TwoPhaseIterator with FilteredIdSetIterator     - TwoPhaseIterator is experimental, BitSetIterator "for internal purposes only".     - FilteredDocIdSetIterator => DocIdSetIterator using another undelying DocIdSetIterator, which reflects our existing approach.     - Move iterator logic from RangeWeight to RangeScorer (less parameter passing needed) - Replace ConstantScoreWeight by Weight     - ConstantScoreWeight => "for internal purposes only". Also it is only meant for constant scores equal to the boost of the wrapped query, which is not what we do with RangedSearches (matches depend on document field values).     - Simple dummy implementation of explain() method to pass e.g. score information when using debug mode.

--- java/solr_plugins/RangedSearches/src/de/uni_tuebingen/ub/ixTheo/rangeSearch/RangeWeight.java
+++ java/solr_plugins/RangedSearches/src/de/uni_tuebingen/ub/ixTheo/rangeSearch/RangeWeight.java
@@ -2,29 +2,22 @@ package de.uni_tuebingen.ub.ixTheo.rangeSearch;
 
 
 import java.io.IOException;
-import com.carrotsearch.hppc.IntFloatHashMap;
-import com.carrotsearch.hppc.IntFloatMap;
+import java.util.Set;
 import org.apache.lucene.document.Document;
-import org.apache.lucene.index.LeafReader;
 import org.apache.lucene.index.LeafReaderContext;
-import org.apache.lucene.search.ConstantScoreWeight;
-import org.apache.lucene.search.DocIdSetIterator;
+import org.apache.lucene.index.Term;
+import org.apache.lucene.search.Explanation;
 import org.apache.lucene.search.Scorer;
-import org.apache.lucene.search.TwoPhaseIterator;
 import org.apache.lucene.search.Weight;
-import org.apache.lucene.util.BitSet;
-import org.apache.lucene.util.BitSetIterator;
-import org.apache.lucene.util.FixedBitSet;
 
 
-public abstract class RangeWeight extends ConstantScoreWeight {
+public abstract class RangeWeight extends Weight {
     protected final static float NOT_RELEVANT = Float.NEGATIVE_INFINITY;
     protected final Range[] ranges;
     protected final Weight weight;
-    protected final IntFloatMap scoring = new IntFloatHashMap();
 
     public RangeWeight(final RangeQuery query, final Range[] ranges, final Weight weight) {
-        super(query, 1.0F);
+        super(query);
         this.ranges = ranges;
         this.weight = weight;
     }
@@ -32,25 +25,29 @@ public abstract class RangeWeight extends ConstantScoreWeight {
     abstract protected boolean matches(final Document document);
     abstract protected float customScore(final Document doc);
 
-    protected BitSet getMatchingDocuments(final DocIdSetIterator iterator, final LeafReaderContext leafReaderContext) throws IOException {
-        final LeafReader reader = leafReaderContext.reader();
-        final BitSet matchingDocs = new FixedBitSet(reader.maxDoc());
-        for (int i = iterator.nextDoc(); i != DocIdSetIterator.NO_MORE_DOCS; i = iterator.nextDoc()) {
-            final Document doc = reader.document(i);
-            if (matches(doc)) {
-                matchingDocs.set(i);
-                scoring.put(i, customScore(doc));
-            }
+    // This is just a dummy implementation, override in subclass to get more detailed explanations.
+    // Add "debug=true" to a solr query to see the explanations in the response's debug section.
+    protected Explanation explain(final Document document) {
+        if (matches(document)) {
+            return Explanation.match(customScore(document), "match");
+        } else {
+            return Explanation.noMatch("no match");
         }
-        int index = matchingDocs.nextSetBit(0);
-        if (index >= 0 && index < matchingDocs.length()) {
-            return matchingDocs;
-        }
-        return null;
     }
 
     @Override
-    public final Scorer scorer(LeafReaderContext context) throws IOException {
+    public Explanation explain(LeafReaderContext context, int documentId) throws IOException {
+        return explain(context.reader().document(documentId));
+    }
+
+    @Override
+    @Deprecated
+    public void extractTerms(Set<Term> terms) {
+        // This is just a dummy, the function is deprecated (but mandatory) in Solr 8.
+    }
+
+    @Override
+    public final RangeScorer scorer(LeafReaderContext context) throws IOException {
         if (this.weight == null) {
             return null;
         }
@@ -58,23 +55,6 @@ public abstract class RangeWeight extends ConstantScoreWeight {
         if (scorer == null) {
             return null;
         }
-        final DocIdSetIterator iterator = scorer.iterator();
-        final BitSet matchingDocs = this.getMatchingDocuments(iterator, context);
-        if (matchingDocs != null) {
-            final DocIdSetIterator approximation = new BitSetIterator(matchingDocs, matchingDocs.length());
-            TwoPhaseIterator twoPhase = new TwoPhaseIterator(approximation) {
-                public boolean matches() throws IOException {
-                    int doc = this.approximation.docID();
-                    return matchingDocs.get(doc);
-                }
-
-                public float matchCost() {
-                    return 10.0F;
-                }
-            };
-            return new RangeScorer(this, scoring, twoPhase);
-        } else {
-            return null;
-        }
+        return new RangeScorer(this, this.weight, context);
     }
 }
