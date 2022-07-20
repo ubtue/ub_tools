6a54f9c78825f81ff8d2f7078c7ec0d05de99385
Adrii Lysohor <LysogorAnd@gmail.com>
2022-07-20 09:57:16 +0200

    RSS information test 2

--- cpp/test/SyndicationFormat_test.cc
+++ cpp/test/SyndicationFormat_test.cc
@@ -13,34 +13,25 @@
         "\t- rss_url: link to RSS data\n");
 }
 
-bool DownloadRSSData(const std::string &url, std::ofstream &newDataFile) {
+std::string DownloadRSSData(const std::string &url) {
     const std::string DOWNLOAD_URL(url);
 
     Downloader downloader(DOWNLOAD_URL, Downloader::Params(), 15 * 1000);
     if (downloader.anErrorOccurred()) {
         LOG_WARNING("Error while downloading data for RSS URL " + url + ": " + downloader.getLastErrorMessage());
-        return false;
     }
 
-    const std::string &parseDocument(downloader.getMessageBody());
-
-    newDataFile << parseDocument;
-
-    return true;
+    return downloader.getMessageBody();
 }
 
 int Main(int argc, char *argv[]) {
     if (argc != 2)
         Usage();
 
-    std::string fileName("/tmp/rssData");
-    std::ofstream newDataFile(fileName);
-    std::string RSSURL(argv[1]);
+    const std::string RSSURL(argv[1]);
 
-    DownloadRSSData(RSSURL, newDataFile);
-
-    const std::string xml(FileUtil::ReadStringOrDie(fileName));
-    SyndicationFormat::AugmentParams augmentParams;
+    const std::string xml(DownloadRSSData(RSSURL));
+    const SyndicationFormat::AugmentParams augmentParams;
     std::string errorMessage;
     const auto syndicationFormat(SyndicationFormat::Factory(xml, augmentParams, &errorMessage));
     if (not errorMessage.empty())
