42119719b685760fc6a927b16f5b11cccf4e074d
jriedl <johannes.riedl@uni-tuebingen.de>
2022-08-17 10:21:10 +0200

    Add minor randomized sleep interval

--- cpp/bibelwissenschaft/extract_names.sh
+++ cpp/bibelwissenschaft/extract_names.sh
@@ -11,8 +11,10 @@ OUTFILE_PREFIX=$2
 for bibwiss_type in "WiReLex" "WiBiLex"; do
     marc_grep $1 'if "TYP"=="'${bibwiss_type}'" extract "856u"' traditional | \
     awk '{print $2}' | \
-    xargs -I '{}' /bin/sh -c $'./translate_url_multiple "$1" | \
-    jq -r \'.[] | [ if ((.tags | length) != 0) then "Reference" else empty end, if ((.creators | length) != 0) then "Author" else empty end, .url, .title, .tags[].tag, (.creators[] | del(.creatorType) | flatten | reverse |join(","))] | to_entries | [.[].value] | @csv\'' _ '{}' \
+    xargs -I '{}' /bin/bash -c $'
+        interval=$((1 + $RANDOM % 3)); echo "Sleeping ${interval} seconds" >2; sleep ${interval};
+        ./translate_url_multiple "$1" | \
+        jq -r \'.[] | [ if ((.tags | length) != 0) then "Reference" else empty end, if ((.creators | length) != 0) then "Author" else empty end, .url, .title, .tags[].tag, (.creators[] | del(.creatorType) | flatten | reverse |join(","))] | to_entries | [.[].value] | @csv\'' _ '{}' \
     > ${OUTFILE_PREFIX}_${bibwiss_type}.csv
 done
 
