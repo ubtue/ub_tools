e896acdc7442b5951b35bc2ecf1fd16a98d3d94c
Steven Lolong <steven.lolong@uni-tuebingen.de>
2022-12-02 09:34:36 +0100

    channge the evaluation order of condition in ' 123.sh'

--- cpp/data/system_updates/123.sh
+++ cpp/data/system_updates/123.sh
@@ -3,12 +3,12 @@
 set -o errexit
 vuscript=/etc/profile.d/vufind.sh
 
-if !(grep --quiet "VUFIND_LOCAL_MODULES" "$vuscript") && [ $TUEFIND_FLAVOUR=="ixtheo" ] 
+if  [ $TUEFIND_FLAVOUR=="ixtheo" ] && !(grep --quiet "VUFIND_LOCAL_MODULES" "$vuscript")
 then
   sed --in-place '/VUFIND_LOCAL_DIR/ s/$/\nexport\ VUFIND_LOCAL_MODULES=TueFindSearch,TueFind,IxTheo/' "$vuscript"
 fi
 
-if !(grep --quiet "VUFIND_LOCAL_MODULES" "$vuscript") && [ $TUEFIND_FLAVOUR=="krimdok" ] 
+if [ $TUEFIND_FLAVOUR=="krimdok" ] && !(grep --quiet "VUFIND_LOCAL_MODULES" "$vuscript")  
 then
   sed --in-place '/VUFIND_LOCAL_DIR/ s/$/\nexport\ VUFIND_LOCAL_MODULES=TueFindSearch,TueFind,KrimDok/' "$vuscript"
 fi
