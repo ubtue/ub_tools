15195e8de30800a4e6477d1f8986e13ce76650a3
Adrii Lysohor <LysogorAnd@gmail.com>
2022-12-14 15:38:25 +0100

    Update DOI link 4

--- cpp/data/system_updates/123.vufind.sql
+++ cpp/data/system_updates/123.vufind.sql
@@ -1,1 +1,2 @@
-ALTER TABLE tuefind_publications ADD doi_notification DATETIME NULL AFTER external_document_guid;
+ALTER TABLE tuefind_publications ADD doi VARCHAR(255) NULL AFTER external_document_guid;
+ALTER TABLE tuefind_publications ADD doi_notification DATETIME NULL AFTER doi;
--- cpp/dspace/doi.cc
+++ cpp/dspace/doi.cc
@@ -20,36 +20,50 @@ using namespace std;
 const std::string DSPACE_SERVER_URL("https://publikationen.uni-tuebingen.de/rest/items/");
 const std::string DOI_KEY("dc.relation.uri");
 const std::string PUBLIKATION_TITLE_KEY("dc.title");
-unsigned int INSTANCE_COUNT(15000);
+const std::string DOI_URL_FORMAT("http://dx.doi.org/");
 const std::vector<std::string> NOTIFICATION_EMAIL_ADRESSES = { "andrii.lysohor@uni-tuebingen.de" };
 
-void SendNotificationsForDOI(const std::string email_subject, const std::string email_message) {
+void SendNotificationsForDOI(const std::string &email_subject, const std::string &email_message) {
     if (EmailSender::SimplerSendEmail("no-reply@ub.uni-tuebingen.de", NOTIFICATION_EMAIL_ADRESSES, email_subject, email_message,
                                       EmailSender::MEDIUM)
         > 299)
         LOG_ERROR("Failed to send the DOI notification email!");
 }
 
-void UpdateItem(DbConnection * const db_writer, const std::string publication_id) {
-    db_writer->queryOrDie("UPDATE tuefind_publications SET doi_notification = NOW() WHERE id= " + publication_id + "");
+void UpdateItem(DbConnection * const &db_writer, const std::string &doi, const std::string &publication_id) {
+    db_writer->queryOrDie("UPDATE tuefind_publications SET doi = " + db_writer->escapeAndQuoteString(doi)
+                          + ", doi_notification = NOW() WHERE id= " + db_writer->escapeAndQuoteString(publication_id));
 }
 
-std::string ParseDataFromXML(std::shared_ptr<const JSON::ArrayNode> top_node_array, const std::string fild) {
+std::string ParseDataFromXML(std::shared_ptr<const JSON::ArrayNode> top_node_array, const std::string &field) {
     std::string results;
     for (const auto &item_iter : *top_node_array) {
         const std::string key(JSON::LookupString("/key", item_iter, ""));
         const std::string value(JSON::LookupString("/value", item_iter, ""));
-        if (fild == key) {
+        if (field == key) {
             results = value;
         }
     }
     return results;
 }
 
-bool DownloadAndUpdate(DbConnection * const db_writer, const std::string &external_document_id, const std::string &publication_id) {
+std::string do_replace(const std::string &in, const std::string &from, const std::string &to) {
+    return std::regex_replace(in, std::regex(from), to);
+}
+
+std::string GetClearDOI(const std::string &doi_link) {
+    size_t substring_length = doi_link.find(DOI_URL_FORMAT);
+    if (substring_length > 0) {
+        return doi_link;
+    } else {
+        return do_replace(doi_link, DOI_URL_FORMAT, "");
+    }
+}
+
+bool DownloadAndUpdate(DbConnection * const &db_writer, const std::string &external_document_id, const std::string &publication_id) {
     const std::string DOWNLOAD_URL(DSPACE_SERVER_URL + external_document_id + "/metadata");
 
-    Downloader downloader(DOWNLOAD_URL, Downloader::Params(), INSTANCE_COUNT);
+    Downloader downloader(DOWNLOAD_URL, Downloader::Params());
     if (downloader.anErrorOccurred()) {
         EmailSender::SimplerSendEmail("no-reply@ub.uni-tuebingen.de", NOTIFICATION_EMAIL_ADRESSES,
                                       "Error while downloading data from DSpace API",
@@ -75,10 +89,11 @@ bool DownloadAndUpdate(DbConnection * const db_writer, const std::string &extern
 
     if (!doi_link.empty()) {
         const std::string publication_title = ParseDataFromXML(top_node_array, PUBLIKATION_TITLE_KEY);
-        LOG_INFO("Processing: " + doi_link);
-        UpdateItem(db_writer, publication_id);
+        const std::string doi = GetClearDOI(doi_link);
+        LOG_INFO("Processing: " + doi);
         SendNotificationsForDOI("DOI link notification",
-                                "DOI link: " + doi_link + " successfully generated for publication " + publication_title);
+                                "DOI link: " + doi + " successfully generated for publication " + publication_title);
+        UpdateItem(db_writer, doi, publication_id);
     }
 
     return true;
