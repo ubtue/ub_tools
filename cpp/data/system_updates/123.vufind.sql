8250776f88eea6cd3566d84ebf188b5ecddb2f3a
Adrii Lysohor <LysogorAnd@gmail.com>
2022-12-14 12:08:53 +0100

    Update DOI link 3

--- cpp/data/system_updates/123.vufind.sql
+++ cpp/data/system_updates/123.vufind.sql
@@ -1,2 +1,1 @@
-ALTER TABLE tuefind_publications ADD doi_link VARCHAR(255) NULL AFTER external_document_guid;
-ALTER TABLE tuefind_publications ADD doi_notification DATE NULL AFTER doi_link;
+ALTER TABLE tuefind_publications ADD doi_notification DATETIME NULL AFTER external_document_guid;
--- cpp/dspace/doi.cc
+++ cpp/dspace/doi.cc
@@ -17,35 +17,49 @@
 
 using namespace std;
 
-const std::string DspaceServerURL("https://publikationen.uni-tuebingen.de/rest/items/");
-const std::string DOIKey("dc.relation.uri");
-const std::string PublicationTitleKey("dc.title");
-const std::vector<std::string> notification_email_addresses = { "andrii.lysohor@uni-tuebingen.de" };
+const std::string DSPACE_SERVER_URL("https://publikationen.uni-tuebingen.de/rest/items/");
+const std::string DOI_KEY("dc.relation.uri");
+const std::string PUBLIKATION_TITLE_KEY("dc.title");
+unsigned int INSTANCE_COUNT(15000);
+const std::vector<std::string> NOTIFICATION_EMAIL_ADRESSES = { "andrii.lysohor@uni-tuebingen.de" };
 
 void SendNotificationsForDOI(const std::string email_subject, const std::string email_message) {
-    if (EmailSender::SimplerSendEmail("no-reply@ub.uni-tuebingen.de", notification_email_addresses, email_subject, email_message,
-                                      EmailSender::VERY_HIGH)
+    if (EmailSender::SimplerSendEmail("no-reply@ub.uni-tuebingen.de", NOTIFICATION_EMAIL_ADRESSES, email_subject, email_message,
+                                      EmailSender::MEDIUM)
         > 299)
         LOG_ERROR("Failed to send the DOI notification email!");
 }
 
-void UpdateItem(DbConnection * const db_writer, const std::string doi_link, const std::string publication_id) {
-    db_writer->queryOrDie("UPDATE tuefind_publications SET doi_link = " + db_writer->escapeAndQuoteString(doi_link)
-                          + ",doi_notification = CURDATE() WHERE id= " + publication_id + "");
+void UpdateItem(DbConnection * const db_writer, const std::string publication_id) {
+    db_writer->queryOrDie("UPDATE tuefind_publications SET doi_notification = NOW() WHERE id= " + publication_id + "");
 }
 
-bool DownloadAndUpdate(DbConnection * const db_writer, const std::string external_document_id, const std::string publicationID) {
-    const std::string DOWNLOAD_URL(DspaceServerURL + external_document_id + "/metadata");
+std::string ParseDataFromXML(std::shared_ptr<const JSON::ArrayNode> top_node_array, const std::string fild) {
+    std::string results;
+    for (const auto &item_iter : *top_node_array) {
+        const std::string key(JSON::LookupString("/key", item_iter, ""));
+        const std::string value(JSON::LookupString("/value", item_iter, ""));
+        if (fild == key) {
+            results = value;
+        }
+    }
+    return results;
+}
 
-    Downloader downloader(DOWNLOAD_URL, Downloader::Params(), 15 * 1000);
+bool DownloadAndUpdate(DbConnection * const db_writer, const std::string &external_document_id, const std::string &publication_id) {
+    const std::string DOWNLOAD_URL(DSPACE_SERVER_URL + external_document_id + "/metadata");
+
+    Downloader downloader(DOWNLOAD_URL, Downloader::Params(), INSTANCE_COUNT);
     if (downloader.anErrorOccurred()) {
-        LOG_WARNING("Error while downloading data for id : " + downloader.getLastErrorMessage());
+        EmailSender::SimplerSendEmail("no-reply@ub.uni-tuebingen.de", NOTIFICATION_EMAIL_ADRESSES,
+                                      "Error while downloading data from DSpace API",
+                                      "Error while downloading data for id : " + downloader.getLastErrorMessage(), EmailSender::MEDIUM);
         return false;
     }
 
     const HttpHeader http_header(downloader.getMessageHeader());
     if (http_header.getStatusCode() != 200) {
-        LOG_WARNING("DOI returned HTTP status code " + std::to_string(http_header.getStatusCode()) + "! for item id: " + publicationID);
+        LOG_WARNING("DOI returned HTTP status code " + std::to_string(http_header.getStatusCode()) + "! for item id: " + publication_id);
         return false;
     }
 
@@ -57,36 +71,24 @@ bool DownloadAndUpdate(DbConnection * const db_writer, const std::string externa
 
     const std::shared_ptr<const JSON::ArrayNode> top_node_array(JSON::JSONNode::CastToArrayNodeOrDie("full_tree", full_tree));
 
-    std::string PublicationTitle;
+    const std::string doi_link = ParseDataFromXML(top_node_array, DOI_KEY);
 
-    for (auto item_iter : *top_node_array) {
-        const std::string title_key(JSON::LookupString("/key", item_iter, ""));
-        const std::string title_value(JSON::LookupString("/value", item_iter, ""));
-        if (PublicationTitleKey == title_key) {
-            PublicationTitle = title_value;
-        }
-    }
-
-    for (auto item_iter : *top_node_array) {
-        const std::string one_key(JSON::LookupString("/key", item_iter, ""));
-        const std::string DOILink(JSON::LookupString("/value", item_iter, ""));
-        if (DOIKey == one_key) {
-            std::cout << "Processing: " << DOILink << '\n';
-            UpdateItem(db_writer, DOILink, publicationID);
-            SendNotificationsForDOI("DOI link notification",
-                                    "DOI link: " + DOILink + " successfully generated for publication " + PublicationTitle);
-        }
+    if (!doi_link.empty()) {
+        const std::string publication_title = ParseDataFromXML(top_node_array, PUBLIKATION_TITLE_KEY);
+        LOG_INFO("Processing: " + doi_link);
+        UpdateItem(db_writer, publication_id);
+        SendNotificationsForDOI("DOI link notification",
+                                "DOI link: " + doi_link + " successfully generated for publication " + publication_title);
     }
 
     return true;
 }
 
 int main() {
-    auto db_reader((DbConnection::VuFindMySQLFactory()));
-
     if (not VuFind::GetTueFindFlavour().empty()) {
         auto db_writer(DbConnection::VuFindMySQLFactory());
-        db_reader.queryOrDie("SELECT * FROM tuefind_publications WHERE doi_link IS NULL OR doi_link=''");
+        auto db_reader((DbConnection::VuFindMySQLFactory()));
+        db_reader.queryOrDie("SELECT * FROM tuefind_publications WHERE doi_notification IS NULL");
         auto result_set(db_reader.getLastResultSet());
         DbTransaction transaction(&db_writer);
         while (const auto row = result_set.getNextRow()) {
