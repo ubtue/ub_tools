79fa653f132a7b79abcc0e082e9497ee8a5eee29
jriedl <johannes.riedl@uni-tuebingen.de>
2022-12-20 10:52:29 +0100

    Utitily to extract GND numbers

---
+++ cpp/reference_works/degruyter/extract_unique_names_and_associate.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -o errexit -o nounset -o pipefail
+
+
+if [ $# != 2 ]; then
+    echo "Usage: $0 marc_file unique_authors.txt"
+    exit 1
+fi
+
+
+marc_file="$1"
+authors_file="$2"
+
+
+marc_grep ${marc_file} 'if "100a" exists extract "100a"' traditional \
+    | sed --regexp-extended -e 's/^100 //' > ${authors_file} 
+
+marc_grep ${marc_file} 'if "700a" exists extract "700a"' traditional \
+    | sed --regexp-extended -e 's/^700 //' >> ${authors_file}
+
+
+cat ${authors_file} | sort | uniq | sponge ${authors_file}
+
+
+export AUTHOR_LOOKUP_PATH="/usr/local/ub_tools/cpp/reference_works/bibelwissenschaft"
+cat ${authors_file} | \
+    xargs -I'{}'  /bin/bash -c 'echo -e "$@":\\t$(${AUTHOR_LOOKUP_PATH}/swb_author_lookup --sloppy-filter "$@")' _ '{}' | sponge ${authors_file}
