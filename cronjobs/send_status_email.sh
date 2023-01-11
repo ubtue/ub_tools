f29bf6ed94e176dfb506eaa80994f46aca2a045d
jriedl <johannes.riedl@uni-tuebingen.de>
2023-01-11 16:03:15 +0100

    Fix typo

--- cronjobs/send_status_email.sh
+++ cronjobs/send_status_email.sh
@@ -20,4 +20,4 @@ log_dir="$4"
 priority=$( [ "$program_exit_code" -eq 0 ] && echo "low" || echo "high" )
 body=$( [ $program_exit_code -eq 0   ] && echo "--message-body=Success" || \
         echo --message-body-file="$log_dir/$program_base_name.log" )
-        send_email --recipients="$email" --priority="$priority" --subject=" $program_base_name (from $(hostname))" "$body"
+        send_email --recipients="$email" --priority="$priority" --subject="$program_base_name (from $(hostname))" "$body"
