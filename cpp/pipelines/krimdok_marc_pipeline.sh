cccd8ad8e562aed8217379d405b6b24102ceb49e
Steven Lolong <steven.lolong@uni-tuebingen.de>
2023-01-13 10:31:00 +0100

    add 'Add Wikidata IDs to Authority Data' in the file 'krimdok_marc_pipelines.sh'

--- cpp/pipelines/krimdok_marc_pipeline.sh
+++ cpp/pipelines/krimdok_marc_pipeline.sh
@@ -171,12 +171,18 @@ StartPhase "Add BEACON Information to Authority Data"
 EndPhase || Abort) &
 wait
 
+StartPhase "Add Wikidata IDs to Authority Data"
+(add_authority_external_ref Normdaten-partially-augmented2-"${date}".mrc \
+                            Normdaten-partially-augmented3-"${date}".mrc \
+                            /usr/local/var/lib/tuelib/gnd_to_wiki.csv >> "${log}" 2>&1 && \
+EndPhase || Abort) &
+wait
 
 StartPhase "Appending Literary Remains Records"
 (create_literary_remains_records --no-subsystems \
                                  GesamtTiteldaten-post-phase"$((PHASE-2))"-"${date}".mrc \
                                  GesamtTiteldaten-post-phase"$PHASE"-"${date}".mrc \
-                                 Normdaten-partially-augmented2-"${date}".mrc \
+                                 Normdaten-partially-augmented3-"${date}".mrc \
                                  Normdaten-fully-augmented-"${date}".mrc >> "${log}" 2>&1 && \
 EndPhase || Abort) &
 wait
