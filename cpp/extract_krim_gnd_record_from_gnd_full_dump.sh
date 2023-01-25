12c8654e884aa14f9ac8926b94f1a8aef66817e8
jriedl <johannes.riedl@uni-tuebingen.de>
2023-01-25 11:05:57 +0100

    Handle redirect file truncation problem

--- cpp/extract_krim_gnd_record_from_gnd_full_dump.sh
+++ cpp/extract_krim_gnd_record_from_gnd_full_dump.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -o errexit -o nounset
 
-if [[ $# != 2 ]]; then
-    echo "Usage: $0 krim_keywords.mrc gnd_full_dump_mrc"
+if [[ $# != 3 ]]; then
+    echo "Usage: $0 krim_keywords.mrc gnd_full_dump_mrc out_file.mrc"
     exit 1
 fi
 
@@ -11,18 +11,21 @@ krim_extract_commands=$(mktemp /tmp/krim_extract_commands_XXX.sh)
 chmod 755 ${krim_extract_commands}
 
 function CleanUp {
-    rm ${krim_extract_commands}
+    rm -i ${krim_extract_commands}
 }
 
 trap CleanUp EXIT
 
 krim_keywords="$1"
 export gnd_full_dump="$2"
+export out_file="$3"
 
 
+>${out_file}
 marc_grep ${krim_keywords} '"024a"' traditional | awk  '{print $2}' | 
-    xargs  -I'{}' -n 1000 /bin/bash -c  $'echo marc_grep "${gnd_full_dump}" \\\'if \'"024a"=="\'$(echo \("$@"\) | tr " " \'|\' | sed -r -e \'s/[|]$//\')\'"\'extract *\\\' marc_binary' '{}' > ${krim_extract_commands}
+  xargs  -I'{}' -n 1000 /bin/bash -c  $'echo marc_grep "${gnd_full_dump}" \\\'if \'"024a"=="\'$(echo \("$@"\) | tr " " \'|\' | sed -r -e \'s/[|]$//\')\'"\' extract *\\\' marc_binary' '{}' > ${krim_extract_commands}
 
-${krim_extract_commands}
+sed -i '1i #!/bin/bash' ${krim_extract_commands}
+${krim_extract_commands} | sponge ${out_file}
 
 
