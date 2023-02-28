df402cc6c082be29864e4f9d8f19fd038ca7eb3a
jriedl <johannes.riedl@uni-tuebingen.de>
2023-02-28 17:59:21 +0100

    Rewrite for marc_filter input

--- cpp/reference_works/brill/metadata_conversion/associate_rgg4_titles.sh
+++ cpp/reference_works/brill/metadata_conversion/associate_rgg4_titles.sh
@@ -12,20 +12,35 @@ web_titles="$2"
 output="$3"
 
 function RemoveTempFiles {
-    rm ${tmpfile1} 
+    rm ${tmpfile1} ${tmpfile2}
 }
 
 trap RemoveTempFiles EXIT
 tmpfile1=$(mktemp -t asisociate_rgg4XXXXX.txt)
+tmpfile2=$(mktemp -t asisociate_rgg4XXXXX.txt)
 ./associate_rgg4_titles ${orig_titles} ${web_titles} ${tmpfile1}
 cat <(cat ${tmpfile1} | grep -v '|||' | sed -rn '/\w+:$/q;p') | \
     `#Replace double expressions to save names etc` \
-    sed -re 's/(\b(\w+)\s+\2\b(.*))\s[|].*/\1 | \2\3/' #| \
-    `#Escape ->`
-# Get Multicandidates
-cat <(cat ${tmpfile1} | sed -rn '/\w+:$/,$ p' | grep -v '||||')
+    sed -re 's/(\b(\w+)\s+\2\b(.*))\s[|].*/\1 | \2\3/' | \
+    `#Escape ->` \
+    sed -re 's/->/\\-\\>/g' | \
+    sed -re 's/ [|] /->/' \
+    > ${tmpfile2}
+
+# Get multicandidates
+#cat <(cat ${tmpfile1} | sed -rn '/\w+:$/,$ p' | grep -v '||||')
 # Get unassociated
 cat  <(cat ${tmpfile1} | grep '||||') | \
     `#Replace double expressions, but replace strange space character before` \
     sed -re 's/ / /g' | \
-    sed -re 's/(((\b\w+\b\s\b[^.]+\b)[.]?)\s+\3(.*))\s[|]+.*/\1 | \2/'
+    sed -re 's/(((\b\w+\b\s\b[^.]+\b)[.]?)\s+\3(.*))\s[|]+.*/\1 | \2/' | \
+    sed -re 's/ [|] /->/' | \
+    grep -v '||||' >> ${tmpfile2}
+
+
+    cat ${tmpfile2} | \
+    sed -re 's/[(]/\\(/g' | \
+    sed -re 's/[)]/\\)/g' | \
+    sed -re 's/[[]/\\[/g' | \
+    sed -re 's/[]]/\\]/g' \
+    > ${output}
