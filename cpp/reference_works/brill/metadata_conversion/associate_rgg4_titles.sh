b3733fd3f9f681c541e1a0e7e276645d5c99086e
jriedl <johannes.riedl@uni-tuebingen.de>
2023-03-02 11:47:46 +0100

    Integrate RGG4 cleanup

--- cpp/reference_works/brill/metadata_conversion/associate_rgg4_titles.sh
+++ cpp/reference_works/brill/metadata_conversion/associate_rgg4_titles.sh
@@ -12,12 +12,13 @@ web_titles="$2"
 output="$3"
 
 function RemoveTempFiles {
-    rm ${tmpfile1} ${tmpfile2}
+    rm ${tmpfile1} ${tmpfile2} ${tmpfile3}
 }
 
 trap RemoveTempFiles EXIT
-tmpfile1=$(mktemp -t asisociate_rgg4XXXXX.txt)
-tmpfile2=$(mktemp -t asisociate_rgg4XXXXX.txt)
+tmpfile1=$(mktemp -t associate_rgg4XXXXX.txt)
+tmpfile2=$(mktemp -t associate_rgg4XXXXX.txt)
+tmpfile3=$(mktemp -t associate_rgg4XXXXX.txt)
 ./associate_rgg4_titles ${orig_titles} ${web_titles} ${tmpfile1}
 cat <(cat ${tmpfile1} | grep -v '|||' | sed -rn '/\w+:$/q;p') | \
     `#Replace double expressions to save names etc` \
@@ -28,15 +29,15 @@ cat <(cat ${tmpfile1} | grep -v '|||' | sed -rn '/\w+:$/q;p') | \
     > ${tmpfile2}
 
 # Get multicandidates
-#cat <(cat ${tmpfile1} | sed -rn '/\w+:$/,$ p' | grep -v '||||')
+cat <(cat ${tmpfile1} | sed -rn '/\w+:$/,$ p' | grep -v '||||') > ${output%.txt}_multicandidates.txt
 # Get unassociated
 cat  <(cat ${tmpfile1} | grep '||||') | \
     `#Replace double expressions, but replace strange space character before` \
     sed -re 's/ / /g' | \
     sed -re 's/(((\b\w+\b\s\b[^.]+\b)[.]?)\s+\3(.*))\s[|]+.*/\1 | \2/' | \
     sed -re 's/ [|] /->/' | \
+    tee ${tmpfile3} | \
     grep -v '||||' >> ${tmpfile2}
-
 
     cat ${tmpfile2} | \
     sed -re 's/[(]/\\(/g' | \
@@ -44,3 +45,5 @@ cat  <(cat ${tmpfile1} | grep '||||') | \
     sed -re 's/[[]/\\[/g' | \
     sed -re 's/[]]/\\]/g' \
     > ${output}
+
+cat ${tmpfile3} | grep '||||' > ${output%.txt}_unassociated.txt
