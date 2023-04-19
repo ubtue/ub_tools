a51d0f4397045f04b47939f8d43adfa62a88a658
Mario Trojan <mario.trojan@uni-tuebingen.de>
2023-04-19 13:50:48 +0200

    REMID: Prepare iteration of results with > 1000 records

--- cpp/harvest/remid.sh
+++ cpp/harvest/remid.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 # This file's main purpose is documentation
+set -o errexit -o nounset
 
-FILE_ORI="remid.xml"
-FILE_TEMP="remid_tmp.xml"
-FILE_TEMP1="remid_tmp1.xml"
-FILE_TEMP_CLEAN="remid_tmp_clean.xml"
-FILE_NEW="ixtheo_remid_`date +'%y%m%d'`_001.xml"
+declare -r FILE_ORI="remid.xml"
+declare -r FILE_TEMP="remid_tmp.xml"
+declare -r FILE_TEMP1="remid_tmp1.xml"
+declare -r FILE_TEMP_CLEAN="remid_tmp_clean.xml"
+declare -r FILE_NEW="ixtheo_remid_`date +'%y%m%d'`_001.xml"
 
 # Download MARC XML from Hebis via SRU
-# (be sure to increase the "maximumRecords" parameter if necessary, right now there are ~500 records)
-wget 'http://sru.hebis.de/sru/DB=2.1?query=pica.abr+%3D+%22REMID%22+or+pica.prv+%3D+%22REMID%22&version=1.1&operation=searchRetrieve&stylesheet=http%3A%2F%2Fsru.hebis.de%2Fsru%2F%3Fxsl%3DsearchRetrieveResponse&recordSchema=marc21&maximumRecords=10000&startRecord=1&recordPacking=xml&sortKeys=LST_Y%2Cpica%2C0%2C%2C' -O $FILE_ORI
+
+# maximumRecords is limited to 1000. Since we have about 1300 datasets,
+# we need to download the data in multiple steps and merge them together afterwards
+declare -r RECORDS_PER_CALL=1000
+startRecord=1
+wget "http://sru.hebis.de/sru/DB=2.1?query=pica.abr+%3D+%22REMID%22+or+pica.prv+%3D+%22REMID%22&version=1.1&operation=searchRetrieve&recordSchema=marc21&maximumRecords=${RECORDS_PER_CALL}&startRecord=${startRecord}&recordPacking=xml&sortKeys=LST_Y%2Cpica%2C0%2C%2C" -O $FILE_ORI
 
 # Remove certain tags from non-MARC namespaces so we can use our tools
 grep -vE 'xml-stylesheet|srw:|diag:|:dc|:diag|:xcql|<startRecord|</startRecord|<maximumRecords|</maximumRecords|<sortKeys|</sortKeys|<rob:|</rob:' $FILE_ORI > $FILE_TEMP
@@ -22,8 +27,6 @@ echo '</collection>' >> $FILE_TEMP
 
 # Delete empty line
 sed -i '/^[[:space:]]*$/d' $FILE_TEMP
-
-# currently waiting for @relhei to specify which metadata needs to be changed before Uploading to BSZ.
 
 # remove tag(s) to avoid duplication of field with the same sub-field content and value
 marc_filter $FILE_TEMP $FILE_TEMP_CLEAN --remove-fields '852:  \x1FaDE-Tue135' \
