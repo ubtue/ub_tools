f4b0f50e84e3e6d04391ce312e754ecb6e2ca7be
Steven Lolong <steven.lolong@uni-tuebingen.de>
2023-05-05 08:28:04 +0200

    just remove unnecessary variable assignment

--- cpp/harvest/marc_issn_lookup.cc
+++ cpp/harvest/marc_issn_lookup.cc
@@ -73,7 +73,6 @@ struct CacheEntry {
 
     CacheEntry(MARC::Record &record) {
         is_valid_ = false;
-        is_online_ = false;
         is_online_ = record.isElectronicResource();
 
         for (auto &field : record) {
