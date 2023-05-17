471ad058ca753ac45dc66c5bd1b1b100dbe465a1
jriedl <johannes.riedl@uni-tuebingen.de>
2023-05-17 17:25:10 +0200

    Fix compile problem

--- cpp/keibi/generate_authority_data_from_afo_register.cc
+++ cpp/keibi/generate_authority_data_from_afo_register.cc
@@ -21,8 +21,8 @@
 #include <iostream>
 #include <string>
 #include <unordered_set>
-#include "MARC.h"
 #include "FileUtil.h"
+#include "MARC.h"
 #include "StringUtil.h"
 #include "TextUtil.h"
 #include "util.h"
@@ -44,20 +44,17 @@ struct AfOEntry {
     std::string literature_reference_;
     std::string comment_;
 
-    AfOEntry(const unsigned &entry_num, const std::string &keyword,
-             const std::string &internal_reference_keyword,
-             const std::string &literature_reference, const std::string &comment) :
-             entry_num_(entry_num), keyword_(keyword),
-             internal_reference_keyword_(internal_reference_keyword),
-             literature_reference_(literature_reference),
-             comment_(comment) {}
-    AfOEntry(const std::string keyword) : AfOEntry(0, keyword, "", "", "") {}
+    AfOEntry(const unsigned &entry_num, const std::string &keyword, const std::string &internal_reference_keyword,
+             const std::string &literature_reference, const std::string &comment)
+        : entry_num_(entry_num), keyword_(keyword), internal_reference_keyword_(internal_reference_keyword),
+          literature_reference_(literature_reference), comment_(comment) { }
+    AfOEntry(const std::string keyword): AfOEntry(0, keyword, "", "", "") { }
     bool operator==(const AfOEntry &rhs) const { return keyword_ == rhs.keyword_; }
-    std::string toString() const { return std::to_string(entry_num_) + " AAA " + keyword_  + " BBB " + internal_reference_keyword_
-                                    + " CCC " + literature_reference_ +  " DDD "  + comment_;
+    std::string toString() const {
+        return std::to_string(entry_num_) + " AAA " + keyword_ + " BBB " + internal_reference_keyword_ + " CCC " + literature_reference_
+               + " DDD " + comment_;
     }
-    __attribute__((unused))
-    friend std::ostream &operator<<(std::ostream &output, const AfOEntry &entry);
+    __attribute__((unused)) friend std::ostream &operator<<(std::ostream &output, const AfOEntry &entry);
 };
 
 
@@ -66,14 +63,13 @@ std::ostream &operator<<(std::ostream &output, const AfOEntry &entry) {
     return output;
 }
 
-} // unamed namespace
+} // namespace
 
 namespace std {
 
-template <> struct hash<AfOEntry> {
-    inline size_t operator()(const AfOEntry &afo_entry) const {
-        return hash<std::string>()(afo_entry.keyword_);
-    }
+template <>
+struct hash<AfOEntry> {
+    inline size_t operator()(const AfOEntry &afo_entry) const { return hash<std::string>()(afo_entry.keyword_); }
 };
 
 } // namespace std
@@ -91,17 +87,17 @@ void AddToAfOMultiset(const std::string &afo_file_path, AfOMultiSet * const afo_
     TextUtil::ParseCSVFileOrDie(afo_file_path, &lines, '\t', '\0');
     unsigned linenum(0);
     for (auto &line : lines) {
-       ++linenum;
-       if (not StringUtil::IsUnsignedNumber(line[0])) {
-           LOG_WARNING("Invalid content in line " + std::to_string(linenum) + "(" + StringUtil::Join(line, '\t') + ")");
-           continue;
-       }
+        ++linenum;
+        if (not StringUtil::IsUnsignedNumber(line[0])) {
+            LOG_WARNING("Invalid content in line " + std::to_string(linenum) + "(" + StringUtil::Join(line, '\t') + ")");
+            continue;
+        }
 
-       // Add missing columns
-       for (auto i = line.size(); i < ROWS_IN_CSV; ++i)
-           line.push_back("");
-       AfOEntry afo_entry(std::stoi(line[0]), line[1], line[2], line[3], line[4]);
-       afo_multi_set->emplace(afo_entry);
+        // Add missing columns
+        for (auto i = line.size(); i < ROWS_IN_CSV; ++i)
+            line.push_back("");
+        AfOEntry afo_entry(std::stoi(line[0]), line[1], line[2], line[3], line[4]);
+        afo_multi_set->emplace(afo_entry);
     }
 }
 
@@ -117,7 +113,7 @@ void CleanCSVAndWriteToTempFile(const std::string &afo_file_path, FileUtil::Auto
     }
 }
 
-MARC::Record * CreateNewRecord(const std::string &ppn, const AfOMultiSetRange &range) {
+MARC::Record *CreateNewRecord(const std::string &ppn, const AfOMultiSetRange &range) {
     if (range.first == std::end(AfOMultiSet()))
         return nullptr;
 
@@ -125,14 +121,14 @@ MARC::Record * CreateNewRecord(const std::string &ppn, const AfOMultiSetRange &r
     new_record->insertControlField("001", ppn);
     if (unlikely(range.first->keyword_.empty())) {
         std::cerr << "KEYWORD EMPTY\n";
-        new_record->insertField("150", { { 'a',  "EMPTY KEYWORD" } });
+        new_record->insertField("150", { { 'a', "EMPTY KEYWORD" } });
     } else
-        new_record->insertField("150", { { 'a',  range.first->keyword_ } });
+        new_record->insertField("150", { { 'a', range.first->keyword_ } });
     for (auto entry = range.first; entry != range.second; ++entry) {
         if (not entry->literature_reference_.empty())
             new_record->insertField("500", { { 'a', entry->literature_reference_ } });
         if (not entry->comment_.empty())
-            new_record->insertField("510", { { 'a', entry->comment_ } } );
+            new_record->insertField("510", { { 'a', entry->comment_ } });
         if (not entry->internal_reference_keyword_.empty())
             new_record->insertField("530", { { 'a', entry->internal_reference_keyword_ } });
     }
@@ -150,7 +146,6 @@ std::string AssemblePPN(unsigned id) {
 } // unnamed namespace
 
 
-
 int Main(int argc, char *argv[]) {
     if (argc < 3)
         Usage();
@@ -158,10 +153,10 @@ int Main(int argc, char *argv[]) {
     const std::string marc_output_path(argv[argc - 1]);
     std::vector<std::string> afo_file_paths;
     for (int arg_index = 1; arg_index < argc - 1; ++arg_index)
-         afo_file_paths.emplace_back(argv[arg_index]);
+        afo_file_paths.emplace_back(argv[arg_index]);
 
     AfOMultiSet afo_multi_set;
-    for (const auto afo_file_path : afo_file_paths) {
+    for (const auto &afo_file_path : afo_file_paths) {
         FileUtil::AutoTempFile tmp_file;
         CleanCSVAndWriteToTempFile(afo_file_path, &tmp_file);
         AddToAfOMultiset(tmp_file.getFilePath(), &afo_multi_set);
@@ -169,17 +164,16 @@ int Main(int argc, char *argv[]) {
 
     const std::unique_ptr<MARC::Writer> marc_writer(MARC::Writer::Factory(marc_output_path));
     unsigned id(0);
-    for (auto entry_iterator = afo_multi_set.begin(); entry_iterator != afo_multi_set.end(); /* intentionally empty */ ) {
+    for (auto entry_iterator = afo_multi_set.begin(); entry_iterator != afo_multi_set.end(); /* intentionally empty */) {
         if (auto same_keyword_range = afo_multi_set.equal_range(*entry_iterator); same_keyword_range.first != same_keyword_range.second) {
             auto new_record(CreateNewRecord(AssemblePPN(++id), same_keyword_range));
             if (new_record) {
-               marc_writer->write(*new_record);
-               delete new_record;
+                marc_writer->write(*new_record);
+                delete new_record;
             }
             entry_iterator = same_keyword_range.second;
         } else
-          ++entry_iterator;
+            ++entry_iterator;
     }
     return EXIT_SUCCESS;
 }
-
