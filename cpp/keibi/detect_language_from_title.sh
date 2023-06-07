962790dbb93a045be312e8c77822865f320ce67c
jriedl <johannes.riedl@uni-tuebingen.de>
2023-06-07 10:42:50 +0200

    Address further parsing exceptions

--- cpp/keibi/detect_language_from_title.sh
+++ cpp/keibi/detect_language_from_title.sh
@@ -15,9 +15,10 @@ marc_grep "${keibi_mrc}" '"245a"' control_number_and_traditional | \
     grep -v '^$' | `#It tuned out there are titles with multiple newlines in it...` \
     cut  -f1,3- -d ':' | `#Trow way tag field`\
     tr '\n' '\0' | \
-    tr -d '()\r:' | `#Prevent parsing error`  \
+    tr -d '()\r' | sed "s/['\"]//g" | `#Prevent parsing error`  \
     xargs -0 -I'{}' sh -c 'echo  "$@" | cut -f1 -d:; echo "$@" | cut -f2 -d: ' _ '{}' | `#split PPN and title` \
     sed --expression 's|<[^>]*>||g' | \
+    tr -d ':' | \
     awk '{if (NR % 2 == 0) { system("curl --silent -X POST -H \"Content-Type: application/text\" \
          -d\""$0"\" \"http://'"${lang_base_url}"'/get_language\"");} else {print $0;}}' | \
     awk '{if (NR % 2 == 0) { print $0 | "jq --raw-output .lang"; close("jq --raw-output .lang"); } else {print $0}}' | \
