e24786bfe1fd92e69ee46a7edf984ade288f845a
LysogorAndGmail <LysogorAnd@gmail.com>
2023-06-22 09:42:56 +0200

    Angabe Land im Benutzerkonto überarbeiten #2479 2

--- cpp/data/system_updates/135.sh
+++ cpp/data/system_updates/135.sh
@@ -2,8 +2,6 @@
 set -o errexit
 
 if [[ -e /usr/local/vufind ]]; then
-    # We explicitly need to use sudo here, even if we're already root, or it will fail,
-    # see https://stackoverflow.com/questions/16151018/how-to-fix-npm-throwing-error-without-sudo
     OLD_DIR=$PWD
     cd /usr/local/vufind
     sudo composer install
