a42829f4e21a9e027bb0931d743850a843acb968
Mario Trojan <mario.trojan@uni-tuebingen.de>
2023-07-04 14:13:53 +0200

    dspace_check_dois: optimize email sending & use internal struct

--- cpp/dspace/dspace_check_dois.cc
+++ cpp/dspace/dspace_check_dois.cc
@@ -20,15 +20,44 @@ namespace {
 
 
 const std::string DSPACE_SERVER_URL("https://publikationen.uni-tuebingen.de/rest/items/");
-const std::string DOI_KEY("dc.identifier.uri");
-const std::string PUBLICATION_TITLE_KEY("dc.title");
 const std::string DOI_URL_PREFIX("http://dx.doi.org/");
 const std::vector<std::string> NOTIFICATION_EMAIL_ADRESSES = { "mario.trojan@uni-tuebingen.de" };
 
 
-void SendNotificationsForDOI(const std::string &email_subject, const std::string &email_message) {
-    if (EmailSender::SimplerSendEmail("no-reply@ub.uni-tuebingen.de", NOTIFICATION_EMAIL_ADRESSES, email_subject, email_message,
-                                      EmailSender::MEDIUM)
+struct DSpaceItem {
+    std::string author_;
+    std::string doi_;
+    std::string title_;
+
+    DSpaceItem(const std::string &json) {
+        std::shared_ptr<JSON::JSONNode> full_tree;
+        JSON::Parser parser(json);
+        if (not parser.parse(&full_tree))
+            LOG_ERROR("failed to parse JSON (" + parser.getErrorMessage());
+
+        const std::shared_ptr<const JSON::ArrayNode> top_node_array(JSON::JSONNode::CastToArrayNodeOrDie("full_tree", full_tree));
+        for (const auto &item_iter : *top_node_array) {
+            const std::string key(JSON::LookupString("/key", item_iter, ""));
+            const std::string value(JSON::LookupString("/value", item_iter, ""));
+            if (key == "dc.title")
+                title_ = value;
+            else if (key == "dc.contributor.author")
+                author_ = value;
+            else if (key == "dc.identifier.uri") {
+                if (StringUtil::StartsWith(value, DOI_URL_PREFIX)) {
+                    doi_ = StringUtil::ReplaceString(DOI_URL_PREFIX, "", value);
+                }
+            }
+        }
+    }
+};
+
+
+void SendNotificationsForItem(const DSpaceItem &dspace_item) {
+    const std::string subject(dspace_item.author_);
+    const std::string body(dspace_item.title_ + "\n" + DOI_URL_PREFIX + dspace_item.doi_);
+
+    if (EmailSender::SimplerSendEmail("no-reply@ub.uni-tuebingen.de", NOTIFICATION_EMAIL_ADRESSES, subject, body, EmailSender::MEDIUM)
         > 299)
         LOG_ERROR("Failed to send the DOI notification email!");
 }
@@ -36,28 +65,7 @@ void SendNotificationsForDOI(const std::string &email_subject, const std::string
 
 void UpdateItem(DbConnection * const &db_writer, const std::string &doi, const std::string &publication_id) {
     db_writer->queryOrDie("UPDATE tuefind_publications SET doi = " + db_writer->escapeAndQuoteString(doi)
-                          + ", doi_notification_datetime = NOW() WHERE id= " + db_writer->escapeAndQuoteString(publication_id));
-}
-
-
-std::string ParseDataFromJSON(std::shared_ptr<const JSON::ArrayNode> top_node_array, const std::string &field) {
-    for (const auto &item_iter : *top_node_array) {
-        const std::string key(JSON::LookupString("/key", item_iter, ""));
-        const std::string value(JSON::LookupString("/value", item_iter, ""));
-        if (field == key)
-            return value;
-    }
-
-    return "";
-}
-
-
-std::string GetClearDOI(const std::string &doi_url) {
-    size_t found(doi_url.find(DOI_URL_PREFIX));
-    if (found == std::string::npos)
-        return doi_url;
-    else
-        return StringUtil::ReplaceString(DOI_URL_PREFIX, "", doi_url);
+                          + ", doi_notification_datetime = NOW() WHERE id=" + db_writer->escapeAndQuoteString(publication_id));
 }
 
 
@@ -84,25 +92,17 @@ void DownloadAndUpdate(DbConnection * const &db_writer, const std::string &exter
     // for sample document with DOI, see:
     // curl -s -H "Accept: application/json"
     // "https://publikationen.uni-tuebingen.de/rest/items/712e63be-0d92-4d8d-aae0-63da7f121797/metadata"
-    const std::string &json_document(downloader.getMessageBody());
-    std::shared_ptr<JSON::JSONNode> full_tree;
-    JSON::Parser parser(json_document);
-    if (not parser.parse(&full_tree))
-        LOG_ERROR("failed to parse JSON (" + parser.getErrorMessage());
+    const std::string &json(downloader.getMessageBody());
+    const DSpaceItem dspace_item(json);
 
-    const std::shared_ptr<const JSON::ArrayNode> top_node_array(JSON::JSONNode::CastToArrayNodeOrDie("full_tree", full_tree));
-    const std::string doi_url = ParseDataFromJSON(top_node_array, DOI_KEY);
-
-    if (doi_url.empty()) {
+    if (dspace_item.doi_.empty()) {
         LOG_INFO("No DOI found yet for item id " + publication_id);
         return;
     }
 
-    const std::string publication_title = ParseDataFromJSON(top_node_array, PUBLICATION_TITLE_KEY);
-    const std::string doi = GetClearDOI(doi_url);
-    LOG_INFO("Processing: " + doi);
-    SendNotificationsForDOI("DOI link notification", "DOI link: " + doi + " successfully generated for publication " + publication_title);
-    UpdateItem(db_writer, doi, publication_id);
+    LOG_INFO("Processing: " + dspace_item.doi_);
+    SendNotificationsForItem(dspace_item);
+    UpdateItem(db_writer, dspace_item.doi_, publication_id);
 }
 
 
@@ -119,7 +119,6 @@ int main() {
 
     db_reader.queryOrDie("SELECT * FROM tuefind_publications WHERE doi_notification_datetime IS NULL");
     auto result_set(db_reader.getLastResultSet());
-    DbTransaction transaction(&db_writer);
     while (const auto row = result_set.getNextRow()) {
         DownloadAndUpdate(&db_writer, row["external_document_guid"], row["id"]);
     }
