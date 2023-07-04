c5df0106908558e32abcf93b10d706d6bb964821
Mario Trojan <mario.trojan@uni-tuebingen.de>
2023-07-04 12:49:08 +0200

    137.vufind.sql: Add UNIQUE INDEX publication_doi

--- cpp/data/system_updates/137.vufind.sql
+++ cpp/data/system_updates/137.vufind.sql
@@ -1,2 +1,3 @@
 ALTER TABLE tuefind_publications ADD doi VARCHAR(255) DEFAULT NULL AFTER external_document_guid;
 ALTER TABLE tuefind_publications ADD doi_notification DATETIME DEFAULT NULL AFTER doi;
+CREATE UNIQUE INDEX `publication_doi` ON tuefind_publications (`doi`);
