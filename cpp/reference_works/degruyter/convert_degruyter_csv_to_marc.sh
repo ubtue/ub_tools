b08fdcff89744f14b0bcb9e9cb6abb04474cf565
jriedl <johannes.riedl@uni-tuebingen.de>
2023-10-05 13:15:47 +0200

    Add work specific author files

--- cpp/reference_works/degruyter/convert_degruyter_csv_to_marc.sh
+++ cpp/reference_works/degruyter/convert_degruyter_csv_to_marc.sh
@@ -3,7 +3,8 @@ set -o errexit -o nounset
 
 trap RemoveTempFiles EXIT
 
-AUTHORS_FILE="authors.txt"
+
+AUTHORS_DIRECTORY="./authors"
 
 function RemoveTempFiles {
    for tmpfile in ${tmpfiles[@]}; do
@@ -29,5 +30,10 @@ tmpfiles+=(${tmp_file2})
 ./convert_degruyter_csv_to_marc ${pseudo_ppn_prefix} <(cat ${csv_file} | \
     sed -re 's/,+$//g' | sed 1d) ${tmp_file1}
 ./clean_and_convert_degruyter_refworks.sh ${tmp_file1} ${tmp_file2}
-./add_author_gnds_to_marc ${tmp_file2} ${out_file} ${AUTHORS_FILE}
+authors_file=${AUTHORS_DIRECTORY}/authors_${pseudo_ppn_prefix}.txt
+if [! -r ${authors_file} ];
+    echo "Generating authors file: ${authors_file}"
+    ./extract_unique_names_and_associate.sh ${tmp_file2} ${authors_file}
+fi
+./add_author_gnds_to_marc ${tmp_file2} ${out_file} ${authors_file}
 marc_grep ${out_file} 'if "001"==".*" extract *' traditional > ${out_file%.xml}.txt
