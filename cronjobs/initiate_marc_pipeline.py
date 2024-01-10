6fd5a082222f2ce3a471b065e87f79d597888d5c
jriedl <johannes.riedl@uni-tuebingen.de>
2024-01-10 17:47:53 +0100

    Individually remove remnat PPNs part one

--- cronjobs/initiate_marc_pipeline.py
+++ cronjobs/initiate_marc_pipeline.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 import datetime
 import glob
+import json
 import urllib.request, urllib.error, urllib.parse
 import os
 import sys
+import subprocess
 import time
 import traceback
 import util
-
 
 # Clear the index to do away with old data that might remain otherwise
 # Since no commit is executed here we avoid the empty index problem
@@ -38,6 +39,47 @@ def OptimizeSolrIndex(index):
         sys.exit(-1)
 
 
+def GetPPNsInIndex(index):
+   try:
+      url = "http://localhost:8983/solr/biblio/export"
+      values = r'q=id:*&sort=id+desc&fl=id'
+      data = values.encode('utf-8')
+      request = urllib.request.Request(url, data)
+      with urllib.request.urlopen(request) as response:
+          json_data = json.loads(response.read().decode('utf-8'))
+          ppns = set([ id.get("id") for id in json_data['response']['docs'] ])
+          return ppns
+   except Exception as e:
+        util.SendEmail("MARC-21 Pipeline", "Failed determine all index PPNs from \"" + index + "\" [" + str(e) + "]!", priority=1)
+        sys.exit(-1)
+
+
+def GetPPNsInMarcFile(title_args):
+    try:
+       ppns_as_lines = subprocess.run([util.Which("marc_grep"), title_args, r'"001"', "no_label"], stdout=subprocess.PIPE).stdout.decode('utf8')
+       return set(ppns_as_lines.splitlines())
+    except Exception as e:
+        util.SendEmail("MARC-21 Pipeline", "Failed determine all MARC PPNs from \"" + title_args + "\" [" + str(e) + "]!", priority=1)
+        sys.exit(-1)
+
+
+
+def RemoveExcessRecordsFromIndex(marc_file, index):
+    index_ppns = GetPPNsInIndex(index)
+    marc_file_ppns = GetPPNsInMarcFile(marc_file)
+    to_delete =  index_ppns - marc_file_ppns
+    try:
+        url = "http://localhost:8983/solr/" + index + "/update?commit=true"
+        headers = {"Content-Type": "application/json"}
+        values = r'{ "delete" : { "query" : "filter(id:(' +  ' '.join(to_delete) + r'))" } }'
+        data = values.encode('utf-8')
+        request = urllib.request.Request(url, data, headers)
+        urllib.request.urlopen(request, timeout=300)
+    except Exception as e:
+        util.SendEmail("MARC-21 Pipeline", "Failed to remove excess records from \"" + index + "\" [" + str(e) + "]!", priority=1)
+        sys.exit(-1)
+
+
 solrmarc_log_summary = "/tmp/solrmarc_log.summary"
 import_log_summary = "/tmp/import_into_vufind_log.summary"
 
@@ -53,8 +95,11 @@ def ImportIntoVuFind(title_pattern, authority_pattern, log_file_name):
     if len(title_args) != 1:
         util.Error("\"" + title_pattern + "\" matched " + str(len(title_args))
                    + " files! (Should have matched exactly 1 file!)")
-    ClearSolrIndex(title_index)
+    if clear_solr_index:
+        ClearSolrIndex(title_index)
     util.ExecOrDie(vufind_dir + "/import-marc.sh", title_args, log_file_name)
+    if not clear_solr_index:
+        RemoveExcessRecords(title_args, log_file_name)
     OptimizeSolrIndex(title_index)
 
     # import authority data
@@ -115,17 +160,32 @@ def WriteImportFinishedFile():
         import_finished.write(time.strftime("%Y%m%d-%H%M%S"))
 
 
+def IsClearSolrIndex():
+    if len(sys.argv) == 4:
+        if sys.argv[1] == "--clear-solr-index":
+            sys.argv.pop(1)
+            return True
+        else:
+            util.default_email_recipient = sys.argv[2]
+            util.SendEmail("MARC-21 Pipeline Kick-Off (Failure)", "Invalid optional argument: \""
+                           + sys.argv[1] + "\"\n", priority=1)
+            sys.exit(-1)
+    return False
+
+
 def Main():
-    if len(sys.argv) != 3:
-         print("invalid arguments! usage: initiate_marc_pipeline.py <default email recipient> <MARC21 pipeline script name>")
+    if len(sys.argv) != 3 and len(sys.argv) != 4:
+         print("invalid arguments! usage: initiate_marc_pipeline.py [--clear-solr-index] <default email recipient> <MARC21 pipeline script name>")
          util.SendEmail("MARC-21 Pipeline Kick-Off (Failure)",
-                        "This script needs to be called with two arguments,\n"
-                        + "the default email recipient and the name of the MARC-21\n"
+                        "This script needs to be called with two mandatory arguments\n"
+                        + "the default email recipient and the name of the MARC-21.\n"
+                        + "Optionally --clear-solr-index can be passed as first argument\n"
                         + "pipeline script to be executed.\n", priority=1)
          sys.exit(-1)
-
+    clear_solr_index = IsClearSolrIndex()
     util.default_email_recipient = sys.argv[1]
     pipeline_script_name = sys.argv[2]
+
     if not os.access(pipeline_script_name, os.X_OK):
          print("Pipeline script not found or not executable: " + pipeline_script_name)
          util.SendEmail("MARC-21 Pipeline Kick-Off (Failure)", "Pipeline script not found or not executable: \""
@@ -141,7 +201,7 @@ def Main():
             util.Error("BSZ data file must end in .tar.gz!")
         file_name_list = util.ExtractAndRenameBSZFiles(bsz_data)
 
-        RunPipelineAndImportIntoSolr(pipeline_script_name, file_name_list[0], conf)
+        RunPipelineAndImportIntoSolr(pipeline_script_name, file_name_list[0], conf, clear_solr_index)
         util.WriteTimestamp()
         DeleteReftermMutex()
         WriteImportFinishedFile()
