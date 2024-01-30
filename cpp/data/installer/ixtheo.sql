d574cfa76f53f87cb4111df311d7702a4b880e28
jriedl <johannes.riedl@uni-tuebingen.de>
2024-01-30 17:20:21 +0100

    Fix Typo

--- cpp/data/installer/ixtheo.sql
+++ cpp/data/installer/ixtheo.sql
@@ -42,7 +42,7 @@ CREATE TABLE keyword_translations (
   KEY keyword_translations_idx_translation (translation(30)),
   KEY keyword_translations_idx_gnd_code (gnd_code),
   KEY keyword_translations_idx_status (status),
-  KEY keywprd_translations_idx_translation_disabled (translation_disabled),
+  KEY keyword_translations_idx_translation_disabled (translation_disabled),
   KEY keyword_translations_idx_prev_version_id (prev_version_id),
   KEY keyword_translations_idx_next_version_id (next_version_id),
   KEY keyword_translations_idx_origin (origin)
