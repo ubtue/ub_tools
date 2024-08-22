5fd390461838fcd4748f1ee4a914a6727b8ebe87
jriedl <johannes.riedl@uni-tuebingen.de>
2024-08-22 16:49:22 +0200

    Use module based approach

--- ixtheo_notation/DSPy/dspy_ixtheo_notation.py
+++ ixtheo_notation/DSPy/dspy_ixtheo_notation.py
@@ -20,20 +20,20 @@ def SetupChatAI(config):
     model = config.get("Model", "model")
     # API configuration
     api_key = os.environ.get('CHAT_AI_API_KEY')
-    
+
     if not api_key:
         print("Export CHAT_AI_API_KEY first")
         exit(1)
-    
+
     root = logging.getLogger()
     root.setLevel(logging.DEBUG)
-    
+
     handler = logging.StreamHandler(sys.stdout)
     handler.setLevel(logging.INFO)
     formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     root.addHandler(handler)
-    
+
     chat_ai = dspy.OpenAI(
         api_key = api_key,
         api_provider = "ChatAI",
@@ -49,37 +49,44 @@ def answer_quality_metric(predicted_classification, manual_classification):
     return abs(len(set(predicted_classification) & set(manual_classification) - len(set(manual_classification))))
 
 
-def ProcessRecord(config, item, output_directory):
-    ixtheo_notations = Path(config.get("Notations", "notations_file")).read_text();
-    additional_context = Path(str(config.get("Context", "additional_context_file"))).read_text()
-    context_desc=[ixtheo_notations, additional_context]
+class IxTheoAutoClassification(dspy.Module):
+    def __init__(self, config):
+        ixtheo_notations = Path(config.get("Notations", "notations_file")).read_text();
+        additional_context = Path(str(config.get("Context", "additional_context_file"))).read_text()
+        context_desc=[ixtheo_notations, additional_context]
 
-    class AssignIxTheoNotation(dspy.Signature):
-        """Assign an Ixtheo notation based on the given keywords"""
-        context = dspy.InputField(desc=context_desc)
-        input = dspy.InputField()
-        ixtheo_notations = dspy.OutputField(desc="""A set of two or three letter notations as JSON array, not more than 5. 
-                                                    Do not output the textual description of the notations""")
-        rationale = dspy.OutputField(desc="A rationale for the selection of the labels")
-    
-    
-    class AssignNewKeywords(dspy.Signature):
-        """You are a classifier and assign one or more labels to text. The text is given as a json file with id, title, keywords and summary. Use title, keywords and the summary  Avoid notations with only one letter. Generate a summary of the topics first with a bias on title and keywords. Determine the main topic. Determine whether or not the topic is concerned with a topic of theology. Determine the place and time of the topics. Then generate a new set of keywords. Please return your answer in JSON format"""
-        input_data = dspy.InputField(desc="Information as JSON object")
-        new_keywords = dspy.OutputField(desc="A set of newly assigned keywords as JSON object")
-        rationale = dspy.OutputField(desc="A rationale for the selection of the keywords as JSON object with key keywords")
+        class AssignIxTheoNotation(dspy.Signature):
+            """Assign an Ixtheo notation based on the given keywords"""
+            context = dspy.InputField(desc=context_desc)
+            input_data = dspy.InputField()
+            ixtheo_notations = dspy.OutputField(desc="""A set of two or three letter notations as JSON array, not more than 5.
+                                                        Do not output the textual description of the notations""")
+            rationale = dspy.OutputField(desc="A rationale for the selection of the labels")
 
-    
-    generate_new_keywords = dspy.ChainOfThought(AssignNewKeywords)
-    generate_notations = dspy.ChainOfThought(AssignIxTheoNotation)
+        class AssignNewKeywords(dspy.Signature):
+            """You are a classifier and assign one or more labels to text. The text is given as a json file with id, title, keywords and summary. Use title, keywords and the summary  Avoid notations with only one letter. Generate a summary of the topics first with a bias on title and keywords. Determine the main topic. Determine whether or not the topic is concerned with a topic of theology. Determine the place and time of the topics. Then generate a new set of keywords. Please return your answer in JSON format"""
+            input_data = dspy.InputField(desc="Information as JSON object")
+            new_keywords = dspy.OutputField(desc="A set of newly assigned keywords as JSON object")
+            rationale = dspy.OutputField(desc="A rationale for the selection of the keywords as JSON object with key keywords")
+        super().__init__()
+        self.generate_new_keywords = dspy.ChainOfThought(AssignNewKeywords)
+        self.generate_notations = dspy.ChainOfThought(AssignIxTheoNotation)
 
-    new_keywords = generate_new_keywords(input_data=item)
-    notations = generate_notations(input=new_keywords.new_keywords, context="")
-    print(f"New notations: {notations.ixtheo_notations}")
-    print("\n-----------------------------------------------\n")
-    print(f"New Keywords: {new_keywords.new_keywords!r}")
-    print(f"New Keywords rationale: {new_keywords.rationale!r}")
-    print(f"Notation rationale: {notations.rationale!r}")
+
+    def forward(self, input_data):
+        new_keywords = self.generate_new_keywords(input_data=input_data)
+        notations = self.generate_notations(input_data=new_keywords.new_keywords, context="")
+        print(f"New notations: {notations.ixtheo_notations}")
+        print("\n-----------------------------------------------\n")
+        print(f"New Keywords: {str(new_keywords.new_keywords)}")
+        print(f"New Keywords rationale: {str(new_keywords.rationale)}")
+        print(f"Notation rationale: {str(notations.rationale)}")
+        return notations
+
+
+
+    def run(self, input_data):
+        return self.forward(input_data)
 
 
 def GetRandomOffsets(num_of_samples, fulltext_parsed):
@@ -127,12 +134,16 @@ def Main():
     if use_samples:
        jq_array_element_selector = GetRandomOffsets(number_of_samples, ReadFulltext(augmented_tocs_with_notations))
 
-
     items = pyjq.all(jq_array_element_selector, ReadFulltext(augmented_tocs_with_notations))
+    ixtheo_auto_classification = IxTheoAutoClassification(config)
     for item in items:
         print(pyjq.first('.id', item))
-        ProcessRecord(config, str(pyjq.first('del(.ixtheo_notation)', item)), output_directory)
+        #new_notations = ProcessRecord(config, str(pyjq.first('del(.ixtheo_notation)', item)), output_directory)
+        new_notations = ixtheo_auto_classification(str(pyjq.first('del(.ixtheo_notation)', item)))
+        print("\n************************************************\n")
+        print("New notations: " + str(new_notations.ixtheo_notations))
         print("Original Notations: " + str(pyjq.first('.ixtheo_notation', item)))
+
         print("\n###############################################\n")
 
 if __name__ == "__main__":
