8fa41cc5c99dc2b2598d34dfcc579e961dc53b2d
jriedl <johannes.riedl@uni-tuebingen.de>
2024-10-07 15:59:47 +0200

    Insert fields without PPN at the end

--- cpp/harvest/add_non_k10plus_787_information.cc
+++ cpp/harvest/add_non_k10plus_787_information.cc
@@ -71,10 +71,10 @@ void ProcessRecord(const bool verbose, MARC::Record * const record, const Missin
             LOG_INFO("Adding information for study " + study_id + " to record " + record->getControlNumber());
         const auto information(study_information.at(study_id));
         if (information.first.empty()) {
-            record->insertField("787", MARC::Subfields({ { 't', information.first } }));
+            record->insertFieldAtEnd("787", MARC::Subfields({ { 't', information.first } }));
             continue;
         }
-        record->insertField("787", MARC::Subfields({ { 'a', information.second }, { 't', information.first } }));
+        record->insertFieldAtEnd("787", MARC::Subfields({ { 'a', information.second }, { 't', information.first } }));
     }
 }
 
