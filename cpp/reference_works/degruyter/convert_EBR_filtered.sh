465285d7ef26ab5169fbecfb26c31731cd188baa
jriedl <johannes.riedl@uni-tuebingen.de>
2024-11-28 16:25:41 +0100

    Adjust counting scheme to avoid pseudo PPN duplicates

--- cpp/reference_works/degruyter/convert_EBR_filtered.sh
+++ cpp/reference_works/degruyter/convert_EBR_filtered.sh
@@ -24,7 +24,6 @@ output_dir="$3"
 tmp_dir_template=$(basename $0 .sh)"_XXXXXX"
 tmp_dir=$(mktemp --directory -t ${tmp_dir_template})
 filtered_csv="${tmp_dir}/ebr_filtered_$(date +'%y%m%d').csv"
-#tmpfiles+=(${filtered_csv})
 generate_filtered_output="${tmp_dir}/generate_filtered_output.log"
 tmpfiles+=(${generate_filtered_output})
 
@@ -33,17 +32,30 @@ tmpfiles+=(${generate_filtered_output})
 REFWORKS=$(basename ${filtered_csv} | sed -re 's/([^_]+)_.*/\1/g')
 for refwork in ${REFWORKS}; do
     if [ $refwork != "ebr" ]; then
-        echo "Invalid refwork ${refwork}"
-        echo "Filename must match ebr_xxx.csv"
+        echo "Invalid refwork ${refwork}" >&2
+        echo "Filename must match ebr_xxx.csv" >&2
         exit 1
     fi
-    echo "Converting ${refwork}"
+    echo "Converting ${refwork}" >&2
     refwork_csv_file=$(ls ${tmp_dir}/${refwork}*.csv)
-    echo "Using file ${refwork_csv_file}"
+    echo "Using file ${refwork_csv_file}" >&2
     converted_file=ixtheo_${refwork}_$(date +'%y%m%d')_001.xml
     ./convert_degruyter_csv_to_marc.sh \
         $(echo ${refwork} | awk '{print toupper($0)}')  \
         "${refwork_csv_file}" \
         "${output_dir}/${converted_file}"
     tmpfiles+=(${refwork_csv_file})
+    #make sure we try to avoid already used pseudo PPNS
+    #so offset these by the number of already present EBR records in K10Plus
+    echo "Fix pseudo PPNs" >&2
+    cat ${generate_filtered_output}
+    ebr_records_present=$(cat ${generate_filtered_output} | egrep 'Counted [0-9]+ EBR records' | sed -re 's/Counted ([[:digit:]]+) EBR records/\1/')
+    echo "${ebr_records_present} records already present" >&2
+    cat ${output_dir}/${converted_file} | xmlstarlet ed -N marc=http://www.loc.gov/MARC21/slim \
+        -N 'xsi=http://www.w3.org/2001/XMLSchema-instance' \
+        -u '//marc:controlfield[@tag="001"]' \
+        -x  'concat("EBR000", number(substring(.,4)) + '"${ebr_records_present}"')' | \
+    sponge "${output_dir}/${converted_file}"
+    marc_grep "${output_dir}/${converted_file}" 'if "001"==".*" extract *' traditional > $output_dir/${converted_file%.xml}.txt
+    echo "Finished..." >&2
 done
