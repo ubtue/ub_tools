b70340ba763fc9efe1d96cf9da77e14973255fa3
hlindeboom <hjordis.lindeboom@uni-tuebingen.de>
2025-04-02 10:12:13 +0200

    Adding cronjob to alert on new search results in KrimDok

---
+++ cpp/data/system_updates/158.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [[ $TUEFIND_FLAVOUR == "krimdok" ]]; then
+    root_crontab=/var/spool/cron/crontabs/root
+
+    if [ ! -f ${root_crontab} ]; then
+        echo "${root_crontab} does not seem to exist - Skipping"
+        exit 0
+    fi
+
+    if [ ! -r ${root_crontab} ]; then
+        echo "${root_crontab} not readable - Aborting"
+        exit 1
+    fi
+
+    cron_job="10 22 * * * php $VUFIND_HOME/public/index.php scheduledsearch notify"
+    if crontab -l | grep -Fxq "$cron_job"; then
+        echo "Cron job already exists - Skipping"
+    else
+        (crontab -l; echo "$cron_job") | crontab -
+        echo "Cron job added successfully"
+    fi
+fi
