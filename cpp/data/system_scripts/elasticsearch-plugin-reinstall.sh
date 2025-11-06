22642cfd5c10ce405c58caeaaa03ab96837db865
root <root@mb029.ub.uni-tuebingen.de>
2025-11-06 14:57:24 +0100

    simplified logic

--- cpp/data/system_scripts/elasticsearch-plugin-reinstall.sh
+++ cpp/data/system_scripts/elasticsearch-plugin-reinstall.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 
-found=false
-
 while IFS= read -r line; do
     if [[ $line == elasticsearch* ]]; then
         echo "Detected elasticsearch line: $line"
         plugin_name="analysis-icu"
         echo "Removing plugin: $plugin_name"
-        sudo /usr/share/elasticsearch/bin/elasticsearch-plugin remove "$plugin_name" || true
+        /usr/share/elasticsearch/bin/elasticsearch-plugin remove "$plugin_name"
         echo "Installing plugin: $plugin_name"
-        sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install "$plugin_name" --batch
-        found=true
-        break
+        /usr/share/elasticsearch/bin/elasticsearch-plugin install "$plugin_name" --batch
+        exit 0;
     fi
 done
 
-if [ "$found" = false ]; then
-    echo "Elasticsearch either not installed or not upgraded. Nothing to do."
-fi
+echo "Elasticsearch either not installed or not upgraded. Nothing to do."
