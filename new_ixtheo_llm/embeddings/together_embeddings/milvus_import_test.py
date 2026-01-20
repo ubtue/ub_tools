6a4ba1b7d3b16d1c7961ccf4462b11534d1c23e8
jriedl <johannes.riedl@uni-tuebingen.de>
2026-01-20 10:45:31 +0100

    Minor adjustments

--- new_ixtheo_llm/embeddings/together_embeddings/milvus_import_test.py
+++ new_ixtheo_llm/embeddings/together_embeddings/milvus_import_test.py
@@ -5,25 +5,27 @@ from langchain_core.documents import Document
 from langchain_text_splitters import RecursiveCharacterTextSplitter,RecursiveJsonSplitter
 from langchain_together import TogetherEmbeddings
 from langchain_milvus import Milvus
-from solr_retriever import SolrRetriever
+from solr_retriever import SolrRetriever, SolrBatchRetriever
 
 
-solr_retriever = SolrRetriever()
-docs = solr_retriever._get_relevant_documents("*:*")
-#text_splitter = RecursiveCharacterTextSplitter(chunk_size=200, chunk_overlap=20)
-#splits = text_splitter.create_documents(docs)
+#solr_retriever = SolrRetriever()
+#docs = solr_retriever._get_relevant_documents("*:*")
+solr_retriever = SolrBatchRetriever('-id:L*')
+while True:
+  docs = solr_retriever.get_next_batch()
+  if not docs:
+           break
 
-embeddings = TogetherEmbeddings(
-    model="Alibaba-NLP/gte-modernbert-base"
-)
+  embeddings = TogetherEmbeddings(
+     model="Alibaba-NLP/gte-modernbert-base"
+   )
 
-# Connect to Milvus (local standalone)
-vectorstore = Milvus.from_documents(
- #   documents=splits,
-    documents=docs,
-    embedding=embeddings,
-    connection_args={"uri": "./milvus_demo.db"},  # Milvus Lite file
-    enable_dynamic_field=True,
-    collection_name="marc_rag_demo",
-    drop_old=True,  # Clear existing data
-)
+  # Connect to Milvus (local standalone)
+  vectorstore = Milvus.from_documents(
+      documents=docs,
+      embedding=embeddings,
+      connection_args={"uri": "./milvus_demo.db"},  # Milvus Lite file
+      enable_dynamic_field=True,
+      collection_name="marc_rag_demo",
+      drop_old=True,  # Clear existing data
+  )
