044c7ce373ceaffd5c808bdafc034f1842859cca
Steven Lolong <steven.lolong@uni-tuebingen.de>
2026-05-28 16:57:37 +0200

    update the system update script

--- cpp/data/system_updates/168.sh
+++ cpp/data/system_updates/168.sh
@@ -1,21 +1,29 @@
 #!/bin/bash
 
+FILE="/usr/local/var/lib/tuelib/Elasticsearch.conf"
+
+if [ ! -f "$FILE" ]; then
+    exit 1
+fi
+
+
 authorization_args=()
 username_login_args=()
 
 URL_ADD_PIPELINE="http://localhost:9200/_ingest/pipeline/add_timestamp"
 URL_SET_DEFAULT_PIPELINE_FULLTEXT_CACHE="http://localhost:9200/full_text_cache/_settings"
 URL_SET_DEFAULT_PIPELINE_FULLTEXT_CACHE_HTML="http://localhost:9200/full_text_cache_html/_settings"
+URL_SET_DEFAULT_PIPELINE_FULLTEXT_CACHE_URLS="http://localhost:9200/full_text_cache_urls/_settings"
 
-TOKEN=$(inifile_lookup "/usr/local/var/lib/tuelib/Elasticsearch.conf" Elasticsearch token)
+TOKEN=$(inifile_lookup "$FILE" Elasticsearch token)
 
 if [ $? -eq 0 ]; then
   if [ -n "$TOKEN" ]; then
     authorization_args=(-H "Authorization: ApiKey $TOKEN")
   fi
 else
-  username=$(inifile_lookup "/usr/local/var/lib/tuelib/Elasticsearch.conf" Elasticsearch username)
-  password=$(inifile_lookup "/usr/local/var/lib/tuelib/Elasticsearch.conf" Elasticsearch password)
+  username=$(inifile_lookup "$FILE" Elasticsearch username)
+  password=$(inifile_lookup "$FILE" Elasticsearch password)
 
   if [ $? -eq 0 ]; then
     if [ -n "$username" ] && [ -n "$password" ]; then
@@ -50,6 +58,14 @@ curl -X PUT "$URL_SET_DEFAULT_PIPELINE_FULLTEXT_CACHE" \
       }'
 
 curl -X PUT "$URL_SET_DEFAULT_PIPELINE_FULLTEXT_CACHE_HTML" \
+    "${username_login_args[@]}" \
+    "${authorization_args[@]}" \
+    -H "Content-Type: application/json" \
+    -d '{
+        "index.default_pipeline": "add_timestamp"
+      }'
+
+curl -X PUT "$URL_SET_DEFAULT_PIPELINE_FULLTEXT_CACHE_URLS" \
     "${username_login_args[@]}" \
     "${authorization_args[@]}" \
     -H "Content-Type: application/json" \
