521001c6fd7c3a7d77a1abb3842c8e0627163f1e
Steven Lolong <steven.lolong@uni-tuebingen.de>
2026-05-28 11:34:06 +0200

    get credential from the config

--- cpp/data/system_updates/168.sh
+++ cpp/data/system_updates/168.sh
@@ -1,18 +1,58 @@
 #!/bin/bash
-set -o errexit
 
-# The authorization header is need when running v9 or newer, but not when running v8 with basic auth. The API key can be generated in the Elasticsearch management interface. It should have the "manage_pipeline" role, which is sufficient for creating ingest pipelines.
-curl -X PUT "http://localhost:9200/_ingest/pipeline/add_timestamp" \
-  # -H "Authorization: ApiKey YOUR_TOKEN" \
-  -H "Content-Type: application/json" \
-  -d '{
-    "description": "Add last_update timestamp",
-    "processors": [
-      {
-        "set": {
-          "field": "last_update",
-          "value": "{{_ingest.timestamp}}"
+authorization_args=()
+username_login_args=()
+
+URL_ADD_PIPELINE="http://localhost:9200/_ingest/pipeline/add_timestamp"
+URL_SET_DEFAULT_PIPELINE_FULLTEXT_CACHE="http://localhost:9200/full_text_cache/_settings"
+URL_SET_DEFAULT_PIPELINE_FULLTEXT_CACHE_HTML="http://localhost:9200/full_text_cache_html/_settings"
+
+TOKEN=$(inifile_lookup "/usr/local/var/lib/tuelib/Elasticsearch.conf" Elasticsearch token)
+
+if [ $? -eq 0 ]; then
+  if [ -n "$TOKEN" ]; then
+    authorization_args=(-H "Authorization: ApiKey $TOKEN")
+  fi
+else
+  username=$(inifile_lookup "/usr/local/var/lib/tuelib/Elasticsearch.conf" Elasticsearch username)
+  password=$(inifile_lookup "/usr/local/var/lib/tuelib/Elasticsearch.conf" Elasticsearch password)
+
+  if [ $? -eq 0 ]; then
+    if [ -n "$username" ] && [ -n "$password" ]; then
+      username_login_args=(-u "$username:$password")
+    fi
+  fi
+fi
+
+
+curl -X PUT "$URL_ADD_PIPELINE" \
+    "${username_login_args[@]}" \
+    "${authorization_args[@]}" \
+    -H "Content-Type: application/json" \
+    -d '{
+      "description": "Add last_update timestamp",
+      "processors": [
+        {
+          "set": {
+            "field": "last_update",
+            "value": "{{_ingest.timestamp}}"
+          }
         }
-      }
-    ]
-  }'
+      ]
+    }'
+
+curl -X PUT "$URL_SET_DEFAULT_PIPELINE_FULLTEXT_CACHE" \
+    "${username_login_args[@]}" \
+    "${authorization_args[@]}" \
+    -H "Content-Type: application/json" \
+    -d '{
+        "index.default_pipeline": "add_timestamp"
+      }'
+
+curl -X PUT "$URL_SET_DEFAULT_PIPELINE_FULLTEXT_CACHE_HTML" \
+    "${username_login_args[@]}" \
+    "${authorization_args[@]}" \
+    -H "Content-Type: application/json" \
+    -d '{
+        "index.default_pipeline": "add_timestamp"
+      }'
