b1a7c4e978c8e0f75824711a36c0eec1f6b68d38
Mario Trojan <mario.trojan@uni-tuebingen.de>
2026-08-26 11:33:51 +0200

    installer: Add missing path separator

--- cpp/installer.cc
+++ cpp/installer.cc
@@ -812,7 +812,7 @@ void ConfigureVuFind(const bool production, const VuFindSystemType vufind_system
 
     // Copy Solr Installation file from network drive if exists (fallback will be to download, which might take ages)
     const std::string VUFIND_DOWNLOADS_DIR_REMOTE("/mnt/ZE020150/FID-Entwicklung/");
-    const std::string VUFIND_DOWNLOADS_DIR_LOCAL(VUFIND_DIRECTORY + "downloads/");
+    const std::string VUFIND_DOWNLOADS_DIR_LOCAL(VUFIND_DIRECTORY + "/downloads/");
     FileUtil::Directory vufind_downloads_dir(VUFIND_DOWNLOADS_DIR_REMOTE);
     for (const auto entry : vufind_downloads_dir) {
         if (entry.getType() == DT_REG && RegexMatcher::Matched("solr-\\d+\\.\\d+\\.\\d\\.tgz", entry.getName())) {
